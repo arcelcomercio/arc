0c36cceea8bbc4d4952d608eff631acbc9f7b6bf
Edgar <edgar.gamarra21@gmail.com>
2019-01-29 11:30:11 -0500

    se agrego el enlace

--- components/features/breaking-news/default.jsx
+++ components/features/breaking-news/default.jsx
@@ -6,26 +6,31 @@ import "./breaking-news.css";
 
 @Consumer
 class BreakingNews extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    // const content = this.props.globalContent;
-    const { tags, title, subTitle } = this.props.customFields;
-    return (
-      <div className="BreakingNews">
-        <div className="box combine" {...this.props.editableField('tags')}>{tags}</div>
-        <div className="box" {...this.props.editableField('title')}>{title}</div>
-        <div className="box" {...this.props.editableField('subTitle')}>{subTitle}</div>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+    }
+    render() {
+        // const content = this.props.globalContent;
+        const { tags, title, subTitle } = this.props.customFields;
+        return (
+            <div className="BreakingNews">
+                <div className="box combine" {...this.props.editableField('tags')}>{tags}</div>
+                <div className="box" {...this.props.editableField('title')}>
+                    <a href={"https://www.google.com/"} >
+                        {title}
+                    </a>
+
+                </div>
+                <div className="box" {...this.props.editableField('subTitle')}>{subTitle}</div>
+            </div>
+        );
+    }
 }
 BreakingNews.propTypes = {
-  customFields: PropTypes.shape({
-    tags: PropTypes.string,
-    title: PropTypes.string,
-    subTitle: PropTypes.string
-  })
+    customFields: PropTypes.shape({
+        tags: PropTypes.string,
+        title: PropTypes.string,
+        subTitle: PropTypes.string
+    })
 };
 export default BreakingNews;
