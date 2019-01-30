9f5d0a62f07796623a4071a6a1a2f42b168e8ae5
Edgar <edgar.gamarra21@gmail.com>
2019-01-30 15:00:09 -0500

    se agrego la funcionalidad close

--- components/features/breaking-news/default.jsx
+++ components/features/breaking-news/default.jsx
@@ -6,58 +6,80 @@ import "./breaking-news.css";
 
 @Consumer
 class BreakingNews extends Component {
-    constructor(props) {
-        super(props);
-        const { contentService, contentConfigValues } = this.props.customFields.articleConfig
-        this.fetchContent({
-            article: {
-                source: contentService,
-                query: contentConfigValues
-            }
-        })
+  constructor(props) {
+    super(props);
+    const {
+      contentService,
+      contentConfigValues
+    } = this.props.customFields.articleConfig;
+    
+    this.state={
+        contentBreakingNews:'content-BreakingNews'
     }
-    render() {
-        // const content = this.props.globalContent;
-        const content = this.state.article
-        const { tags, title, link, subTitle } = this.props.customFields;
-        let subtitulo = subTitle;
-        debugger
-        if (subtitulo.length > 29) {
-            subtitulo = "caracteres no validos";
-        }
+    this.fetchContent({
+      article: {
+        source: contentService,
+        query: contentConfigValues
+      }
+    });
+  }
+  handleOnclickClose = () => {
+    this.setState({
+      contentBreakingNews: "content-BreakingNews hidden"
+    });
+  };
+  render() {
+    // const content = this.props.globalContent;
+    // const content = this.state.article
+    const { headlines, subheadlines } = this.state.article;
+    debugger;
+    const { tags, title, link, subTitle } = this.props.customFields;
 
-        return (
-            <div className="BreakingNews">
-                <div className="box combine" {...this.props.editableField('tags')}>
-                    <div className="lavel">
-                        <span>{tags}</span>
-                    </div>
+    let objContent = {
+      title: title || headlines.basic,
+      subTitle: subTitle || subheadlines.basic
+    };
 
-                </div>
-                <div className="box" {...this.props.editableField('title')}>
-                    <a href={link ? link : '#'} >
-                        <h4>
-                            {title || (content && content.headlines && content.headlines.basic)}
-                        </h4>
-                    </a>
-                </div>
-                <div className="box" {...this.props.editableField('subTitle')}>
-                    <h5>
-                        {subtitulo || (content && content.subheadlines && content.subheadlines.basic)}
-                    </h5>
-
-                </div>
+    return (
+      <div className={this.state.contentBreakingNews}>
+        <span className="close" onClick={this.handleOnclickClose}>
+          x
+        </span>
+        <div className="BreakingNews">
+          <div className="box combine" {...this.props.editableField("tags")}>
+            <div className="lavel">
+              <span>{tags}</span>
             </div>
-        );
-    }
+          </div>
+          <div className="box" {...this.props.editableField("title")}>
+            <a href={link ? link : "#"}>
+              <h4>
+                {objContent.title}
+                {/* {title || (content && content.headlines && content.headlines.basic)} */}
+              </h4>
+            </a>
+          </div>
+          <div className="box" {...this.props.editableField("subTitle")}>
+            <h5>
+              {objContent.subTitle.length > 29
+                ? "no valido"
+                : objContent.subTitle}
+              {/* {content.subheadlines.basic} */}
+              {/* {subTitle || (content && content.subheadlines && content.subheadlines.basic)} */}
+            </h5>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 BreakingNews.propTypes = {
-    customFields: PropTypes.shape({
-        tags: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        link: PropTypes.string.isRequired,
-        subTitle: PropTypes.string.isRequired,
-        articleConfig: PropTypes.contentConfig('article'),
-    })
+  customFields: PropTypes.shape({
+    tags: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    subTitle: PropTypes.string.isRequired,
+    articleConfig: PropTypes.contentConfig("article")
+  })
 };
 export default BreakingNews;
