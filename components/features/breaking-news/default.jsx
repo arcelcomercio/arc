5bf0f8452eb4827f3a10b2ee777574e82a0109c6
Edgar <edgar.gamarra21@gmail.com>
2019-02-01 15:41:54 -0500

    se modificaron lso estilos de breaking news

--- components/features/breaking-news/default.jsx
+++ components/features/breaking-news/default.jsx
@@ -8,21 +8,19 @@ import "./breaking-news.scss";
 class BreakingNews extends Component {
   constructor(props) {
     super(props);
-    const {
-      contentService,
-      contentConfigValues
-    } = this.props.customFields.articleConfig || {};
+    const { contentService, contentConfigValues } =
+      this.props.customFields.articleConfig || {};
 
     this.state = {
       contentBreakingNews: "content-BreakingNews"
     };
-    if(contentService !== undefined && contentConfigValues !== undefined){
-        this.fetchContent({
-            article: {
-              source: contentService,
-              query: contentConfigValues
-            }
-        });
+    if (contentService !== undefined && contentConfigValues !== undefined) {
+      this.fetchContent({
+        article: {
+          source: contentService,
+          query: contentConfigValues
+        }
+      });
     }
   }
 
@@ -48,11 +46,22 @@ class BreakingNews extends Component {
   render() {
     // const content = this.props.globalContent;
     // const content = this.state.article
-    console.log('this.state.article', this.state.article);
+    console.log("this.state.article", this.state.article);
     const { headlines, subheadlines } = this.state.article || {};
-    const { backgroundColor, tags, title, subTitle, isExternalLink, link, articleConfig } = this.props.customFields;
-    const webUrlService = articleConfig !== undefined && articleConfig.contentConfigValues !== undefined?
-        articleConfig.contentConfigValues.website_url:'';
+    const {
+      backgroundColor,
+      tags,
+      title,
+      subTitle,
+      isExternalLink,
+      link,
+      articleConfig
+    } = this.props.customFields;
+    const webUrlService =
+      articleConfig !== undefined &&
+      articleConfig.contentConfigValues !== undefined
+        ? articleConfig.contentConfigValues.website_url
+        : "";
     let objContent = {
       title: title || (headlines && headlines.basic),
       subTitle: subTitle || (subheadlines && subheadlines.basic),
@@ -63,17 +72,13 @@ class BreakingNews extends Component {
         <span className="close" onClick={this.handleOnclickClose}>
           x
         </span>
-        <div className="BreakingNews">
-          <div className="box combine" {...this.props.editableField("tags")}>
-            <div className="lavel">
-              <span>{tags}</span>
-            </div>
+        <div className="BreakingNews"{...this.props.editableField("tags")}>
+          <div className="volada">
+            <span>{tags}</span>
           </div>
           <div className="box" {...this.props.editableField("title")}>
             <a href={objContent.link} target="_blank">
-              <h2>
-                {objContent.title}
-              </h2>
+              <h2>{objContent.title}</h2>
             </a>
           </div>
         </div>
@@ -84,33 +89,33 @@ class BreakingNews extends Component {
 BreakingNews.propTypes = {
   customFields: PropTypes.shape({
     backgroundColor: PropTypes.oneOf([
-        'bn-color-1',
-        'bn-color-2',
-        'bn-color-3',
-        'bn-color-4'
+      "bn-color-1",
+      "bn-color-2",
+      "bn-color-3",
+      "bn-color-4"
     ]).tag({
-        name: 'Color de fondo',
-        labels: {
-            'bn-color-1': 'Color 1',
-            'bn-color-2': 'Color 2',
-            'bn-color-3': 'Color 3',
-            'bn-color-4': 'Color 4'
-        },
-        defaultValue: 'bn-color-1'
+      name: "Color de fondo",
+      labels: {
+        "bn-color-1": "Color 1",
+        "bn-color-2": "Color 2",
+        "bn-color-3": "Color 3",
+        "bn-color-4": "Color 4"
+      },
+      defaultValue: "bn-color-1"
     }),
-    tags: PropTypes.string.tag({name: 'Etiqueta'}),
+    tags: PropTypes.string.tag({ name: "Etiqueta" }),
     title: PropTypes.string.isRequired.tag({
-        name: 'Título',
-        description: 'Dejar vacio para que tome el título de la historia'
+      name: "Título",
+      description: "Dejar vacio para que tome el título de la historia"
     }),
-    subTitle: PropTypes.string.tag({name: 'Descripción', hidden: true}),
+    subTitle: PropTypes.string.tag({ name: "Descripción", hidden: true }),
     articleConfig: PropTypes.contentConfig("story").tag({
-        name: 'Configuración de nota interna'
+      name: "Configuración de nota interna"
     }),
-    isExternalLink: PropTypes.bool.tag({name: '¿Nota externa?'}),
+    isExternalLink: PropTypes.bool.tag({ name: "¿Nota externa?" }),
     link: PropTypes.string.tag({
-        name: 'Link externo', 
-        fieldType: 'url'
+      name: "Link externo",
+      fieldType: "url"
     })
   })
 };
