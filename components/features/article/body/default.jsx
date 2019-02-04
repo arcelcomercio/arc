a44ff5ca8eb997331897f147f628aaefb9133ccd
Your Name <henrry11985@gmail.com>
2019-02-04 16:30:23 -0500

    notas content

--- components/features/article/body/default.jsx
+++ components/features/article/body/default.jsx
@@ -3,6 +3,7 @@ import Image from "@arc-core-components/element_image";
 import Heading from './types/heading'
 import Subheading from './types/subheading'
 import Video from './types/video'
+import Gallery from './types/gallery'
 
 import Consumer from "fusion:consumer";
 import React, { Component, Fragment } from "react";
@@ -19,8 +20,6 @@ class ContentArticleBody extends Component {
       imageClasses: "visual__image visual__image--cover"
     };
 
-  
-
     return (
       <Fragment>
       <Heading />
@@ -35,9 +34,14 @@ class ContentArticleBody extends Component {
               return <Image url={element.url} alt="" className="visual__image visual__image--cover" />
             }
             if (type === 'video') {
-              return <Video data={element.embed_html} />
-          }
-        }}
+              return <Video data={element.embed_html} className="visual__image visual__image--cover"/>
+            }
+            if (type === 'gallery') {
+             // return <Gallery  />
+             return <Gallery data={element} className="image--cover"/>
+            }
+            }
+        }
       />
     )}
       </Fragment>
