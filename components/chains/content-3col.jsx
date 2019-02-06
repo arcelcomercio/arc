016ce75458f43da45507fc6314541ccd1ec13e57
Carlos Fernández <hello@cajotafer.com>
2019-02-06 13:07:49 -0500

    fixed bugs with grid-content styles

--- components/chains/content-3col.jsx
+++ components/chains/content-3col.jsx
@@ -2,7 +2,6 @@ import React, { Fragment, Component } from 'react'
 
 const styles = [
     'content--grid-base',
-    'content-layout',
     'content--1col',
     'content--2col',
     'content--3col',
