4aeabb540238844bf15a80816953066acfb59df9
Your Name <henrry11985@gmail.com>
2019-02-06 17:37:43 -0500

    nota galeria

--- components/features/article/header/default.jsx
+++ components/features/article/header/default.jsx
@@ -1,6 +1,8 @@
 import Heading from './types/heading'
 import Subheading from './types/subheading'
 import Gallery from './types/gallery'
+import Share from './types/share'
+import  './article.css';
 
 import Consumer from "fusion:consumer";
 import React, { Component, Fragment } from "react";
@@ -10,13 +12,14 @@ import PropTypes from "prop-types";
 class ArticleHeader extends Component {
   render() {
     const { content_elements: contentElements } = this.props.globalContent;
-    const { content_elements: galleryElements } = (typeof contentElements === "undefined" )?'':contentElements[0];
+    const { content_elements: galleryElements } = (typeof contentElements === "undefined") ? '' : contentElements[0];
     //console.log(contentElements);debugger;
     return (
       <Fragment>
-        <div class={(galleryElements) ? '_gallery col-3':'col-3' }>
+        <div class={(galleryElements) ? '_gallery col-3' : 'col-3'}>
           <Heading />
           <Subheading />
+          <Share />
           {contentElements && contentElements.map((value, key) => {
             if (key == 0 && value.type == 'gallery') {
               return <Gallery data={value} id={key} />;
