81a883e4126743676282b49e84f0284dad89f75b
Edgar <edgar.gamarra21@gmail.com>
2019-02-07 17:07:28 -0500

    se agregaron los customfield para personalizar el componente

--- components/features/lista/default.jsx
+++ components/features/lista/default.jsx
@@ -1,10 +1,76 @@
 import Consumer from "fusion:consumer";
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import HeaderList from "./HeaderList";
-import { ListItemNews } from "./ItemNews";
+// import HeaderList from "./HeaderList";
+// import { ListItemNews } from "./ItemNews";
 import "./lista.css";
 
+const HeaderList = ({ title, background, seeMore }) => {
+  return (
+    <div className={"lista-header " + background}>
+      <div className="title">
+        <h4>{title} </h4>
+      </div>
+      {seeMore && (
+        <SeeMore />
+      )}
+    </div>
+  );
+};
+const SeeMore = () =>(<div className="more-news"><a href="#"><h4>ver mas</h4></a></div>)
+const ImageNews = () => {
+  return (
+    <figure>
+      <a href="https://elcomercio.pe/vamos/peru/cinco-restaurantes-encontraras-mejores-causas-lima-fotos-noticia-604647">
+        <picture>
+          <source
+            data-type="srcset"
+            srcSet="https://img.elcomercio.pe/files/listing_ec_home_bloque5/files/article_content_ec_fotos/uploads/2019/02/05/5c59eaa4ad426.jpeg"
+            media="(max-width: 639px)"
+          />
+          <img
+            datatype="src"
+            src="https://img.elcomercio.pe/files/article_content_ec_fotos/uploads/2019/02/05/5c59eaa4ad426.jpeg"
+          />
+        </picture>
+      </a>
+    </figure>
+  );
+};
+
+const TimeItem = ({ time }) => <div className="time">{time}</div>;
+
+const ItemNews = ({ seeHour, seeImageNews, time, title, urlNews }) => {
+  return (
+    <article className="item-news">
+      {seeImageNews && <ImageNews />}
+      {seeHour && <TimeItem time={time} />}
+      <div className="page-link">
+        <a href={urlNews}>
+          <h3 className="bold">{title}</h3>
+        </a>
+      </div>
+    </article>
+  );
+};
+const ListItemNews = ({ seeHour, seeImageNews, listNews }) => {
+  let classListItems =
+    listNews.length > 3 ? "list-news-items scrol" : "list-news-items";
+
+  return (
+    <div className={classListItems}>
+      {listNews.map(({ time, title, urlNews }, index) => (
+        <ItemNews
+          seeHour={seeHour}
+          seeImageNews={seeImageNews === true && index === 0 ? true : false}
+          time={time}
+          title={title}
+          urlNews={urlNews}
+        />
+      ))}
+    </div>
+  );
+};
 @Consumer
 class Lista extends Component {
   constructor(props) {
@@ -13,11 +79,13 @@ class Lista extends Component {
     const {
       title,
       background = "",
-      seeMore = true,
-      seeHour = false,
-      seeImageNews = false,
-      listNews
-    } = this.props;
+      seeMore,
+      seeHour,
+      seeImageNews
+      // listNews
+    } = this.props.customFields || {};
+    const { listNews } = this.props;
+
     this.state = {
       title,
       background,
@@ -27,7 +95,9 @@ class Lista extends Component {
       listNews
     };
   }
+
   render() {
+    debugger;
     return (
       <div className="List">
         <HeaderList
@@ -47,8 +117,8 @@ class Lista extends Component {
 
 Lista.defaultProps = {
   // title: 'ÚLTIMO MINUTO'
-  title: "Politica",
-  background: "color-backgroud-light-blue",
+  //title: "Politica",
+  //background: "color-backgroud-light-blue",
   listNews: [
     {
       time: "14:15",
@@ -77,24 +147,36 @@ Lista.defaultProps = {
       urlNews:
         "https://elcomercio.pe/politica/presidente-independiente-columna-maria-alejandra-campos-noticia-604782"
     }
-  ],
-  seeMore: false,
-  seeHour: false,
-  seeImageNews: false
+  ]
+  //seeMore: false,
+  //seeHour: false,
+  //seeImageNews: false
 };
 Lista.propTypes = {
   customFields: PropTypes.shape({
-    title: PropTypes.string.tag({ name: "Título" }),
+    background: PropTypes.oneOf([
+      "color-backgroud-light-blue",
+      "color-backgroud-white"
+    ]).tag({
+      name: "Color de fondo cabecera",
+      labels: {
+        "color-backgroud-light-blue": "celeste",
+        "color-backgroud-white": "blanco"
+      },
+      defaultValue: "color-backgroud-light-blue"
+    }),
+
+    title: PropTypes.string.isRequired.tag({ name: "Título" }),
     seeMore: PropTypes.bool.tag({ name: "Ver más" }),
     seeHour: PropTypes.bool.tag({ name: "Ver hora" }),
     seeImageNews: PropTypes.bool.tag({ name: "Ver imagen" })
-  }),
+  })
 
   //title: PropTypes.string.isRequired,
-  background: PropTypes.string.isRequired,
+  //background: PropTypes.string.isRequired,
   //seeMore: PropTypes.bool.isRequired,
   //seeHour: PropTypes.bool.isRequired,
-  seeImageNews: PropTypes.bool,
+  //seeImageNews: PropTypes.bool,
   //defaultProps: PropTypes.array
 };
 
