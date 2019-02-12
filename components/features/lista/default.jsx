88f9907b73a66b7e2feb29009276519425f02cbc
Edgar <edgar.gamarra21@gmail.com>
2019-02-12 17:30:33 -0500

    se retiraron los comentarios y debugger

--- components/features/lista/default.jsx
+++ components/features/lista/default.jsx
@@ -1,60 +1,64 @@
 import Consumer from "fusion:consumer";
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-// import HeaderList from "./HeaderList";
-// import { ListItemNews } from "./ItemNews";
+
 import "./lista.css";
 
-const HeaderList = ({ nameSection, background, seeMore, seeMoreurl }) => {
+const HeaderList = ({ titleList, background, seeMore, seeMoreurl }) => {
   return (
     <div className={"lista-header " + background}>
       <div className="title">
-        <h4>{nameSection} </h4>
+        <h4>{titleList} </h4>
       </div>
       {seeMore && <SeeMore seeMoreurl={seeMoreurl} />}
     </div>
   );
 };
 
-const SeeMore = ({seeMoreurl}) => (
+const SeeMore = ({ seeMoreurl }) => (
   <div className="more-news">
-    <a href={seeMoreurl} >
+    <a href={seeMoreurl}>
       <h4>ver mas</h4>
     </a>
   </div>
 );
-const ImageNews = ({urlNews, promo_items}) => {
-  //"https://img.elcomercio.pe/files/article_content_ec_fotos/uploads/2019/02/05/5c59eaa4ad426.jpeg"
-  // srcSet="https://img.elcomercio.pe/files/listing_ec_home_bloque5/files/article_content_ec_fotos/uploads/2019/02/05/5c59eaa4ad426.jpeg"
-  //"https://elcomercio.pe/vamos/peru/cinco-restaurantes-encontraras-mejores-causas-lima-fotos-noticia-604647"
-  debugger
-  let imagen = promo_items.basic? promo_items.basic.url||'' :'';
-  
+const ImageNews = ({ urlNews, promo_items }) => {
+  let imagen = promo_items.basic ? promo_items.basic.url || "" : "";
   return (
     <figure>
-      <a href={urlNews} >
-        <picture>
-          <source
-            data-type="srcset"
-            srcSet={imagen}
-            media="(max-width: 639px)"
-          />
-          <img
-            datatype="src"
-            src={imagen}
-          />
-        </picture>
-      </a>
+      {imagen ? (
+        <a href={urlNews}>
+          <picture>
+            <source
+              data-type="srcset"
+              srcSet={imagen}
+              media="(max-width: 639px)"
+            />
+            <img datatype="src" src={imagen} />
+          </picture>
+        </a>
+      ):
+      <span> no hay imagen</span>
+      }
     </figure>
   );
 };
 
 const TimeItem = ({ time }) => <div className="time">{time}</div>;
 
-const ItemNews = ({ seeHour, seeImageNews, time, title, urlNews, promo_items }) => {
+const ItemNews = ({
+  seeHour,
+  seeImageNews,
+  time,
+  title,
+  urlNews,
+  promo_items
+}) => {
   return (
     <article className="item-news">
-      {seeImageNews && <ImageNews urlNews={urlNews} promo_items={promo_items} />}
+      {seeImageNews && (
+        <ImageNews urlNews={urlNews} promo_items={promo_items} />
+      )}
       {seeHour && <TimeItem time={time} />}
       <div className="page-link">
         <a href={urlNews}>
@@ -68,24 +72,34 @@ const ListItemNews = ({ seeHour, seeImageNews, listNews }) => {
   let classListItems =
     listNews.length > 3 ? "list-news-items scrol" : "list-news-items";
   //let nuevalista =[];
-  debugger
+
   return (
     <div className={classListItems}>
-      {listNews.map(({ display_date, headlines:{basic}, canonical_url, promo_items }, index) => {
-        let fechaPublicacion = new Date(display_date);
-        
-        return(
-        <ItemNews
-          key={index}
-          seeHour={seeHour}
-          seeImageNews={seeImageNews === true && index === 0 ? true : false}
-          time={fechaPublicacion.getHours()+ ':'+fechaPublicacion.getMinutes()+'-'}
-          title={basic}
-          urlNews={canonical_url}
-          promo_items ={promo_items||''}
-        />)
-        
-      })}
+      {listNews.map(
+        (
+          { display_date, headlines: { basic }, canonical_url, promo_items },
+          index
+        ) => {
+          let fechaPublicacion = new Date(display_date);
+
+          return (
+            <ItemNews
+              key={index}
+              seeHour={seeHour}
+              seeImageNews={seeImageNews === true && index === 0 ? true : false}
+              time={
+                fechaPublicacion.getHours() +
+                ":" +
+                fechaPublicacion.getMinutes() +
+                "-"
+              }
+              title={basic}
+              urlNews={canonical_url}
+              promo_items={promo_items || ""}
+            />
+          );
+        }
+      )}
     </div>
   );
 };
@@ -95,7 +109,7 @@ class Lista extends Component {
     super(props);
 
     const {
-      nameSection,
+      titleList,
       background = "",
       seeMore,
       seeMoreurl,
@@ -103,9 +117,9 @@ class Lista extends Component {
       seeImageNews,
       secction
     } = this.props.customFields || {};
-    debugger
+
     this.state = {
-      nameSection,
+      titleList,
       background,
       seeMore,
       seeMoreurl,
@@ -113,11 +127,10 @@ class Lista extends Component {
       seeImageNews,
       secction,
       data: []
-    }; 
-    
+    };
   }
 
-  componentDidMount =() =>{
+  componentDidMount = () => {
     const { fetched } = this.getContent(
       "get-lis-news",
       {
@@ -127,12 +140,10 @@ class Lista extends Component {
       this.filterSchema()
     );
     fetched.then(response => {
-      debugger
       this.setState({
         data: response.content_elements
       });
-      
-    })
+    });
   };
 
   filterSchema() {
@@ -153,13 +164,13 @@ class Lista extends Component {
       }
     }
     `;
-  };
+  }
 
   render() {
     return (
       <div className="List">
         <HeaderList
-          nameSection={this.state.nameSection}
+          titleList={this.state.titleList}
           background={this.state.background}
           seeMore={this.state.seeMore}
           seeMoreurl={this.state.seeMoreurl}
@@ -176,16 +187,12 @@ class Lista extends Component {
 
 Lista.propTypes = {
   customFields: PropTypes.shape({
-    secction: PropTypes.oneOf([
-      'politica',
-      'economia',
-      'lastnews'
-    ]).tag({
-      name:'Sección',
-      labels:{
-        'politica':'Política',
-        'economia':'economia',
-        'lastnews':'Ultimo minuto',
+    secction: PropTypes.oneOf(["politica", "economia", "lastnews"]).tag({
+      name: "Sección",
+      labels: {
+        politica: "Política",
+        economia: "economia",
+        lastnews: "Ultimo minuto"
       },
       defaultValue: "politica"
     }),
@@ -201,11 +208,11 @@ Lista.propTypes = {
       defaultValue: "color-backgroud-light-blue"
     }),
 
-    nameSection: PropTypes.string.isRequired.tag({ name: "Sección" }),
+    titleList: PropTypes.string.isRequired.tag({ name: "Título de la lista" }),
     seeMore: PropTypes.bool.tag({ name: "Ver más" }),
     seeHour: PropTypes.bool.tag({ name: "Ver hora" }),
     seeImageNews: PropTypes.bool.tag({ name: "Ver imagen" }),
-    seeMoreurl: PropTypes.string.tag({name:"Ver más url"})
+    seeMoreurl: PropTypes.string.tag({ name: "Ver más url" })
   })
 
   //title: PropTypes.string.isRequired,
