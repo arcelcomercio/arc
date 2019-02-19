2df7584a1463dbfb033cf06a3ca26cb11094a4ee
Edgar <edgar.gamarra21@gmail.com>
2019-02-19 18:57:24 -0500

    se copio la maqueta del componente y se modificaron los estilos

---
+++ components/features/separador/Default.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+//import "./_separador.scss";
+const SeparatorItem = () => {
+  return (
+    <article className="separator__body__item">
+      <div className="separator__body__item__detail">
+        <h2 className="separator__body__item__detail__title">
+          <a href="https://elcomercio.pe/somos/firmas/choros-dos-satanizacion-indiscrimada-motocicleta-jaime-bedoya-noticia-608112">
+            Así era Kukín Flores, el niño prodigio de Cantolao, por Daniel
+            Peredo
+          </a>
+        </h2>
+      </div>
+      <figure>
+        <a href="https://elcomercio.pe/somos/firmas/choros-dos-satanizacion-indiscrimada-motocicleta-jaime-bedoya-noticia-608112">
+          <img src="https://img.elcomercio.pe/files/listing_ec_home_separador_standar_nolazy/uploads/2019/02/18/5c6b4eba39457.jpeg" />
+        </a>
+      </figure>
+    </article>
+  );
+};
+class Separador extends Component {
+  render() {
+    return (
+      <div className="separator">
+        <h1 className="separator__header__title">
+          <a href="https://elcomercio.pe/mundo/eeuu/muro-trump-declaratoria-emergencia-frontera-estados-unidos-mexico-graficos-prototipos-quiere-construir-bbc-noticia-599781">
+            MURO DE TRUMP
+          </a>
+        </h1>
+        <div className="separator__body">
+          <SeparatorItem />
+          <SeparatorItem />
+          <SeparatorItem />
+          <SeparatorItem />
+        </div>
+      </div>
+    );
+  }
+}
+
+{
+  /**/
+}
+export default Separador;
