f962d01559c2b9f6eb2a66e59f937eb57a995da1
Carlos Fernández <hello@cajotafer.com>
2019-02-20 14:09:03 -0500

    Small changes to general-styles partials. Also first tests of resize listeners to render on demand

--- components/features/layout/header/default.jsx
+++ components/features/layout/header/default.jsx
@@ -1,5 +1,5 @@
 import Consumer from 'fusion:consumer'
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import Button from '../../../../resources/components/button'
 import { FormatClassName } from '../../../../resources/utilsJs/utilities'
 
@@ -46,50 +46,104 @@ const styles = FormatClassName({
         'icon--fire'
     ]
 })
+
 @Consumer
 class Header extends Component {
+    constructor(props){
+        super(props)
+        //------ Checks if you are in desktop or not
+        this.state = {
+            device: this.setDevice()
+        }
+    }
 
+    setDevice = () => {
+        const wsize = window.innerWidth
+
+        if(wsize < 640) {
+            return 'mobile'
+        } else if(wsize >= 640 && wsize < 1024){ 
+            return 'tablet'
+        } else {
+            return 'desktop'
+        }   
+    }
+
+    handleResize = () => {
+        const wsize = window.innerWidth
+
+        //------ Set the new state if you change from mobile to desktop
+        if(wsize >= 1024 && this.state.device !== 'desktop') {
+            this.setState({
+                device: 'desktop'
+            });
+            this.dispatchEvent('displayChange', this.state.device)
+        } else {
+            //------ Set the new state if you change from desktop to mobile
+            if(wsize < 1024 && wsize >= 640 && this.state.device !== 'tablet') {
+                this.setState({
+                    device: 'tablet'
+                });
+                this.dispatchEvent('displayChange', this.state.device)
+            } else {
+                //------ Set the new state if you change from desktop to mobile
+                if(wsize < 640 && this.state.device !== 'mobile') {
+                    this.setState({
+                        device: 'mobile'
+                    });
+                    this.dispatchEvent('displayChange', this.state.device)
+                }
+            }
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize);
+    }
+x
     render() {
 
         return(
-            <header className={styles.header} >
-                <div className={styles.headerMain}>
-                    <span>29 DE ENERO, 2019</span>
-                    <img 
-                        src={`${this.props.contextPath}/resources/dist/${this.props.arcSite}/images/logo.png`} 
-                        alt={`Logo de ${this.props.arcSite}`}
-                        className={styles.headerLogo}
-                    />
-                    <div className={styles.headerButtonContainer}>
-                        <Button
-                            iconClass={styles.headerBtnIconLogin}
-                            btnText='Ingresar'
-                            btnClass={styles.headerBtnLogin}
-                            btnLink='#'
+            this.state.device === 'desktop' ?
+                <header className={styles.header} >
+                    <div className={styles.headerMain}>
+                        <span>29 DE ENERO, 2019</span>
+                        <img 
+                            src={`${this.props.contextPath}/resources/dist/${this.props.arcSite}/images/logo.png`} 
+                            alt={`Logo de ${this.props.arcSite}`}
+                            className={styles.headerLogo}
                         />
-                        <Button
-                            btnText='Suscríbete'
-                            btnClass={styles.headerBtnSubscribe}
-                            btnLink='#'
-                        />
+                        <div className={styles.headerButtonContainer}>
+                            <Button
+                                iconClass={styles.headerBtnIconLogin}
+                                btnText='Ingresar'
+                                btnClass={styles.headerBtnLogin}
+                                btnLink='#'
+                            />
+                            <Button
+                                btnText='Suscríbete'
+                                btnClass={styles.headerBtnSubscribe}
+                                btnLink='#'
+                            />
+                        </div>
                     </div>
-                </div>
-                <ul className={styles.headerFeatured}>
-                    <li className={styles.headerFeaturedItem}>
-                        <i className={styles.headerFeaturedItemIcon}></i>
-                        LOS TEMAS DE HOY 
-                    </li>
-                    <li className={styles.headerFeaturedItem}>CNM </li>
-                    <li className={styles.headerFeaturedItem}>Vizcarra </li>
-                    <li className={styles.headerFeaturedItem}>Congreso </li>
-                    <li className={styles.headerFeaturedItem}>Poder Judicial </li>
-                    <li className={styles.headerFeaturedItem}>Corrupción </li>
-                </ul>    
-            </header>
+                    <ul className={styles.headerFeatured}>
+                        <li className={styles.headerFeaturedItem}>
+                            <i className={styles.headerFeaturedItemIcon}></i>
+                            LOS TEMAS DE HOY 
+                        </li>
+                        <li className={styles.headerFeaturedItem}>CNM </li>
+                        <li className={styles.headerFeaturedItem}>Vizcarra </li>
+                        <li className={styles.headerFeaturedItem}>Congreso </li>
+                        <li className={styles.headerFeaturedItem}>Poder Judicial </li>
+                        <li className={styles.headerFeaturedItem}>Corrupción </li>
+                    </ul>    
+                </header>
+            : null
         )
     }
 }
 
-Header.static = true
+//Header.static = true
 
 export default Header
--- components/features/layout/nav/default.jsx
+++ components/features/layout/nav/default.jsx
@@ -1,5 +1,5 @@
 import Consumer from 'fusion:consumer'
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Button from '../../../../resources/components/button'
 import { FormatClassName } from '../../../../resources/utilsJs/utilities'
 
@@ -32,28 +32,99 @@ const styles = FormatClassName({
     navList: [
         'flex-center',
         'nav__list'
+    ],
+    navLogo: [
+        'nav__logo'
+    ],
+    headerButtonContainer: [
+        'flex-center',
+        'header__main__btn-container'
+    ],
+    headerBtnLogin: [
+        'flex-center-vertical',
+        'btn',
+        'bg-color--white'
+    ],
+    headerBtnSubscribe: [
+        'flex-center-vertical',
+        'btn',
+        'bg-color--link'
+    ],
+    headerBtnIconLogin: [
+        'icon--login'
     ]
 })
+
 @Consumer
 class Nav extends Component {
+    constructor(props){
+        super(props)
+        //------ Checks if you are in desktop or not
+        this.state = {
+            device: this.setDevice()
+        }
+    }
+
+    setDevice = () => {
+        const wsize = window.innerWidth
+
+        if(wsize < 640) {
+            return 'mobile'
+        } else if(wsize >= 640 && wsize < 1024){ 
+            return 'tablet'
+        } else {
+            return 'desktop'
+        }   
+    }
+
+    handleDevice = (device) => {
+        this.setState({
+            device: device
+        })
+    }
+
+    componentDidMount() {
+        this.addEventListener('displayChange', this.handleDevice)
+    }
 
     render() {
 
         return(
             <nav className={styles.nav}>
                 <div className={styles.navButtonContainer}>
-                    <Button
-                        iconClass={styles.navButtonIconSearch}
-                        btnClass={styles.navButton}
-                        btnText='Buscar'
-                        btnLink='#'
-                    />
-                    <Button
-                        iconClass={styles.navButtonIconMenu}
-                        btnClass={styles.navButton}
-                        btnText='Secciones'
-                        btnLink='#'
-                    />
+                    {
+                        this.state.device === 'desktop' &&
+                        <Fragment>
+                            <Button
+                                iconClass={styles.navButtonIconSearch}
+                                btnClass={styles.navButton}
+                                btnText='Buscar'
+                                btnLink='#'
+                                />
+                            <Button
+                                iconClass={styles.navButtonIconMenu}
+                                btnClass={styles.navButton}
+                                btnText='Secciones'
+                                btnLink='#'
+                                />
+                        </Fragment>
+                    }
+                    {
+                        this.state.device === 'tablet' &&
+                            <Fragment>
+                                <Button
+                                    iconClass={styles.navButtonIconSearch}
+                                    btnClass={styles.navButton}
+                                    btnLink='#'
+                                    />
+                                <Button
+                                    iconClass={styles.navButtonIconMenu}
+                                    btnClass={styles.navButton}
+                                    btnText='Secciones'
+                                    btnLink='#'
+                                    />
+                            </Fragment>  
+                    }
                 </div>
                 <ul className={styles.navList}>
                     <li>Politica</li>
@@ -62,10 +133,34 @@ class Nav extends Component {
                     <li>Economia</li>
                     <li>Opinion</li>
                 </ul>
-                <div className={styles.navButtonContainer}>
-                    <a className={styles.navButtonFeatured} href="#">ZONA EJECUTIVA</a>
-                    <a className={styles.navButtonFeatured} href="#">CONSTRUYE BIEN</a>
-                </div>
+                <img
+                    src='https://www.woodwing.com/sites/default/files/assets/cases-new/elcomercio_logo_white_2x-2.png'
+                    /* src={`${this.props.contextPath}/resources/dist/${this.props.arcSite}/images/logo.png`} */ 
+                    alt={`Logo de ${this.props.arcSite}`}
+                    className={styles.navLogo}
+                />
+                {
+                    this.state.device === 'desktop' 
+                        ?
+                        <div className={styles.navButtonContainer}>
+                            <a className={styles.navButtonFeatured} href="#">ZONA EJECUTIVA</a>
+                            <a className={styles.navButtonFeatured} href="#">CONSTRUYE BIEN</a>
+                        </div>
+                        :
+                        <div className={styles.headerButtonContainer}>
+                            <Button
+                                iconClass={styles.headerBtnIconLogin}
+                                btnText='Ingresar'
+                                btnClass={styles.headerBtnLogin}
+                                btnLink='#'
+                            />
+                            <Button
+                                btnText='Suscríbete'
+                                btnClass={styles.headerBtnSubscribe}
+                                btnLink='#'
+                            />
+                        </div>
+                }
             </nav>
         )
     }
