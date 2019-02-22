3ca54aa925b4d86590a8431c3628e7ee65663fb7
Edward Sanchez <esanchez@rmgperustaff.com>
2019-02-22 17:35:08 -0500

    [UPDATE] - Se agrego la animacion del sidebar - nav/nav-sidebar

--- components/features/layout/nav/default.jsx
+++ components/features/layout/nav/default.jsx
@@ -43,12 +43,22 @@ class Nav extends Component {
     this.state = {
       device: this.setDevice(),
       services: [],
+      statusSidebar: false
     }
+
+    this.handleOpenSectionsSidebar = this.handleOpenSectionsSidebar.bind(this)
   }
 
   componentDidMount() {
     this.addEventListener('displayChange', this.handleDevice)
     this.fetch()
+  }
+
+  handleOpenSectionsSidebar = () => {
+    this.setState({
+      statusSidebar: !this.state.statusSidebar
+    })
+    console.log(this.state.statusSidebar)
   }
 
   // ------ Sets the initial device state
@@ -135,7 +145,7 @@ class Nav extends Component {
                   iconClass={classes.navButtonIconMenu}
                   btnClass={classes.navButton}
                   btnText="Secciones"
-                  btnLink="#"
+                  onClick={this.handleOpenSectionsSidebar}
                 />
               </Fragment>
             )}
@@ -149,12 +159,13 @@ class Nav extends Component {
                   btnClass={classes.navButton}
                   btnLink="#"
                 />
-                <Button
+                {/* <Button
                   iconClass={classes.navButtonIconMenu}
                   btnClass={classes.navButton}
                   btnText="Secciones"
                   btnLink="#"
-                />
+                /> */}
+                <button onClick={this.test}>Test</button>
               </Fragment>
             )}
             {device === 'mobile' && (
@@ -218,7 +229,7 @@ class Nav extends Component {
             </div>
           )}
         </div>
-        <NavSidebar sections={sections} />
+        <NavSidebar sections={sections} showSidebar={this.state.statusSidebar} />
       </nav>
     )
   }
--- resources/components/button.jsx
+++ resources/components/button.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default (props) => {
 
-    const { iconClass, btnClass, btnText, btnLink } = props
+    const { iconClass, btnClass, btnText, btnLink, onClick } = props
 
     return(
-        <a className={btnClass} href={btnLink}>
+        <a className={btnClass} href={btnLink} onClick={onClick}>
             {iconClass && <i className={iconClass}></i>}
             {btnText}
         </a>
