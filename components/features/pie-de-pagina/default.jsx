5f95de0d2a244f29666f0413e8f410bc8f1bf907
Alvaro <ahuamani@rmgperustaff.com>
2019-02-22 10:32:20 -0500

    html initial structure

--- components/features/pie-de-pagina/default.jsx
+++ components/features/pie-de-pagina/default.jsx
@@ -1,34 +1,41 @@
-import PropTypes from "prop-types";
-import Consumer from "fusion:consumer";
-import React, { Fragment, Component } from "react";
-import { FormatClassName } from "../../../../resources/utilsJs/utilities";
+// import PropTypes from 'prop-types'
+import Consumer from 'fusion:consumer'
+import React, { Component } from 'react'
+import { FormatClassName } from '../../../resources/utilsJs/utilities'
 
-const classes = FormatClassName({});
+const classes = FormatClassName({
+  footer: ['footer-container'],
+  footerInfo: ['footer-container__info'],
+  footerSections: ['footer-container__sections'],
+  footerContact: ['footer-container__contact'],
+  footerSites: ['footer-container__sites'],
+})
 
 @Consumer
-class Footer extends Component {
+class PieDePagina extends Component {
   constructor(props) {
-    super(props);
-    this.state = {};
+    super(props)
+    this.state = {}
     // this.fetch();
   }
 
   fetch() {
-    const { fetched } = this.getContent();
+    const { fetched } = this.getContent()
     fetched.then(response => {
-      console.log(response);
-    });
+      console.log(response)
+    })
   }
-
-  castSection(res) {}
 
   render() {
     return (
-      <footer className={classes.footerContainer}>
-        <div>asd</div>
+      <footer className={classes.footer}>
+        <div className={classes.footerInfo}>1</div>
+        <div className={classes.footerSections}>2</div>
+        <div className={classes.footerContact}>3</div>
+        <div className={classes.footerSites}>4</div>
       </footer>
-    );
+    )
   }
 }
 
-export default Footer;
+export default PieDePagina
