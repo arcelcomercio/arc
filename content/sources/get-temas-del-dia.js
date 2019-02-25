7cccac3adf9981a6af7be318ceff20004f7f54d6
cesar <cesar.contreras.medina.3600@gmail.com>
2019-02-25 18:58:35 -0500

    cambios en get-temas-del-dia

--- content/sources/get-temas-del-dia.js
+++ content/sources/get-temas-del-dia.js
@@ -1,21 +1,16 @@
 
-//https://api.sandbox.elcomercio.arcpublishing.com/site/v3/navigation/elcomercio
-//?hierarchy=navegacion-cabecera-tema-del-dia
-
 
 const resolve = (query) => {
     const urlEndpoint = `/site/v3/navigation/`; //modificar para 
 
-    //if(query.hasOwnProperty('website')){
+    if(query.hasOwnProperty('website')){
     return `${urlEndpoint}${query.website}/?hierarchy=navegacion-cabecera-tema-del-dia`
-    //}
-    // else{
-    //     throw new Error(`NO existe la pagina web que esta buscando!!!!`) 
-    // }
+    }
+    else{
+        throw new Error(`NO existe la pagina web que esta buscando!!!!`) 
+    }
 }
 
-// recibe la funcion resolve, le envias el squema de grapQL para filtrar
-//y defines el tipo valor del parametro que recibe
 
 export default {
     resolve,
