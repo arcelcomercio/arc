cdb03a8dd2e0be0b41d84e149f3f0a6185b83fee
Rolly <rolly.sanchez@fractalservicios.pe>
2019-03-01 10:50:38 -0500

    refactoring styles

--- components/features/apertura-extraordinaria/_children/apertura-extraordinaria.jsx
+++ components/features/apertura-extraordinaria/_children/apertura-extraordinaria.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Data from './data'
 
 const classes = {
-  aperturae: 'aperturae padding-normal',
-  aperturaeSection: 'aperturae__section text-center',
-  aperturaeContent: 'aperturae__content',
-  aperturaeTitle: 'aperturae__title',
-  aperturaeSubtitle: 'aperturae__subtitle',
-  aperturaeAuthor: 'aperturae__author',
-  aperturaeMultimedia: 'aperturae__multimedia',
+  aperturaE: 'apertura-e padding-normal',
+  aperturaESection: 'apertura-e__section text-center',
+  aperturaEContent: 'apertura-e__content',
+  aperturaETitle: 'apertura-e__title',
+  aperturaESubtitle: 'apertura-e__subtitle',
+  aperturaEAuthor: 'apertura-e__author',
+  aperturaEMultimedia: 'apertura-e__multimedia',
 }
 
 const AperturaExtraordinariaChildren = props => {
@@ -18,32 +18,32 @@ const AperturaExtraordinariaChildren = props => {
   return (
     <div
       className={`
-            ${classes.aperturae} 
-            aperturae--${elem.multimediaOrientation} 
+            ${classes.aperturaE} 
+            apertura-e--${elem.multimediaOrientation} 
             text-${elem.contentOrientation}
             `}
     >
-      <div className={classes.aperturaeSection}>
+      <div className={classes.aperturaESection}>
         <a href={elem.sectionLink} {...editableField('section')}>
           {elem.section}
         </a>
       </div>
-      <div className={classes.aperturaeContent}>
-        <div className={classes.aperturaeTitle}>
+      <div className={classes.aperturaEContent}>
+        <div className={classes.aperturaETitle}>
           <a href={elem.link} {...editableField('title')}>
             {elem.title}
           </a>
         </div>
-        <div className={classes.aperturaeSubtitle}>
+        <div className={classes.aperturaESubtitle}>
           <a href={elem.link} {...editableField('subTitle')}>
             {elem.subTitle}
           </a>
         </div>
-        <div className={classes.aperturaeAuthor}>
+        <div className={classes.aperturaEAuthor}>
           <a href={elem.authorLink}>{elem.author}</a>
         </div>
       </div>
-      <div className={classes.aperturaeMultimedia}>
+      <div className={classes.aperturaEMultimedia}>
         <img src={elem.multimedia} alt={elem.title} />
       </div>
     </div>
