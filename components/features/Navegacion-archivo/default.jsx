f6012b717e45e5f65b85c7e7741ffcc3fb88362f
Edward Sanchez <esanchez@rmgperustaff.com>
2019-03-07 17:04:22 -0500

    [ADD] - terminada y testeada paginacion de archivo - navegacion-archivo/default

--- components/features/Navegacion-archivo/default.jsx
+++ components/features/Navegacion-archivo/default.jsx
@@ -1,70 +1,138 @@
-import React from 'react'
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react'
 
-const getFiveDays = (comingDate) => {
-	let fecha;
-	if (comingDate) {
-		const arrDate = comingDate.split('-')
-		fecha = arrDate
-			.map(el => el.startsWith('0') ? el.slice(1) : el)
-			.map(el => Number(el))
-			.map((el, index) => index === 1 ? (el - 1) : el)
-	}
-
-	const date = fecha ? new Date(...fecha) : new Date()
-	// const daysInThisMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
-	const daysInPastMonth = new Date(date.getFullYear(), (date.getMonth() + 1) - 1, 0).getDate()
-	const pastMonth = new Date(date.getFullYear(), (date.getMonth() + 1) - 1, 0).getMonth() + 1
-	const formarPastMonth = pastMonth < 10 ? `0${pastMonth}` : pastMonth
-	const today = date.getDate()
-	const month = (date.getMonth() + 1) < 10 ? `0${(date.getMonth() + 1)}` : (date.getMonth() + 1)
-	// const year = date.getFullYear()
-	const fiveDays = []
-	let ciclo = 0
-
-	for (let i = today; i > (today - 5); i--) {
-		if (i < 1) {
-			fiveDays.push(`${daysInPastMonth - ciclo}/${formarPastMonth}`)
-			ciclo += 1
-		} else {
-			const formatDay = i < 10 ? `0${i}` : i
-			fiveDays.push(`${formatDay}/${month}`)
+class renderPagination extends Component {
+	constructor() {
+		super()
+		this.state = {
+			dateIterator: this.evalDate().fiveDays
 		}
 	}
 
-	return fiveDays.reverse()
-}
+	// Remueve el año para imprimirlo en el componente Ej: 2010-10-10 => 10/10
+	clearDate(date) {
+		return date.slice(date.indexOf('-') + 1).split('-').reverse().join('/')
+	}
 
+	// Devuelve un array con 5 fechas anterioes a el Dia actual o a la fecha pasada como parametro
+	getFiveDays(comingDate) {
+		let fecha;
+		if (comingDate) {
+			const inputDate = new Date(comingDate)
+			const today = new Date()
+			if (inputDate > today) {
+				// Si la fecha en el parametro es mayor a HOY, devuelve HOY
+				fecha = null
+			} else {
+				// Si la fecha es menor, se formatea y se pasa como parametro al new Date
+				const arrDate = comingDate.split('-')
+				fecha = arrDate
+					.map(el => el.startsWith('0') ? el.slice(1) : el)
+					.map(el => Number(el))
+					.map((el, index) => index === 1 ? (el - 1) : el)
+			}
+		}
 
-const evalDate = () => {
-	const dateTest = ''
-	return dateTest && dateTest !== '' ? getFiveDays(dateTest) : getFiveDays()
-}
+		const formatDate = (date) => date < 10 ? `0${date}` : date
 
-const dateIterator = evalDate()
+		const date = fecha ? new Date(...fecha) : new Date()
+		const today = date.getDate()
+		const month = (date.getMonth() + 1)
+		const formatMonth = formatDate(month)
+		const year = date.getFullYear()
+		const daysInPastMonth = new Date(year, month - 1, 0).getDate()
+		const pastMonth = new Date(year, month - 1, 0).getMonth() + 1
+		const passYear = new Date(year, month - 1, 0).getFullYear()
+		const formarPastMonth = formatDate(pastMonth)
+		const fiveDays = []
+		const getNextDay = new Date(year, date.getMonth(), (date.getDate() + 1))
+		const nextDay = `${getNextDay.getFullYear()}-${formatDate((getNextDay.getMonth()) + 1)}-${formatDate(getNextDay.getDate())}`
+		let ciclo = 0
 
-const renderPagination = () => {
-	return(
-		<div className="pagination-file">
-			<ul className="pagination-file__list">
-				<li className="pagination-file__item">
-					<a className="pagination-file__link" href="/">Anterior</a>
-				</li>
-				{dateIterator.map((el, index) => {
-					return <li key={index} className={`pagination-file__item ${index === dateIterator.length - 1 ? 'active' : ''}`}>
-									{
-										index === dateIterator.length - 1 
-										? <p className="pagination-file__link">{el}</p>
-										: <a className="pagination-file__link" href="/">{el}</a>
-									}
-									
-								</li>
-				})}
-				<li className="pagination-file__item">
-					<a className="pagination-file__link" href="/">Siguiente</a>
-				</li>
-			</ul>
-		</div>
-	)
+		for (let i = today; i > (today - 5); i--) {
+			if (i < 1) {
+				if (pastMonth > month) {
+					// Si el mes pasado es mayor al mes actual, entonces pertenece a otro año
+					fiveDays.push(`${passYear}-${formarPastMonth}-${daysInPastMonth - ciclo}`)
+					ciclo += 1
+				} else {
+					// Si no, pertenece a este año pero en el mes anterior 
+					fiveDays.push(`${year}-${formarPastMonth}-${daysInPastMonth - ciclo}`)
+					ciclo += 1
+				}
+			} else {
+				// Si no, devuelve los 5 dias antes de HOY
+				fiveDays.push(`${year}-${formatMonth}-${formatDate(i)}`)
+			}
+		}
+
+		return {
+			fiveDays: fiveDays.reverse(),
+			nextDay
+		}
+	}
+
+	// Obtener la fecha del path o devolver vacio
+	getDateURL() {
+		const regEx = /^\d{4}-\d{2}-\d{2}$/
+		const path = window.location.pathname
+		const lastPath = path.slice(path.lastIndexOf('/') + 1)
+		if (lastPath.match(regEx)) {
+			return lastPath
+		}
+		return ''
+	}
+
+	// Si Hay en el path una fecha, la pasa como parametro a la funcion, si no se ejecuta la acutal
+	evalDate() {
+		const dateTest = this.getDateURL()
+		if (dateTest && dateTest !== '') {
+			return this.getFiveDays(dateTest)
+		}
+		return this.getFiveDays()
+	}
+
+	// Devuelve el link del <Anterior> en paginacion
+	getLastDay() {
+		const { dateIterator } = this.state
+		return dateIterator[dateIterator.length - 2]
+	}
+
+	getQueryURL() {
+		return window.location.search
+	}
+
+	// Devuelve el link del <Siguiente> en paginacion
+	getnextDay() {
+		return this.evalDate().nextDay
+	}
+
+	render() {
+		const { dateIterator } = this.state
+		return (
+			<div className="pagination-file">
+				<ul className="pagination-file__list">
+					<li className="pagination-file__item">
+						<a className="pagination-file__link" href={`/archivo/${this.getLastDay().concat(this.getQueryURL())}`}>Anterior</a>
+					</li>
+					{dateIterator.map((el, index) => {
+						return <li key={index} className={`pagination-file__item ${index === dateIterator.length - 1 ? 'active' : ''}`}>
+							{
+								index === dateIterator.length - 1
+									? <p className="pagination-file__link">{this.clearDate(el)}</p>
+									: <a className="pagination-file__link" href={`/archivo/${el.concat(this.getQueryURL())}`}>{this.clearDate(el)}</a>
+							}
+
+						</li>
+					})}
+					{this.getDateURL() !== ''
+						? (<li className="pagination-file__item">
+							<a className="pagination-file__link" href={`/archivo/${this.getnextDay().concat(this.getQueryURL())}`}>Siguiente</a>
+						</li>) : ''}
+				</ul>
+			</div>
+		)
+	}
 }
 
 export default renderPagination
