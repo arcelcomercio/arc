a453685e3eb1555d2df1bd187bffa502f9aca0e3
Alvaro <ahuamani@rmgperustaff.com>
2019-03-07 18:52:16 -0500

    agregando api para testear el resolver

---
+++ content/sources/resolver-archivo.js
@@ -0,0 +1,83 @@
+const getActualDate = () => {
+  const today = new Date()
+  let dd = today.getDate()
+  let mm = today.getMonth() + 1 // January is 0!
+
+  const yyyy = today.getFullYear()
+  if (dd < 10) {
+    dd = `0${dd}`
+  }
+  if (mm < 10) {
+    mm = `0${mm}`
+  }
+  return `${yyyy}-${mm}-${dd}`
+}
+
+const resolve = key => {
+  // /archivo/seccion/18-09     ["", "archivo", "seccion", "18-09"]
+  const auxValues = key.fullPath.split('/')
+  const params = {
+    page: auxValues[1],
+    section: auxValues[2] ? auxValues[2] : '',
+    date: auxValues[3] ? auxValues[3] : '',
+  }
+  if (!key.website) {
+    throw new Error('This content source requires a website')
+  }
+  if (!key.startDate || !key.finalDate) {
+    throw new Error('This content source requires a start date and final date')
+  }
+  const body = {
+    query: {
+      bool: {
+        must: [
+          {
+            term: {
+              type: 'story',
+            },
+          },
+          {
+            range: {
+              publish_date: {
+                gte: `${
+                  params.date ? params.date : getActualDate()
+                }T00:00:00-05:00`, // 2019-03-05T00:00:00-05:00
+                lte: `${
+                  params.date ? params.date : getActualDate()
+                }T23:59:59-05:00`, // 2019-03-06T00:00:00-05:00
+              },
+            },
+          },
+          {
+            term: {
+              'revision.published': 'true',
+            },
+          },
+        ],
+      },
+    },
+  }
+
+  if (params.section !== 'todas') {
+    body.query.bool.must.push({
+      term: {
+        'taxonomy.sites.path': `/${params.section}`,
+      },
+    })
+  }
+
+  const requestUri = `/content/v4/search/published?sort=publish_date:desc&website=${
+    key.website
+  }&body=${JSON.stringify(body)}`
+
+  return requestUri
+}
+
+export default {
+  resolve,
+  schemaName: 'stories',
+  params: {
+    fullPath: 'text',
+    website: ' text',
+  },
+}
