95952218e728947334d4aadc875008a79dd2c969
Edgar <edgar.gamarra21@gmail.com>
2019-03-19 15:24:12 -0500

    esqueleto del contenedor

--- components/features/container1/default.jsx
+++ components/features/container1/default.jsx
@@ -1,7 +1,30 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import Consumer from 'fusion:consumer'
 
+const classes={
+    container:'container',
+    title:'container__title'
+}
+// eslint-disable-next-line react/require-render-return
 @Consumer
-class Default extends Component {}
+class Default extends Component {
+  constructor(props) {
+    super(props)
+
+    
+    console.log('adadadadada')
+  }
+
+  render() {
+    return (
+      <div className="content-grid-base col-3">
+        <div className={classes.container}>
+            <h1 className={classes.title}>Economia</h1>
+        </div>
+        <div>Cuerpo</div>
+      </div>
+    )
+  }
+}
 
 export default Default
