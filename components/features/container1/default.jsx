3c3f9f9a19430e705340638e7e0484e4a68cb31f
Edgar <edgar.gamarra21@gmail.com>
2019-03-20 10:43:42 -0500

    consumo del resolver

--- components/features/container1/default.jsx
+++ components/features/container1/default.jsx
@@ -1,27 +1,44 @@
 import React, { Component } from 'react'
 import Consumer from 'fusion:consumer'
-
-const classes={
-    container:'container',
-    title:'container__title'
+import CardNotice from './../../../resources/components/listado-noticias'
+const classes = {
+  container: 'container',
+  title: 'container__title',
 }
 // eslint-disable-next-line react/require-render-return
 @Consumer
 class Default extends Component {
+  // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props)
 
-    
-    console.log('adadadadada')
   }
 
+
   render() {
+    const {
+      globalContent: { content_elements: contentElements },
+      arcSite,
+    } = this.props
+    const params = {
+      data: contentElements || [],
+      arcSite,
+    }
     return (
       <div className="content-grid-base col-3">
         <div className={classes.container}>
-            <h1 className={classes.title}>Economia</h1>
+          <h1 className={classes.title}>Economia</h1>
         </div>
-        <div>Cuerpo</div>
+        <div>
+          {params.data.map((el, index) => (
+            <CardNotice
+              key={index}
+              formato="row"
+              data={el}
+              arcSite={params.arcSite}
+            />
+          ))}
+        </div>
       </div>
     )
   }
