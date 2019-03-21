c7de2a95b9bff5b583a9e658ebbe7c244f2b690b
Carlos Fernández <hello@cajotafer.com>
2019-03-21 10:08:27 -0500

    [UPDATE] - Título para sección refactorioptimizado

--- components/features/global/título-de-seccion/default.jsx
+++ components/features/global/título-de-seccion/default.jsx
@@ -4,35 +4,21 @@ import React, { Component } from 'react'
 const classes = {
   title: 'full-width text-left margin-top',
 }
-
 @Consumer
 class ListTitle extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: '',
-    }
-  }
-
-  componentDidMount() {
-    const { isAdmin } = this.props
-    if (isAdmin) {
-      this.setState({
-        title: 'EL TÍTULO SÓLO SE MOSTRARÁ EN LA PÁGINA PUBLICADA',
-      })
-    } else {
-      const {
-        globalContent: { section_name: sectionName },
-      } = this.props
-      this.setState({
-        title: sectionName,
-      })
-    }
-  }
-
   render() {
-    const { title } = this.state
-    return <h1 className={classes.title}>{title.toUpperCase()}</h1>
+    const {
+      isAdmin,
+      globalContent: { section_name: sectionName },
+    } = this.props
+
+    return (
+      <h1 className={classes.title}>
+        {isAdmin
+          ? 'EL TÍTULO SÓLO SE MOSTRARÁ EN LA PÁGINA PUBLICADA'
+          : sectionName.toUpperCase()}
+      </h1>
+    )
   }
 }
 
