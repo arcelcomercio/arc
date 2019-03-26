cfd477c259e2552e157a1c2ed23f39f2a97919fa
Alvaro <ahuamani@rmgperustaff.com>
2019-03-26 15:08:03 -0500

    redefinido el content source navigation__by-hierarchy

--- content/sources/navigation__by-hierarchy.js
+++ content/sources/navigation__by-hierarchy.js
@@ -1,20 +1,23 @@
-const resolve = key => {
-  const requestUri = `/site/v3/navigation/${key.website}/?hierarchy=${key.hierarchy}`
-
-  const hasWebsite = Object.prototype.hasOwnProperty.call(key, 'website')
-  const hasHierarchy = Object.prototype.hasOwnProperty.call(key, 'hierarchy')
-
-  if (hasWebsite && hasHierarchy)
-    return requestUri
-
-  throw new Error('This content source requires a website and hierarchy')
+const schemaName = 'navigation'
+const params = {
+  website: 'text',
+  hierarchy: 'text',
+  // section: 'text',
 }
 
-export default {
-  resolve,
-  schemaName: 'navigation',
-  params: {
-    website: 'text',
-    hierarchy: 'text'
+export const createContentSource = site => {
+  const resolve = (key = {}) => {
+    const serviceSite = key['arc-site'] || site
+    const endpoint = `/site/v3/navigation/${serviceSite}/?hierarchy=${key.hierarchy ||
+      'default'}`
+
+    return endpoint
+  }
+  return {
+    resolve,
+    params,
+    schemaName,
   }
 }
+
+export default createContentSource('Arc Site Is Not Defined')
