edfcbc4c9e6b0a97aea61cd5a84903ff68a07fa6
cesar <cesar.contreras.medina.3600@gmail.com>
2019-04-12 16:30:45 -0500

    cambio en codigo de apertura extraordinaria

--- resources/components/apertura-extraordinaria.jsx
+++ resources/components/apertura-extraordinaria.jsx
@@ -1,3 +1,77 @@
+// import React from 'react'
+// import renderHTML from 'react-render-html'
+// import { getIcon } from '../utilsJs/helpers'
+
+// const classes = {
+//   aperturaE: `apertura-e padding-normal`,
+//   aperturaESection: 'apertura-e__section text-center',
+//   aperturaEContent: 'apertura-e__content',
+//   aperturaETitle: 'apertura-e__title',
+//   aperturaESubtitle: 'apertura-e__subtitle',
+//   aperturaEAuthor: 'apertura-e__author',
+//   aperturaEMultimedia: 'apertura-e__multimedia',
+//   iconGallery: 'apertura-e__icon-gallery',
+//   iconGalleryContainer: 'apertura-e__icon-gallery-container',
+// }
+
+// const getMultimediaIcon = mediaType => {
+//   let icon
+//   switch (mediaType) {
+//     case 'G':
+//       icon = classes.iconGallery
+//       break
+//     default:
+//       return ''
+//   }
+//   return (
+//     <span className={`${classes.iconGallery}`}>
+//       <span className={`${classes.iconGalleryContainer}`}>
+//         <i>{mediaType}</i>
+//       </span>
+//     </span>
+//   )
+// }
+// const AperturaExtraordinaria = props => {
+//   const {
+//     data,
+//     multimediaOrientation = 'bottom',
+//     contentOrientation = 'left',
+//     isSection = false,
+//     multimediaType,
+//   } = props
+
+//   return (
+//     <div
+//       className={`${
+//         classes.aperturaE
+//       } apertura-e--${multimediaOrientation} text-${contentOrientation}`}>
+//       {!isSection && (
+//         <div className={classes.aperturaESection}>
+//           <a href={data.sectionLink}>{data.section}</a>
+//         </div>
+//       )}
+//       <div className={classes.aperturaEContent}>
+//         <div className={classes.aperturaETitle}>
+//           <a href={data.link}>{data.title}</a>
+//         </div>
+//         <div className={classes.aperturaESubtitle}>
+//           <a href={data.link}>{data.subTitle}</a>
+//         </div>
+//         <div className={classes.aperturaEAuthor}>
+//           <a href={data.authorLink}>{data.author}</a>
+//         </div>
+//       </div>
+//       <div className={classes.aperturaEMultimedia}>
+//         {renderHTML(data.embedMultimedia)}
+//         <script src="https://d1tqo5nrys2b20.cloudfront.net/sandbox/powaBoot.js?org=elcomercio" />
+//         {getMultimediaIcon(getIcon(multimediaType))}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default AperturaExtraordinaria
+
 import React from 'react'
 import renderHTML from 'react-render-html'
 import { getIcon } from '../utilsJs/helpers'
@@ -15,21 +89,16 @@ const classes = {
 }
 
 const getMultimediaIcon = mediaType => {
-  let icon
-  switch (mediaType) {
-    case 'G':
-      icon = classes.iconGallery
-      break
-    default:
-      return ''
-  }
-  return (
-    <span className={`${classes.iconGallery}`}>
-      <span className={`${classes.iconGalleryContainer}`}>
-        <i>{mediaType}</i>
+  if (mediaType === 'G') {
+    return (
+      <span className={`${classes.iconGallery}-G`}>
+        <span className={`${classes.iconGalleryContainer}-G`}>
+          <i>{mediaType}</i>
+        </span>
       </span>
-    </span>
-  )
+    )
+  }
+  return null
 }
 const AperturaExtraordinaria = props => {
   const {
