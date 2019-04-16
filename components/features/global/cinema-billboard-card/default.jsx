e5b5ec0267cfcdbb7fd6806f545f14b8c06d9bcd
Alvaro <ahuamani@rmgperustaff.com>
2019-04-16 14:39:42 -0500

    [add, global/cinema/billboard/card] conectado con el api de cartelera

--- components/features/global/cinema-billboard-card/default.jsx
+++ components/features/global/cinema-billboard-card/default.jsx
@@ -1,90 +1,105 @@
 import Consumer from 'fusion:consumer'
 import React, { Component } from 'react'
 
-const dummy = {
-  movies: [
-    {
-      name: 'Parque mágico',
-      id: 'm-1',
-    },
-    {
-      name: 'After: Aquí empieza todo',
-      id: 'm-2',
-    },
-    {
-      name: 'Luchando con mi familia',
-      id: 'm-3',
-    },
-    {
-      name: 'La casa que Jack construyó',
-      id: 'm-4',
-    },
-    {
-      name: 'Papá youtuber',
-      id: 'm-5',
-    },
-  ],
-  theaters: [
-    {
-      name: 'Ate Vitarte Cineplanet Santa Clara      S/15',
-      id: 't-1',
-    },
-    {
-      name:
-        'Cine Star Aviación	                                                         S/ 10',
-      id: 't-2',
-    },
-    {
-      name:
-        'Cine Star Benavides	                                                        S/ 9',
-      id: 't-3',
-    },
-    {
-      name:
-        'Cine Star Excelsior	                                                            S/ 7,50',
-      id: 't-4',
-    },
-    {
-      name:
-        'Cine Star Metro Comas	                                                            S/ 8',
-      id: 't-5',
-    },
-  ],
-}
-
 @Consumer
 class CinemaBillboardCard extends Component {
   constructor(props) {
     super(props)
     this.state = {
       movieSelected: '',
-      theaterSelected: '',
+      cinemaSelected: '',
+      premiereData: {
+        title: '',
+        img: '',
+        url: '',
+        alt: '',
+      },
+      billboardData: {
+        moviesList: [],
+        cinemasList: [],
+      },
     }
-    this.handleChange = this.handleChange.bind(this)
+    this.handleMovieSelected = this.handleMovieSelected.bind(this)
+    this.handleCinemaSelected = this.handleCinemaSelected.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.getBillboard()
   }
 
-  handleChange(event) {
+  getBillboard() {
+    const { fetched } = this.getContent('cinema-billboard', { website: '' })
+
+    fetched.then(response => {
+      const { peliculas, cines, estrenos } = response
+
+      const moviesList = Object.values(peliculas)
+      const cinemasList = cines
+
+      // TODO: Agregar rezise de esta imagen
+      const premiereData = {
+        title: estrenos[0].name,
+        img: estrenos[0].poster.sizes.poster,
+        url: estrenos[0].url,
+        alt: estrenos[0].body,
+      }
+
+      this.setState({
+        premiereData,
+        billboardData: { moviesList, cinemasList },
+      })
+    })
+  }
+
+  handleMovieSelected(event) {
     this.setState({ movieSelected: event.target.value })
   }
 
+  handleCinemaSelected(event) {
+    this.setState({ cinemaSelected: event.target.value })
+  }
+
+  handleSubmit(event) {
+    const { contextPath, arcSite } = this.props
+    const { movieSelected, cinemaSelected } = this.state
+
+    const moviePath = movieSelected || 'peliculas'
+    const cinemaPath = cinemaSelected || 'cines'
+
+    const fullPath =
+      !movieSelected && !cinemaSelected ? '' : `${moviePath}/${cinemaPath}`
+
+    window.location.href = `${contextPath}/cartelera/${fullPath}?_website=${arcSite}`
+    event.preventDefault()
+  }
+
   render() {
-    console.log(this.state)
-    const { movieSelected, theaterSelected } = this.state
+    const {
+      movieSelected,
+      cinemaSelected,
+      billboardData,
+      premiereData,
+    } = this.state
+
+    const { contextPath, arcSite } = this.props
 
     return (
       <div className="card-cinema padding-normal row-1">
         <article className="position-relative">
           <span className="card-cinema__gradient full-width" />
           <h3 className="card-cinema__category">
-            <a className="card-cinema__link" href="/cartelera">
+            <a
+              className="card-cinema__link"
+              href={`${contextPath}/cartelera?_website=${arcSite}`}>
               Cartelera
             </a>
           </h3>
           <figure className="card-cinema__figure">
-            <a href="/cartelera/luchando...">
+            <a
+              href={`${contextPath}/cartelera/peliculas/${
+                premiereData.url
+              }?_website=${arcSite}`}>
               <img
-                src="https://cde.3.elcomercio.pe/cines/0/1/7/0/0/1700906/poster.jpg"
-                alt=""
+                src={premiereData.img}
+                alt={premiereData.alt}
                 className="full-width card-cinema__img"
               />
             </a>
@@ -92,7 +107,11 @@ class CinemaBillboardCard extends Component {
           <div className="card-cinema__detail full-width">
             <span className="card-cinema__premiere">Estreno</span>
             <h2 className="card-cinema__p-title">
-              <a className="card-cinema__p-link" href="/cartelera/luchando...">
+              <a
+                className="card-cinema__p-link"
+                href={`${contextPath}/cartelera/peliculas/${
+                  premiereData.url
+                }?_website=${arcSite}`}>
                 Luchando con mi familia
               </a>
             </h2>
@@ -103,36 +122,48 @@ class CinemaBillboardCard extends Component {
           <form
             action="/cartelera/search"
             method="post"
-            className="card-cinema__form">
+            className="card-cinema__form"
+            onSubmit={this.handleSubmit}>
             <div className="card-cinema__selects-container">
               <select
                 name="movie"
                 className="card-cinema__select"
                 value={movieSelected}
-                onChange={this.handleChange}>
+                onChange={this.handleMovieSelected}>
                 <option
                   value=""
-                  selected
+                  defaultValue
                   disabled
                   className="card-cinema__option">
                   PELÍCULAS
                 </option>
-                {dummy.movies.map(movie => (
+                {billboardData.moviesList.map(movie => (
                   <option
-                    value={movie.name}
+                    value={movie.url}
                     className="card-cinema__option"
-                    key={movie.id}>
-                    {movie.name}
+                    key={movie.mid}>
+                    {movie.title}
                   </option>
                 ))}
               </select>
-              <select name="theater" className="card-cinema__select">
-                <option value selected disabled className="card-cinema__option">
+              <select
+                name="theater"
+                className="card-cinema__select"
+                value={cinemaSelected}
+                onChange={this.handleCinemaSelected}>
+                <option
+                  value=""
+                  defaultValue
+                  disabled
+                  className="card-cinema__option">
                   CINES
                 </option>
-                {dummy.theaters.map(theater => (
-                  <option className="card-cinema__option" key={theater.id}>
-                    {theater.name}
+                {billboardData.cinemasList.map(cinema => (
+                  <option
+                    value={cinema.url}
+                    className="card-cinema__option"
+                    key={cinema.cid}>
+                    {cinema.nombre}
                   </option>
                 ))}
               </select>
