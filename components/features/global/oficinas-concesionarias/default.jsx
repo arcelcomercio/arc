6174e4730bcc085bf0e170d9df489dcbd48b66b5
cesar <cesar.contreras.medina.3600@gmail.com>
2019-04-22 14:33:03 -0500

    creada default.jsx de oficinas concesionarias

---
+++ components/features/global/oficinas-concesionarias/default.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const oficinasConcesionarias = () => {
+  return (
+    <Fragment>
+      <h1>oficina concesionaria</h1>
+    </Fragment>
+  )
+}
+
+export default oficinasConcesionarias
