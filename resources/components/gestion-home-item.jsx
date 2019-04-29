5bd1cc5dcb5adec7df841c4a009469c21db7a446
cesar <cesar.contreras.medina.3600@gmail.com>
2019-04-29 18:53:18 -0500

    agregados classes

--- resources/components/gestion-home-item.jsx
+++ resources/components/gestion-home-item.jsx
@@ -9,6 +9,8 @@ const classes = {
   // opinionthreeline: 'separator__opinion--item-threeline',
 
   blogItem: 'item__blog-item',
+  blogItemTop: 'item__blog-top',
+  blogItemBottom: 'item__blog-bottom',
   blogItemLeft: 'item__blog-item-left',
   blogItemRight: 'item__blog-item-right',
   blogItemDateHour: 'item__blog-item-date-hour',
@@ -22,19 +24,23 @@ const GestionHomeItem = props => {
 
   return (
     <article className={classes.blogItem}>
-      <div className={classes.blogItemLeft}>
+      <div className={classes.blogItemTop}>
         <div className={classes.blogItemDateHour}>{dateHour}</div>
-        <figure className={classes.blogItemImage}>
-          <img src={imageUrl} alt={author || ''} />
-        </figure>
       </div>
-      <div className={classes.blogItemRight}>
-        <h3>
-          <a href={itemUrl}>{titleItem}</a>
-        </h3>
-        <h5>
-          <a href={authorUrl}>{author}</a>
-        </h5>
+      <div className={classes.blogItemBottom}>
+        <div className={classes.blogItemLeft}>
+          <figure className={classes.blogItemImage}>
+            <img src={imageUrl} alt={author || ''} />
+          </figure>
+        </div>
+        <div className={classes.blogItemRight}>
+          <h3>
+            <a href={itemUrl}>{titleItem}</a>
+          </h3>
+          <h5>
+            <a href={authorUrl}>{author}</a>
+          </h5>
+        </div>
       </div>
     </article>
   )
