16b9b06f8178e228d200b4d0655552b0b89f1d9e
Alvaro <ahuamani@rmgperustaff.com>
2019-05-01 19:07:04 -0500

    [wip, /post-header] agregado lista de botones compartir

--- components/features/blog/post-header/default.jsx
+++ components/features/blog/post-header/default.jsx
@@ -1,32 +1,79 @@
 import Consumer from 'fusion:consumer'
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 
 @Consumer
-class BlogPostHeader extends Component {
+class BlogPostHeader extends PureComponent {
   constructor(...props) {
     super(...props)
-    this.state = {}
+    this.state = {
+      currentList: 'firstList',
+    }
+
+    this.shareButtons = {
+      firstList: [
+        {
+          icon: 'L',
+          link: '',
+        },
+        {
+          icon: 'F',
+          link: '',
+        },
+        {
+          icon: 'W',
+          link: '',
+        },
+      ],
+      secondList: [
+        {
+          icon: 'P',
+          link: '',
+        },
+        {
+          icon: 'T',
+          link: '',
+        },
+        {
+          icon: 'IM',
+          link: '',
+        },
+      ],
+    }
   }
 
-  more = () => {
-    console.log('asdasd')
+  handleMoreButton = () => {
+    const { currentList } = this.state
+    const newList = currentList === 'firstList' ? 'secondList' : 'firstList'
+    this.setState({ currentList: newList })
   }
 
   render() {
+    const { currentList } = this.state
     return (
-      <div>
-        <h1>Venganza y esperanza</h1>
-        <ul>
-          <li>
-            <a href="/#">l</a>
+      <div className="post-header padding-normal">
+        <h1 className="post-header__title">No nos distraigas mucho, Alan...</h1>
+        <ul className="post-header__list flex">
+          {this.shareButtons[currentList].map((item, i) => (
+            <li className="post-header__item flex-center-vertical flex--justify-center">
+              <a href="/#">
+                <i>{item.icon}</i>
+                <span className="post-header__share">
+                  {i === 2 && currentList === 'secondList'
+                    ? 'Imprimir'
+                    : 'Compartir'}
+                </span>
+              </a>
+            </li>
+          ))}
+
+          <li className="post-header__more">
+            <button
+              className="post-header__button flex-center-vertical flex--justify-center"
+              type="button"
+              onClick={e => this.handleMoreButton(e)}>
+              <i>{currentList === 'firstList' ? '+' : '-'}</i>
+            </button>
           </li>
-          <li>
-            <a href="/#">f</a>
-          </li>
-          <li>
-            <a href="/#">w</a>
-          </li>
-          <div onClick={this.more()}>+</div>
         </ul>
       </div>
     )
