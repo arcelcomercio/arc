9cea59305af6a37edb62c306005f5d1bdd1c4edf
Rolly <rolly.sanchez.26@gmail.com>
2019-05-08 11:17:32 -0500

    style for feature

--- components/features/blog/may-interest-you/_children/card.jsx
+++ components/features/blog/may-interest-you/_children/card.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
 
-const MayInterestYouCard = props => {
-  return <div>0</div>
+const classes = {
+  containerCard: 'bm-interest-you__container--card',
+  image: 'bm-interest-you__image',
+  detail: 'bm-interest-you__detail',
+  category: 'bm-interest-you__category',
+  titleDetail: 'bm-interest-you__title--detail',
+}
+const BlogMayInterestYouChildCard = props => {
+  const { title, image, sectionName, link, linkSection } = props
+  return (
+    <article className={classes.containerCard}>
+      <figure className={classes.image}>
+        <a href={link}>
+          <picture>
+            <source srcset={image} media="(max-width: 640px)" />
+            <img src={image} alt={title} />
+          </picture>
+        </a>
+      </figure>
+      <div className={classes.detail}>
+        <div>
+          <h3 className={classes.category}>
+            <a href={linkSection}>{sectionName}</a>
+          </h3>
+        </div>
+        <div>
+          <h3 className={classes.titleDetail}>
+            <a href={link}>{title}</a>
+          </h3>
+        </div>
+      </div>
+    </article>
+  )
 }
 
-export default MayInterestYouCard
+export default BlogMayInterestYouChildCard
--- components/features/blog/may-interest-you/default.jsx
+++ components/features/blog/may-interest-you/default.jsx
@@ -1,34 +1,49 @@
 import Consumer from 'fusion:consumer'
 import React, { PureComponent } from 'react'
-import renderHTML from 'react-render-html'
-import MayInterestYouCard from './_children/card'
-import { getFullDateIso8601 } from '../../../../resources/utilsJs/helpers'
+import BlogMayInterestYouChildCard from './_children/card'
 
 @Consumer
 class BlogMayInterestYou extends PureComponent {
-  /* constructor(props) {
-    super(props)
-    this.state = {}
-  } */
+  buildParams = (itemRelatedPost, blog, rootPath, website) => {
+    const urlBlog = `${rootPath}/blog/`
+    const link = urlBlog + itemRelatedPost.post_permalink
+    const linkSection = urlBlog + blog.path
+    const imageDefault = `${rootPath}/resources/dist/${website}/images/default-blog.jpg`
+    return {
+      title: itemRelatedPost.post_title || '',
+      image: itemRelatedPost.post_thumbnail || imageDefault,
+      sectionName: blog.blogname,
+      link: itemRelatedPost.post_permalink ? link : '',
+      linkSection: blog.path ? linkSection : '',
+    }
+  }
 
   render() {
     const {
-      globalContent: { post, user },
+      globalContent: { related_posts: relatedPosts, blog },
+      contextPath,
+      arcSite,
     } = this.props || {}
-    const { post_content: postContent, post_date: postDate } = post || {}
-    const { first_name: firstName } = user || {}
-    const formatDate = getFullDateIso8601(postDate)
-    const { day, month, fullYear, hours, minutes } = formatDate || {}
+    const classes = {
+      bmInterestYou: 'bm-interest-you clearfix',
+      titleGeneral: 'bm-interest-you__title--general',
+      containerList: 'bm-interest-you__container--list clearfix',
+    }
     return (
-      <div className="bm-interest-you">
-        Te puede interesar
-        <MayInterestYouCard />
+      <div className={classes.bmInterestYou}>
+        <h4 className={classes.titleGeneral}>Te puede interesar</h4>
+        <div className={classes.containerList}>
+          {relatedPosts.map(item => {
+            const params = this.buildParams(item, blog, contextPath, arcSite)
+            return <BlogMayInterestYouChildCard key={params.link} {...params} />
+          })}
+        </div>
       </div>
     )
   }
 }
 
 BlogMayInterestYou.label = 'Blog - Te puede interesar'
-// BlogMayInterestYou.static = true
+BlogMayInterestYou.static = true
 
 export default BlogMayInterestYou
