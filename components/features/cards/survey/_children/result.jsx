d00e8f255a8cf36488e00cb8fcd35d2204928737
Alvaro <ahuamani@rmgperustaff.com>
2019-05-10 14:26:39 -0500

    fix de estilos

--- components/features/cards/survey/_children/result.jsx
+++ components/features/cards/survey/_children/result.jsx
@@ -3,6 +3,13 @@ import PropTypes from 'prop-types'
 
 import CardsSurveyChildProgressBar from './progress-bar'
 
+const classes = {
+  surveyResult: 'survey-result',
+  list: 'survey-result__list survey-result__mb',
+  itemContainer: 'flex flex--justify-between survey-result__mb',
+  item: 'survey-result__item',
+}
+
 const CardsSurveyChildResult = props => {
   const { choices } = props
   const totalVotes = choices.reduce(
@@ -26,22 +33,20 @@ const CardsSurveyChildResult = props => {
   const indexOfTheHighlight = getIndexBiggestOf(choices)
 
   return (
-    <ul className="card-survey__result">
+    <ul className={classes.surveyResult}>
       {choices.map((result, i) => {
         const isBiggestValue = i === indexOfTheHighlight
-        const textHighightClass = isBiggestValue
-          ? 'survey-result__highlight'
-          : ''
+        const textHighightClass = isBiggestValue ? 'active' : ''
         return (
-          <li
-            key={result.option}
-            className="survey-result__list survey-result__mb">
-            <div className="flex flex--justify-between survey-result__mb">
-              <span className={`survey-result__item ${textHighightClass}`}>
+          <li key={result.option} className={classes.list}>
+            <div className={classes.itemContainer}>
+              <span className={`${classes.item} ${textHighightClass}`}>
                 {result.option}
               </span>
               <span
-                className={`survey-result__item ${textHighightClass}`}>{`${Math.round(
+                className={`${
+                  classes.item
+                } ${textHighightClass}`}>{`${Math.round(
                 (result.votes / totalVotes) * 100
               )}%`}</span>
             </div>
