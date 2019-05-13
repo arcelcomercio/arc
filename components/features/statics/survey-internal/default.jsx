a01cc173ea3c34ed034e006d58414eda5f425e52
Edward Sanchez <esanchez@rmgperustaff.com>
2019-05-13 14:27:56 -0500

    [UPDATE] - configurado data para el resolver

--- components/features/statics/survey-internal/default.jsx
+++ components/features/statics/survey-internal/default.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import Consumer from 'fusion:consumer'
 import SurveyInternalChildSurvey from './_children/survey'
 
-const SurveyInternal = () => {
-  return <SurveyInternalChildSurvey />
+@Consumer
+class SurveyInternal extends PureComponent {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { globalContent, arcSite, contextPath } = this.props
+    const {
+      title = '',
+      dateStart: date = '',
+      choices = [],
+      slugNext: { slug: next = '' },
+      slugPrev: { slug: prev = '' },
+    } = globalContent
+
+    const params = {
+      arcSite,
+      contextPath,
+      title,
+      date,
+      next,
+      prev,
+      choices,
+    }
+    return <SurveyInternalChildSurvey {...params} />
+  }
 }
 
 SurveyInternal.label = 'Encuesta Interna'
