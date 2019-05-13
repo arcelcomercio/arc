34c7485771b93cb9534c13915d12474a83a24c02
Edward Sanchez <esanchez@rmgperustaff.com>
2019-05-13 15:06:41 -0500

    [UPDATE] - agregado post de encuesta y chequeo de cockiees

--- components/features/statics/survey-internal/default.jsx
+++ components/features/statics/survey-internal/default.jsx
@@ -1,12 +1,41 @@
 import React, { PureComponent } from 'react'
 import Consumer from 'fusion:consumer'
 import SurveyInternalChildSurvey from './_children/survey'
+import { setSurveyCookie, getCookie } from '../../../utilities/helpers'
 
 @Consumer
 class SurveyInternal extends PureComponent {
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props)
+    this.currentSurveyId = this.props.globalContent.id
+    this.hasVote = getCookie(`idpoll${this.currentSurveyId}`) || false
+  }
+
+  sendQuiz = optionSelected => {
+    const body = {
+      id: this.currentSurveyId,
+      option: optionSelected,
+      website: 'peru21',
+    }
+    const URL = 'http://jab.pe/f/arc/services/encuesta.php'
+    const { contextPath } = this.props
+    fetch(URL, {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .catch(error => error)
+      .then(response => {
+        if (response.status === 200) {
+          const { globalContent: { slug = '' } = {} } = this.props
+          setSurveyCookie(body.id, 1)
+          window.location.href = `${contextPath}/encuesta/${slug}`
+        }
+      })
   }
 
   render() {
@@ -27,7 +56,10 @@ class SurveyInternal extends PureComponent {
       next,
       prev,
       choices,
+      hasVote: this.hasVote,
+      sendQuiz: this.sendQuiz,
     }
+    console.log(globalContent)
     return <SurveyInternalChildSurvey {...params} />
   }
 }
