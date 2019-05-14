effd7a851bf35ce79e403a6e76494f4b74472b4b
luis.moreno <luis.moreno@comercio.com.pe>
2019-05-14 12:20:34 -0500

    culminacion de la maqueta

--- components/features/extraordinary-story/grid/_children/extraordinary-grid-stories.jsx
+++ components/features/extraordinary-story/grid/_children/extraordinary-grid-stories.jsx
@@ -7,11 +7,11 @@ class ExtraordinaryStoryChildGridStories extends PureComponent {
 
   render() {
     return (
-      <div className="extraordinary-story-grid flex">
-        <div className="story-video-box">
-          <figure>
+      <div className="extraordinary-story-grid flex position-relative">
+        <div className="story-video-box flex-center-vertical">
+          <figure className="full-width">
             <img
-              className="story-video-box__img"
+              className="full-width"
               src="https://img.peru21.pe/files/listing_p21_portada_p21tv_v3_destaque/uploads/2019/05/13/5cd994b00a0a8.jpeg"
               alt=""
             />
@@ -27,45 +27,45 @@ class ExtraordinaryStoryChildGridStories extends PureComponent {
             />
           </div>
           <h2 className="stories-grid__title">Programas del dia</h2>
-          <div className="stories-grid__item-list">
+          <div className="stories-grid__item-list flex">
             <div className="stories-grid-item">
               <a href="#">
                 <img
-                  className="stories-grid-item__img"
+                  className="full-width"
                   src="http://peru21.pe/img/p21tv/21noticias.jpg"
                   alt=""
                 />
-                <p className="stories-grid-item__title">titulo</p>
+                <p className="stories-grid-item__title text-left">titulo</p>
               </a>
             </div>
             <div className="stories-grid-item">
               <a href="#">
                 <img
-                  className="stories-grid-item__img"
+                  className="full-width"
                   src="http://peru21.pe/img/p21tv/lavozdel21.jpg"
                   alt=""
                 />
-                <p className="stories-grid-item__title">titulo</p>
+                <p className="stories-grid-item__title text-left">titulo</p>
               </a>
             </div>
             <div className="stories-grid-item">
               <a href="#">
                 <img
-                  className="stories-grid-item__img"
+                  className="full-width"
                   src="http://peru21.pe/img/p21tv/deportes21.jpg?v2"
                   alt=""
                 />
-                <p className="stories-grid-item__title">titulo</p>
+                <p className="stories-grid-item__title text-left">titulo</p>
               </a>
             </div>
             <div className="stories-grid-item">
               <a href="#">
                 <img
-                  className="stories-grid-item__img"
+                  className="full-width"
                   src="http://peru21.pe/img/p21tv/informe21.jpg?v2"
                   alt=""
                 />
-                <p className="stories-grid-item__title">titulo</p>
+                <p className="stories-grid-item__title text-left">titulo</p>
               </a>
             </div>
           </div>
