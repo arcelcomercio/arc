18cd845ae419d62de99dc085c34cd9110d5955ad
Maria Gabriela Rangel <mrangel@rmgperustaff.com>
2019-05-20 17:50:16 -0500

    Modificaciones en estilo menu mobile, cabecera somos

--- components/features/layout/header/_children/somos.jsx
+++ components/features/layout/header/_children/somos.jsx
@@ -2,38 +2,55 @@ import React, { Fragment } from 'react'
 
 const classses = {
   logocontent: 'header-somos',
-  logoimg: 'header-somos__logo-img',
-  menumovil: 'header-somos__menu-movil',
-  asidemenu: 'header-somos__menu-movil',
-  asidemenuscroller: '',
-  asidemenucontent: '',
+  iconmenuwrapper: 'header-somos__icon-wrapper',
+  menuicon: 'icon-menu header-somos__icon',
+  logoicon: 'header-somos__logo-wrapper',
+  logoimgwrapper: 'header-somos__img-wrapper',
+  logoimg: 'header-somos__img',
 }
 
 const HeaderChildSomos = () => {
   return (
     <Fragment>
       <div className={classses.logocontent}>
-        <a href="/">men</a>
-        <a href="/">
-          <img
-            className={classses.logoimg}
-            src="https://img.elcomercio.pe/img/somos/logo_somos.png?1557531956"
-            alt="somos"
-          />
-        </a>
-        <a href="/"><img src="/resources/dist/elcomercio/images/letra-c-white.png" alt="logo"/></a>
-      </div>
-      <nav className="header-menu">
-      <div className="header-menu__content">
-        <div className="header-menu__search">
-          <input type="text" placeholder="Buscar" className="header-menu__search-input"/>
+        <div className={classses.iconmenuwrapper}>
+          <a href="/"><i className={classses.menuicon}></i></a>
         </div>
+        <div className={classses.logoimgwrapper}>
+          <a href="/">
+            <img
+              className={classses.logoimg}
+              src="https://img.elcomercio.pe/img/somos/logo_somos.png?1557531956"
+              alt="somos"
+              />
+          </a>
+        </div>
+        <div className={classses.logoicon}>
+          <a href="/"><i className="icon-comercio"></i></a>
+        </div>
+      </div>
+   {/*    Agregar clase .header-menu--active para mostrar el menu en mobile */}
+      <nav className="header-menu">
+        <div className="header-menu__content">
+          <div className="header-menu__close">
+            <i className="icon-close header-menu__close-icon"></i>
+            </div>
+          <div className="header-menu__search">
+            <input type="text" placeholder="Buscar" className="header-menu__search-input"/>
+            <i className="icon-search header-menu__search-icon"></i>
+          </div>
+          <div className="header-menu__login">
+            <a href="/" className="flex flex--justify-center">
+            <i className="icon-user header-menu__login-icon"></i>
+            <p className="header-menu__login-text">Ingresa a tu cuenta</p>
+            </a>
+          </div>
           <ul className="header-menu__list">
-            <li className="header-menu__item first">
+            <li className="header-menu__item-link">
               <a
                 href="https://elcomercio.pe/somos?ref=somos/test-de-proust&amp;ft=menu"
-                className="header-menu__link-icon active">
-                i
+                className="header-menu__link-icon">
+                <i className="icon-home"></i>
               </a>
             </li>
             <li className="header-menu__item">
@@ -77,8 +94,8 @@ const HeaderChildSomos = () => {
               </a>
             </li>
           </ul>
-          </div>
-      </nav>
+        </div>
+    </nav>
     </Fragment>
   )
 }
