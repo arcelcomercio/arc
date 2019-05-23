02553ffceb859955bc25e1730583e76c08c6237b
henrry11985 <henrry11985@gmail.com>
2019-05-23 11:38:30 -0500

    La carpeta _dependencies debe estar fuera de la carpeta _children

---
+++ components/features/article/body/_dependencies/schema-filter.js
@@ -0,0 +1,44 @@
+export default `
+    {
+        basic{
+        promo_items{
+          basic_video {
+            type
+            promo_items {
+              basic {
+                type 
+                url
+              }
+            }
+          }
+          basic_gallery {
+            type 
+            promo_items {
+              basic {
+                type 
+                url
+              }
+            }
+          }
+          basic {
+            type 
+            url
+          }
+        }
+        canonical_url
+        publish_date
+        website
+        website_url
+        headlines{
+          basic
+        }
+        credits {
+            by {
+                type 
+                name
+                url
+            }
+        }
+      }
+    }
+    `
