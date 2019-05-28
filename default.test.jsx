86c76f35aa354d63ef4a6518665e881cc2c47697
Rolly <rolly.sanchez.26@gmail.com>
2019-05-28 12:35:58 -0500

    add test default component

--- default.test.jsx
+++ default.test.jsx
@@ -1,4 +1,22 @@
+import React from "react";
+import { shallow } from "enzyme";
+import ExtraordinaryStory from "./components/global-components/extraordinary-story";
 
+describe("AddToCart", () => {
+  beforeEach(() => {
+    global.Sales = {
+      addItemToCart: jest.fn()
+    };
+    global.console.log = jest.fn();
+  });
+  it("should render", () => {
+    const wrapper = shallow(<ExtraordinaryStory />);
+    expect(wrapper).toBeDefined();
+  });
+});
+
+/*
 test('two plus two is four', () => {
   expect(2 + 2).toBe(4)
 })
+*/
