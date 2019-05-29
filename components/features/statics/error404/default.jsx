174ebcf20f1609d73e3ae54ddfed976286de4569
Carlos Fernández <hello@cajotafer.com>
2019-05-29 12:54:51 -0500

    [UPDATE] statics/error404 - Optimización en estilos por CodeReview.

--- components/features/statics/error404/default.jsx
+++ components/features/statics/error404/default.jsx
@@ -1,76 +1,74 @@
-
-import React, {PureComponent} from 'react'
+import React, { PureComponent } from 'react'
 import Consumer from 'fusion:consumer'
-import SearchInput from '../../../global-components/search-input'
 import PropTypes from 'prop-types'
 
-
+import SearchInput from '../../../global-components/search-input'
 
 const classes = {
-    container:'error__container',
-    iconCaretDown: 'error__icon',
-    title:'error__title',
-    content:'error__content',
-    linkBox:'error__link-box',
-    link:'error__link',
-    searchBox: 'error__search-box'
+  container: 'error__container text-center full-width position-relative',
+  icon: 'error__icon position-absolute',
+  title: 'error__title title mg-bottom-10',
+  content: 'error__content text-sm pd-top-15 pd-bottom-15 inline-b',
+  link: 'error__link inline-b text-sm pd-top-15 pd-bottom-15',
+  searchBox: 'error__search-box pd-top-10 pd-bottom-20',
 }
 
 @Consumer
-class Error404 extends PureComponent{
-    render(){
-        const {
-            contextPath,
-            customFields,
-            editableField,
-            siteProperties = {},
-            globalContentConfig = {}
-          } = this.props
+class Error404 extends PureComponent {
+  render() {
+    const {
+      contextPath,
+      customFields,
+      editableField,
+      siteProperties = {},
+      globalContentConfig = {},
+    } = this.props
 
-          const { messages: { errorTitle, errorDescription }  = {}} = siteProperties
+    const { messages: { errorTitle, errorDescription } = {} } = siteProperties
+    const { title = errorTitle, description = errorDescription } = customFields
 
-        const { title = errorTitle, description = errorDescription } = customFields
-        return(
-            <>
-                <div className={classes.container}>
-                    <i className={classes.iconCaretDown}/>
-                    <h3 className={classes.title} {...editableField('title')}>{title}</h3>
-                    <p className={classes.content} {...editableField('description')}>{description}</p>
-                    <div className={classes.searchBox}>
-                        <SearchInput
-                            globalContentConfig = {globalContentConfig}
-                            contextPath = {contextPath}
-                        />
-                    </div>
-                    <p className={classes.linkBox}>
-                        <a href="#"className={classes.link}>
-                            Volver a la página principal
-                        </a>
-                    </p>
-                </div>
-                <div className="datafromAjax">
-
-                </div>
-            </>
-            
-        )
-    }
+    return (
+      <>
+        <div role="group" className={classes.container}>
+          <i className={classes.icon} />
+          <h3 className={classes.title} {...editableField('title')}>
+            {title}
+          </h3>
+          <p className={classes.content} {...editableField('description')}>
+            {description}
+          </p>
+          <div role="search" className={classes.searchBox}>
+            <SearchInput
+              globalContentConfig={globalContentConfig}
+              contextPath={contextPath}
+            />
+          </div>
+          <a href={contextPath} className={classes.link}>
+            Volver a la página principal
+          </a>
+        </div>
+        {/* <div className="datafromAjax" /> TODO: Falta implementar esta parte */}
+      </>
+    )
+  }
 }
 
 Error404.label = 'Error 404'
+Error404.static = true
 
 Error404.propTypes = {
-    customFields: PropTypes.shape({
-        title : PropTypes.string.isRequired.tag({
-            name: 'Título',
-            description: 'ingrese el titulo de manera obligatoria, Ejm: ¡Oops! ... Página no encontrada',
-            default: 'lalala'
-        }),
-        description: PropTypes.string.isRequired.tag({
-            name: 'Descripción',
-            description: 'Ingrese un texto descriptivo, Ejm: La página que buscas no existe ...'
-        })
-    })
+  customFields: PropTypes.shape({
+    title: PropTypes.string.tag({
+      name: 'Título',
+      description:
+        'Ingrese un título personalizado, Ejm: ¡Oops! ... Página no encontrada',
+    }),
+    description: PropTypes.string.tag({
+      name: 'Descripción',
+      description:
+        'Ingrese un texto descriptivo, Ejm: La página que buscas no existe ...',
+    }),
+  }),
 }
 
 export default Error404
