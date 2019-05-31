8ce300af01280033981981e57a2582089057cd6e
Edgar <edgar.gamarra21@gmail.com>
2019-05-31 11:45:57 -0500

    prueba helper

--- components/utilities/helpers.test.js
+++ components/utilities/helpers.test.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import { shallow, mount } from 'enzyme'
-import { isTSAnyKeyword } from '@babel/types';
+// import React from 'react'
+// import { shallow, mount } from 'enzyme'
 import {reduceWord} from '@components/helpers'
 
 describe('Test reduceWord', () => {
     it("funcion appendScript",()=>{
-        const word ="prueba longitud menor a 145";
-        const len = 10;
+        const word ="prueba longitud mayor a la longitud";
+        const len = 100;
         const finalText ="...adios"
+
+        ;
+        expect(reduceWord(word, len,  finalText)).toBe("prueba longitud mayor a la longitud");
     })
 
-    reduceWord(word,len,  finalText);
+    
 })
 
 
