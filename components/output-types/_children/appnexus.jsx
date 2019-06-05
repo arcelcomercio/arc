1aa367cacdc1a6b154efbf6def6935c3c839363d
Edward Sanchez <esanchez@rmgperustaff.com>
2019-06-05 11:36:47 -0500

    [UPDATE] - cambio de tipos de notas

--- components/output-types/_children/appnexus.jsx
+++ components/output-types/_children/appnexus.jsx
@@ -26,17 +26,22 @@ const getVars = ({ arcSite, isStory, requestUri, port = 'port1' }) => {
     path = requestUri.includes('?_website')
       ? requestUri.slice(0, requestUri.length - 20)
       : requestUri
-    const sectionList = path.split('/').slice(1)
-    section = sectionList[0].replace('-', '')
-    const hasNew = sectionList[sectionList.length - 1].endsWith('-noticia')
-    if (hasNew && sectionList.length >= 3) {
-      subsection = sectionList[1].replace('-', '')
-    }
-    if (!hasNew && sectionList.length >= 2) {
-      subsection = sectionList[1].replace('-', '')
+    if (path === '/') {
+      section = 'home'
+    } else {
+      const sectionList = path.split('/').slice(1)
+      section =
+        sectionList[0] === 'noticia' ? 'tags' : sectionList[0].replace('-', '')
+      const hasNew = sectionList[sectionList.length - 1].endsWith('-noticia')
+      if (hasNew && sectionList.length >= 3) {
+        subsection = sectionList[1].replace('-', '')
+      }
+      if (!hasNew && sectionList.length >= 2) {
+        subsection = sectionList[1].replace('-', '')
+      }
     }
   }
-  // const { section, subsection } = globalContent
+
   return `
   var type_space = '${typeSpace}'
   var site = '${getSite(site)}'
