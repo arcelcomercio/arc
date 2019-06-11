b7ff5d59003d4dc842568979697a2226d986f483
luis.moreno <luis.moreno@comercio.com.pe>
2019-06-11 14:57:47 -0500

    modificaciones de estilos en archivo de card-opinion

--- components/features/cards/opinion/_children/card.jsx
+++ components/features/cards/opinion/_children/card.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 
 const classes = {
-  opinion: 'opinion-card',
-  head: 'opinion-card__head',
-  title: 'opinion-card__title',
+  opinion: 'opinion-card grid',
+  head: 'opinion-card__head grid align-center',
+  title: 'opinion-card__title text-center',
   oneline: 'opinion-card__oneline',
   twoline: 'opinion-card__twoline',
   threeline: 'opinion-card__threeline',
-  body: 'opinion-card__body',
+  body: 'opinion-card__body grid',
   item: 'opinion-card__item',
-  seccion: 'opinion-card__seccion',
-  icono: 'opinion-card__icono',
-  nombreSeccion: 'opinion-card__nombreseccion',
-  titleNew: 'opinion-card__titleNew',
+  section: 'opinion-card__section grid align-center',
+  imageContainer:
+    'opinion-card__image-container grid align-center justify-center',
+  image: 'opinion-card__image',
+  name: 'opinion-card__section-name',
+  nameLink: 'opinion-card__name-link',
+  storyTitle: 'opinion-card__story-title text-left overflow-hidden',
+  titleLink: 'opinion-card__title-link',
   orange: 'text_orange',
 }
 
@@ -26,18 +30,28 @@ const OpinionItem = ({
 }) => {
   return (
     <div className={classes.item}>
-      <div className={classes.seccion}>
-        <h3 className={classes.nombreSeccion}>
-          <a href={urlSection}>{sectionName}</a>
+      <div className={classes.section}>
+        <h3 className={classes.name}>
+          <a className={classes.nameLink} href={urlSection}>
+            {sectionName}
+          </a>
         </h3>
-        <div className={`${classes.titleNew} ${linesNumber}`}>
+        <div className={`${classes.storyTitle} ${linesNumber}`}>
           <h2>
-            <a href={urlNew}>{title}</a>
+            <a className={classes.titleLink} href={urlNew}>
+              {title}
+            </a>
           </h2>
         </div>
       </div>
-      <div className={classes.icono}>
-        <img data-type="src" src={urlImg} data-src={urlImg} alt="" />
+      <div className={classes.imageContainer}>
+        <img
+          className={classes.image}
+          data-type="src"
+          src={urlImg}
+          data-src={urlImg}
+          alt=""
+        />
       </div>
     </div>
   )
