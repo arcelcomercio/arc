b4173b7c66ab2051758aea898ff5e28d257771fc
luis.moreno <luis.moreno@comercio.com.pe>
2019-06-11 19:02:57 -0500

    refactorizacion de codigo de more-read

--- components/features/cards/most-read/_children/item.jsx
+++ components/features/cards/most-read/_children/item.jsx
@@ -10,15 +10,16 @@ const CardMostReadChildItem = props => {
 
   const classes = {
     mostReadItem: 'flex more-read-item',
-    figure: `more-read-item__figure more-read-item__figure--icon more-read-item__figure--${type}`,
-    img: 'more-read-item__img',
-    detail: 'more-read-item__detail',
+    figure: `more-read-item__figure position-relative overflow-hidden h-full w-full more-read-item__figure--icon more-read-item__figure--${type}`,
+    img: 'w-full h-full object-cover',
+    detail: 'more-read-item__detail overflow-hidden',
+    link: 'more-read-item__link w-full',
   }
 
   return (
     <div className={classes.mostReadItem}>
       {viewImage && (
-        <a href={websiteUrl}>
+        <a className={classes.link} href={websiteUrl}>
           <figure className={classes.figure}>
             <img className={classes.img} src={imageUrl} alt={title} />
           </figure>
