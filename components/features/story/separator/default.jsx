3aaa4a2ccec6e27ab4f37fcbc4ca790890f82958
Rolly <rolly.sanchez.26@gmail.com>
2019-06-20 17:16:40 -0500

    refactoring separador artículo

--- components/features/story/separator/default.jsx
+++ components/features/story/separator/default.jsx
@@ -2,6 +2,7 @@ import Consumer from 'fusion:consumer'
 import React, { PureComponent } from 'react'
 import withSizes from 'react-sizes'
 
+import { sizeDevice } from '../../separators/_dependencies/functions'
 import schemaFilter from './_dependencies/schema-filter'
 import StorySeparatorChildItem from './_children/item'
 import StoryData from '../../../utilities/story-data'
@@ -15,93 +16,31 @@ const classes = {
   mvideo: 'story-separator--video',
 }
 
-@withSizes(({ width }) => ({ isMobile: width < 640 }))
+@withSizes(({ width }) => sizeDevice(width))
 @Consumer
 class StorySeparator extends PureComponent {
   constructor(props) {
     super(props)
-    const { arcSite: website, globalContent } = props
+    const { arcSite, globalContent } = props
     const { taxonomy: { primary_section: { path: section } = {} } = {} } =
       globalContent || {}
 
+    this.fetchDataApi(arcSite, section, 7)
+  }
+
+  fetchDataApi = (arcSite, section, storiesQty) => {
     this.fetchContent({
-      data: {
+      dataApi: {
         source: 'story-feed-by-section',
         query: {
-          website,
+          website: arcSite,
           section,
-          stories_qty: 7,
+          stories_qty: storiesQty,
         },
         filter: schemaFilter,
       },
     })
   }
-
-  /* getSeccionPrimary = dataArticle => {
-    const lastSection = '/'
-    const splitText = dataArticle
-      ? lastSection + dataArticle.path.slice(1).split('/')[0]
-      : '/politica'
-    return splitText
-  } */
-
-  /*   componentDidMount = () => {
-    window.addEventListener('resize', this.handleResize)
-    this.getContentApi()
-  } */
-
-  /*   getContentApi = () => {
-    let storiesQty = 7
-    const { device } = this.state
-
-    if (device === 'mobile') storiesQty = 0
-
-    const { arcSite, globalContent } = this.props
-    const section = this.getSeccionPrimary(globalContent.taxonomy || {})
-    const { fetched } = this.getContent(
-      'story-feed-by-section',
-      {
-        website: arcSite,
-        section,
-        stories_qty: storiesQty,
-      },
-      schemaFilter
-    )
-    fetched.then(response => {
-      const { content_elements: contentElements } = response || {}
-      const { website_url: websiteUrl = '' } = globalContent || {}
-      this.setState({
-        stories: contentElements || [],
-        excluir: websiteUrl,
-      })
-    })
-  } */
-
-  // FIXME: Temporal
-  /*   handleResize = () => {
-    const wsize = window.innerWidth
-    const { device, stories } = this.state
-
-    if (wsize >= 840 && device !== 'desktop') {
-      this.setState({
-        device: 'desktop',
-      })
-      if (!stories) this.getContentApi()
-    }
-    if (wsize < 840 && device !== 'mobile')
-      this.setState({
-        device: 'mobile',
-      })
-  }
-
-  // FIXME: Temporal
-  setDevice = () => {
-    const wsize = window.innerWidth
-    if (wsize < 840) {
-      return 'mobile'
-    }
-    return 'desktop'
-  } */
 
   renderItems(stories, excluir) {
     const { deployment, contextPath, arcSite } = this.props
@@ -140,16 +79,18 @@ class StorySeparator extends PureComponent {
   }
 
   render() {
-    const { data: { content_elements: stories } = {} } = this.state
+    const { dataApi: { content_elements: stories } = {} } = this.state
     const { globalContent, isMobile } = this.props
     const { website_url: excluir } = globalContent || {}
-
-    return isMobile ? (
-      ''
-    ) : (
-      <div className={classes.separator}>
-        <div className={classes.body}>{this.renderItems(stories, excluir)}</div>
-      </div>
+    console.log('render 3')
+    return (
+      !isMobile && (
+        <div className={classes.separator}>
+          <div className={classes.body}>
+            {this.renderItems(stories, excluir)}
+          </div>
+        </div>
+      )
     )
   }
 }
