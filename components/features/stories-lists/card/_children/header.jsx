e86624c3f86f193bbb4a29278a19e71df9643092
Maria Gabriela Rangel <mrangel@rmgperustaff.com>
2019-06-24 15:25:59 -0500

    Modificaciones estilos listado de noticias

--- components/features/stories-lists/card/_children/header.jsx
+++ components/features/stories-lists/card/_children/header.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const classes = {
   header: `stories-l-header flex justify-between items-center w-auto pr-20 pl-20`,
   seeMore: `stories-l-header__see-more bg-white pt-5 pb-5 pr-10 pl-10 text-gray-200 text-sm`,
-  title: 'stories-l-header__title flex items-center full-height text-gray-300',
+  title:
+    'stories-l-header__title flex items-center full-height text-gray-300 uppercase',
 }
 const StoriesListsCardChildHeader = ({
   titleList,
