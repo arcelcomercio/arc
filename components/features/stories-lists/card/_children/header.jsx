d2b91207abe0fc70b9fe2e59b261b7a85fb190c3
Alvaro <ahuamani@rmgperustaff.com>
2019-06-24 17:03:02 -0500

    [Fix] ver más centrado verticalmente

--- components/features/stories-lists/card/_children/header.jsx
+++ components/features/stories-lists/card/_children/header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const classes = {
   header: `stories-l-header flex justify-between items-center w-auto pr-20 pl-20`,
-  seeMore: `stories-l-header__see-more bg-white pt-5 pb-5 pr-10 pl-10 text-gray-200 text-sm`,
+  seeMore: `stories-l-header__see-more flex items-center bg-white pt-5 pb-5 pr-10 pl-10 text-gray-200 text-sm`,
   title:
     'stories-l-header__title flex items-center full-height text-gray-300 uppercase',
 }
