372d607b568e249701c1167e6eed4101a6cf6e4e
Maria Gabriela Rangel <mrangel@rmgperustaff.com>
2019-06-25 17:49:34 -0500

    Modificaciones estilos footer

--- components/features/layout/footer/default.jsx
+++ components/features/layout/footer/default.jsx
@@ -5,24 +5,31 @@ import { setDevice } from '../../../utilities/resizer'
 
 const classes = {
   footer: 'footer w-full grid',
-  info: 'footer__info bg-gray-300 p-20',
-  sections: 'footer__sections bg-gray-300',
-  contact: 'footer__contact bg-gray-300',
-  sites: 'footer__sites flex justify-center mt-15',
-  sitesList: 'footer__sites-list flex justify-center flex-wrap w-full p-0',
+  info: 'footer__info p-20  bg-primary position-relative',
+  sections: 'footer__sections  bg-primary position-relative md:pt-20 pb-20',
+  contact: 'footer__contact  bg-primary md:pt-20 pb-20',
+  sites: 'footer__sites flex flex-col mt-15 pl-30 pr-30 pb-20',
+  sitesList: 'footer__sites-list flex flex-wrap w-full p-0 pt-20 bg-gray-100',
   sitesItem:
-    'footer__sites-item mb-5 pl-10 pr-10 border-r-1 border-solid border-black text-sm text-gray-200 line-h-xs',
+    'footer__sites-item mb-5 pr-10 text-sm text-gray-300 line-h-xs uppercase flex items-center',
+  sitesItemTitle: 'text-sm text-gray-300 line-h-xs uppercase',
   sitesLink: 'footer__sites-link text-gray-200',
   legalList: 'footer__legal-list text-md',
-  legalItem: 'footer__legal-item mb-10 text-gray-200 line-h-sm text-xs',
+  legalItem:
+    'footer__legal-item mb-10 text-gray-200 line-h-none text-xs primary-font',
   logoContainer: 'footer__logo footer__logo-container block mb-15',
   logoImg: 'w-full',
-  list: 'footer__list p-20',
-  listItem:
-    'footer__list-item pt-10 pb-10 border-b-1 border-solid border-black',
-  listTitle:
-    'footer__list-title pt-10 pb-10 border-b-1 border-solid border-black uppercase font-bold text-sm text-gray-200',
+  list: 'footer__list pt-0 pb-20 pr-20 pl-20 md:pl-30',
+  listItem: 'footer__list-item pt-10',
+  listTitle: 'footer__list-title  pt-10 pb-10 uppercase text-sm text-gray-200',
   listLink: 'footer__list-link capitalize text-gray-200 text-sm',
+  textContent: 'pt-20 pb-0 pl-20',
+  socialTitle: 'footer__social-title mb-20 uppercase text-sm',
+  listSocial: 'footer__social flex pl-20',
+  listSocialItem: 'footer__social-item',
+  listSocialLink: 'footer__social-link',
+  facebookIcon: 'footer__social-icon icon-facebook pr-15',
+  twitterIcon: 'footer__social-icon icon-twitter',
 }
 
 @Consumer
@@ -176,20 +183,29 @@ class LayoutFooter extends PureComponent {
               </li>
             ))}
           </ul>
-          <ul className={classes.list}>
-            <li className={classes.listTitle}>Síguenos</li>
-            {footer.socialNetworks.map(el => (
-              <li className={classes.listItem} key={el.url}>
-                <a className={classes.listLink} href={el.url}>
-                  {el.name}
-                </a>
-              </li>
-            ))}
+          <div className={classes.textContent}>
+            <p className={classes.socialTitle}>Síguenos</p>
+          </div>
+          <ul className={classes.listSocial}>
+            <li className={classes.listSocialItem}>
+              <a
+                href="https://www.facebook.com/elcomercio.pe"
+                className={classes.listSocialLink}>
+                <i className={classes.facebookIcon} />
+              </a>
+            </li>
+            <li className={classes.listSocialItem}>
+              <a
+                href="https://twitter.com/elcomercio_peru"
+                className={classes.listSocialLink}>
+                <i className={classes.twitterIcon} />
+              </a>
+            </li>
           </ul>
         </div>
         <div className={classes.sites}>
+          <p className={classes.sitesItemTitle}>Red el comercio</p>
           <ul className={classes.sitesList}>
-            <li className={classes.sitesItem}>Visite también:</li>
             {gecSites.map(site => {
               if (site.arcSite !== arcSite) {
                 return (
