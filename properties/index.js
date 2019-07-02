e749647bae472ea671f7792c00b4fc38202b1bc6
Edward Sanchez <esanchez@rmgperustaff.com>
2019-07-02 11:09:35 -0500

    [UPDATE] - siteProperties para los links de signwall

--- properties/index.js
+++ properties/index.js
@@ -14,12 +14,27 @@ export default {
   },
   messages: {
     errorTitle: '¡Oops! la pagina no fue encontrada',
-    errorDescription: 'La página que buscas no existe, probablemente el enlace que usaste es erróneo, intenta ubicarlo en la página principal o usa el buscador para encontrar la noticia que buscas:',
+    errorDescription:
+      'La página que buscas no existe, probablemente el enlace que usaste es erróneo, intenta ubicarlo en la página principal o usa el buscador para encontrar la noticia que buscas:',
+  },
+  signwall: {
+    ORIGIN_API: 'https://api-sandbox.elcomercio.pe',
+    ORIGIN_PAYWALL:
+      'https://elcomercio-elcomercio-sandbox.cdn.arcpublishing.com/arc/subs/p.js',
+    ORIGIN_IDENTITY_SDK:
+      'https://arc-subs-sdk.s3.amazonaws.com/sandbox/sdk-identity.min.js',
+    ORIGIN_SALES_SDK:
+      'https://arc-subs-sdk.s3.amazonaws.com/sandbox/sdk-sales.min.js',
+    ORIGIN_ECOID: 'https://pre.ecoid.pe',
+    FACEBOOK_APPID: '287130908774061',
+    GOOGLE_CLIENTID:
+      '1038751133917-m10doi4gb3tl4u01s4uaje60vbukh2bs.apps.googleusercontent.com',
   },
   ids: {
     opta: '782834e1fd5a215304e57cddad80b844',
   },
-  gecSites: [{
+  gecSites: [
+    {
       name: 'elcomercio.pe',
       arcSite: 'elcomercio',
       url: 'https://elcomercio.pe',
