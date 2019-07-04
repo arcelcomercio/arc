4754d012dbedcc18b569d719b31c5ff419a7c134
Carlos Fernández <hello@cajotafer.com>
2019-07-04 16:29:17 -0500

    [FIX - 6960] <main> cambiado por <div role=main>

--- components/layouts/sidebar.jsx
+++ components/layouts/sidebar.jsx
@@ -14,16 +14,16 @@ const classes = {
 const SidebarLayout = ({ children = [] }) => {
   return (
     <div className={classes.layout}>
-      <div role="complementary" className={classes.zocalo}>
-        {children[0] /* Zocalo izquierda */}
-      </div>
+      <div className={classes.zocalo}>{children[0] /* Zocalo izquierda */}</div>
       <div className={classes.contentContainer}>
         {children[1] /* Publicidad Top */}
         {children[2] /* Barra de navegación */}
         {children[3] /* Cabecera de página */}
         {children[4] /* Encabezado */}
         <div className={classes.content}>
-          <main className={classes.main}>{children[5] /* Contenido */}</main>
+          <div role="main" className={classes.main}>
+            {children[5] /* Contenido */}
+          </div>
           <aside className={classes.sidebar}>
             {children[6] /* Barra lateral */}
           </aside>
@@ -31,9 +31,7 @@ const SidebarLayout = ({ children = [] }) => {
         </div>
         {children[8] /* Pie de página */}
       </div>
-      <div role="complementary" className={classes.zocalo}>
-        {children[9] /* Zocalo derecha */}
-      </div>
+      <div className={classes.zocalo}>{children[9] /* Zocalo derecha */}</div>
     </div>
   )
 }
