2182886308390080f5ca164a3039747b5176b9da
luis.moreno <luis.moreno@comercio.com.pe>
2019-07-08 12:56:51 -0500

    update

--- components/features/story/header/_children/gallery-slider.jsx
+++ components/features/story/header/_children/gallery-slider.jsx
@@ -8,14 +8,10 @@ const classes = {
   figure: 'story-gallery-slider__figure position-relative',
   image: 'story-gallery-slider__img w-full object-fit-cover',
   caption: 'story-gallery-slider__caption pt-20 pb-20 flex',
-  captionImage:
-    'story-gallery-slider__caption-image pt-10 pl-15 text-sm text-white secondary-font line-h-sm',
-  quantity:
-    'story-gallery-slider__quantity title-xs flex items-center justify-center',
-  leftArrow:
-    'story-gallery-slider__arrows story-gallery-slider__arrows--left icon-left text-white title-lg flex items-center justify-center h-full position-absolute top-0',
-  rightArrow:
-    'story-gallery-slider__arrows story-gallery-slider__arrows--right icon-right text-white title-lg flex items-center justify-center h-full position-absolute top-0',
+  captionImage: `story-gallery-slider__caption-image pt-10 pl-15 text-sm text-white secondary-font line-h-sm`,
+  quantity: `story-gallery-slider__quantity title-xs flex items-center justify-center`,
+  leftArrow: `story-gallery-slider__arrows story-gallery-slider__arrows--left icon-left text-white title-lg flex items-center justify-center h-full position-absolute top-0`,
+  rightArrow: `story-gallery-slider__arrows story-gallery-slider__arrows--right icon-right text-white title-lg flex items-center justify-center h-full position-absolute top-0`,
 }
 
 class StoryHeaderChildGallerySlider extends PureComponent {
@@ -25,15 +21,17 @@ class StoryHeaderChildGallerySlider extends PureComponent {
       contentElementGallery: { content_elements: contentElements = [] },
     } = props || {}
 
+    this.containerSliders = document.querySelector('[role="slider"]')
+
     this.state = {
-      dataSlider: contentElements,
-      steps: contentElements.length,
+      listSlider: contentElements,
+      totalSlides: contentElements.length,
       sliderWidth: contentElements.length * 100,
       slideWidth: 100 / contentElements.length,
-      position: getUrlParameter(contentElements),
+      currentSlide: getUrlParameter(contentElements),
     }
 
-    this.step = getUrlParameter()
+    this.currentSlider = getUrlParameter()
   }
 
   componentDidMount() {
@@ -53,105 +51,103 @@ class StoryHeaderChildGallerySlider extends PureComponent {
   }
 
   _handlePrevSlider = () => {
-    const { steps, slideWidth } = this.state
+    const { totalSlides, slideWidth } = this.state
     this.step -= 1
-    if (this.step < 0) this.step = steps - 1
+    if (this.step < 0) this.step = totalSlides - 1
     this.setState({
-      position: -slideWidth * this.step,
+      currentSlide: -slideWidth * this.step,
     })
 
     window.history.pushState(null, '', this._getUrlGalleryImage())
   }
 
   _handleNextSlider = () => {
-    const { steps, slideWidth, dataSlider } = this.state
+    const { totalSlides, slideWidth, listSlider } = this.state
     this.step += 1
 
-    if (dataSlider.length <= this.step) {
+    if (listSlider.length <= this.step) {
       window.location.href = '/'
     }
 
-    if (this.step >= steps) this.step = 0
+    if (this.step >= totalSlides) this.step = 0
     this.setState({
-      position: -slideWidth * this.step,
+      currentSlide: -slideWidth * this.step,
     })
 
     window.history.pushState(null, '', this._getUrlGalleryImage())
   }
 
-  _getUrlGalleryImage = () => {
-    const pathFoto = `${window.location.href.split('?')[0]}?foto=${this.step +
-      1}`
-    return pathFoto
-  }
+  _getUrlGalleryImage = () =>
+    `${window.location.href.split('?')[0]}?foto=${this.step + 1}`
 
   render() {
-    const { dataSlider = [], sliderWidth, slideWidth, position } = this.state
+    const {
+      listSlider = [],
+      sliderWidth,
+      slideWidth,
+      currentSlide,
+    } = this.state
 
     const sliderStyle = {
       width: `${sliderWidth}%`,
-      transform: `translateX(${position}%)`,
+      transform: `translateX(${currentSlide}%)`,
     }
     const slideStyle = {
       width: `${slideWidth}%`,
     }
     return (
       <>
-        {dataSlider.length > 0 && (
+        {listSlider.length > 0 && (
           <section className={classes.elementsSlider} id="story-galery">
             <div
               role="slider"
-              aria-valuenow={dataSlider.length}
+              aria-valuenow={listSlider.length}
               aria-valuemin="1"
               aria-valuemax="10"
               className={classes.body}>
-              <>
-                <ul style={sliderStyle} className={classes.content}>
-                  {dataSlider.map((element, i) => (
-                    <li
-                      key={element._id}
-                      style={slideStyle}
-                      className={classes.element}>
-                      <div className={classes.figure}>
-                        <figure>
-                          <img
-                            src={element.resized_urls ? '' : element.url}
-                            alt={element.subtitle}
-                            className={classes.image}
-                          />
-                        </figure>
-                      </div>
-                      <figcaption className={classes.caption}>
-                        <span className={classes.quantity}>
-                          {i + 1}/{dataSlider.length}
-                        </span>
-                        <p className={classes.captionImage}>
-                          {element.subtitle}
-                        </p>
-                      </figcaption>
-                    </li>
-                  ))}
-                </ul>
-                {/*          {dataSlider && dataSlider.length > 1 && (
-                  <div role="navigation" className={classes.arrowsBox}> */}
-                <i
-                  role="button"
-                  tabIndex="0"
-                  className={classes.leftArrow}
-                  onClick={this._handlePrevSlider}
-                  onKeyDown={this._controlKeysSlider}
-                />
-                <i
-                  role="button"
-                  tabIndex="0"
-                  id="icon-right"
-                  className={classes.rightArrow}
-                  onClick={this._handleNextSlider}
-                  onKeyDown={this._controlKeysSlider}
-                />
-                {/*      </div>
-                )} */}
-              </>
+              <ul style={sliderStyle} className={classes.content}>
+                {listSlider.map((element, i) => (
+                  <li
+                    key={element._id}
+                    style={slideStyle}
+                    className={classes.element}>
+                    <div className={classes.figure}>
+                      <figure>
+                        <img
+                          src={element.resized_urls ? '' : element.url}
+                          alt={element.subtitle}
+                          className={classes.image}
+                        />
+                      </figure>
+                    </div>
+                    <figcaption className={classes.caption}>
+                      <span className={classes.quantity}>
+                        {i + 1}/{listSlider.length}
+                      </span>
+                      <p className={classes.captionImage}>{element.subtitle}</p>
+                    </figcaption>
+                  </li>
+                ))}
+              </ul>
+              {/*          {dataSlider && dataSlider.length > 1 && (
+                <div role="navigation" className={classes.arrowsBox}> */}
+              <i
+                role="button"
+                tabIndex="0"
+                className={classes.leftArrow}
+                onClick={this._handlePrevSlider}
+                onKeyDown={this._controlKeysSlider}
+              />
+              <i
+                role="button"
+                tabIndex="0"
+                id="icon-right"
+                className={classes.rightArrow}
+                onClick={this._handleNextSlider}
+                onKeyDown={this._controlKeysSlider}
+              />
+              {/*      </div>
+              )} */}
             </div>
           </section>
         )}
