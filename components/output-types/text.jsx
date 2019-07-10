19570317912030362d2df984ac85af1199ca66d1
Edgar <edgar.gamarra21@gmail.com>
2019-07-10 15:47:16 -0500

    modificacion del output type ads a text y desacoplamiento del contenido temporal

---
+++ components/output-types/text.jsx
@@ -0,0 +1,10 @@
+import AdsContent from './_dependencies/ads/ads-content'
+
+const text = () => {
+  const content = AdsContent()
+  return content
+}
+
+text.contentType = 'text/plain'
+
+export default text
