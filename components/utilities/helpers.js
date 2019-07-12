4ce23979b9af40d538664a2c232c2b442a362231
henrry11985 <henrry11985@gmail.com>
2019-07-11 20:11:49 -0500

    [FIX] publimetro youtube

--- components/utilities/helpers.js
+++ components/utilities/helpers.js
@@ -1,6 +1,4 @@
-import {
-  addResizedUrlItem
-} from './thumbs'
+import { addResizedUrlItem } from './thumbs'
 import ConfigParams from './config-params'
 
 export const reduceWord = (word, len = 145, finalText = '...') => {
@@ -27,9 +25,9 @@ export const formatDate = date => {
 
   const fechaEntrante = date.slice(0, 10)
   const fecha =
-    fechaEntrante === fechaGenerada ?
-    date.slice(date.indexOf('T') + 1, 16) :
-    fechaEntrante
+    fechaEntrante === fechaGenerada
+      ? date.slice(date.indexOf('T') + 1, 16)
+      : fechaEntrante
   return fecha
 }
 
@@ -51,13 +49,13 @@ export const formatDateLocalTimeZone = publishDateString => {
     formatHour = `${year}-${formatMonth}-${formatDay}`
   } else {
     const hora =
-      publishDate.getHours() < 10 ?
-      `0${publishDate.getHours()}` :
-      `${publishDate.getHours()}`
+      publishDate.getHours() < 10
+        ? `0${publishDate.getHours()}`
+        : `${publishDate.getHours()}`
     const minuts =
-      publishDate.getMinutes() < 10 ?
-      `0${publishDate.getMinutes()}` :
-      `${publishDate.getMinutes()}`
+      publishDate.getMinutes() < 10
+        ? `0${publishDate.getMinutes()}`
+        : `${publishDate.getMinutes()}`
     formatHour = `${hora}:${minuts}`
   }
   return formatHour
@@ -94,9 +92,9 @@ export const formatDayMonthYear = (date, showHour = true) => {
   } ${fecha.getUTCDate()} de ${
     arrayMeses[fecha.getUTCMonth()]
   } del ${fecha.getUTCFullYear()}`
-  return showHour ?
-    `${dateFormatter}, ${fecha.getHours()}:${fecha.getMinutes()}` :
-    dateFormatter
+  return showHour
+    ? `${dateFormatter}, ${fecha.getHours()}:${fecha.getMinutes()}`
+    : dateFormatter
 }
 
 // ex: 2019-04-29 22:34:13 or 2019/04/29T22:34:13
@@ -196,12 +194,12 @@ export const metaPaginationUrl = (
   siteUrl,
   isQuery
 ) => {
-  return requestUri.match(patternPagination) != null ?
-    `${siteUrl}${requestUri.replace(
+  return requestUri.match(patternPagination) != null
+    ? `${siteUrl}${requestUri.replace(
         patternPagination,
         `${isQuery ? '&page=' : '/'}${pageNumber}`
-      )}` :
-    `${siteUrl}${
+      )}`
+    : `${siteUrl}${
         isQuery ? requestUri : `${requestUri.split('?')[0]}${pageNumber}`
       }${
         isQuery
@@ -216,18 +214,16 @@ export const getMetaPagesPagination = (
   globalContent,
   patternPagination
 ) => {
-  const {
-    next,
-    previous
-  } = globalContent || {}
+  const { next, previous } = globalContent || {}
   const pages = {
-    current: requestUri.match(patternPagination) ?
-      parseInt(
-        requestUri
-        .match(patternPagination)[0]
-        .split(`${isQuery ? '=' : '/'}`)[1],
-        10
-      ) : 1,
+    current: requestUri.match(patternPagination)
+      ? parseInt(
+          requestUri
+            .match(patternPagination)[0]
+            .split(`${isQuery ? '=' : '/'}`)[1],
+          10
+        )
+      : 1,
     next: false,
     prev: false,
   }
@@ -287,9 +283,9 @@ export const getCookie = cookieName => {
 
 export const formatSlugToText = (text = '') => {
   if (!text) return null
-  const splitText = text.slice(1).includes('/') ?
-    text.slice(1).split('/') :
-    text.split('/')
+  const splitText = text.slice(1).includes('/')
+    ? text.slice(1).split('/')
+    : text.split('/')
   const lastSection = splitText[splitText.length - 1]
   return lastSection
     .charAt(0)
@@ -309,7 +305,7 @@ export const removeLastSlash = url => {
 }
 
 export const addSlashToEnd = url => {
-  if (url && url.trim() === '/' ) return url
+  if (url && url.trim() === '/') return url
   return url && !url.endsWith('/') ? `${url}/` : url
 }
 
@@ -325,12 +321,7 @@ export const defaultImage = ({
   )
 }
 
-export const createScript = ({
-  src,
-  async,
-  defer,
-  textContent = ''
-}) => {
+export const createScript = ({ src, async, defer, textContent = '' }) => {
   const node = document.createElement('script')
   if (src) {
     node.type = 'text/javascript'
@@ -365,7 +356,8 @@ export const breadcrumbList = (url, siteUrl) => {
       if (i === 1 || (i === 2 && dataSeccion.length === 4)) {
         const separator = '/'
         arrayData[i] = {
-          name: element.charAt(0).toUpperCase() +
+          name:
+            element.charAt(0).toUpperCase() +
             element.slice(1).replace('-', ' '),
           url: siteUrl + separator + element,
         }
@@ -377,11 +369,7 @@ export const breadcrumbList = (url, siteUrl) => {
 }
 
 export const getUrlParameter = () => {
-  const {
-    location: {
-      href: loc
-    } = {}
-  } = window || {}
+  const { location: { href: loc } = {} } = window || {}
   const getString = loc.split('?')[1] || ''
   const tmp = getString.split('foto=') || []
   return parseInt(tmp[1], 0) || 0
@@ -404,9 +392,9 @@ export const getMultimediaIcon = multimediaType => {
 
 export const optaWidgetHtml = html => {
   const matches = html.match(/<opta-widget(.*?)><\/opta-widget>/)
-  const matchesResult = matches ?
-    matches[1].replace(/="/g, '=').replace(/" /g, '&') :
-    ''
+  const matchesResult = matches
+    ? matches[1].replace(/="/g, '=').replace(/" /g, '&')
+    : ''
 
   const rplOptaWidget = `<amp-iframe class="media" width="1" height="1" layout="responsive" sandbox="allow-scripts allow-same-origin allow-popups" allowfullscreen frameborder="0" src="${
     ConfigParams.OPTA_WIDGET
@@ -470,12 +458,13 @@ export const facebookHtml = html => {
 }
 
 export const youtubeHtml = html => {
-  const strYoutube =
-    '/<iframe width="(.*?)" height="(.*?)" src="https://www.youtube.com/embed/(.*?)"></iframe>/g'
   const rplYoutube =
     '<amp-youtube class="media" data-videoid="$3" layout="responsive" width="$1" height="$2"></amp-youtube>'
 
-  return html.replace(strYoutube, rplYoutube)
+  return html.replace(
+    /<iframe width="(.*?)" height="(.*?)" src="https:\/\/www.youtube.com\/embed\/(.*?)"(.*)><\/iframe>/,
+    rplYoutube
+  )
 }
 
 export const instagramHtml = html => {
@@ -522,12 +511,7 @@ export const ampHtml = (html = '') => {
   return resultData
 }
 
-export const publicidadAmp = ({
-  dataSlot,
-  placementId,
-  width,
-  height
-}) => {
+export const publicidadAmp = ({ dataSlot, placementId, width, height }) => {
   const resultData = createMarkup(`
   <amp-ad width="${width}" height="${height}" type="doubleclick"
   data-slot="${dataSlot}"
