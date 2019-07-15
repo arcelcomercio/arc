75972a7cefcb8eed3c55e8e5fad649a49407bf6d
Waldo J <w.saccaco@gmail.com>
2019-07-15 10:56:41 -0500

    fix content source

--- content/sources/paywall-campaing.js
+++ content/sources/paywall-campaing.js
@@ -1,40 +1,46 @@
-const resolve = ({campaing}) => {
-    return `https://api-sandbox.gestion.pe/retail/public/v1/offer/preview/${campaing}`
-  }
+const resolve = ({ campaing }) => {
+  return `https://api-sandbox.gestion.pe/retail/public/v1/offer/preview/${campaing}`
+}
 
-  //https://api-sandbox.gestion.pe
-  //paywall-gestion-sandbox
-  
-  export default {
-    resolve,
-    params: {
-      campaing: 'text',
-    },
-    transform(data){
-      const {name, attributes, pricingStrategies} = data.products[0]
-      const plans = pricingStrategies.map(({pricingStrategyId, priceCode, description, rates}) => {
-        const [price] = rates;
-        const {amount, billingFrequency} = price ;
+//https://api-sandbox.gestion.pe
+//paywall-gestion-sandbox
+
+export default {
+  resolve,
+  params: {
+    campaing: 'text',
+  },
+  transform(data) {
+    const { name, attributes, pricingStrategies } = data.products[0]
+
+    const plans = pricingStrategies.map(
+      ({ pricingStrategyId, priceCode, description, rates }) => {
+        const [price] = rates
+        const { amount, billingFrequency } = price
         return {
           priceCode,
           pricingStrategyId,
           description: JSON.parse(description),
-          amount: parseInt(amount),
-          billingFrequency
+          amount: parseInt(amount, 10),
+          billingFrequency,
         }
-       });
-      const summary = attributes.reduce((prev, item) => {
-        const {name, value} = item;
-        const _value = value.replace(/<p>|<\/p>/g, "")
-        if(name === 'feature'){
-          prev[name].push(_value);  
-        }else{
-          prev[name] = _value;
-        }
-        return prev;
-      }, {feature: []})
+      }
+    )
 
-      return {name, summary, plans}
-    }
-  }
-  
+    const summary = attributes.reduce(
+      (prev, { name: _name, value }) => {
+        const prez = prev
+        const _value = value.replace(/<p>|<\/p>/g, '')
+        if (_name === 'feature') {
+          prez[_name].push(_value)
+        } else {
+          prez[_name] = _value
+        }
+        return prez
+      },
+      { feature: [] }
+    )
+
+    return { name, summary, plans }
+  },
+}
