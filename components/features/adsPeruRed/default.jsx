38fb903f5ccabfdf75a45bb5d4cdfd72007a857a
luis.moreno <luis.moreno@comercio.com.pe>
2019-07-16 10:40:39 -0500

    feature de perured

---
+++ components/features/adsPeruRed/default.jsx
@@ -0,0 +1,72 @@
+import Consumer from 'fusion:consumer'
+import React, { PureComponent } from 'react'
+import customFields from './_dependencies/custom-field'
+
+const classes = {
+  adsContainer: 'wrap-perured bg-white p-0 pt-10 m-0 mt-10 mb-10',
+  header: 'perured__header m-0 mx-auto',
+  title: 'perured__title text-left font-bold mb-5',
+  adsBox: 'perured__adsbox m-0 mx-auto',
+  footer: `perured__footer overflow-hidden h-auto mt-0 mx-auto mb-10 p-0 pl-10 pr-10 flex flex-row-reverse`,
+  iconContainer: 'perured__iconContainer pl-10',
+  icon: 'perured__icon',
+  footerTextContainer: 'perured__textContainer',
+  footerText: 'perured__text font-bold m-0',
+}
+@Consumer
+class Ads extends PureComponent {
+  render() {
+    const {
+      isAdmin,
+      outputType,
+      customFields: { isDesktop, isMobile } = {},
+    } = this.props
+
+    const addEmptyBackground = () => (isAdmin ? 'bg-base-100' : '')
+
+    const hideInDevice = () => {
+      let classDevice = ''
+      if (isDesktop && !isMobile) classDevice = 'no-mobile'
+      else if (!isDesktop && isMobile) classDevice = 'no-desktop'
+      return classDevice
+    }
+
+    const device = isDesktop ? 'd' : 'm'
+
+    return (
+      <>
+        {outputType !== 'amp' && (isDesktop || isMobile) && (
+          <div
+            className={`${
+              classes.adsContainer
+            } ${addEmptyBackground()} ${hideInDevice()}`}>
+            <div className={`${classes.header} perured-header-${device}`}>
+              <p className={classes.title}>Anuncios de interés</p>
+            </div>
+            <div id={`cnt-perured-${device}`} className={classes.adsBox} />
+            <div className={`${classes.footer} perured-footer-${device}`}>
+              <div className={classes.iconContainer}>
+                <img
+                  src="https://cdn.perured.pe/static/desktop/i/logo_perured.png?pr"
+                  alt=""
+                  className={classes.icon}
+                />
+              </div>
+              <div className={classes.footerTextContainer}>
+                <p className={classes.footerText}>Recomendado por:</p>
+              </div>
+            </div>
+          </div>
+        )}
+      </>
+    )
+  }
+}
+
+Ads.propTypes = {
+  customFields,
+}
+
+Ads.label = 'Publicidad PeruRed'
+
+export default Ads
