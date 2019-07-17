cc550a1d31db49e5297ba31c7c8afe1732309909
Edgar <edgar.gamarra21@gmail.com>
2019-07-17 15:17:03 -0500

    vista movil separador blog

--- components/features/separators/blog/default.jsx
+++ components/features/separators/blog/default.jsx
@@ -10,7 +10,8 @@ const BLOG_BASE = '/blog'
 
 const classes = {
   header: 'blog-separator__header flex flex-row justify-center mb-30 pt-30 position-relative items-center  ',
-  
+  contentItem:'flex overflow-hidden',
+  contentItemMovil:'flex flex-col overflow-hidden',
   titleSeparator:
     'blog-separator__blog uppercase title-sm text-gray-300 blog-separator__title-header flex flex-row justify-left items-center',
   seeMoreText: 'blog-separator__see-more-text pt-10 pb-10 pr-10 pl-10 position-absolute right-0',
@@ -22,7 +23,7 @@ class SeparatorBlog extends PureComponent {
   constructor(props) {
     super(props)
     const { arcSite, isMobile, isTablet } = this.props
-    this.fetchDataApi(arcSite, getStoriesQty(isMobile, isTablet))
+    this.fetchDataApi(arcSite, 5)
   }
 
   fetchDataApi = (arcSite, storiesQty) => {
@@ -41,16 +42,18 @@ class SeparatorBlog extends PureComponent {
     const { dataApi = {} } = this.state
     const { arcSite, contextPath, deployment, isMobile, isTablet } = this.props
     let listPost = Object.values(dataApi)
-    listPost = listPost.slice(0, getStoriesQty(isMobile, isTablet))
+    listPost = listPost.slice(0, 5)
+
     return (
       <div>
         <div className={classes.header}>
           <a className={classes.titleSeparator} href={BLOG_BASE}>
             FIRMAS
           </a>
-          <a href="www.google.com.pe" className={classes.seeMoreText}>ver mas</a>
+          {!isMobile && (<a href="www.google.com.pe" className={classes.seeMoreText}>ver mas</a>) }
+          
         </div>
-        <div className="flex overflow-hidden">
+        <div className={isMobile? classes.contentItemMovil : classes.contentItem}>
           {listPost &&
             listPost.map(post => {
               const {
