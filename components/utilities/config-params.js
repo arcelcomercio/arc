ace68e455dc68299d2964c57ba82f2b7f7abb1e1
henrry11985 <henrry11985@gmail.com>
2019-07-19 11:01:52 -0500

    [fix] cambio de tag en taboola para publimetro

--- components/utilities/config-params.js
+++ components/utilities/config-params.js
@@ -36,6 +36,17 @@ class ConfigParams {
 
   static OPTA_WIDGET = 'https://assets.trome.pe/opta'
 
+  // nombre de los Sitios
+  static SITE_PUBLIMETRO = 'publimetro'
+
+  static SITE_GESTION = 'gestion'
+
+  static SITE_DEPOR = 'depor'
+
+  static SITE_ELCOMERCIO = 'elcomercio'
+
+  static SITE_PERU21 = 'peru21'
+
   // opta
   static OPTA_CSS_LINK =
     'https://secure.widget.cloud.opta.net/v3/css/v3.football.opta-widgets.css'
@@ -43,7 +54,7 @@ class ConfigParams {
   static OPTA_JS_LINK =
     'https://secure.widget.cloud.opta.net/v3/v3.opta-widgets.js'
 
-  // Image Size 
+  // Image Size
   static IMAGE_ORIGINAL = 'original'
 
   static LANDSCAPE_XL = 'landscape_xl'
@@ -63,7 +74,7 @@ class ConfigParams {
   static PORTRAIT_MD = 'portrait_md'
 
   static PORTRAIT_S = 'portrait_s'
-  
+
   static PORTRAIT_XS = 'portrait_xs'
 
   static SQUARE_XL = 'square_xl'
@@ -73,7 +84,7 @@ class ConfigParams {
   static SQUARE_MD = 'square_md'
 
   static SQUARE_S = 'square_s'
-  
+
   static SQUARE_XS = 'square_xs'
 }
 
@@ -91,15 +102,15 @@ export const sizeImg = () => {
     landscape_md: {
       width: 300,
       height: 157,
-    },    
+    },
     landscape_s: {
       width: 220,
       height: 161,
-    },    
+    },
     landscape_xs: {
       width: 118,
       height: 72,
-    },    
+    },
     // portrait
     // TODO: actualizar tamaño
     portrait_xl: {
@@ -115,17 +126,17 @@ export const sizeImg = () => {
       width: 300,
       height: 374,
     },
-    // TODO: actualizar tamaño    
+    // TODO: actualizar tamaño
     portrait_s: {
       width: 161,
       height: 220,
-    },    
+    },
     portrait_xs: {
       width: 75,
       height: 90,
-    }, 
+    },
     // square
-    // TODO: actualizar tamaño a todos 
+    // TODO: actualizar tamaño a todos
     square_xl: {
       width: 900,
       height: 900,
@@ -141,12 +152,12 @@ export const sizeImg = () => {
     square_s: {
       width: 150,
       height: 150,
-    },    
+    },
     square_xs: {
       width: 75,
       height: 75,
-    },  
-    // TODO: Eliminar estos tamaños despues de actualizar 
+    },
+    // TODO: Eliminar estos tamaños despues de actualizar
     // los tamaños de las imagenes de todos los componentes.
     small: {
       width: 100,
