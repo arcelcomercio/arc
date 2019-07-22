b98a019012935992e21c2cf8a10f768757840bdb
Raul Contreras <rcontreras@rmgperustaff.com>
2019-07-22 12:25:32 -0500

    Remueve valores de pago iniciales para prueba

--- components/features/paywall/content/_children/wizard-payment/_children/form-pay/index.jsx
+++ components/features/paywall/content/_children/wizard-payment/_children/form-pay/index.jsx
@@ -20,12 +20,6 @@ const RegisterSchema = schema({})
 
 const FormPay = ({ onSubmit, onReset }) => (
   <Formik
-    initialValues={{
-      cardMethod: 'visa',
-      cardNumber: '4437030140190994',
-      expiryDate: '10/2021',
-      cvv: '123',
-    }}
     validate={values => {
       // FIXME: Validaciones y errores
       // return RegisterSchema(values)
