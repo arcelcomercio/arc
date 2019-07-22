c0b389731c49de81b419a3266794d7f0f8d86240
Edward Sanchez <esanchez@rmgperustaff.com>
2019-07-22 15:34:27 -0500

    [FIX] - cambio de fecha para mostrar en tabloide

--- properties/sites/peru21.js
+++ properties/sites/peru21.js
@@ -6,6 +6,7 @@ export default {
   colorPrimary: '#f7c600',
   siteDomain: 'peru21.pe',
   siteUrl: 'https://peru21.pe',
+  linkTabloide: 'https://peruquiosco.pe/',
   resizerUrl: 'https://elcomercio-peru21-prod.cdn.arcpublishing.com/resizer',
   resizerSecretKeyEnvVar: '',
   urlPreroll:
