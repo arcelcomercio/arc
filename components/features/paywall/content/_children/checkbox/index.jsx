85589ea0fb47b19c5c19752a001744dd4cffbdbc
Raul Contreras <rcontreras@rmgperustaff.com>
2019-07-24 15:42:04 -0500

    Añade estilo de error a checkbox

--- components/features/paywall/content/_children/checkbox/index.jsx
+++ components/features/paywall/content/_children/checkbox/index.jsx
@@ -23,7 +23,7 @@ const Checkbox = ({
         onBlur={onBlur}
         {...props}
       />
-      <S.StyledCheckbox checked={checked}>
+      <S.StyledCheckbox hasError={hasError} checked={checked}>
         <S.Svg width="18" height="18" viewBox="0 0 24 24">
           <polyline points="20 6 9 17 4 12" />
         </S.Svg>
