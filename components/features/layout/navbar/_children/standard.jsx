cb62d90072de969b7b75b2675f327603f1688aae
Jorge Dueñas <jduenas@rmgperustaff.com>
2019-07-26 10:36:41 -0500

    Update standard.jsx

--- components/features/layout/navbar/_children/standard.jsx
+++ components/features/layout/navbar/_children/standard.jsx
@@ -8,7 +8,7 @@ import SignWallHard from '../../../signwall/_main/signwall/hard'
 import SignWallVerify from '../../../signwall/_main/signwall/verify'
 import SignWallReset from '../../../signwall/_main/signwall/reset'
 import SignWallRelogin from '../../../signwall/_main/signwall/relogin'
-import SignWallPaywall from '../../../signwall/_main/signwall/paywall'
+// import SignWallPaywall from '../../../signwall/_main/signwall/paywall'
 
 import Menu from './menu'
 // import Ads from '../../../../global-components/ads'
