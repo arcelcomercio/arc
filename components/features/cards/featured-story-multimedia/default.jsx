c878ffc3b957449372c38e19b2e4265a77239a12
Carlos Fernández <hello@cajotafer.com>
2019-07-31 17:22:32 -0500

    [UPDATE] - featured-story-multimedia - CODE REVIEW

--- components/features/cards/featured-story-multimedia/default.jsx
+++ components/features/cards/featured-story-multimedia/default.jsx
@@ -4,10 +4,12 @@ import PropTypes from 'prop-types'
 import { useContent } from 'fusion:content'
 import { useFusionContext } from 'fusion:context'
 
+// CR: DataStory -> StoryData
 import DataStory from '../../../utilities/story-data'
 import { formatDateLocalTimeZone } from '../../../utilities/helpers'
 import Icon from '../../../global-components/multimedia-icon'
 
+// CR: FeaturedStoryMultimedia -> CardFeaturedStoryMultimedia
 const FeaturedStoryMultimedia = props => {
   const {
     customFields: { section = '' },
@@ -20,6 +22,11 @@ const FeaturedStoryMultimedia = props => {
     query: { section },
   })
 
+  /**
+   * CR: Sería mejor destructurar sólamente la data que necesitas
+   * de una vez desde StoryData en lugar de crear la instancia.
+   */
+  // CR: DataStory -> StoryData
   const DataInstance = new DataStory({
     data,
     arcSite,
@@ -29,6 +36,7 @@ const FeaturedStoryMultimedia = props => {
   })
 
   return (
+    // CR: Este feature tiene las características para ser un article.
     <div className="featured-multimedia flex flex-col col-1 row-1 bg-black p-20">
       <a className="text-primary-color mb-25 text-md" href="/section">
         OJO VIDEOS
@@ -39,22 +47,40 @@ const FeaturedStoryMultimedia = props => {
         <picture>
           <img
             className="featured-multimedia__img object-cover"
+            /**
+             * CR: agregar "w-full"
+             * en lugar de width: 100%; en scss.
+             * */
             src={DataInstance.multimediaLandscapeMD}
             alt=""
+            // CR: Es importante agregar una descripción en alt=
           />
           <Icon type={DataInstance.multimediaType} iconClass="" />
         </picture>
       </a>
-      <div className="text-primary-color text-md mb-10 secondary-font font-bold">
+      <div
+        /** CR: debería usarse una etiqueta TIME y enviarle datetime="..."
+         * https://www.w3schools.com/tags/tag_time.asp */
+        className="text-primary-color text-md mb-10 secondary-font font-bold">
         {DataInstance.date && formatDateLocalTimeZone(DataInstance.date)}
       </div>
       <a
+        /**
+         * CR: Usar una etiqueta <h2> para este título
+         */
         className="text-white text-md flex-1 line-h-sm"
         href={DataInstance.websiteLink}>
         {DataInstance.title}
       </a>
       <a
         className="featured-multimedia__button bg-primary text-white secondary-font flex justify-center items-center rounded-sm font-bold"
+        /**
+         * CR:
+         * Se está usando margin: 0 auto; en los estilos,
+         * si se va a centrar de esa manera, mejor agregar "m-0 mx-auto" aquí.
+         * Si se va a centrar de esa manera, no deberian ser necesarias
+         * las clases "flex justify-center items-center"
+         * */
         href="/section">
         Ver ediciones
       </a>
@@ -66,9 +92,14 @@ FeaturedStoryMultimedia.label = 'Destaque multimedia'
 FeaturedStoryMultimedia.static = true
 
 FeaturedStoryMultimedia.propTypes = {
+  // CR: Me da dolor pero acordamos sacar los customFields a _dependencies.
   customFields: PropTypes.shape({
     section: PropTypes.string.tag({
       name: 'URL de la sección',
+      /**
+       * CR: Validar si se puede agregar una descripción
+       * similar a como se hace con los otros featured-stories.
+       */
     }),
   }),
 }
