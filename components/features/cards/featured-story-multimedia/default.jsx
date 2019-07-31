6f22936c693c324264dc43a881631591e146d168
Alvaro <ahuamani@rmgperustaff.com>
2019-07-31 16:18:53 -0500

    [wip] data formateada mostrada en la estructura del destaque

--- components/features/cards/featured-story-multimedia/default.jsx
+++ components/features/cards/featured-story-multimedia/default.jsx
@@ -4,48 +4,57 @@ import PropTypes from 'prop-types'
 import { useContent } from 'fusion:content'
 import { useFusionContext } from 'fusion:context'
 
+import DataStory from '../../../utilities/story-data'
+import { formatDateLocalTimeZone } from '../../../utilities/helpers'
+import Icon from '../../../global-components/multimedia-icon'
+
 const FeaturedStoryMultimedia = props => {
   const {
     customFields: { section = '' },
   } = props
 
-  // const { arcSite } = useFusionContext()
+  const { arcSite, contextPath, deployment } = useFusionContext()
 
   const data = useContent({
     source: 'story-by-section',
     query: { section },
   })
-  console.log(
-    'DATAsss-->',
+
+  const DataInstance = new DataStory({
     data,
-    'PROPS-->',
-    props,
-    'CONTEXT-->',
-    useFusionContext()
-  )
+    arcSite,
+    contextPath,
+    deployment,
+    defaultImgSize: 'sm',
+  })
+
   return (
     <div className="featured-multimedia flex flex-col col-1 row-1 bg-black p-20">
       <a className="text-primary-color mb-25 text-md" href="/section">
         OJO VIDEOS
       </a>
-      <a className="mb-25" href="/section">
+      <a
+        className="mb-25 bg-gray-300 position-relative"
+        href={DataInstance.websiteLink}>
         <picture>
           <img
-            className="featured-multimedia__img"
-            src="https://cdne.ojo.pe/thumbs/uploads/img/2019/07/01/osiptel-bloqueo-un-millon-y-medio-de-celulare-320359-798549-jpg_272x148.jpg"
+            className="featured-multimedia__img object-cover"
+            src={DataInstance.multimediaLandscapeMD}
             alt=""
           />
+          <Icon type={DataInstance.multimediaType} iconClass="" />
         </picture>
       </a>
-      <div className="text-primary-color text-md mb-10 secondary-font">
-        22/05/2019
+      <div className="text-primary-color text-md mb-10 secondary-font font-bold">
+        {DataInstance.date && formatDateLocalTimeZone(DataInstance.date)}
       </div>
-      <a className="text-white text-md flex-1 line-h-sm" href="/section">
-        Joven sorprende a su novia y le pide matrimonio en pleno concierto de
-        Gianmarco
+      <a
+        className="text-white text-md flex-1 line-h-sm"
+        href={DataInstance.websiteLink}>
+        {DataInstance.title}
       </a>
       <a
-        className="featured-multimedia__button bg-primary text-white secondary-font flex justify-center items-center rounded-sm"
+        className="featured-multimedia__button bg-primary text-white secondary-font flex justify-center items-center rounded-sm font-bold"
         href="/section">
         Ver ediciones
       </a>
@@ -54,7 +63,7 @@ const FeaturedStoryMultimedia = props => {
 }
 
 FeaturedStoryMultimedia.label = 'Destaque multimedia'
-// FeaturedStoryMultimedia.static = true
+FeaturedStoryMultimedia.static = true
 
 FeaturedStoryMultimedia.propTypes = {
   customFields: PropTypes.shape({
