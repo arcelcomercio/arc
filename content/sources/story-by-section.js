5824ae797049307dc8ff60dcaf32582dae53a228
Carlos Fernández <hello@cajotafer.com>
2019-07-31 18:10:37 -0500

    [UPDATE] - story-by-section - CODE REVIEW

--- content/sources/story-by-section.js
+++ content/sources/story-by-section.js
@@ -1,22 +1,28 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import request from 'request-promise-native'
-import { resizerSecret, CONTENT_BASE } from 'fusion:environment'
-import { addResizedUrls } from '@arc-core-components/content-source_content-api-v4'
+import {
+  resizerSecret,
+  CONTENT_BASE
+} from 'fusion:environment'
+import {
+  addResizedUrls
+} from '@arc-core-components/content-source_content-api-v4'
 import getProperties from 'fusion:properties'
-import { addResizedUrlsToStory } from '../../components/utilities/helpers'
+import {
+  addResizedUrlsToStory
+} from '../../components/utilities/helpers'
 
 const removeLastSlash = section => {
   if (section === '/') return section
-  return section && section.endsWith('/')
-    ? section.slice(0, section.length - 1)
-    : section
+  return section && section.endsWith('/') ?
+    section.slice(0, section.length - 1) :
+    section
 }
 
 let website = ''
 const schemaName = 'story'
 
-const params = [
-  {
+const params = [{
     name: 'section',
     displayName: 'Sección(es)',
     type: 'text',
@@ -45,21 +51,41 @@ export const itemsToArray = (itemString = '') => {
 
 const pattern = (key = {}) => {
   website = key['arc-site'] || 'Arc Site no está definido'
-  const { section, excludeSections, feedOffset } = key
+  const {
+    section,
+    excludeSections,
+    feedOffset
+  } = key
 
   const sectionsExcluded = itemsToArray(excludeSections)
 
   const clearSection = removeLastSlash(section)
+  /** 
+   * CR: removeLastSlash() sólo quitará el / final de una sección.
+   * Si se espera recibir más de una sección, removeLastSlash() debe ejecutarse
+   * dentro de itemsToArray().
+   * 
+   * Si se espera que siempre venga una sección, debería tomarse en consideración
+   * eliminar todo lo referente a excludeSections e itemsToArray.
+   * 
+   * Si harás esta mejora recuerda dejar el código lo más limpio posible pls.
+   * */
   const newSection =
-    clearSection === '' || clearSection === undefined || clearSection === null
-      ? '/'
-      : clearSection
+    clearSection === '' || clearSection === undefined || clearSection === null ?
+    '/' :
+    clearSection
+  /**
+   * CR: Esta asignación de newSection es lo mismo que hacer. Lo probé en consola.
+   * 
+   * const clearSection = removeLastSlash(section) || '/' (signo de interrogación)
+   * 
+   * Si haráś esta mejora recuerda modificar lo que esté relacionado a newSection.
+   */
 
   const body = {
     query: {
       bool: {
-        must: [
-          {
+        must: [{
             term: {
               'revision.published': 'true',
             },
@@ -70,29 +96,26 @@ const pattern = (key = {}) => {
             },
           },
         ],
-        must_not: [
-          {
-            nested: {
-              path: 'taxonomy.sections',
-              query: {
-                bool: {
-                  must: [
-                    {
-                      terms: {
-                        'taxonomy.sections._id': sectionsExcluded,
-                      },
+        must_not: [{
+          nested: {
+            path: 'taxonomy.sections',
+            query: {
+              bool: {
+                must: [{
+                    terms: {
+                      'taxonomy.sections._id': sectionsExcluded,
                     },
-                    {
-                      term: {
-                        'taxonomy.sections._website': website,
-                      },
+                  },
+                  {
+                    term: {
+                      'taxonomy.sections._website': website,
                     },
-                  ],
-                },
+                  },
+                ],
               },
             },
           },
-        ],
+        }, ],
       },
     },
   }
@@ -104,8 +127,7 @@ const pattern = (key = {}) => {
         path: 'taxonomy.sections',
         query: {
           bool: {
-            must: [
-              {
+            must: [{
                 terms: {
                   'taxonomy.sections._id': sectionsIncluded,
                 },
@@ -128,6 +150,12 @@ const pattern = (key = {}) => {
     uri: `${CONTENT_BASE}/site/v3/website/${website}/section?_id=${newSection}`,
     ...options,
   }).then(resp => {
+    /**
+     *  CR: no sé que tan pequeño o grande sea el contenido de resp
+     * pero quizás valga la pena destructurar simplementa lo que necesitamos,
+     * ({ name }) quizás también "status" si se va a usar para el error de
+     * la línea siguiente.
+     */
     if (Object.prototype.hasOwnProperty.call(resp, 'status'))
       throw new Error('Sección no encontrada')
     return request({
@@ -135,8 +163,14 @@ const pattern = (key = {}) => {
         0}&sort=publish_date:desc&single=true`,
       ...options,
     }).then(data => {
+      /**
+       * CR: simplementa "data" podría llamarse storyData desde el inicio, sino,
+       * recuerda que es storyData, no dataStory.
+       */
       const dataStory = data
-      const { resizerUrl } = getProperties(website)
+      const {
+        resizerUrl
+      } = getProperties(website)
       return {
         ...(addResizedUrlsToStory(
           [dataStory],
@@ -151,6 +185,13 @@ const pattern = (key = {}) => {
 }
 
 const fetch = key => pattern(key)
+/**
+ * CR: el nombre del método pattern() ya no tiene sentido :C
+ * yo le pondría resolve() porque ya hace todo de una vez, no sólo arma el pattern.
+ * Pero se que no se hará y mis palabras serán en vano, pero el día en que no esté
+ * sabré que al menos lo intenté y no todo se quedó sólo en pensamientos e ilusiones
+ * pasajeras.
+ */
 
 const source = {
   fetch,
