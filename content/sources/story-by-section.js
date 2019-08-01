bea47f925036afd263260a3291ceea5623ecca85
Alvaro <ahuamani@rmgperustaff.com>
2019-08-01 15:27:31 -0500

    [update] cambios de CR en story-by-section

--- content/sources/story-by-section.js
+++ content/sources/story-by-section.js
@@ -1,28 +1,26 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import request from 'request-promise-native'
-import {
-  resizerSecret,
-  CONTENT_BASE
-} from 'fusion:environment'
-import {
-  addResizedUrls
-} from '@arc-core-components/content-source_content-api-v4'
+import { resizerSecret, CONTENT_BASE } from 'fusion:environment'
+import { addResizedUrls } from '@arc-core-components/content-source_content-api-v4'
 import getProperties from 'fusion:properties'
-import {
-  addResizedUrlsToStory
-} from '../../components/utilities/helpers'
+import { addResizedUrlsToStory } from '../../components/utilities/helpers'
 
+/**
+ * Esta función se usa para controlar el slash final, ideada unicamente para
+ * cuando la sección venga por un resolver.
+ */
 const removeLastSlash = section => {
   if (section === '/') return section
-  return section && section.endsWith('/') ?
-    section.slice(0, section.length - 1) :
-    section
+  return section && section.endsWith('/')
+    ? section.slice(0, section.length - 1)
+    : section
 }
 
 let website = ''
 const schemaName = 'story'
 
-const params = [{
+const params = [
+  {
     name: 'section',
     displayName: 'Sección(es)',
     type: 'text',
@@ -49,43 +47,19 @@ export const itemsToArray = (itemString = '') => {
   })
 }
 
-const pattern = (key = {}) => {
+const fetch = (key = {}) => {
   website = key['arc-site'] || 'Arc Site no está definido'
-  const {
-    section,
-    excludeSections,
-    feedOffset
-  } = key
+  const { section, excludeSections, feedOffset } = key
 
   const sectionsExcluded = itemsToArray(excludeSections)
 
-  const clearSection = removeLastSlash(section)
-  /** 
-   * CR: removeLastSlash() sólo quitará el / final de una sección.
-   * Si se espera recibir más de una sección, removeLastSlash() debe ejecutarse
-   * dentro de itemsToArray().
-   * 
-   * Si se espera que siempre venga una sección, debería tomarse en consideración
-   * eliminar todo lo referente a excludeSections e itemsToArray.
-   * 
-   * Si harás esta mejora recuerda dejar el código lo más limpio posible pls.
-   * */
-  const newSection =
-    clearSection === '' || clearSection === undefined || clearSection === null ?
-    '/' :
-    clearSection
-  /**
-   * CR: Esta asignación de newSection es lo mismo que hacer. Lo probé en consola.
-   * 
-   * const clearSection = removeLastSlash(section) || '/' (signo de interrogación)
-   * 
-   * Si haráś esta mejora recuerda modificar lo que esté relacionado a newSection.
-   */
+  const clearSection = removeLastSlash(section) || '/'
 
   const body = {
     query: {
       bool: {
-        must: [{
+        must: [
+          {
             term: {
               'revision.published': 'true',
             },
@@ -96,38 +70,42 @@ const pattern = (key = {}) => {
             },
           },
         ],
-        must_not: [{
-          nested: {
-            path: 'taxonomy.sections',
-            query: {
-              bool: {
-                must: [{
-                    terms: {
-                      'taxonomy.sections._id': sectionsExcluded,
+        must_not: [
+          {
+            nested: {
+              path: 'taxonomy.sections',
+              query: {
+                bool: {
+                  must: [
+                    {
+                      terms: {
+                        'taxonomy.sections._id': sectionsExcluded,
+                      },
                     },
-                  },
-                  {
-                    term: {
-                      'taxonomy.sections._website': website,
+                    {
+                      term: {
+                        'taxonomy.sections._website': website,
+                      },
                     },
-                  },
-                ],
+                  ],
+                },
               },
             },
           },
-        }, ],
+        ],
       },
     },
   }
 
-  if (section && section !== '/') {
-    const sectionsIncluded = itemsToArray(section)
+  if (clearSection && clearSection !== '/') {
+    const sectionsIncluded = itemsToArray(clearSection)
     body.query.bool.must.push({
       nested: {
         path: 'taxonomy.sections',
         query: {
           bool: {
-            must: [{
+            must: [
+              {
                 terms: {
                   'taxonomy.sections._id': sectionsIncluded,
                 },
@@ -147,33 +125,20 @@ const pattern = (key = {}) => {
   const encodedBody = encodeURI(JSON.stringify(body))
 
   return request({
-    uri: `${CONTENT_BASE}/site/v3/website/${website}/section?_id=${newSection}`,
+    uri: `${CONTENT_BASE}/site/v3/website/${website}/section?_id=${clearSection}`,
     ...options,
   }).then(resp => {
-    /**
-     *  CR: no sé que tan pequeño o grande sea el contenido de resp
-     * pero quizás valga la pena destructurar simplementa lo que necesitamos,
-     * ({ name }) quizás también "status" si se va a usar para el error de
-     * la línea siguiente.
-     */
     if (Object.prototype.hasOwnProperty.call(resp, 'status'))
       throw new Error('Sección no encontrada')
     return request({
       uri: `${CONTENT_BASE}/content/v4/search/published?body=${encodedBody}&website=${website}&size=1&from=${feedOffset ||
         0}&sort=publish_date:desc&single=true`,
       ...options,
-    }).then(data => {
-      /**
-       * CR: simplementa "data" podría llamarse storyData desde el inicio, sino,
-       * recuerda que es storyData, no dataStory.
-       */
-      const dataStory = data
-      const {
-        resizerUrl
-      } = getProperties(website)
+    }).then(storyData => {
+      const { resizerUrl } = getProperties(website)
       return {
         ...(addResizedUrlsToStory(
-          [dataStory],
+          [storyData],
           resizerUrl,
           resizerSecret,
           addResizedUrls
@@ -183,15 +148,6 @@ const pattern = (key = {}) => {
     })
   })
 }
-
-const fetch = key => pattern(key)
-/**
- * CR: el nombre del método pattern() ya no tiene sentido :C
- * yo le pondría resolve() porque ya hace todo de una vez, no sólo arma el pattern.
- * Pero se que no se hará y mis palabras serán en vano, pero el día en que no esté
- * sabré que al menos lo intenté y no todo se quedó sólo en pensamientos e ilusiones
- * pasajeras.
- */
 
 const source = {
   fetch,
