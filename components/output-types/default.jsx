62d1926d3e255daeea7138af89ae83c4f60db8e6
Carlos Fernández <hello@cajotafer.com>
2019-08-14 09:58:50 -0500

    [UPDATE] - Eliminada propiedad no usada en output-types/default.jsx

--- components/output-types/default.jsx
+++ components/output-types/default.jsx
@@ -14,7 +14,6 @@ export default ({
   deployment,
   arcSite,
   globalContent,
-  globalContentConfig,
   // CssLinks,
   Fusion,
   Libs,
@@ -83,9 +82,7 @@ export default ({
   const keywords =
     metaValue('keywords') && !metaValue('keywords').match(/content/)
       ? metaValue('keywords')
-      : `Noticias, ${
-          siteProperties.siteName
-        }, Peru, Mundo, Deportes, Internacional, Tecnologia, Diario, Cultura, Ciencias, Economía, Opinión`
+      : `Noticias, ${siteProperties.siteName}, Peru, Mundo, Deportes, Internacional, Tecnologia, Diario, Cultura, Ciencias, Economía, Opinión`
 
   const twitterCardsData = {
     twitterUser: siteProperties.social.twitter.user,
@@ -218,9 +215,7 @@ export default ({
         <noscript>
           <iframe
             title="Google Tag Manager - No Script"
-            src={`https://www.googletagmanager.com/ns.html?id=${
-              siteProperties.googleTagManagerId
-            }`}
+            src={`https://www.googletagmanager.com/ns.html?id=${siteProperties.googleTagManagerId}`}
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
