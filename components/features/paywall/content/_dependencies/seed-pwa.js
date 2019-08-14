9bda8de8d9be22ba11d6091a14ebc750bb4cd3bf
Waldo J <w.saccaco@gmail.com>
2019-08-14 17:36:01 -0500

    fix paywall origin

--- components/features/paywall/content/_dependencies/seed-pwa.js
+++ components/features/paywall/content/_dependencies/seed-pwa.js
@@ -1,6 +1,3 @@
-import getService from '../../_dependencies/services'
-
-
 export default {
     isPWA(){
         return !!window.nativeConnection
@@ -13,7 +10,7 @@ export default {
           }
     },
     _onMessage({origin, data}, callback){
-        if(origin !== getService('PWA_DOMAIN')) return
+        if(origin !== window.location.origin) return
         const userIdentity = this.parse(data);
         if(userIdentity.accessToken && userIdentity.refreshToken){
             window.localStorage.setItem('ArcId.USER_INFO', data)
