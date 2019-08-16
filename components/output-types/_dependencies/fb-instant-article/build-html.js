5a60bc497c074140b9416054ed04cfe2b6a9c175
Edgar <edgar.gamarra21@gmail.com>
2019-08-16 18:11:09 -0500

    logica para videos de gold fish

--- components/output-types/_dependencies/fb-instant-article/build-html.js
+++ components/output-types/_dependencies/fb-instant-article/build-html.js
@@ -1,44 +1,50 @@
 import { AnalyticsScript, ScriptElement, ScriptHeader } from './scripts'
-
+import ConfigParams from '../../../utilities/config-params'
 const buildIframeAdvertising = urlAdvertising => {
   return `<figure class="op-ad"><iframe width="300" height="250" style="border:0; margin:0;" src="${urlAdvertising}"></iframe></figure>`
 }
 
-const buildParagraph = paragraph => {
+const buildParagraph = (paragraph, type = '') => {
   let result = null
 
-  if (paragraph.includes('<iframe')) {
-    // valida si el parrafo contiene un iframe con video o foto
-
-    result = `<figure class="op-interactive">${paragraph}</figure>`
-  } else if (paragraph.includes('<img')) {
-    const imageUrl = paragraph.match(/img.+"(http(?:[s])?:\/\/[^"]+)/)
-      ? paragraph.match(/img.+"(http(?:[s])?:\/\/[^"]+)/)[1]
-      : ''
-
-    const imageAlt = paragraph.match(/alt="([^"]+)?/)
-      ? paragraph.match(/alt="([^"]+)?/)[1]
-      : ''
-
-    if (imageUrl !== '') {
-      result = `<figure class="op-interactive"><img width="560" height="315" src="${imageUrl}" alt="${imageAlt}" /></figure>`
-    } else {
-      result = ''
-    }
-
-    result = `<figure class="op-interactive"><img frameborder="0" width="560" height="315" src="${imageUrl}" alt="${imageAlt}" /></figure>`
-  } else if (
-    paragraph.includes('<blockquote class="instagram-media"') ||
-    paragraph.includes('<blockquote class="twitter-tweet"')
-  ) {
-    // ára twitter y para instagram
-    result = `<figure class="op-interactive"><iframe>${paragraph}</iframe></figure>`
-  } else if (paragraph.includes('https://www.facebook.com/plugins')) {
-    result = `<figure class="op-interactive"><iframe>${paragraph}</iframe></figure>`
+  if (type === ConfigParams.ELEMENT_VIDEO) {
+    result = `<iframe src="https://d1tqo5nrys2b20.cloudfront.net/sandbox/powaEmbed.html?org=elcomercio&env=sandbox&api=sandbox&uuid=${paragraph}" width="640" height="400" data-category-id="sample" data-aspect-ratio="0.5625" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>`
   } else {
-    // si no comple con las anteriores condiciones es un parrafo de texto y retorna el contenido en etiquetas p
-    result = `<p>${paragraph}</p>`
+    if (paragraph.includes('<iframe')) {
+      // valida si el parrafo contiene un iframe con video o foto
+
+      result = `<figure class="op-interactive">${paragraph}</figure>`
+    } else if (paragraph.includes('<img')) {
+      const imageUrl = paragraph.match(/img.+"(http(?:[s])?:\/\/[^"]+)/)
+        ? paragraph.match(/img.+"(http(?:[s])?:\/\/[^"]+)/)[1]
+        : ''
+
+      const imageAlt = paragraph.match(/alt="([^"]+)?/)
+        ? paragraph.match(/alt="([^"]+)?/)[1]
+        : ''
+
+      if (imageUrl !== '') {
+        result = `<figure class="op-interactive"><img width="560" height="315" src="${imageUrl}" alt="${imageAlt}" /></figure>`
+      } else {
+        result = ''
+      }
+
+      result = `<figure class="op-interactive"><img frameborder="0" width="560" height="315" src="${imageUrl}" alt="${imageAlt}" /></figure>`
+    } else if (
+      paragraph.includes('<blockquote class="instagram-media"') ||
+      paragraph.includes('<blockquote class="twitter-tweet"')
+    ) {
+      // ára twitter y para instagram
+      result = `<figure class="op-interactive"><iframe>${paragraph}</iframe></figure>`
+    } else if (paragraph.includes('https://www.facebook.com/plugins')) {
+      result = `<figure class="op-interactive"><iframe>${paragraph}</iframe></figure>`
+    } else {
+      // si no comple con las anteriores condiciones es un parrafo de texto y retorna el contenido en etiquetas p
+
+      result = `<p>${paragraph}</p>`
+    }
   }
+
   return result
 }
 
@@ -66,10 +72,17 @@ const ParagraphshWithAdds = ({
   let IndexAdd = 0
   let resultParagraph = ''
 
-  
-  paragraphsNews.forEach(paragraphItem => {
+  paragraphsNews.forEach(({ payload: paragraphItem, type }) => {
     let paragraph = paragraphItem.trim().replace(/<\/?br[^<>]+>/, '')
     // el primer script de publicidad se inserta despues de las primeras 50 palabras (firstAdd)
+
+    if (paragraphItem === null) {
+      console.log('parrafo!!!!!!!')
+      console.log(paragraphItem)
+
+      console.log('typo!!!!!!!')
+      console.log(type)
+    }
 
     let paragraphwithAdd = ''
     const originalParagraph = paragraph
@@ -84,14 +97,14 @@ const ParagraphshWithAdds = ({
       if (countWords >= firstAdd) {
         countWords = 0
 
-        paragraphwithAdd = `${buildParagraph(originalParagraph)} ${
+        paragraphwithAdd = `${buildParagraph(originalParagraph, type)} ${
           arrayadvertising[IndexAdd]
             ? buildIframeAdvertising(arrayadvertising[IndexAdd])
             : ''
         }`
         IndexAdd += 1
       } else {
-        paragraphwithAdd = `${buildParagraph(originalParagraph)}`
+        paragraphwithAdd = `${buildParagraph(originalParagraph, type)}`
       }
 
       newsWithAdd.push(`${paragraphwithAdd}`)
@@ -109,14 +122,14 @@ const ParagraphshWithAdds = ({
 
       if (countWords >= nextAdds) {
         countWords = 0
-        paragraphwithAdd = `${buildParagraph(originalParagraph)} ${
+        paragraphwithAdd = `${buildParagraph(originalParagraph, type)} ${
           arrayadvertising[IndexAdd]
             ? buildIframeAdvertising(arrayadvertising[IndexAdd])
             : ''
         }`
         IndexAdd += 1
       } else {
-        paragraphwithAdd = `${buildParagraph(originalParagraph)}`
+        paragraphwithAdd = `${buildParagraph(originalParagraph, type)}`
       }
       newsWithAdd.push(`${paragraphwithAdd}`)
     }
