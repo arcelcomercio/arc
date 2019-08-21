76a63761815d278d1737a121b9c6028050fceafb
Edward Sanchez <esanchez@rmgperustaff.com>
2019-08-21 12:12:17 -0500

    [ADD] - Seccion video

--- components/features/video/section/_children/section-video.jsx
+++ components/features/video/section/_children/section-video.jsx
@@ -4,35 +4,59 @@ export default () => {
   return (
     <div className="section-video">
       <div className="section-video__top">
-        <div className="section-video__left" />
+        <div className="section-video__left">
+          <iframe
+            title="video"
+            width="560"
+            height="315"
+            className="section-video__frame"
+            src="https://www.youtube.com/embed/-sRBWb1_6ys"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          />
+        </div>
         <div className="section-video__right">
-          <div className="section-video__breadcrumbs">
-            <a href="/">Video</a>
-            <a href="/">Mundo</a>
+          <div>
+            <div className="section-video__breadcrumbs">
+              <span className="section-video__sub">
+                <a href="/">Video</a>
+              </span>
+              <span className="section-video__sub">
+                <a href="/">Mundo</a>
+              </span>
+            </div>
+            <h1 className="section-video__title">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h1>
+            <p className="section-video__subtitle">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non,
+              voluptas nesciunt. Maxime rerum doloribus enim earum dignissimos
+              ad dolore assumenda.
+            </p>
           </div>
-          <h1 className="section-video__title">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Consequatur, incidunt fuga eos obcaecati vitae.
-          </h1>
-          <p className="section-video__subtitle">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non,
-            voluptas nesciunt. Maxime rerum doloribus enim earum dignissimos ad
-            dolore assumenda. Maiores fugiat voluptas doloribus excepturi, sit
-            ducimus at minus delectus.
-          </p>
           <p className="section-video__author">Foto y video (AFP)</p>
         </div>
       </div>
       <div className="section-video__bottom">
         <div className="section-video__share">
-          <button type="button" className="btn btn-facebook">
-            Compartir
+          <button type="button" className="section-video__btn">
+            <span className="icon-facebook" />
+          </button>
+          <button type="button" className="section-video__btn">
+            <span className="icon-twitter" />
+          </button>
+          <button type="button" className="section-video__btn">
+            <span className="icon-linkedin" />
+          </button>
+          <button type="button" className="section-video__btn">
+            <span className="icon-share" />
           </button>
         </div>
         <div className="section-video__detail">
-          <span>15 de Agosto de 2019</span>
-          <span>13:25</span>
-          <span>Duracion: 00:40</span>
+          <span className="section-video__text">15 de Agosto de 2019</span>
+          <span className="section-video__text">13:25</span>
+          <span className="section-video__text">Duracion: 00:40</span>
         </div>
       </div>
     </div>
