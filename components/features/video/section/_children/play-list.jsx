a75eb88afc350cd564e08811763f060c710c7b1f
Edward Sanchez <esanchez@rmgperustaff.com>
2019-08-22 10:34:27 -0500

    [UPDATE] - agregada barra fixed

---
+++ components/features/video/section/_children/play-list.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className="play-list">
+      <h2 className="play-list__name">PLAYLIST</h2>
+      <div className="play-list__wrapper">
+        <div className="play-list__item">
+          <a className="play-list__image-box" href="/">
+            <img
+              src="https://gestion.pe/resizer/HtZ5lHa1hiUNhQ2GucK2Z7Bsays=/940x569/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/DHSJUAVFTVA2LE6ICFNJO7I25U.jpg"
+              className="play-list__image"
+              alt=""
+            />
+            <span className="play-list__duration">0:41</span>
+          </a>
+          <h3 className="play-list__title">
+            <a href="/">Lorem ipsum dolor sit amet consectetur.</a>
+          </h3>
+        </div>
+        <div className="play-list__item">
+          <a className="play-list__image-box" href="/">
+            <img
+              src="https://gestion.pe/resizer/HtZ5lHa1hiUNhQ2GucK2Z7Bsays=/940x569/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/DHSJUAVFTVA2LE6ICFNJO7I25U.jpg"
+              className="play-list__image"
+              alt=""
+            />
+            <span className="play-list__duration">0:41</span>
+          </a>
+          <h3 className="play-list__title">
+            <a href="/">Lorem ipsum dolor sit amet consectetur.</a>
+          </h3>
+        </div>
+        <div className="play-list__item">
+          <a className="play-list__image-box" href="/">
+            <img
+              src="https://gestion.pe/resizer/HtZ5lHa1hiUNhQ2GucK2Z7Bsays=/940x569/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/DHSJUAVFTVA2LE6ICFNJO7I25U.jpg"
+              className="play-list__image"
+              alt=""
+            />
+            <span className="play-list__duration">0:41</span>
+          </a>
+          <h3 className="play-list__title">
+            <a href="/">Lorem ipsum dolor sit amet consectetur.</a>
+          </h3>
+        </div>
+        <div className="play-list__item">
+          <a className="play-list__image-box" href="/">
+            <img
+              src="https://gestion.pe/resizer/HtZ5lHa1hiUNhQ2GucK2Z7Bsays=/940x569/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/DHSJUAVFTVA2LE6ICFNJO7I25U.jpg"
+              className="play-list__image"
+              alt=""
+            />
+            <span className="play-list__duration">0:41</span>
+          </a>
+          <h3 className="play-list__title">
+            <a href="/">Lorem ipsum dolor sit amet consectetur.</a>
+          </h3>
+        </div>
+      </div>
+    </div>
+  )
+}
