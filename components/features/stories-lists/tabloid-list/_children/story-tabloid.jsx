cf8db65658ac65392ec09b659d154723796876dd
Edward Sanchez <esanchez@rmgperustaff.com>
2019-08-23 18:02:30 -0500

    [UPDATE] - Grilla para listado

--- components/features/stories-lists/tabloid-list/_children/story-tabloid.jsx
+++ components/features/stories-lists/tabloid-list/_children/story-tabloid.jsx
@@ -1,5 +1,27 @@
 import React from 'react'
 
 export default () => {
-  return <h1>Hola</h1>
+  return (
+    <div className="stories-l-tabloid">
+      <div className="stories-l-tabloid__item">
+        <a className="stories-l-tabloid__image-link" href="/">
+          <picture>
+            <source
+              srcSet="https://picsum.photos/260/300"
+              media="(max-width: 640px)"
+            />
+            <img
+              className="stories-l-tabloid__image"
+              src="https://picsum.photos/260/300"
+              alt=""
+            />
+          </picture>
+        </a>
+        <a href="/" className="stories-l-tabloid__date">
+          10/01/2019
+        </a>
+        <p className="stories-l-tabloid__section">El otoron</p>
+      </div>
+    </div>
+  )
 }
