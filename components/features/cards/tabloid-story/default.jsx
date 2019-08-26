c25461f71231bca2973ec5e302f610dfec8a837c
Alvaro <ahuamani@rmgperustaff.com>
2019-08-26 12:07:51 -0500

    [fix] lazy tabloide

--- components/features/cards/tabloid-story/default.jsx
+++ components/features/cards/tabloid-story/default.jsx
@@ -7,70 +7,92 @@ import StoryData from '../../../utilities/story-data'
 import getLatinDate from '../../../utilities/date-name'
 
 const CardsTabloidStory = props => {
-  const {
+  /* const {
     customFields: {
       storyConfig: { contentService = '', contentConfigValues = {} } = {},
     } = {},
-  } = props
+  } = props */
+  const { customFields: { section = '', storyNumber = 0 } = {} } = props
 
-  const { arcSite, contextPath, deployment } = useFusionContext()
+  const { arcSite, contextPath, deployment, isAdmin } = useFusionContext()
 
-  const data = useContent({
-    source: contentService,
-    query: contentConfigValues,
-    filter: `
-    display_date
-    websites {
-      ${arcSite} {
-        website_url
-      }
-    }
-    headlines{
-      basic
-    }
-    taxonomy {
-      primary_section { 
-        name 
-        path
-      }
-    }
-    promo_items {
-      basic { 
-        url 
-        type 
-        resized_urls { 
-          landscape_md
-        } 
-      }
-      basic_video {
-        promo_items {
-          basic { 
-            url 
-            type 
-            resized_urls { 
-              landscape_md
-            } 
+  const { content_elements: contentElements = [] } =
+    useContent({
+      source: 'story-feed-by-section-with-custom-presets',
+      query: {
+        section,
+        stories_qty: 1,
+        feedOffset: storyNumber,
+        preset1: '394x222',
+        preset2: '5x5',
+      },
+      filter: `
+      {
+        content_elements {
+          display_date
+          websites {
+            ${arcSite} {
+              website_url
+            }
+          }
+          headlines{
+            basic
+          }
+          taxonomy {
+            primary_section { 
+              name 
+              path
+            }
+          }
+          promo_items {
+            basic { 
+              url 
+              type 
+              resized_urls { 
+                preset1
+                preset2
+              } 
+            }
+            basic_video {
+              promo_items {
+                basic { 
+                  url 
+                  type 
+                  resized_urls { 
+                    preset1
+                    preset2
+                  } 
+                }
+              }
+            }
+            basic_gallery {
+              promo_items {
+                basic { 
+                  url 
+                  type 
+                  resized_urls { 
+                    preset1
+                    preset2
+                  } 
+                }
+              }
+            }
           }
         }
       }
-      basic_gallery {
-        promo_items {
-          basic { 
-            url 
-            type 
-            resized_urls { 
-              landscape_md
-            } 
-          }
-        }
-      }
-    }
     `,
-  })
+    }) || {}
+  const data = contentElements[0] || {}
+
+  const {
+    promo_items: {
+      basic: { resized_urls: { preset1, preset2: lazyImg } = {} } = {},
+    } = {},
+  } = data
 
   const {
     websiteLink, // { websites { ${arcsite} { website_url } } }
-    multimediaLandscapeMD,
+    multimedia,
     title, // { headlines { basic } }
     date,
     // multimediaType, // { promo_items }
@@ -83,6 +105,7 @@ const CardsTabloidStory = props => {
     deployment,
     defaultImgSize: 'sm',
   })
+  const imageSrc = preset1 || multimedia
 
   return (
     <div className="tabloid-story row-1 bg-base-400">
@@ -102,8 +125,11 @@ const CardsTabloidStory = props => {
         <a className="flex justify-center" href={websiteLink}>
           <picture className="inline-block pl-10 pr-10">
             <img
-              className="tabloid-story__img w-full object-cover"
-              src={multimediaLandscapeMD}
+              className={`${
+                isAdmin ? '' : 'lazy'
+              } tabloid-story__img w-full object-cover`}
+              src={isAdmin ? imageSrc : lazyImg}
+              data-src={imageSrc}
               alt={title}
             />
           </picture>
@@ -118,8 +144,18 @@ CardsTabloidStory.static = true
 
 CardsTabloidStory.propTypes = {
   customFields: PropTypes.shape({
-    storyConfig: PropTypes.contentConfig('story').isRequired.tag({
-      name: 'Configuración del contenido',
+    section: PropTypes.string.tag({
+      name: 'URL de la sección',
+      description:
+        'Si no se coloca la URL de la sección, se renderiza la última historia publicada. Ejemplo: /deporte-total',
+    }),
+    storyNumber: PropTypes.number.tag({
+      name: 'Posición de la historia',
+      description:
+        'Si no se completa el campo, la posición de la historia será 0 (la última historia publicada)',
+      group: 'Configuración',
+      min: 0,
+      defaultValue: 0,
     }),
   }),
 }
