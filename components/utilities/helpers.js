428d67afdd9bdd228a58f27d6b999238318f25ce
henrry11985 <henrry11985@gmail.com>
2019-08-26 16:01:10 -0500

    [Fix videos migrador para AMP]

--- components/utilities/helpers.js
+++ components/utilities/helpers.js
@@ -453,7 +453,9 @@ export const optaWidgetHtml = html => {
     ? matches[1].replace(/="/g, '=').replace(/" /g, '&')
     : ''
 
-  const rplOptaWidget = `<amp-iframe class="media" width="1" height="1" layout="responsive" sandbox="allow-scripts allow-same-origin allow-popups" allowfullscreen frameborder="0" src="${ConfigParams.OPTA_WIDGET}/optawidget?${matchesResult} ></amp-iframe>`
+  const rplOptaWidget = `<amp-iframe class="media" width="1" height="1" layout="responsive" sandbox="allow-scripts allow-same-origin allow-popups" allowfullscreen frameborder="0" src="${
+    ConfigParams.OPTA_WIDGET
+  }/optawidget?${matchesResult} ></amp-iframe>`
   return html.replace(/<opta-widget (.*?)><\/opta-widget>/, rplOptaWidget)
 }
 
@@ -470,6 +472,7 @@ export const imageHtml = html => {
     rplImageCde1
   )
   resHtml = resHtml.replace(/<img (.*)src="(.*)" (.*)>/g, rplImageCde)
+  resHtml = resHtml.replace(/<p><img src="(.*?)">/g, rplImageCde1)
   return resHtml
 }
 
