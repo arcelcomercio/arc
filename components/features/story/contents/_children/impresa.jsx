c8e45a5b713c27ac5a273bfc2798dc0a9de55f86
henrry11985 <henrry11985@gmail.com>
2019-08-27 10:18:16 -0500

    [mejora] Se agrega portada impresa

---
+++ components/features/story/contents/_children/impresa.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Image from '@arc-core-components/element_image'
+
+// Basic flex stuff
+const classes = {
+  paperNav: 'paper-nav ',
+  paperPrev: 'paper-prev position-absolute top-0',
+  paperNext: 'paper-next position-absolute top-0',
+  iconPrev: 'icon-prev',
+  iconNext: 'icon-next',
+}
+
+const RenderRelatedContentImpresa = ({ data: { basic = {} } = {} }) => {
+  console.log('primarySection=>>>>>>>>>>>>>>>>', basic)
+
+  const sizerImg = 'impresa'
+  const ampClass = 'amp-'
+  return (
+    <>
+      <Image
+        width="100%"
+        layout="responsive"
+        imgClassName={classes.image}
+        captionClassName={`${ampClass}${classes.caption}`}
+        sizePreset={sizerImg}
+        {...basic}
+      />
+      <div className={classes.paperNav}>
+        <a href="/impresa/presion-alta-498352" className={classes.paperPrev}>
+          <i className={classes.iconPrev}> </i>
+        </a>
+        <a href="/impresa/coimas-498663" className={classes.paperNext}>
+          <i className={classes.iconNext}> </i>
+        </a>
+      </div>
+    </>
+  )
+}
+export default RenderRelatedContentImpresa
