70ccb594944a8c99e05adb0cea4d2e23786ae093
Alvaro <ahuamani@rmgperustaff.com>
2019-09-04 10:59:07 -0500

    [update] agregado peru21 g21 en mocks, site properties y carpeta de estilos

---
+++ properties/sites/peru21g21.js
@@ -0,0 +1,174 @@
+export default {
+  siteName: 'Peru21 G21',
+  newsletterBrand: 'peru21',
+  googleFonts: 'Lato:300,400,700,900|Exo:500',
+
+  colorPrimary: '#005fa5',
+  siteDomain: 'peru21.pe',
+  siteUrl: 'https://peru21.pe',
+  resizerUrl: 'https://elcomercio-peru21-prod.cdn.arcpublishing.com/resizer',
+  resizerSecretKeyEnvVar: '',
+  urlPreroll:
+    'https://pubads.g.doubleclick.net/gampad/ads?iu=/28253241/Peru21_Preroll&description_url=[placeholder]&env=vp&impl=s&correlator=&tfcd=0&npa=0&gdfp_req=1&output=vast&sz=640x360&unviewed_position_start=1',
+  fbAppId: '1667917060088448',
+  googleTagManagerId: 'GTM-KKQFJ3Z',
+  ampGoogleTagManagerId: 'UA-3055636-4',
+  ampGoogleTagManagerName: 'peru21',
+  charbeatAccountNumber: 57773,
+  idGoogleAnalitics: 'UA-3055636-4',
+  fbArticleStyle: 'LogoPeru21',
+  nameStoryRelated: 'VEA TAMBIÉN',
+  siteDescription:
+    'Noticias de Perú y el mundo en Peru21.pe. Noticias de actualidad, política, deportes, gastronomía, economía y espectáculos.',
+  googleNewsImage: 'https://publimetro.pe/f/i/pub_40.png',
+  assets: {
+    nav: {
+      logo: 'white-logo.png',
+      logoSomos: 'white-logo.png',
+    },
+    header: {
+      logo: 'white-logo.png',
+    },
+    footer: {
+      logo: 'white-logo.png',
+    },
+    seo: {
+      logoAmp: 'logo-peru21-214x60.png',
+      width: 214,
+      height: 60,
+      widthAmp: 110,
+      heightAmp: 28,
+    },
+    tv: {
+      siteLogo: 'favicon.png',
+    },
+  },
+  infoPagesDev: {
+    termsAndConditions: 'OY6E6FMKWVDXFC7P3S7VKRMWAU',
+    guidingPrinciples: '',
+    privacyPolicies: 'U2FDMRFNEVDVJNASRBJD7BWE3I',
+    integratedManagementPolicy: '',
+    arcoProcedure: '',
+    cookiesPolicy: 'F5UCK5TMLZBRPN3OFRXIAW6HWY',
+    aboutUs: '',
+    frequentQuestions: '2ILZRA4AUBFSXK7JLGGN3SZ2XE',
+  },
+  infoPagesProd: {
+    termsAndConditions: 'WBMYZQNJ4FD7HFCQM5RKK4P7UU',
+    guidingPrinciples: '',
+    privacyPolicies: 'ZNXM3U7ERBCV5LO5RRTFWTYDKA',
+    integratedManagementPolicy: '',
+    arcoProcedure: '',
+    cookiesPolicy: 'UVEA77IFONATBI6K2HBEVES75Y',
+    aboutUs: 'TSXLSV7JIVCPHHKSPJHZQWLG2I',
+    frequentQuestions: 'SLSILPSVUZBPTCQVL4VL6VBRXA',
+  },
+
+  legalLinks: [
+    {
+      name: 'Quiénes Somos',
+      url: '/quienes-somos/',
+    },
+    {
+      name: 'Términos y condiciones',
+      url: '/terminos-y-condiciones/',
+    },
+    {
+      name: 'Políticas de Privacidad',
+      url: '/politicas-de-privacidad/',
+    },
+    {
+      name: 'Politicas de Cookies',
+      url: '/politicas-de-cookies/',
+    },
+    {
+      name: 'Preguntas Frecuentes',
+      url: '/preguntas-frecuentes/',
+    },
+  ],
+
+  footer: {
+    siteLegal: [
+      'Empresa Editora El Comercio',
+      'Jr. Miró Quesada 247. Piso 6 Lima 1',
+      'Copyright © Peru21.pe',
+      'Grupo El Comercio - Todos los derechos reservados',
+    ],
+    story: [
+      {
+        position: 'Directora Periodística:',
+        name: 'Cecilia Valenzuela',
+      },
+      {
+        position: 'Diario Perú21: ',
+        name: 'Jr. Santa Rosa 247. Piso 6 Lima 1',
+      },
+      {
+        position: 'Copyright © Peru21.pe',
+        name: 'Grupo El Comercio - Todos los derechos reservados',
+      },
+    ],
+    socialNetworks: [
+      {
+        name: 'facebook',
+        url: 'https://www.facebook.com/peru21',
+      },
+      {
+        name: 'twitter',
+        url: 'https://twitter.com/peru21noticias',
+      },
+    ],
+
+    contacts: [
+      {
+        position: 'Directora Periodística',
+        name: 'Cecilia Valenzuela',
+      },
+      {
+        position: 'Editora Web',
+        name: 'Esther Vargas - esther.vargas@peru21.com',
+      },
+      {
+        position: 'Publicidad Web',
+        name: 'fonoavisos@comercio.pe',
+      },
+    ],
+  },
+  social: {
+    facebook: {
+      name: 'facebook',
+      user: '@peru21.pe',
+      url: 'https://www.facebook.com/peru21',
+    },
+    twitter: {
+      name: 'twitter',
+      user: 'peru21noticias',
+      url: 'https://twitter.com/peru21noticias',
+    },
+    youtube: {
+      name: 'youtube',
+      url: 'https://plus.google.com/u/0/+elcomerciope',
+    },
+  },
+  adsAmp: {
+    dataSlot: 28253241,
+    movil1: 14926789,
+    movil2: 14904301,
+    movil3: 14926493,
+    movil4: 14926495,
+    movil5: 14926496,
+  },
+  listUrlAdvertisings: [
+    'https://d1r08wok4169a5.cloudfront.net/ads-peru21/ads-fia-28253241-p21_ia_interna1-300x250-div-gpt-ad-8599377-31.html',
+    'https://d1r08wok4169a5.cloudfront.net/ads-peru21/ads-fia-28253241-p21_ia_interna2-300x250-div-gpt-ad-8599377-32.html',
+    'https://d1r08wok4169a5.cloudfront.net/ads-peru21/ads-fia-28253241-p21_ia_interna3-300x250-div-gpt-ad-8599377-33.html',
+    'https://d1r08wok4169a5.cloudfront.net/ads-peru21/ads-fia-28253241-p21_ia_interna4-300x250-div-gpt-ad-8599377-34.html',
+    'https://d1r08wok4169a5.cloudfront.net/ads-peru21/ads-fia-28253241-p21_ia_interna5-300x250-div-gpt-ad-8599377-35.html',
+  ],
+  activeSignwall: true,
+  activePaywall: false,
+  tv: {
+    logoUrl: '/peru21tv/',
+    logoAlt: 'Perú21Tv',
+  },
+}
