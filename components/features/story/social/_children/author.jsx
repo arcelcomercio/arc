fdd0a12a769717d5b19cf6a023773372d0a8a96a
henrry11985 <henrry11985@gmail.com>
2019-09-09 16:52:20 -0500

    [update] plantilla para columnistas

---
+++ components/features/story/social/_children/author.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const classes = {
+  author:
+    'story-content__author flex justify-between pt-30 mb-20 flex-col md:flex-row',
+  authorNameLink:
+    'secondary-font font-bold story-content__author-link header__link text-lg text-gray-200 line-h-sm',
+  authorDate:
+    'story-content__date flex items-center secondary-font text-md text-gray-200 line-h-sm',
+  authorEmail:
+    'story-content__author-email secondary-font text-md text-gray-200 line-h-sm',
+  authorImag: 'story-content__author-img',
+}
+
+const StorySocialChildAuthor = ({
+  authorImage,
+  author,
+  authorLink,
+  primarySection,
+  authorEmail,
+}) => {
+  return (
+    <>
+      {primarySection === 'Columnistas' && (
+        <div className={classes.author}>
+          <div className={classes.authorImag}>
+            <img src={authorImage} alt={author} />
+          </div>
+          <div>
+            {author && (
+              <a href={authorLink} className={classes.authorNameLink}>
+                {author}
+              </a>
+            )}
+            {authorEmail && true && (
+              <p className={classes.authorEmail}> {authorEmail} </p>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default StorySocialChildAuthor
