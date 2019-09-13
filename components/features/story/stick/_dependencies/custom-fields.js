161fbe96194cbe971512e3ae3454c8b9c2b4b5ba
cesar.contreras <ccontreras@rmgperustaff.com>
2019-09-13 11:05:28 -0500

    [update] cambios en customfields, stick y siteproperties peru21

--- components/features/story/stick/_dependencies/custom-fields.js
+++ components/features/story/stick/_dependencies/custom-fields.js
@@ -4,20 +4,10 @@ const customFields = PropTypes.shape({
   urlpwd: PropTypes.string.tag({
     name: 'Url PWD ',
     description:
-      'Url de pwd  Ejemplo: https://pwaperu21.page.link/?link=https://peru21.pe',
-  }),
-
-  appData: PropTypes.richtext.tag({
-    name: 'appData news ',
-    description:
-      'appData news Ejemplo: &apn=com.eeec.peru21&amv=30&ibi=com.eeec.Peru21&ipbi=com.eeec.Peru21&isi=991197788&imv=31&ofl=',
-  }),
-
-  href: PropTypes.richtext.tag({
-    name: 'href ',
-    description:
-      'href Ejemplo: &efr=1&utm_source=btn_openapp_note&mt=8&ct=btn_openapp_note',
+      // 'Url de pwd  Ejemplo: https://pwaperu21.page.link/?link=https://peru21.pe',
+      'Url de pwd  Ejemplo: https://pwaperu21.page.link',
   }),
 })
 
 export default customFields
+
