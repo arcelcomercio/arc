bde96a508403a782832fb446079997ecff515d68
cesar.contreras <ccontreras@rmgperustaff.com>
2019-09-16 11:38:17 -0500

    [update] - stick

--- components/features/story/stick/_dependencies/custom-fields.js
+++ components/features/story/stick/_dependencies/custom-fields.js
@@ -4,7 +4,6 @@ const customFields = PropTypes.shape({
   urlpwd: PropTypes.string.tag({
     name: 'Url PWD ',
     description:
-      // 'Url de pwd  Ejemplo: https://pwaperu21.page.link/?link=https://peru21.pe',
       'Url de pwd  Ejemplo: https://pwaperu21.page.link',
   }),
 })
