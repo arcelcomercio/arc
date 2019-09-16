4c15d47d0eb51e64342542a1c456f38335816215
Edward Sanchez <esanchez@rmgperustaff.com>
2019-09-16 11:40:41 -0500

    [ADD] - Componente doblete

---
+++ components/features/cards/doblete/default.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import DobleteCard from './_children/doblete-card'
+
+const Doblete = () => {
+  return <DobleteCard />
+}
+
+Doblete.label = 'Doblete'
+export default Doblete
