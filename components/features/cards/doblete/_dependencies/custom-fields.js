e7f872a2b0990f838c44a224447e7a70d12c5eba
Alvaro <ahuamani@rmgperustaff.com>
2019-09-16 15:33:57 -0500

    [update] CR

--- components/features/cards/doblete/_dependencies/custom-fields.js
+++ components/features/cards/doblete/_dependencies/custom-fields.js
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types'
 
 const customFields = PropTypes.shape({
   storyConfig: PropTypes.contentConfig('story').isRequired.tag({
-    name: 'Configuración de Nota 1',
+    name: 'Contenido de la Nota 1',
   }),
   storyConfig2: PropTypes.contentConfig('story').isRequired.tag({
-    name: 'Configuración de Nota 2',
+    name: 'Contenido de la Nota 2',
   }),
 })
 
