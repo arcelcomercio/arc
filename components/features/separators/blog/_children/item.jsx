a2d38bef5610dac082af598b184814938746cd4f
Maria Gabriela Rangel <mrangel@rmgperustaff.com>
2019-09-16 10:00:27 -0500

    Modificaciones separador blog - Gestion

--- components/features/separators/blog/_children/item.jsx
+++ components/features/separators/blog/_children/item.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const classes = {
   item:
-    'blog-separator__item flex flex-row justify-between pt-0 lg:pt-15 mb-10 bg-white flex-no-wrap w-full lg:flex-col lg:mb-0 lg:mr-5',
+    'blog-separator__item flex flex-row justify-between pt-0 lg:pt-15 mb-10 flex-no-wrap w-full lg:flex-col lg:mb-0 lg:mr-5',
   title:
     'blog-separator__title inline-block w-full overflow-hidden pl-15 pr-15 pb-15 pt-15 lg:pt-0',
   link: 'blog-separator__link text-gray-300 font-normal',
   author:
     'blog-separator__author flex flex-col justify-center  primary-font  pl-15 pr-15',
-  boxImg: 'flex items-center justify-center',
+  boxImg: 'flex items-center',
   img: 'blog-separator__img object-cover',
 }
 
@@ -24,16 +24,6 @@ const SeparatorBlogChildItem = ({
 }) => {
   return (
     <article className={classes.item}>
-      <div>
-        <h3 className={classes.title}>
-          <a href={postLink} className={classes.link}>
-            {postTitle}
-          </a>
-        </h3>
-        <a href={blogUrl} className={classes.author}>
-          {authorName}
-        </a>
-      </div>
       <a className={classes.boxImg} href={postLink}>
         <figure>
           <img
@@ -44,6 +34,16 @@ const SeparatorBlogChildItem = ({
           />
         </figure>
       </a>
+      <div>
+        <a href={blogUrl} className={classes.author}>
+          {authorName}
+        </a>
+        <h3 className={classes.title}>
+          <a href={postLink} className={classes.link}>
+            {postTitle}
+          </a>
+        </h3>
+      </div>
     </article>
   )
 }
