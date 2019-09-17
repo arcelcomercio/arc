876a36093bb709dd604c12aada0594f481fca4c2
Maria Gabriela Rangel <mrangel@rmgperustaff.com>
2019-09-17 11:47:10 -0500

    Modidficaiones comercio

--- components/features/story/gallery/_children/gallery.jsx
+++ components/features/story/gallery/_children/gallery.jsx
@@ -6,7 +6,7 @@ const classes = {
   gallery: 'story-gallery pt-10 pr-20 pl-20 md:pr-0 md:pl-0',
   galleryItem: 'story-gallery__item position-relative mt-30',
   galleryNumber:
-    'story-gallery__number bg-white flex items-center justify-center position-absolute rounded-lg',
+    'story-gallery__number bg-white flex items-center justify-center rounded-lg',
   image: 'story-gallery__img w-full h-full mb-10',
   caption: 'story-gallery__caption text-gray-200 text-sm',
   figure: 'story-gallery__figure',
