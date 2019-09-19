393778ae295f5dfbd3890712612e56ce3b347b7b
Jorge Dueñas <cocotopia@gmail.com>
2019-09-18 23:52:11 -0500

    update free paywall

--- components/features/signwall/_main/panel/menu-profile/index.js
+++ components/features/signwall/_main/panel/menu-profile/index.js
@@ -35,7 +35,6 @@ const closeSession = props => {
 
 // eslint-disable-next-line import/prefer-default-export
 export const MenuProfile = props => {
-  window.console.log(props)
   const { userName, typeLogin, arcSite, emailUser, userNameFB } = props
   return (
     // eslint-disable-next-line react/jsx-filename-extension
