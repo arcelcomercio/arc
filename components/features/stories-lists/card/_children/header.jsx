34615752c5150bc118a14cb06f24d8d07b9b182d
Edward Sanchez <esanchez@rmgperustaff.com>
2019-09-24 10:19:22 -0500

    [UPDATE] - Cambiado el ultimo minuto a diseño de casses

--- components/features/stories-lists/card/_children/header.jsx
+++ components/features/stories-lists/card/_children/header.jsx
@@ -5,6 +5,7 @@ const classes = {
   seeMore: `stories-l-header__see-more flex items-center bg-white pt-5 pb-5 pr-10 pl-10 text-gray-200 text-sm`,
   title:
     'stories-l-header__title flex items-center full-height text-gray-300 uppercase',
+  icon: 'stories-l-header__icon icon-marca',
 }
 const StoriesListsCardChildHeader = ({
   titleList,
@@ -27,6 +28,7 @@ const StoriesListsCardChildHeader = ({
           Ver más
         </a>
       )}
+      <i className={classes.icon} />{' '}
     </div>
   )
 }
