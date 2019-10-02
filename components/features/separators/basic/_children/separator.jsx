1160d45bb1d899eb40b67359ed1d29b807fb2b29
Edward Sanchez <esanchez@rmgperustaff.com>
2019-10-02 11:37:13 -0500

    [UPADTE] - Agregado col-3 por defecto para separador basico cuando este en layout grilla

--- components/features/separators/basic/_children/separator.jsx
+++ components/features/separators/basic/_children/separator.jsx
@@ -3,7 +3,7 @@ import { createMarkup } from '../../../../utilities/helpers'
 import SeparatorItem from './item'
 
 const classes = {
-  separator: `separator bg-white mt-20 w-full pt-0 pr-20 pb-15 pl-20 border-t-1 border-solid`,
+  separator: `separator bg-white mt-20 w-full pt-0 pr-20 pb-15 pl-20 border-t-1 border-solid col-3 `,
   title: 'separator__header-title capitalize pb-20 pt-20 text-left text-lg',
   titleLink: 'separator__header-link font-bold',
   oneline: 'separator__oneline',
