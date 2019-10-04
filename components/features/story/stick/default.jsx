c972a2d9d3aed6b7b25f25d6220ad9e10c966db7
luis.moreno <luis.moreno@comercio.com.pe>
2019-10-04 09:21:47 -0500

    [FIX] manejo de error al no existir variable apntag

--- components/features/story/stick/default.jsx
+++ components/features/story/stick/default.jsx
@@ -84,15 +84,18 @@ class Stick extends PureComponent {
     })
 
     // eslint-disable-next-line no-undef
-    apntag.onEvent('adLoaded', 'ads_m_ticker', () => {
-      const tickerContainer = document.querySelector('#content_ads_m_ticker')
-      this.closeStick()
-      tickerContainer.addEventListener('click', evt => {
-        if (evt.target.classList.contains('zocalo-button-close')) {
-          this.openStick()
-        }
+    if (apntag) {
+      // eslint-disable-next-line no-undef
+      apntag.onEvent('adLoaded', 'ads_m_ticker', () => {
+        const tickerContainer = document.querySelector('#content_ads_m_ticker')
+        this.closeStick()
+        tickerContainer.addEventListener('click', evt => {
+          if (evt.target.classList.contains('zocalo-button-close')) {
+            this.openStick()
+          }
+        })
       })
-    })
+    }
   }
 
   closeStick = () => {
