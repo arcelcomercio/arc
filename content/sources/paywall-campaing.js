1ca6890d0e948add6cf355f456daa84f8544d5a3
Raul Contreras <rcontreras@rmgperustaff.com>
2019-10-09 18:11:01 -0500

    paywall-campaign con fetch

--- content/sources/paywall-campaing.js
+++ content/sources/paywall-campaing.js
@@ -1,61 +1,46 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import request from 'request-promise-native'
 import getProperties from 'fusion:properties'
 import { interpolateUrl } from '../../components/features/paywall/_dependencies/domains'
 
-const parse = string => {
-  try {
-    return JSON.parse(string)
-  } catch (error) {
-    return { err: 'is not a object' }
-  }
-}
-
-// https://api-sandbox.gestion.pe
-// paywall-gestion-sandbox
-// gprint-july-19
-
-export default {
-  resolve(key = {}) {
-    const { documentType = 'DNI', documentNumber, attemptToken } = key
-    const website = key['arc-site']
-    this.document = {
-      documentType,
-      documentNumber,
-    }
-    const {
-      paywall: { urls },
-    } = getProperties(website)
-    return interpolateUrl(
-      urls.originSubscriptions,
-      attemptToken ? { documentType, documentNumber, attemptToken } : undefined
-    )
-  },
-  params: {
-    documentNumber: 'text',
-    documentType: 'text',
-    attemptToken: 'text',
-  },
-  ttl: 20,
-  transform(data) {
-    const [{ sku, attributes, pricingStrategies }] = data.products
+const fetch = (key = {}) => {
+  const site = key['arc-site']
+  const { documentType = 'DNI', documentNumber, attemptToken } = key
+  const {
+    paywall: { urls },
+  } = getProperties(site)
+  const url = interpolateUrl(
+    urls.originSubscriptions,
+    attemptToken ? { documentType, documentNumber, attemptToken } : undefined
+  )
+  return request({
+    uri: url,
+    json: true,
+  }).then(data => {
     const {
       campaign: { name: campaignCode },
       subscriber = {},
       error,
+      products: [{ sku, attributes, pricingStrategies }],
     } = data
-    const { printed = undefined } = subscriber;
-
+    const { printed } = subscriber
     const plans = pricingStrategies.map(
       ({ pricingStrategyId, priceCode, description = '', rates }) => {
         const [price] = rates
         const { amount, billingFrequency } = price
-        const _description = description.replace(/<p>|<\/p>/g, '')
+        let parsedDescription = description.replace(/<p>|<\/p>/g, '')
+        try {
+          parsedDescription = JSON.parse(parsedDescription)
+        } catch (err) {
+          parsedDescription = { err: 'is not a object' }
+        }
         return {
           sku,
           name,
           priceCode,
           pricingStrategyId,
           campaignCode,
-          description: parse(_description),
+          description: parsedDescription,
           amount: parseInt(amount, 10),
           billingFrequency,
         }
@@ -66,16 +51,16 @@ export default {
       (prev, { name: _name, value = '' }) => {
         const prez = prev
         const _value = value.replace(/<p>|<\/p>/g, '')
-        switch(_name){
-          case 'feature': 
-          prez[_name].push(_value)
-          break;
+        switch (_name) {
+          case 'feature':
+            prez[_name].push(_value)
+            break
           case 'title':
             prez[_name] = _value
-          break;
-        default :
-        prez[_name] = _value
-        break;
+            break
+          default:
+            prez[_name] = _value
+            break
         }
         return prez
       },
@@ -89,9 +74,24 @@ export default {
         name,
         summary,
         plans,
-        printedSubscriber: printed ? this.document : undefined,
+        printedSubscriber: printed
+          ? { documentType, documentNumber }
+          : undefined,
       },
       error ? { error } : {}
     )
+  })
+}
+
+// https://api-sandbox.gestion.pe
+// paywall-gestion-sandbox
+// gprint-july-19
+export default {
+  fetch,
+  params: {
+    documentNumber: 'text',
+    documentType: 'text',
+    attemptToken: 'text',
   },
+  ttl: 20,
 }
