9171d0159f4cb1838a13092f171f724aaa12aae7
Alvaro <ahuamani@rmgperustaff.com>
2019-10-16 16:15:12 -0500

    [wip]estilos en el separador destacado

--- components/features/separators/featured/default.jsx
+++ components/features/separators/featured/default.jsx
@@ -6,22 +6,23 @@ const SeparatorFeatured = () => {
   return (
     <div className="featured-separator col-3 flex">
       <div className="featured-separator__title-container pr-10">
-        <h2 className="featured-separator__title text-lg line-h-xs mb-5">
+        <h2 className="featured-separator__title text-lg line-h-xs mb-10 font-bold">
           <a className="featured-separator__title-link" href="/">
-            COPA AMERICA <span className="text-sm">2019</span>
+            COPA AMERICA{' '}
+            <span className="featured-separator__subtitle text-sm">2019</span>
           </a>
         </h2>
         <i className="featured-separator__icon icon-marca" />
       </div>
       {aux.map(() => (
         <div className="featured-separator__story flex pl-10 pr-10 border-l-1 border-dashed">
-          <div className="featured-separator__story-content">
-            <h3 className="featured-separator__story-section font-bold text-xl mb-5">
+          <div className="featured-separator__story-content pr-5">
+            <h3 className="featured-separator__story-section font-bold text-lg mb-5 line-h-xs tertiary-font">
               <a className="featured-separator__section-link" href="/">
                 Entrevista
               </a>
             </h3>
-            <h2 className="featured-separator__story-title text-lg">
+            <h2 className="featured-separator__story-title text-md line-h-xs tertiary-font">
               <a className="featured-separator__story-link" href="/">
                 Oblitas: Hay jugadores que pensamos para esta copa america pero
                 desaparecieron
@@ -32,7 +33,7 @@ const SeparatorFeatured = () => {
             <img
               src="https://elcomercio.pe/resizer/op_jRGDDVRp82KUx5oaJKNK5QMM=/314x374/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/43TH7ER2VRHVTKALWZNCYB5IIA.jpg"
               alt="No olvidarrrrr"
-              className="featured-separator__img w-full"
+              className="featured-separator__img w-full object-cover"
             />
           </a>
         </div>
