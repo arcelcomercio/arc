ee3d338efcee9576ad072287e69ddc0ca0871755
Alvaro <ahuamani@rmgperustaff.com>
2019-10-16 17:23:38 -0500

    [wip] agregado content config al feature separador destacado

--- components/features/separators/featured/default.jsx
+++ components/features/separators/featured/default.jsx
@@ -1,10 +1,58 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useContent } from 'fusion:content'
+import { useFusionContext } from 'fusion:context'
 
-const SeparatorFeatured = () => {
+import StoryData from '../../../utilities/story-data'
+
+const SeparatorFeatured = props => {
+  const {
+    customFields: {
+      storyConfig: { contentService = '', contentConfigValues = {} } = {},
+    } = {},
+  } = props
+
+  const { arcSite, contextPath, deployment } = useFusionContext()
+
+  const { content_elements: contentElements = [] } =
+    useContent({
+      source: contentService,
+      query: contentConfigValues,
+      // filter: schemaFilter(arcSite),
+    }) || {}
+
+  const storyData = new StoryData({
+    arcSite,
+    contextPath,
+    deployment,
+    defaultImgSize: 'sm',
+  })
+
+  const stories = contentElements.map(story => {
+    storyData._data = story
+    const {
+      title,
+      websiteLink,
+      primarySection,
+      primarySectionLink,
+      multimediaPortraitXS,
+      multimediaType,
+    } = storyData
+    return {
+      title,
+      websiteLink,
+      primarySection,
+      primarySectionLink,
+      multimediaPortraitXS,
+      multimediaType,
+    }
+  })
+
+  console.log('contentElements->', contentElements, 'stories->', stories)
+
   const aux = [1, 2, 3, 4]
   return (
-    <div className="featured-separator col-3 flex">
+    <div className="featured-separator col-3 flex p-10">
       <div className="featured-separator__title-container pr-10">
         <h2 className="featured-separator__title text-lg line-h-xs mb-10 font-bold">
           <a className="featured-separator__title-link" href="/">
@@ -14,30 +62,35 @@ const SeparatorFeatured = () => {
         </h2>
         <i className="featured-separator__icon icon-marca" />
       </div>
-      {aux.map(() => (
-        <div className="featured-separator__story flex pl-10 pr-10 border-l-1 border-dashed">
-          <div className="featured-separator__story-content pr-5">
-            <h3 className="featured-separator__story-section font-bold text-lg mb-5 line-h-xs tertiary-font">
-              <a className="featured-separator__section-link" href="/">
-                Entrevista
-              </a>
-            </h3>
-            <h2 className="featured-separator__story-title text-md line-h-xs tertiary-font">
-              <a className="featured-separator__story-link" href="/">
-                Oblitas: Hay jugadores que pensamos para esta copa america pero
-                desaparecieron
-              </a>
-            </h2>
+      {aux.map((el, i) => {
+        return (
+          <div
+            className={`featured-separator__story flex border-l-1 border-dashed pl-10${
+              i === aux.length - 1 ? '' : ' pr-10'
+            }`}>
+            <div className="featured-separator__story-content pr-5">
+              <h3 className="featured-separator__story-section font-bold text-lg mb-5 line-h-xs tertiary-font">
+                <a className="featured-separator__section-link" href="/">
+                  Entrevista
+                </a>
+              </h3>
+              <h2 className="featured-separator__story-title text-md line-h-xs tertiary-font">
+                <a className="featured-separator__story-link" href="/">
+                  Oblitas: Hay jugadores que pensamos para esta copa america
+                  pero desaparecieron
+                </a>
+              </h2>
+            </div>
+            <a className="featured-separator__img-link block" href="/">
+              <img
+                src="https://elcomercio.pe/resizer/op_jRGDDVRp82KUx5oaJKNK5QMM=/314x374/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/43TH7ER2VRHVTKALWZNCYB5IIA.jpg"
+                alt="No olvidarrrrr"
+                className="featured-separator__img w-full object-cover"
+              />
+            </a>
           </div>
-          <a className="featured-separator__img-link block" href="/">
-            <img
-              src="https://elcomercio.pe/resizer/op_jRGDDVRp82KUx5oaJKNK5QMM=/314x374/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/43TH7ER2VRHVTKALWZNCYB5IIA.jpg"
-              alt="No olvidarrrrr"
-              className="featured-separator__img w-full object-cover"
-            />
-          </a>
-        </div>
-      ))}
+        )
+      })}
     </div>
   )
 }
