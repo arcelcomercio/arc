02afc2a266ffdef9d417f3fffe50112d842c0025
Alvaro <ahuamani@rmgperustaff.com>
2019-10-17 12:41:50 -0500

    [update] agregado responsive al feature separador destacado

--- components/features/separators/featured/default.jsx
+++ components/features/separators/featured/default.jsx
@@ -17,7 +17,7 @@ const SeparatorFeatured = props => {
   const { content_elements: contentElements = [] } =
     useContent({
       source: contentService,
-      query: contentConfigValues,
+      query: Object.assign(contentConfigValues, { size: 4, stories_qty: 4 }),
       // filter: schemaFilter(arcSite),
     }) || {}
 
@@ -56,47 +56,61 @@ const SeparatorFeatured = props => {
     stories
   )
 
-  const aux = [1, 2, 3, 4]
   return (
     <div className="featured-separator col-3 flex p-10">
       <div className="featured-separator__title-container pr-10">
         <h2 className="featured-separator__title text-lg line-h-xs mb-10 font-bold">
           <a className="featured-separator__title-link" href="/">
-            COPA AMERICA{' '}
-            <span className="featured-separator__subtitle text-sm">2019</span>
+            COPA AMERICA
           </a>
+          <span className="featured-separator__subtitle text-sm block">
+            2019
+          </span>
         </h2>
-        <i className="featured-separator__icon icon-marca" />
+        <i className="featured-separator__icon icon-marca bg-white p-5 rounded" />
       </div>
-      {aux.map((el, i) => {
-        return (
-          <div
-            className={`featured-separator__story flex border-l-1 border-dashed pl-10${
-              i === aux.length - 1 ? '' : ' pr-10'
-            }`}>
-            <div className="featured-separator__story-content pr-5">
-              <h3 className="featured-separator__story-section font-bold text-lg mb-5 line-h-xs tertiary-font">
-                <a className="featured-separator__section-link" href="/">
-                  Entrevista
-                </a>
-              </h3>
-              <h2 className="featured-separator__story-title text-md line-h-xs tertiary-font">
-                <a className="featured-separator__story-link" href="/">
-                  Oblitas: Hay jugadores que pensamos para esta copa america
-                  pero desaparecieron
-                </a>
-              </h2>
+      {stories.map(
+        ({
+          title,
+          websiteLink,
+          primarySection,
+          primarySectionLink,
+          multimediaPortraitS,
+          // multimediaType,
+        }) => {
+          return (
+            <div className="featured-separator__story flex flex-1 border-l-1 border-dashed pl-10 pr-10">
+              <div className="featured-separator__story-content pr-5">
+                <h3 className="featured-separator__story-section font-bold text-lg mb-5 line-h-xs tertiary-font">
+                  <a
+                    className="featured-separator__section-link"
+                    href={primarySectionLink}>
+                    {primarySection}
+                  </a>
+                </h3>
+                <h2
+                  className="featured-separator__story-title text-md line-h-xs tertiary-font overflow-hidden"
+                  title={title}>
+                  <a
+                    className="featured-separator__story-link"
+                    href={websiteLink}>
+                    {title}
+                  </a>
+                </h2>
+              </div>
+              <a
+                className="featured-separator__img-link block"
+                href={websiteLink}>
+                <img
+                  src={multimediaPortraitS}
+                  alt={title}
+                  className="featured-separator__img w-full object-cover"
+                />
+              </a>
             </div>
-            <a className="featured-separator__img-link block" href="/">
-              <img
-                src="https://elcomercio.pe/resizer/op_jRGDDVRp82KUx5oaJKNK5QMM=/314x374/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/43TH7ER2VRHVTKALWZNCYB5IIA.jpg"
-                alt="No olvidarrrrr"
-                className="featured-separator__img w-full object-cover"
-              />
-            </a>
-          </div>
-        )
-      })}
+          )
+        }
+      )}
     </div>
   )
 }
@@ -107,6 +121,8 @@ SeparatorFeatured.propTypes = {
   customFields: PropTypes.shape({
     storyConfig: PropTypes.contentConfig('stories').isRequired.tag({
       name: 'Configuración del contenido',
+      description:
+        'Este feature siempre mostrará 4 noticias, no se toma en cuenta el campo "cantidad a mostrar"',
     }),
   }),
 }
