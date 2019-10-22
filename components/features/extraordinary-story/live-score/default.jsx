26ccac983cd43ba9d18870154bc4442b5cc4cf14
Alvaro <ahuamani@rmgperustaff.com>
2019-10-22 17:13:17 -0500

    [update] agregado configuración de campos en el feature apertura en vivo

--- components/features/extraordinary-story/live-score/default.jsx
+++ components/features/extraordinary-story/live-score/default.jsx
@@ -6,18 +6,29 @@ import PropTypes from 'prop-types'
 import StoryData from '../../../utilities/story-data'
 import schemaFilter from './_dependencies/schema-filter'
 
+import { getPhotoId } from '../../../utilities/helpers'
+
+const PHOTO_SOURCE = 'photo-by-id'
+
+const PHOTO_SCHEMA = `{
+  resized_urls { 
+    landscape_l   
+  }
+}`
+
 @Consumer
 class ExtraordinaryStoryLifeScore extends Component {
   constructor(props) {
     super(props)
     this.state = {
       results: {},
-      count: 1,
+      count: 0,
     }
 
     const {
       customFields: {
         storyConfig: { contentService = '', contentConfigValues = {} } = {},
+        imgField,
       } = {},
       arcSite,
       contextPath,
@@ -46,26 +57,45 @@ class ExtraordinaryStoryLifeScore extends Component {
         },
       },
     })
+    if (imgField) {
+      const photoId = getPhotoId(imgField)
+      if (photoId) {
+        this.fetchContent({
+          customPhoto: {
+            source: PHOTO_SOURCE,
+            query: {
+              _id: photoId,
+            },
+            filter: PHOTO_SCHEMA,
+          },
+        })
+      }
+    }
   }
 
   componentDidMount() {
+    this.setState({ count: Date.now() })
     const { customFields: { codeField } = {} } = this.props
     if (codeField) {
-      this.fetch()
+      this.fetch(Date.now())
       setInterval(() => {
         this.fetch()
       }, 5000)
     }
   }
 
-  fetch() {
+  fetch(first) {
     const { count } = this.state
     const { customFields: { codeField = '' } = {} } = this.props
 
-    fetch(`https://w.ecodigital.pe/data/depor/${codeField}_xalok.js?_=${count}`)
+    const actualCount = first || count
+
+    fetch(
+      `https://w.ecodigital.pe/data/depor/${codeField}_xalok.js?_=${actualCount}`
+    )
       .then(res => res.json())
       .then(res => {
-        this.setState({ results: res, count: count + 1 })
+        this.setState({ results: res, count: actualCount + 1 })
       })
       .catch(err => console.error(err))
   }
@@ -74,9 +104,18 @@ class ExtraordinaryStoryLifeScore extends Component {
     const {
       results: { data: { equipos: teams = [] } = {} } = {},
       story: { title, subTitle, multimediaLandscapeL, websiteLink } = {},
+      customPhoto: { resized_urls: { landscape_l: landscapeL } = {} } = {},
     } = this.state || {}
-
-    const { customFields: { codeField = '' } = {} } = this.props
+    const {
+      customFields: {
+        codeField = '',
+        titleField = '',
+        subTitleField = '',
+        isLive,
+        imgField,
+      } = {},
+      editableField,
+    } = this.props
 
     const [firstTeam = {}, secondTeam = {}] = teams
 
@@ -88,7 +127,7 @@ class ExtraordinaryStoryLifeScore extends Component {
           <picture className="extraordinary-l-score__picture">
             <img
               className="extraordinary-l-score__img w-full object-cover"
-              src={multimediaLandscapeL}
+              src={landscapeL || imgField || multimediaLandscapeL}
               alt={title}
             />
           </picture>
@@ -109,14 +148,25 @@ class ExtraordinaryStoryLifeScore extends Component {
             </a>
           )}
           <h1 className="extraordinary-l-score__title mb-15">
+            {isLive && (
+              <div className="extraordinary-l-score__live text-white inline-block mr-10">
+                <span className="extraordinary-l-score__live-icon inline-block rounded mr-5" />
+                EN VIVO
+              </div>
+            )}
             <a
               href={websiteLink}
-              className="extraordinary-l-score__title-link text-white title-md font-bold line-h-xs">
-              {title}
+              className="extraordinary-l-score__title-link text-white title-md font-bold line-h-xs"
+              {...editableField('titleField')}
+              suppressContentEditableWarning>
+              {titleField || title}
             </a>
           </h1>
-          <p className="extraordinary-l-score__subtitle text-white hidden md:block text-lg line-h-sm mb-20">
-            {subTitle}
+          <p
+            className="extraordinary-l-score__subtitle text-white hidden md:block text-lg line-h-sm mb-20"
+            {...editableField('subTitleField')}
+            suppressContentEditableWarning>
+            {subTitleField || subTitle}
           </p>
         </div>
       </div>
@@ -133,6 +183,24 @@ ExtraordinaryStoryLifeScore.propTypes = {
     }),
     storyConfig: PropTypes.contentConfig('story').isRequired.tag({
       name: 'Configuración del contenido',
+    }),
+    isLive: PropTypes.bool.tag({
+      name: 'En vivo',
+    }),
+    titleField: PropTypes.string.tag({
+      name: 'Título',
+      group: 'Editar campos',
+      description: 'Dejar vacío para tomar el valor original de la historia.',
+    }),
+    subTitleField: PropTypes.string.tag({
+      name: 'Bajada',
+      group: 'Editar campos',
+      description: 'Dejar vacío para tomar el valor original de la historia.',
+    }),
+    imgField: PropTypes.string.tag({
+      name: 'Imagen',
+      group: 'Editar campos',
+      description: 'Dejar vacío para tomar el valor original de la historia.',
     }),
   }),
 }
