c5d705f6be277852ccca4d1838044aca8b541d6d
Cajotafer <hello@cajotafer.com>
2019-10-23 12:28:01 -0500

    [UPDATE] - Documentada la funcion defaultImage y eliminado el origen dinamico para la ruta.

--- components/utilities/helpers.js
+++ components/utilities/helpers.js
@@ -369,12 +369,24 @@ export const addParamToEndPath = (path, param) => {
   return addParam(path, param)
 }
 
+/**
+ * @param {object} objeto Propiedades necesarias para armar la URL de la imagen por defecto.
+ * @param {function} objeto.deployment Agrega un parámetro al final de la cadena 
+ * con la versión de deployment. Viene desde Fusion.
+ * @param {string} objeto.contextPath Normalmente /pf/. Viene desde fusion.
+ * @param {string} objeto.arcSite Identificador del sitio actual. Viene desde fusion.
+ * @param {string} [objeto.size=lg] Tamaño de la imagen por defecto. Hay tres opciones 
+ * 'sm', 'md' y 'lg'. Definido manualmente.
+ * 
+ * @returns {string} URL de la imagen por defecto desde /resources/dist/...
+ */
 export const defaultImage = ({
   deployment,
   contextPath,
   arcSite,
   size = 'lg',
 }) => {
+
   if (size !== 'lg' && size !== 'md' && size !== 'sm') return ''
 
   const site = () => {
@@ -384,18 +396,9 @@ export const defaultImage = ({
     return domain
   }
 
-  const getOrigin = () => {
-    let origin = `https://${site()}`
-
-    if (typeof window !== 'undefined') {
-      // eslint-disable-next-line prefer-destructuring
-      origin = window.location.origin
-    }
-    
-    return origin
-  }
-
-  return deployment(`${getOrigin()}${contextPath}/resources/dist/${arcSite}/images/default-${size}.png`)
+  return deployment(
+    `https://${site()}${contextPath}/resources/dist/${arcSite}/images/default-${size}.png`
+  )
 }
 
 export const createScript = ({ src, async, defer, textContent = '' }) => {
