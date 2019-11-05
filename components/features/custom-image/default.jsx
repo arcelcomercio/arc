e658e2671211c5d774c113373212d10aebdc7ebd
jorge linares <jorge.developer@gmail.com>
2019-11-05 12:03:56 -0500

    componente imagen con enlace cambios

--- components/features/custom-image/default.jsx
+++ components/features/custom-image/default.jsx
@@ -1,45 +1,43 @@
-import React, { PureComponent } from 'react'
-import Consumer from 'fusion:consumer';
+import React from 'react'
 import customFields from './_dependencies/custom-fields'
 
 const classes = { lazy: "lazy" };
 
-@Consumer
-class CustomImage extends PureComponent {
- 
-  render() {
-    const {
-      customFields: {
-        imgUrlDesktop = '',
-        imgUrlMobile = '',
-        imgTitle = '',
-        imgAlt,
-        imgLink = '',
-        imgWidth = 0
-      } = {},
-    } = this.props
+const CustomImage = props => 
+{ 
+  const {
+    customFields: {
+      imgUrlDesktop = '',
+      imgUrlMobile = '',
+      imgTitle = '',
+      imgAlt,
+      imgLink = '',
+      imgWidth = 0
+    } = {},
+  } = props;
 
-    if(imgUrlDesktop === '') 
-      return <div>Modulo imagen, clic en editar para configurar.</div> 
-    const picture = (
-      <picture>
-        { imgUrlMobile !== '' && <source media="(max-width: 650px)" srcSet={imgUrlMobile} className={classes.lazy} /> }
-        <img 
-          src={imgUrlDesktop} 
-          alt={imgAlt}
-          {...imgTitle !== '' ? {title:imgTitle} : {}}
-          {...imgWidth > 0 ? {width:imgWidth} : {}}
-          className={classes.lazy}
-        />
-      </picture>
-    );
-    
-    if(imgLink !== ''){
-      return (<div><a href={imgLink}>{ picture }</a></div>);
-    }
+  if(imgUrlDesktop === '') 
+    return <div>Modulo imagen, clic en editar para configurar.</div> 
 
-    return <div>{picture}</div>;
+  const picture = (
+    <picture>
+      { imgUrlMobile !== '' && <source media="(max-width: 650px)" srcSet={imgUrlMobile} className={classes.lazy} /> }
+      <img 
+        src={imgUrlDesktop} 
+        alt={imgAlt}
+        {...imgTitle !== '' ? {title:imgTitle} : {}}
+        {...imgWidth > 0 ? {width:imgWidth} : {}}
+        className={classes.lazy}
+      />
+    </picture>
+  );
+  
+  if(imgLink !== ''){
+    return (<div><a href={imgLink}>{ picture }</a></div>);
   }
+
+  return <div>{picture}</div>;
+
 }
 
 CustomImage.propTypes = {
