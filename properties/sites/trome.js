9d7b920590e00b73a281b00b51b985a191b01b96
Cajotafer <hello@cajotafer.com>
2019-11-06 12:57:36 -0500

    [ADD] - Config base para Trome

---
+++ properties/sites/trome.js
@@ -0,0 +1,174 @@
+export default {
+  siteName: 'Trome',
+  sitemapNewsName: 'Trome',
+  newsletterBrand: 'trome',
+  /*   googleFonts: 'Noticia+Text:400,700|Roboto+Condensed:400,700|Roboto:400,700', */
+  colorPrimary: '#E06437',
+  siteDomain: 'trome.pe',
+  siteUrl: 'https://trome.pe',
+  resizerUrl: 'https://elcomercio-trome-prod.cdn.arcpublishing.com/resizer',
+  resizerSecretKeyEnvVar: '',
+  urlPreroll:
+    'https://pubads.g.doubleclick.net/gampad/ads?iu=/28253241/DEPOR_Preroll&description_url=[placeholder]&env=vp&impl=s&correlator=&tfcd=0&npa=0&gdfp_req=1&output=vast&sz=640x360&unviewed_position_start=1',
+  fbAppId: 'fbappidDEEEEEPOR',
+  googleTagManagerId: 'GTM-PFPMXLF',
+  ampGoogleTagManagerId: 'UA-3055636-8',
+  ampGoogleTagManagerName: 'trome',
+  charbeatAccountNumber: 99999,
+  idGoogleAnalitics: 'UA-3055636-8',
+  fbArticleStyle: 'LogoTrome',
+  nameStoryRelated: 'RELACIONADAS',
+  siteDescription:
+    'Noticias de Perú y el mundo en Trome.pe. Noticias de actualidad, política, deportes, gastronomía, economía y espectáculos.',
+  googleNewsImage: 'https://publimetro.pe/f/i/pub_40.png',
+  infoPagesDev: {
+    termsAndConditions: '',
+    guidingPrinciples: '',
+    privacyPolicies: '',
+    integratedManagementPolicy: '',
+    arcoProcedure: '',
+    cookiesPolicy: '',
+    aboutUs: '',
+    frequentQuestions: '',
+  },
+  infoPagesProd: {
+    termsAndConditions: 'GKC4YH3FBZGGJNFEPGO7KMPN7U',
+    guidingPrinciples: '',
+    privacyPolicies: 'NJZXERE3WVG5DB65JP7PGTLCV4',
+    integratedManagementPolicy: '',
+    arcoProcedure: '',
+    cookiesPolicy: '5ORUCK2RHBCH5CUYD4XIAVLQ2E',
+    aboutUs: '',
+    frequentQuestions: '',
+  },
+
+  assets: {
+    seo: {
+      logoAmp: 'logo-amp.png',
+      width: 230,
+      height: 60,
+      widthAmp: 178,
+      heightAmp: 44,
+    },
+    premium: {
+      logo: 'favicon.png',
+    },
+    nav: {
+      logoSomos: 'logo.png',
+    },
+  },
+
+  messages: {
+    errorTitle: '¡Oops! la pagina no fue encontrada en Trome',
+    errorDescription:
+      'La página que buscas no existe, probablemente el enlace que usaste es erróneo, intenta ubicarlo en la página principal o usa el buscador para encontrar la noticia que buscas:',
+  },
+
+  legalLinks: [
+    {
+      name: 'Términos y condiciones',
+      url: '/terminos-servicio',
+    },
+    {
+      name: 'Políticas de Privacidad',
+      url: '/politicas-privacidad',
+    },
+    {
+      name: 'Politicas de Cookies',
+      url: '/politicas-cookies',
+    },
+  ],
+
+  footer: {
+    siteLegal: [
+      'Empresa Editora Trome',
+      'Jr. Santa Rosa #300 Lima 1 Perú',
+      'Copyright © Trome.pe',
+      'Trome - Todos los derechos reservados',
+    ],
+
+    socialNetworks: [
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/tuitdepor',
+      },
+      {
+        name: 'Facebook',
+        url: 'https://www.facebook.com/depor.pe',
+      },
+
+      {
+        name: 'Google+',
+        url: 'https://plus.google.com/u/0/+deporpe',
+      },
+      {
+        name: 'Pinterest',
+        url: 'https://www.pinterest.com/deporpe',
+      },
+      {
+        name: 'RSS ',
+        url: 'https://depor.com/arcio/rss',
+      },
+    ],
+
+    contacts: [
+      {
+        position: 'Director Periodístico General de Deporte',
+        name: 'Guillermo Denegri',
+      },
+      {
+        position: 'Director Periodístico',
+        name: 'Walter Corzo.',
+      },
+    ],
+    corporateInfo: {
+      name: 'Grupo El Comercio',
+      direction: 'Jr. Santa Rosa #300 Lima 1, Perú',
+    },
+    draftingContact: [
+      {
+        name: 'Redacción',
+        value: '311-6500(2858) trome@trome.pe',
+      },
+      {
+        name: 'Publicidad Web',
+        value: 'Fonoavisos@comercio.com.pe',
+      },
+    ],
+    copyrightText:
+      'Copyright© Trome.pe - Grupo El Comercio - Todos los derechos reservados.',
+  },
+  social: {
+    facebook: {
+      name: 'facebook',
+      user: '@depor.com',
+      url: 'https://www.facebook.com/depor.com',
+    },
+    twitter: {
+      name: 'twitter',
+      user: 'tuitdepor',
+      url: 'https://twitter.com/tuitdepor',
+    },
+    youtube: {
+      name: 'youtube',
+      url: 'https://plus.google.com/u/0/+deporcom',
+    },
+  },
+  adsAmp: {
+    dataSlot: 28253241,
+    movil1: 15011765,
+    movil2: 15011768,
+    movil3: 15011769,
+    movil4: 15011770,
+    movil5: 15011771,
+  },
+  listUrlAdvertisings: [
+    'https://d1r08wok4169a5.cloudfront.net/ads-depor/ads-fia-28253241-dep_ia_interna1-300x250-div-gpt-ad-8599377-11.html',
+    'https://d1r08wok4169a5.cloudfront.net/ads-depor/ads-fia-28253241-dep_ia_interna2-300x250-div-gpt-ad-8599377-12.html',
+    'https://d1r08wok4169a5.cloudfront.net/ads-depor/ads-fia-28253241-dep_ia_interna3-300x250-div-gpt-ad-8599377-13.html',
+    'https://d1r08wok4169a5.cloudfront.net/ads-depor/ads-fia-28253241-dep_ia_interna4-300x250-div-gpt-ad-8599377-14.html',
+    'https://d1r08wok4169a5.cloudfront.net/ads-depor/ads-fia-28253241-dep_ia_interna5-300x250-div-gpt-ad-8599377-15.html',
+  ],
+  activeSignwall: true,
+  activePaywall: false,
+}
