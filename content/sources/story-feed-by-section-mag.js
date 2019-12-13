7ebc1e66ab01b855feb3f07dcc2cae7df099939d
Cajotafer <hello@cajotafer.com>
2019-12-13 16:06:09 -0500

    [UPDATE] - Mejor filtrado en content sources para - tiempo de respuesta

--- content/sources/story-feed-by-section-mag.js
+++ content/sources/story-feed-by-section-mag.js
@@ -1,9 +1,7 @@
-import {  CONTENT_BASE } from 'fusion:environment'
-
+import { CONTENT_BASE } from 'fusion:environment'
 
 const resolve = () => {
-
-const requestUri = `${CONTENT_BASE}/content/v4/search/published?sort=publish_date:desc&from=0&size=50&website=elcomerciomag&q`
+  const requestUri = `${CONTENT_BASE}/content/v4/search/published?q=canonical_website:elcomerciomag+AND+publish_date:%7Bnow-7d%20TO%20*%7D+AND+type:story+AND+revision.published:true&sort=publish_date:desc&from=0&size=50&website=elcomerciomag`
 
   return requestUri
 }
--- content/sources/story-feed-by-website.js
+++ content/sources/story-feed-by-website.js
@@ -4,45 +4,45 @@ import getProperties from 'fusion:properties'
 import { addResizedUrlsToStory } from '../../components/utilities/helpers'
 
 const params = [
-    {
-        name: 'website',
-        displayName: 'Sitio web',
-        type: 'text',
-    },
-    {
-        name: 'stories_qty',
-        displayName: 'Cantidad de historias',
-        type: 'number',
-    },
+  {
+    name: 'website',
+    displayName: 'Sitio web',
+    type: 'text',
+  },
+  {
+    name: 'stories_qty',
+    displayName: 'Cantidad de historias',
+    type: 'number',
+  },
 ]
 
 const resolve = ({ website, stories_qty: storiesQty }) => {
+  if (!website) throw new Error('Website no ha sido definido')
 
-    if (!website) throw new Error('Website no ha sido definido')
+  const sourceExclude = `&_sourceExclude=owner,address,workflow,content_elements,type,revision,language,source,distributor,planning,additional_properties,publishing,related_content`
 
-    const sourceExclude = `&_sourceExclude=owner,address,workflow,content_elements,type,revision,language,source,distributor,planning,additional_properties,publishing,related_content`
+  const requestUri = `/content/v4/search/published?sort=publish_date:desc&from=0&size=${storiesQty ||
+    50}&website=${website}${sourceExclude}&q=type:story+AND+revision.published:true+AND+canonical_website:${website}+AND+publish_date:%7Bnow-15d%20TO%20*%7D`
 
-    const requestUri = `/content/v4/search/published?sort=publish_date:desc&from=0&size=${storiesQty || 50}&website=${website}${sourceExclude}&q=type:story+AND+revision.published:true`
-
-    return requestUri
+  return requestUri
 }
 
 const transform = (data, key) => {
-    const website = key['arc-site'] || 'Arc Site no está definido'
-    const dataStories = data
-    const { resizerUrl } = getProperties(website)
-    dataStories.content_elements = addResizedUrlsToStory(
-        dataStories.content_elements,
-        resizerUrl,
-        resizerSecret,
-        addResizedUrls
-    )
-    return dataStories
+  const website = key['arc-site'] || 'Arc Site no está definido'
+  const dataStories = data
+  const { resizerUrl } = getProperties(website)
+  dataStories.content_elements = addResizedUrlsToStory(
+    dataStories.content_elements,
+    resizerUrl,
+    resizerSecret,
+    addResizedUrls
+  )
+  return dataStories
 }
 
 export default {
-    resolve,
-    transform,
-    params,
-    ttl: 120,
+  resolve,
+  transform,
+  params,
+  ttl: 120,
 }
