79e6c0a218b7690fa9c7b473ad806f8e2a63dbd9
Cajotafer <hello@cajotafer.com>
2019-12-19 15:57:55 -0500

    [ADD] - utilities/included-fields

---
+++ components/utilities/included-fields.js
@@ -0,0 +1,3 @@
+export const promoItems = `promo_items.basic.type,promo_items.basic.url,promo_items.basic.resized_urls,promo_items.basic_video.promo_items.basic.url,promo_items.basic_video.promo_items.basic.resized_urls,promo_items.basic_gallery.promo_items.basic.url,promo_items.basic_gallery.promo_items.basic.resized_urls,promo_items.youtube_id.content`
+
+export const primarySection = `taxonomy.primary_section.path,taxonomy.primary_section.name`
