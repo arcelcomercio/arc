493c349c1e3e5796d425fb2a5590c57e118f85bb
Cajotafer <hello@cajotafer.com>
2019-12-19 17:52:04 -0500

    [UPDATE] - featured-story-list + presets + includedFields

--- components/utilities/included-fields.js
+++ components/utilities/included-fields.js
@@ -1,3 +1,7 @@
 export const includePromoItems = `promo_items.basic.type,promo_items.basic.url,promo_items.basic.resized_urls,promo_items.basic_video.promo_items.basic.url,promo_items.basic_video.promo_items.basic.resized_urls,promo_items.basic_gallery.promo_items.basic.url,promo_items.basic_gallery.promo_items.basic.resized_urls,promo_items.youtube_id.content`
 
+export const includePromoItemsCaptions = `promo_items.basic.subtitle,promo_items.basic.caption,promo_items.basic_video.promo_items.basic.subtitle,promo_items.basic_video.promo_items.basic.caption,promo_items.basic_gallery.promo_items.basic.subtitle,promo_items.basic_gallery.promo_items.basic.caption`
+
 export const includePrimarySection = `taxonomy.primary_section.path,taxonomy.primary_section.name`
+
+export const includeSections = `taxonomy.sections.path,taxonomy.sections.name`
