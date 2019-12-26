bcc295c3e37db76bbb838e8123681219601947e6
Cajotafer <hello@cajotafer.com>
2019-12-26 18:09:45 -0500

    [UPDATE] - tv/featured + presets + includedFields

--- components/features/tv/featured/_dependencies/schema-filter.js
+++ components/features/tv/featured/_dependencies/schema-filter.js
@@ -1,40 +1,38 @@
 export default `
   {
-    content_elements {
-      promo_items {
-        basic { 
-          url 
-          type 
-          resized_urls { 
-            preset1
-            preset2
-            preset3
-          } 
-        }
-        basic_video {
-          _id
-          promo_items {
-            basic { 
-              url 
-              type 
-              resized_urls { 
-                preset1
-                preset2
-                preset3
-              } 
-            }
+    promo_items {
+      basic { 
+        url 
+        type 
+        resized_urls { 
+          preset1
+          preset2
+          preset3
+        } 
+      }
+      basic_video {
+        _id
+        promo_items {
+          basic { 
+            url 
+            type 
+            resized_urls { 
+              preset1
+              preset2
+              preset3
+            } 
           }
         }
-        youtube_id {
-          content
-        }
-        basic_html {
-          content
-        }
       }
-      headlines { basic }
-      display_date
+      youtube_id {
+        content
+      }
+      basic_html {
+        content
+      }
     }
+    headlines { basic }
+    display_date
     section_name
   }
   `
