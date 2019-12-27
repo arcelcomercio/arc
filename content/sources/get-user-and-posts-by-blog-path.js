725b70b7f8e028b9b6a6f2683ee47785b889c931
Cajotafer <hello@cajotafer.com>
2019-12-27 16:50:34 -0500

    [FIX] - /blog - si la pagina es < 0 redirecciona al blog

--- content/sources/get-user-and-posts-by-blog-path.js
+++ content/sources/get-user-and-posts-by-blog-path.js
@@ -24,21 +24,29 @@ const resolve = key => {
   const blogPath = key.blog_path
   const postsLimit = key.posts_limit || 16
   const postsOffset = key.posts_offset || 0
-  const pagination = postsOffset > 0 ? postsLimit * (postsOffset - 1) : 0
+  const pagination =
+    postsOffset > 0 ? postsLimit * (postsOffset - 1) : postsOffset
 
   if (!blogPath) throw new 'El Blog esta vacio, o no existe'()
 
   const website = key['arc-site']
   const {
     api: { blog: urlApiblog = '' },
+    siteUrl,
   } = getProperties(website)
 
-  return `${urlApiblog}?json=get_user_and_posts_by_blog_path&blog_path=${blogPath}&posts_limit=${postsLimit}&posts_offset=${pagination}&token=${process
-    .env.TOKEN_BLOG || BLOG_TOKEN}`
+  if (postsOffset < 0)
+    throw new RedirectError(`${siteUrl}/blog/${blogPath}/`, 301)
+
+  return `${urlApiblog}?json=get_user_and_posts_by_blog_path&blog_path=${blogPath}&posts_limit=${postsLimit}&posts_offset=${pagination}&token=xcvbn$987HUNJ$765`
 }
 
 const transform = (data, { 'arc-site': arcSite }) => {
-  if (!data || (data && data.status !== 'ok' && data.status !== 200)) {
+  if (
+    !data ||
+    (data && data.status !== 'ok' && data.status !== 200) ||
+    (data && data.posts && !data.posts.length > 0)
+  ) {
     const { siteUrl } = getProperties(arcSite)
     throw new RedirectError(`${siteUrl}/blog/`, 301)
   }
