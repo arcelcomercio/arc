93432302beefb0503f98cae9921ddaed75e085c2
Alvaro <ahuamani@rmgperustaff.com>
2019-12-27 09:47:59 -0500

    Agregado url de prod para los espacios de dfp

--- content/sources/get-dfp-spaces.js
+++ content/sources/get-dfp-spaces.js
@@ -1,4 +1,9 @@
-const devUrl = 'https://d37z8six7qdyn4.cloudfront.net'
+import ENV from 'fusion:environment'
+
+const devUrl =
+  ENV.ENVIRONMENT === 'elcomercio'
+    ? 'https://d2dvq461rdwooi.cloudfront.net'
+    : 'https://d37z8six7qdyn4.cloudfront.net'
 
 const resolve = ({ 'arc-site': website, page, sectionSlug }) => {
   if (!website) throw new Error('Arcsite no declarado')
