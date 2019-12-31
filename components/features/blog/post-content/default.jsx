5511405fa7b738fe6dd24ee57922e6b768dd2450
Cajotafer <hello@cajotafer.com>
2019-12-31 18:05:38 -0500

    [UPDATE] - blog/post-content -> Hooks

--- components/features/blog/post-content/default.jsx
+++ components/features/blog/post-content/default.jsx
@@ -1,5 +1,6 @@
-import Consumer from 'fusion:consumer'
-import React, { PureComponent } from 'react'
+import React from 'react'
+import { useFusionContext } from 'fusion:context'
+
 import renderHTML from 'react-render-html'
 import { getFullDateIso8601 } from '../../../utilities/helpers'
 
@@ -11,34 +12,31 @@ const classes = {
   story: 'post-content__story full primary-font mb-30 title-sm line-h-sm',
 }
 
-@Consumer
-class BlogPostContent extends PureComponent {
-  render() {
-    const { globalContent } = this.props
-    const {
-      post: { post_content: postContent, post_date: postDate } = {},
-      user: { first_name: firstName } = {},
-    } = globalContent || {}
-    const formatDate = getFullDateIso8601(postDate)
-    const { day, month, fullYear, hours, minutes } = formatDate || {}
+const BlogPostContent = () => {
+  const { globalContent } = useFusionContext()
+  const {
+    post: { post_content: postContent, post_date: postDate } = {},
+    user: { first_name: firstName } = {},
+  } = globalContent || {}
+  const formatDate = getFullDateIso8601(postDate)
+  const { day, month, fullYear, hours, minutes } = formatDate || {}
 
-    return (
-      <div className={classes.content}>
-        <div className={classes.header}>
-          <span className={classes.author}>{firstName}</span>{' '}
-          <time className={classes.date}>
-            {formatDate && `${day}/${month}/${fullYear} ${hours}:${minutes}`}
-          </time>
-        </div>
-        <div className={classes.story} id="contenedor">
-          <div id="ads_d_inline" />
-          <div id="ads_m_movil_video" />
-          <div id="ads_m_movil3" />
-          <section>{postContent && renderHTML(postContent)}</section>
-        </div>
+  return (
+    <div className={classes.content}>
+      <div className={classes.header}>
+        <span className={classes.author}>{firstName}</span>{' '}
+        <time className={classes.date}>
+          {formatDate && `${day}/${month}/${fullYear} ${hours}:${minutes}`}
+        </time>
       </div>
-    )
-  }
+      <div className={classes.story} id="contenedor">
+        <div id="ads_d_inline" />
+        <div id="ads_m_movil_video" />
+        <div id="ads_m_movil3" />
+        <section>{postContent && renderHTML(postContent)}</section>
+      </div>
+    </div>
+  )
 }
 
 BlogPostContent.label = 'Blog - Contenido del post'
