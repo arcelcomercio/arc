feed189d003f4d50cf05d28769b72e6ff8cb39a4
Cajotafer <hello@cajotafer.com>
2020-01-02 23:00:08 -0500

    [UPDATE] - stories-lists/section-stories -> Hooks

--- components/features/stories-lists/section-stories/default.jsx
+++ components/features/stories-lists/section-stories/default.jsx
@@ -1,5 +1,6 @@
-import React, { PureComponent, Fragment } from 'react'
-import Consumer from 'fusion:consumer'
+import React, { Fragment } from 'react'
+import { useFusionContext } from 'fusion:context'
+import getProperties from 'fusion:properties'
 
 import customFields from './_dependencies/custom-fields'
 import StoryItem from '../../../global-components/story-item'
@@ -11,83 +12,78 @@ const classes = {
   adsBox: 'flex items-center flex-col no-desktop pb-20',
 }
 
-@Consumer
-class StoriesListSectionStories extends PureComponent {
-  hasAds = (index, adsList) => adsList.filter(el => el.pos === index)
+const StoriesListSectionStories = props => {
+  const hasAds = (index, adsList) => adsList.filter(el => el.pos === index)
 
-  render() {
-    const {
-      globalContent,
-      deployment,
-      contextPath,
-      arcSite,
-      globalContentConfig,
-      isAdmin,
-      customFields: customFieldsProps = {},
-      siteProperties: { isDfp = false },
-      metaValue,
-    } = this.props
+  const { customFields: customFieldsProps = {} } = props
+  const {
+    globalContent,
+    deployment,
+    contextPath,
+    arcSite,
+    globalContentConfig,
+    isAdmin,
+    metaValue,
+  } = useFusionContext()
+  const { isDfp = false } = getProperties(arcSite)
+  const { storiesQty = 50, initialStory = 0 } = customFieldsProps
+  const { query: { section = '' } = {} } = globalContentConfig || {}
+  const { content_elements: contentElements } = globalContent || {}
+  const stories = contentElements || []
 
-    const { storiesQty = 50, initialStory = 0 } = customFieldsProps
+  const activeAds = Object.keys(customFieldsProps)
+    .filter(prop => prop.match(/adsMobile(\d)/))
+    .filter(key => customFieldsProps[key] === true)
 
-    const { query: { section = '' } = {} } = globalContentConfig || {}
-    const { content_elements: contentElements } = globalContent || {}
-    const stories = contentElements || []
+  const typeSpace = isDfp ? 'caja' : 'movil'
 
-    const activeAds = Object.keys(customFieldsProps)
-      .filter(prop => prop.match(/adsMobile(\d)/))
-      .filter(key => customFieldsProps[key] === true)
+  const activeAdsArray = activeAds.map(el => {
+    return {
+      name: `${typeSpace}${el.slice(-1)}`,
+      pos: customFieldsProps[`adsMobilePosition${el.slice(-1)}`] || 0,
+      inserted: false,
+    }
+  })
 
-    const typeSpace = isDfp ? 'caja' : 'movil'
+  // Archivo sólo está disponible para secciones principales, no subsecciones.
+  const seeMorePath = `/archivo/${section.split('/')[1]}/`
 
-    const activeAdsArray = activeAds.map(el => {
-      return {
-        name: `${typeSpace}${el.slice(-1)}`,
-        pos: customFieldsProps[`adsMobilePosition${el.slice(-1)}`] || 0,
-        inserted: false,
-      }
-    })
-
-    // Archivo sólo está disponible para secciones principales, no subsecciones.
-    const seeMorePath = `/archivo/${section.split('/')[1]}/`
-
-    return (
-      <div className={classes.listado}>
-        <div>
-          {stories &&
-            stories
-              .slice(initialStory, initialStory + storiesQty)
-              .map((story, index) => {
-                const ads = this.hasAds(index + 1, activeAdsArray)
-                return (
-                  <Fragment key={`Section-storie-${story._id}`}>
-                    <StoryItem
-                      data={story}
-                      {...{ deployment, contextPath, arcSite, isAdmin }}
-                      formato="row"
-                    />
-                    {ads.length > 0 && (
-                      <div className={classes.adsBox}>
-                        <Ads
-                          adElement={ads[0].name}
-                          isDesktop={false}
-                          isMobile
-                          isDfp={isDfp}
-                        />
-                      </div>
-                    )}
-                  </Fragment>
-                )
-              })}
-        </div>
-        <div className={classes.listadoSeeMore}>
-          <a href={seeMorePath} tabIndex="0" role="button">
-            Ver más
-          </a>
-        </div>
+  return (
+    <div className={classes.listado}>
+      <div>
+        {stories &&
+          stories
+            .slice(initialStory, initialStory + storiesQty)
+            .map((story, index) => {
+              const ads = hasAds(index + 1, activeAdsArray)
+              return (
+                <Fragment key={`Section-storie-${story._id}`}>
+                  <StoryItem
+                    data={story}
+                    {...{ deployment, contextPath, arcSite, isAdmin }}
+                    formato="row"
+                  />
+                  {ads.length > 0 && (
+                    <div className={classes.adsBox}>
+                      <Ads
+                        adElement={ads[0].name}
+                        isDesktop={false}
+                        isMobile
+                        isDfp={isDfp}
+                      />
+                    </div>
+                  )}
+                </Fragment>
+              )
+            })}
       </div>
-    )
-  }
+      <div className={classes.listadoSeeMore}>
+        <a href={seeMorePath} tabIndex="0" role="button">
+          Ver más
+        </a>
+      </div>
+    </div>
+  )
 }
 
 StoriesListSectionStories.propTypes = {
