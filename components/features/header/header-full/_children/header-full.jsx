30d1d6f1efb6331a1a1d8112e77fd84413abc642
Rolly <rolly.sanchez.26@gmail.com>
2020-01-03 16:44:13 -0500

    Agregando sidebar menu depor y trome

--- components/features/header/header-full/_children/header-full.jsx
+++ components/features/header/header-full/_children/header-full.jsx
@@ -73,6 +73,16 @@ const classes = {
   shareIcon: 'story-header__icon',
   iconMore: 'story-header__share-icon icon-share text-white',
   navLoader: 'nav__loader-bar position-absolute h-full left-0',
+
+  body: 'nav-sidebar__body pt-15 pr-0 pb-15 pl-0',
+  list: 'nav- sidebar__list',
+  item:
+    'nav-sidebar__item position-relative flex justify-between items-center flex-wrap',
+  containerSubMenu: 'nav-sidebar__container-submenu w-full overflow-hidden',
+  menuArrow: 'nav-sidebar__menu-arrow hidden',
+  labelParentItem:
+    'nav-sidebar__parent-item pl-25 pt-10 pr-20 pb-10 position-absolute right-0',
+  link: 'nav-sidebar__link block p-15 pl-25 text-md text-white',
 }
 
 export default ({
@@ -153,6 +163,56 @@ export default ({
     event.preventDefault()
     if (item === 3) moreList()
     else popUpWindow(item.link, '', 600, 400)
+  }
+
+  const renderSections = (sections, deep, nameId = 'root') => {
+    const aux = deep
+    return (
+      sections &&
+      sections.map(
+        ({
+          children,
+          name = '',
+          _id: id = '',
+          display_name: displayName = '',
+          url = '',
+        }) => {
+          const idElem = `${nameId}-${name || displayName}`.toLowerCase()
+          return (
+            <li className={classes.item} key={`navbar-menu-${url || id}`}>
+              <a
+                href={url || id || '/'}
+                className={`${classes.link}${
+                  deep > 0 ? ` pl-${25 + deep * 15}` : ''
+                }`}>
+                {name || displayName}
+              </a>
+              {children && children.length > 0 && (
+                <>
+                  <input
+                    className={classes.menuArrow}
+                    type="checkbox"
+                    id={idElem}
+                    name="checkbox-submenu"
+                  />
+                  {/** TODO: verificar si se puede mejorar, el input debería estar dentro
+                   * del label pero por problemas de estilos para hecer la funcionalidad
+                   * con puro CSS no se encontró forma.
+                   * */}
+                  <label htmlFor={idElem} className={classes.labelParentItem} />
+                  <ul
+                    className={`${
+                      classes.containerSubMenu
+                    } deep-${deep} ${idElem}`}>
+                    {renderSections(children, aux + 1, idElem)}
+                  </ul>
+                </>
+              )}
+            </li>
+          )
+        }
+      )
+    )
   }
 
   return (
@@ -293,47 +353,11 @@ export default ({
             <div className={`${classes.megaMenu} ${showMenu ? 'active' : ''}`}>
               <div className={classes.megaMenuContainer}>
                 <div className={classes.megaMenuBox}>
-                  {menuList.map(item => {
-                    return (
-                      <div className={classes.megaMenuRow}>
-                        <h3>
-                          <a
-                            href={item.url || item._id || '/'}
-                            className={classes.megaMenuTitle}>
-                            {item.name || item.display_name}
-                          </a>
-                        </h3>
-                        <ul className={classes.megaMenuList}>
-                          {item.children.map(subItem => {
-                            let subItemName =
-                              subItem.name || subItem.display_name
-                            const rawMatch = subItemName.match(/\[.*\]/g)
-                            const match =
-                              rawMatch === null
-                                ? ''
-                                : rawMatch[0].replace('[', '').replace(']', '')
-
-                            if (match) {
-                              subItemName = subItemName.replace(/\[.*\]/g, '')
-                            }
-                            return (
-                              <li className={classes.megaMenuItem}>
-                                <a
-                                  href={subItem.url || subItem._id || '/'}
-                                  className={`${classes.megaMenuLink} ${
-                                    match === 'bold'
-                                      ? 'megamenu__title-link font-bold'
-                                      : ''
-                                  }`}>
-                                  {subItemName}
-                                </a>
-                              </li>
-                            )
-                          })}
-                        </ul>
-                      </div>
-                    )
-                  })}
+                  <div className={classes.body}>
+                    <ul className={classes.list}>
+                      {menuList && renderSections(menuList, 0)}
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
@@ -346,7 +370,7 @@ export default ({
                   <div>
                     <a
                       className={classes.moreLink}
-                      href='/'
+                      href="/"
                       onClick={event => {
                         openLink(event, 3)
                       }}>
