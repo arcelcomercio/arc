ac040a8395c0aa0756040d428fd26d3e928c6701
Cajotafer <hello@cajotafer.com>
2020-01-02 21:23:46 -0500

    [ADD] - header/somos NUEVO

---
+++ components/features/header/somos/_dependencies/formatter.js
@@ -0,0 +1,94 @@
+import schemaFilter from './schema-filter'
+import { formatDayMonthYear } from '../../../../utilities/helpers'
+
+export default class SomosHeader {
+  constructor(
+    deployment,
+    contextPath = '',
+    siteDomain = '',
+    headerProperties = {},
+    arcSite = '',
+    data = {},
+    customLogo = '',
+    customLogoLink = '/',
+    tags = '',
+    showDate = false
+  ) {
+    this.deployment = deployment
+    this.contextPath = contextPath
+    this.siteDomain = siteDomain
+    this.headerProperties = headerProperties
+    this.arcSite = arcSite
+    this.data = data
+    this.customLogo = customLogo
+    this.customLogoLink = customLogoLink
+    this.schema = schemaFilter
+    this.tags = tags
+    this.showDate = showDate
+  }
+
+  getSchema() {
+    return this.schema
+  }
+
+  setData(data) {
+    this.data = data
+  }
+
+  getParams() {
+    const { logo } = this.headerProperties
+    return {
+      logo: {
+        src:
+          this.customLogo ||
+          this.deployment(
+            `${this.contextPath}/resources/dist/${this.arcSite}/images/${logo}`
+          ),
+        link: this.customLogoLink,
+        alt: this.siteDomain,
+      },
+      logoIcon: {
+        link: '/',
+      },
+      firstSection: {
+        url: '/somos',
+      },
+      sections: this.formatSections(),
+      date: {
+        active: this.showDate,
+        value: this.getDate(),
+      },
+      tags: this.tags,
+    }
+  }
+
+  // Función para formatear data de las secciones
+  formatSections = () => {
+    const link = 'link'
+    const { children = [] } = this.data || {}
+    return children.map(el => {
+      let name = el.node_type === link ? el.display_name : el.name
+      const rawMatch = name.match(/\[#.*\]/g)
+      const match =
+        rawMatch === null
+          ? ''
+          : rawMatch[0]
+              .replace('[', '')
+              .replace(']', '')
+              .split(',')
+      if (match) {
+        name = name.replace(/\[#.*\]/g, '')
+      }
+      return {
+        name,
+        url: el.node_type === link ? el.url : el._id,
+        styles: match,
+      }
+    })
+  }
+
+  getDate = () => {
+    return formatDayMonthYear(new Date(), false)
+  }
+  // TODO: Crear función para formatear data de secciones con subsecciones
+}
