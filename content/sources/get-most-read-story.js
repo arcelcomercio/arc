364186ae071089abdf3bfce13d7a561bf7cdc893
Luis Bustamante <luis.bustamante@fractal.com.pe>
2020-01-13 15:18:40 -0500

    UPD: Most read Homologated

--- content/sources/get-most-read-story.js
+++ content/sources/get-most-read-story.js
@@ -2,55 +2,17 @@
 import request from 'request-promise-native'
 import { CONTENT_BASE } from 'fusion:environment'
 
-const flagDev = true
+const flagDev = false
 
-/*
-const uriPostProd = site =>
-  site === 'gestion'
-    ? 'https://do5ggs99ulqpl.cloudfront.net/gestion/9043312/top_premium.json'
-    : 'https://do5ggs99ulqpl.cloudfront.net/elcomercio/21928896/top_premium.json'
-
-const uriPostDev = site =>
-  site === 'gestion'
-    ? 'https://d3ocw6unvuy6ob.cloudfront.net/gestion/9043312/top_premium.json'
-    : 'https://d3ocw6unvuy6ob.cloudfront.net/elcomercio/21928896/top_premium.json'
-*/
-/*
-const codPremiumSite = {
-  elcomercio: '21928896',
-  gestion: '9043312',
-}
-
-const codSite = {
-  depor: '16646171',
-  peru21: '9849434',
-  ojo: '31245754',
-  elbocon: '31246731',
-}
-*/
 const getUriMostRead = (site, isPremium = false, isDev = false) => {
-  //let uriMostReadGeneral = `https://cde.gestion2.e3.pe/doc/0/0/2/7/9/279593.txt`
-  //let uriMostReadGeneral = `https://cde.gestion2.e3.pe/doc/0/0/2/7/9/279593-error.txt`
   let uriMostReadGeneral = `https://d3ocw6unvuy6ob.cloudfront.net/${site}/normal/top.json`
   if (isPremium) {
     uriMostReadGeneral = isDev
       ? `https://d3ocw6unvuy6ob.cloudfront.net/${site}/premium/top.json`
       : `https://do5ggs99ulqpl.cloudfront.net/${site}/premium/top.json`
   }
-
-  //console.log('================1====================')
-  //console.log(`URL de Servicio: ${uriMostReadGeneral}`)
-  //console.log(`idPremium: ${isPremium}`)
-  //console.log('================1====================')
   return uriMostReadGeneral
 }
-
-/* 
-https://d3ocw6unvuy6ob.cloudfront.net/depor/16646171/normal/top.json
-https://d3ocw6unvuy6ob.cloudfront.net/peru21/9849434/normal/top.json
-https://d3ocw6unvuy6ob.cloudfront.net/ojo/31245754/normal/top.json
-https://d3ocw6unvuy6ob.cloudfront.net/elbocon/31246731/normal/top.json
-*/
 
 const options = {
   gzip: true,
@@ -102,7 +64,6 @@ const fetch = (key = {}) => {
   }).then(resp => {
     const arrURL = resp.slice(0, amountStories)
     arrURL.forEach(el => {
-      // eslint-disable-next-line no-param-reassign
       el.path = el.path.match(/((.*)-noticia(.*)\/)(.*)/)[1] || ''
     })
 
@@ -123,7 +84,6 @@ const fetch = (key = {}) => {
     .catch(err => console.log(`PromiseAll error: ${err}`))
   })
   .catch(err => { 
-    //console.log(`PromiseFetch error: ${err}`) 
     return { content_elements: [] }
   })
 }
