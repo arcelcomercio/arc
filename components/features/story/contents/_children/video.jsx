157b12762b2ff4178aabeb7638ea283eb8fae172
dnnstone <vastahuaman@rmgperusaff.com>
2020-01-27 16:34:46 -0500

    resolve conflict with sandbox 3

--- components/features/story/contents/_children/video.jsx
+++ components/features/story/contents/_children/video.jsx
@@ -56,10 +56,10 @@ class StoryContentChildVideo extends PureComponent {
     const {
       siteProperties: { urlPreroll },
       globalContent,
-      arcSite, 
+      arcSite,
       metaValue,
     } = this.props
-    
+
     const {
       taxonomy: {
         primary_section: {
@@ -80,34 +80,36 @@ class StoryContentChildVideo extends PureComponent {
       arcSite === 'elcomercio' ||
       arcSite === 'elcomerciomag'
     ) {
-
       let tipoplantilla = ''
       switch (metaValue('id')) {
         case 'meta_section':
           tipoplantilla = 'sect'
-          break;
+          break
         case 'meta_story':
-          tipoplantilla ='post'
-          break;
+          tipoplantilla = 'post'
+          break
         default:
-          tipoplantilla ='post'
+          tipoplantilla = 'post'
           break
       }
 
       let sectionSlug = this.getSectionSlug(primarySection)
-      if(arcSite === 'publimetro' )
-      {
-        return `https://pubads.g.doubleclick.net/gampad/ads?iu=/28253241/${arcSite}_post_${sectionSlug}_web_preroll&description_url=https%3A%2F%2F${arcSite}.pe%2F&tfcd=0&npa=0&sz=640x360&cust_params=fuente%3Dweb%26publisher%3D${arcSite}%26seccion%3D${sectionSlug}%26tipoplantilla%3D${tipoplantilla}&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=`;
+      if (arcSite === 'publimetro') {
+        return `https://pubads.g.doubleclick.net/gampad/ads?iu=/28253241/${arcSite}_post_${sectionSlug}_web_preroll&description_url=https%3A%2F%2F${arcSite}.pe%2F&tfcd=0&npa=0&sz=640x360&cust_params=fuente%3Dweb%26publisher%3D${arcSite}%26seccion%3D${sectionSlug}%26tipoplantilla%3D${tipoplantilla}&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=`
+      } else {
+        return `https://pubads.g.doubleclick.net/gampad/ads?iu=/28253241/${arcSite}/web/post/${sectionSlug
+          .split('-')
+          .join(
+            ''
+          )}/preroll&description_url=https%3A%2F%2F${arcSite}.pe%2F&tfcd=0&npa=0&sz=640x360&cust_params=fuente%3Dweb%26publisher%3D${arcSite}%26seccion%3D${sectionSlug
+          .split('-')
+          .join(
+            ''
+          )}%26tipoplantilla%3D${tipoplantilla}&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=`
       }
-      else
-       { 
-         return `https://pubads.g.doubleclick.net/gampad/ads?iu=/28253241/${arcSite}/web/post/${sectionSlug.split('-').join('')}/preroll&description_url=https%3A%2F%2F${arcSite}.pe%2F&tfcd=0&npa=0&sz=640x360&cust_params=fuente%3Dweb%26publisher%3D${arcSite}%26seccion%3D${sectionSlug.split('-').join('')}%26tipoplantilla%3D${tipoplantilla}&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=`;}
-      
-    }else{
+    } else {
       return urlPreroll
     }
-
-    
   }
 
   render() {
