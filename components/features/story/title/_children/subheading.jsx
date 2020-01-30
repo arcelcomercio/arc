2e480e843dde7f3ff02c6f5cda010e0438f2b4b9
Maria Gabriela Rangel <mrangel@rmgperustaff.com>
2020-01-30 16:30:00 -0500

    Update: Se modifcó la ubicación de texto premium en notas internas debajo de la bajada (#18830)

--- components/features/story/title/_children/subheading.jsx
+++ components/features/story/title/_children/subheading.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import ConfigParams from '../../../../utilities/config-params'
 
 const classes = {
   description:
     'story-header__news-summary pr-20 pl-20 mb-20 secondary-font line-h-sm text-gray-300 text-xl font-normal',
+  premiumWrapper:
+    'premium__wrapper bg-primary flex justify-center items-center mb-10 ml-20',
+  premiumText:
+    'premium__text flex justify-center items-center text-black font-bold icon-padlock',
 }
 
 const StoryHeaderChildShareSubheading = data => {
-  const { subTitle } = data || {}
+  const { subTitle, isPremium = '', arcSite = '' } = data || {}
 
-  return subTitle && <h2 className={classes.description}> {subTitle}</h2>
+  return (
+    <>
+      {subTitle && <h2 className={classes.description}> {subTitle}</h2>}
+      {isPremium && ConfigParams.SITE_ELCOMERCIO === arcSite && (
+        <div className={classes.premiumWrapper}>
+          <p className={classes.premiumText}>Suscriptor Digital</p>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default StoryHeaderChildShareSubheading
