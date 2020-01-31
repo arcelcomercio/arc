5560916528524e141d5721748a8c46f9b0f44989
Edgar <edgar.gamarra21@gmail.com>
2020-01-31 16:08:38 -0500

    new: se agregaron las validaciones para el comportamiento del sticky en el evento play y el evento scroll (##18883)

--- components/features/stories-lists/stories-video/_children/powa-video-promoted.jsx
+++ components/features/stories-lists/stories-video/_children/powa-video-promoted.jsx
@@ -6,8 +6,9 @@ const classes = {
   listItemTitleDest: 'stories-video__item-dest-title text-white',
 }
 
-const closeSticky = () => {
-  console.log('close Sticky')
+const handleCloseStickyClick = () => {
+  const itemDest = document.querySelector('.stories-video__item-dest')
+  itemDest.classList.remove('sticky')
 }
 
 const ItemVideoCenterDestacado = ({ isAdmin, title, video, autoPlayVideo }) => {
@@ -20,28 +21,27 @@ const ItemVideoCenterDestacado = ({ isAdmin, title, video, autoPlayVideo }) => {
       detail: { powa },
     } = event
 
-    powa.on(window.PoWa.EVENTS.PLAY, playEvent => {
+    powa.on(window.PoWa.EVENTS.PLAY, () => {
       window.addEventListener('scroll', () => {
         const playOf = document.querySelector('.stories-video__wrapper')
         const itemDest = document.querySelector('.stories-video__item-dest')
         const scrollHeight = window.scrollY
 
-        // si esta fuera de foco
-        if (scrollHeight >= playOf.offsetTop) {
+        // si esta fuera de foco por abajo
+        if (scrollHeight >= playOf.offsetTop + playOf.offsetHeight) {
           // + playOf.offsetHeight
           itemDest.classList.add('sticky')
-        }
-
-        if (scrollHeight < playOf.offsetTop) {
+          console.log('offset abajo')
+        }else if (scrollHeight + window.innerHeight < playOf.offsetTop) {
+          // si esta fuera de foco por arriba
+          // console.log('scroll', scrollHeight, 'top cpn', playOf.offsetTop)
+          itemDest.classList.add('sticky')
+          console.log('offset arriba')
+        }else{
           itemDest.classList.remove('sticky')
+          console.log('en pantalla')
         }
 
-        // if (scrollHeight < playOf.offsetTop) {
-        //   console.log('top', playOf.offsetTop, 'scroll height', scrollHeight)
-        // }
-        // else {
-        //   itemDest.classList.remove('sticky')
-        // }
       })
     })
 
@@ -65,7 +65,7 @@ const ItemVideoCenterDestacado = ({ isAdmin, title, video, autoPlayVideo }) => {
         <div className={classes.listBorder}>
           <h2 className={classes.listItemTitleDest}>{title}</h2>
         </div>
-        <span onClick={closeSticky}>X</span>
+        <span onClick={handleCloseStickyClick}>X</span>
       </div>
     </>
   )
