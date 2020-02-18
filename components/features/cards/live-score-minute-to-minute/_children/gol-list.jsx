c49bd0cf50609cacf7438c0bd613099cdf2cd88d
Edgar <edgar.gamarra21@gmail.com>
2020-02-18 12:46:05 -0500

    New: se agrego el subcomponente para el listado de los goles de cada equipo (#19626)

---
+++ components/features/cards/live-score-minute-to-minute/_children/gol-list.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const classes = {
+  list: 'flex flex-row',
+}
+const GolList = () => {
+  return (
+    <ul className={classes.list}>
+      <li>
+        A witsel <span>(P 38)</span>{' '}
+      </li>
+      <li>
+        A witsel <span>(P 38)</span>{' '}
+      </li>
+      <li>
+        A witsel <span>(P 38)</span>{' '}
+      </li>
+    </ul>
+  )
+}
+
+export default GolList
--- components/features/cards/live-score-minute-to-minute/_children/team-score.jsx
+++ components/features/cards/live-score-minute-to-minute/_children/team-score.jsx
@@ -3,17 +3,35 @@ import React from 'react'
 import ItemScore from './item-score'
 import ItemTeamFlag from './item-team-flag'
 import ItemTeamName from './item-team-name'
+import GolList from './gol-list'
+
+const classes = {
+  score: 'flex flex-row',
+  gol: 'flex flex-row',
+}
 
 const LiveScoreMinuteToMinuteTeanScore = ({ firstTeamName = true }) => {
   return (
-    <>
-      <div>
+    <div>
+      <div className={classes.score}>
         {firstTeamName ? <ItemTeamName /> : <ItemScore />}
         <ItemTeamFlag />
         {firstTeamName ? <ItemScore /> : <ItemTeamName />}
-        <div>eventos</div>
       </div>
-    </>
+      <div className={classes.gol}>
+        {firstTeamName ? (
+          <>
+            <GolList />
+            <span>icono</span>
+          </>
+        ) : (
+          <>
+            <span>icono</span>
+            <GolList />
+          </>
+        )}
+      </div>
+    </div>
   )
 }
 
