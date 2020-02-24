a3cd095e226582f9e5eaced427fe484fa18dbe24
dnnstone <vastahuaman@rmgperusaff.com>
2020-02-24 09:24:42 -0500

    News: Standarizando condiciones de Preroll para publimetro #16084

--- components/features/story/contents/_children/video.jsx
+++ components/features/story/contents/_children/video.jsx
@@ -140,9 +140,7 @@ class StoryContentChildVideo extends PureComponent {
       }
 
       const sectionSlug = this.getSectionSlug(primarySection)
-      if (arcSite === 'publimetro') {
-        return `https://pubads.g.doubleclick.net/gampad/ads?iu=/28253241/${arcSiteNew}_post_${sectionSlug}_web_preroll&description_url=https%3A%2F%2F${webSite}%2F&tfcd=0&npa=0&sz=640x360&cust_params=fuente%3Dweb%26publisher%3D${arcSiteNew}%26seccion%3D${sectionSlug}%26tipoplantilla%3D${tipoplantilla}&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=`
-      }
+
       return `https://pubads.g.doubleclick.net/gampad/ads?iu=/28253241/${arcSiteNew}/web/post/${sectionSlug
         .split('-')
         .join(
