6a920db2a2e529995ad15af62a5ed0d5e386b31d
jorge linares <jorge.developer@gmail.com>
2020-02-26 18:15:30 -0500

    Fix: Correcion de estilos para lifweek diseñadores (#19888)

--- components/features/lifweek/default.jsx
+++ components/features/lifweek/default.jsx
@@ -4,23 +4,25 @@ import { useFusionContext } from 'fusion:context'
 import ENV from "fusion:environment";
 
 const classes = {
-    lifweek: 'lifweek',
-    seccion_title: 'section-title mb-20 mt-20',
-    designer_profile: 'designer-profile flex',
-    designer_profile_image: 'designer-profile__image',
-    designer_profile_detail: 'designer-profile__detail lg:mt30',
-    designer_profile_name: 'designer-profile__name',
-    designer_profile_bio: 'designer-profile__bio',
-    designer_profile_divider: 'designer-profile__divider mt-30 mb-20',
+    lifweek: 'lifweek-designers',
+    seccion_title: 'lifweek-designers__section-title mb-20 mt-20',
+    profile: 'lifweek-designers__profile flex flex-row flex-wrap mb-40',
+    profile_image: 'lifweek-designers__profile-image',
+    profile_detail: 'lifweek-designers__profile-detail pl-20',
+    profile_name: 'lifweek-designers__profile-name',
+    profile_name_blue: 'lifweek-designers__profile-name_blue inline-block',
+    profile_bio: 'lifweek-designers__profile-bio',
+    profile_divider: 'lifweek-designers__profile-divider mt-30 mb-20',
 
-    box_designers: 'box-designers mb-10 pb-20 pt-30',
-    box_designers_title: 'box-designers__title inline-block mb-20',
-    box_designers_content: 'box-designers__content flex',
-    item_designer: 'designer-item position-relative m-10',
-    item_designer_image: 'designer-item__image',
-    item_designer_name: 'designer-item__name inline-block',
-    item_designer_lastname: 'designer-item__lastname w-full inline-block'
-
+    list: 'lifweek-designers__list mb-10 pb-20 pt-30',
+    list_title: 'lifweek-designers__list-title inline-block mb-20',
+    list_title_bold: 'lifweek-designers__list-title_bold',
+    list_content: 'lifweek-designers__list-content flex flex-row flex-wrap',
+    list_item: 'lifweek-designers__list-item position-relative m-10',
+    list_link: 'lifweek-designers__list-link block',
+    list_content_name: 'lifweek-designers__list-content_name position-absolute',
+    list_name: 'lifweek-designers__list-name inline-block',
+    list_lastname: 'lifweek-designers__list-lastname w-full inline-block'
 }
 
 const LIFWEEK_SOURCE = 'get-designers-lifweek';
@@ -31,7 +33,7 @@ const Lifweek = () =>
         source: LIFWEEK_SOURCE
     })
 
-    const { arcSite } = useFusionContext()
+    const { arcSite, isAdmin } = useFusionContext()
 
     const _env = ENV.ENVIRONMENT === 'elcomercio' ? 'prod' : 'sandbox'
 
@@ -40,7 +42,7 @@ const Lifweek = () =>
     const currentUrl   = window.location.href.split('?')[0];
     let   slugDesigner = currentUrl.match(/([^/]*)\/*$/)[1];
 
-    if(slugDesigner === 'disenadores')
+    if(slugDesigner === 'disenadores' || isAdmin)
     {
         // eslint-disable-next-line prefer-destructuring
         slugDesigner = Object.keys(participantes)[0];                
@@ -52,23 +54,23 @@ const Lifweek = () =>
     return (
         <div className={classes.lifweek}>
             <h1 className={classes.seccion_title}>Diseñadores</h1>
-            <div className={classes.designer_profile}>
-                <div className={classes.designer_profile_image}>
+            <div className={classes.profile}>
+                <div className={classes.profile_image}>
                     <figure>
                         <img src={ perfilDesigner.imagen } alt={separateName[0]}/>
                     </figure>
                 </div>
-                <div className={classes.designer_profile_detail}>
-                    <h2 className={classes.designer_profile_name}>
-                        <span>{ separateName[0] }</span> {separateName[1]}
+                <div className={classes.profile_detail}>
+                    <h2 className={classes.profile_name}>
+                        <span className={classes.profile_name_blue}>{ separateName[0] }</span> {separateName[1]}
                     </h2>
-                    <p className={classes.designer_profile_bio}>{ perfilDesigner.bio }</p>
-                    <div className={classes.designer_profile_divider}></div>
+                    <p className={classes.profile_bio}>{ perfilDesigner.bio }</p>
+                    <div className={classes.profile_divider}></div>
                 </div>
             </div>
-            <div className={classes.box_designers}>
-                <h3 className={classes.box_designers_title}>Galería de <b>Diseñadores &amp; Marcas</b></h3>
-                <div className={classes.box_designers_content}>
+            <div className={classes.list}>
+                <h3 className={classes.list_title}>Galería de <span className={classes.list_title_bold}>Diseñadores &amp; Marcas</span></h3>
+                <div className={classes.list_content}>
                 {
                     Object.keys(participantes).map((key) => {
                         if(slugDesigner === key){
@@ -78,13 +80,13 @@ const Lifweek = () =>
                         const itemUrl  = `/lifweek/disenadores/${key}/${_env !== "prod" ? `?_website=${arcSite}&` : `?`}`
                         
                         return (
-                            <div className={classes.item_designer}>
-                                <figure className={classes.item_designer_image}>
-                                    <a href={itemUrl}>
+                            <div className={classes.list_item}>
+                                <figure>
+                                    <a href={itemUrl} className={classes.list_link}>
                                         <img src={participantes[key].imagen} width="276" height="354" alt={itemName[0]}/>
-                                        <figcaption>
-                                            <span className={classes.item_designer_name}>{itemName[0]}</span>
-                                            <span className={classes.item_designer_lastname}>{itemName[1]}</span>
+                                        <figcaption className={classes.list_content_name}>
+                                            <span className={classes.list_name}>{itemName[0]}</span>
+                                            <span className={classes.list_lastname}>{itemName[1]}</span>
                                         </figcaption>
                                     </a>
                                 </figure>
