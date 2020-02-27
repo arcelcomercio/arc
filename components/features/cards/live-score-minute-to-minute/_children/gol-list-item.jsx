0a24dd5cf1946c499331a4651b02e6bdbca9b3f5
Maria Gabriela Rangel <mrangel@rmgperustaff.com>
2020-02-27 17:57:01 -0500

    Update: Modificaciones en imagen gol en componente Score (#19900)

--- components/features/cards/live-score-minute-to-minute/_children/gol-list-item.jsx
+++ components/features/cards/live-score-minute-to-minute/_children/gol-list-item.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const classes = {
   golBox: 'score__gol-box flex flex-row-reverse pl-20 pr-20',
   list: 'score__gol-list flex pl-20 pr-20 flex-wrap justify-end items-center',
-  listItem: 'score__gol-item flex flex-wrap mr-10',
+  listItem: 'score__gol-item flex flex-wrap mb-5 mr-10',
   listName: 'score__gol-name text-gray-300 font-bold mr-5',
   listTime: 'score__gol-time text-gray-200',
   golImg: 'score__gol-img object-cover block',
