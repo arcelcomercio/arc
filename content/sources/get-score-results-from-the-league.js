9e9ed960bd08223849bd7568b15858571f9b5a4d
Edgar <edgar.gamarra21@gmail.com>
2020-03-04 11:27:42 -0500

    New: se creo la conexcion entre cs y el apu de resutlados

--- content/sources/get-score-results-from-the-league.js
+++ content/sources/get-score-results-from-the-league.js
@@ -8,87 +8,16 @@ const params = [
 const resolve = (key = {}) => {
   let urlResult = ''
   if (key && key.idLeague && key.idLeague !== '') {
-    urlResult = `http://devresultadosopta.elcomercio.pe/api/v2/match/?format=json&uuid=e3tz6ya6vdcivtg04yvw3kpre`
+    urlResult = `https://devresultadosopta.elcomercio.pe/api/v2/competitioncomponent/?format=json&uuid=${key.idLeague}`
   }
 
   return urlResult
 }
 
-const transform = () => {
-  const dammyData = {
-    leagueNme: 'Champions',
-    listMatchResults1: [
-      {
-        homeTeamShortName: 'Napoli',
-        awayTeamShortName: 'Barcelona',
-        homeTeamScore: 1,
-        awayTeamScore: 1,
-        homeTeamFlag: 'Bandera Home Team',
-        awayTeamFlag: 'Bandera Away Team',
-        matchTime: '6:00 am',
-        matchDate: '11/04',
-        matchStatus: 'Playing',
-      },
-      {
-        homeTeamShortName: 'Napoli',
-        awayTeamShortName: 'Barcelona',
-        homeTeamScore: 1,
-        awayTeamScore: 1,
-        homeTeamFlag: 'Bandera Home Team',
-        awayTeamFlag: 'Bandera Away Team',
-        matchTime: '6:00 am',
-        matchDate: '11/04',
-        matchStatus: 'Fixture',
-      },
-      {
-        homeTeamShortName: 'Napoli',
-        awayTeamShortName: 'Barcelona',
-        homeTeamScore: 1,
-        awayTeamScore: 1,
-        homeTeamFlag: 'Bandera Home Team',
-        awayTeamFlag: 'Bandera Away Team',
-        matchTime: '6:00 am',
-        matchDate: '11/04',
-        matchStatus: 'Played',
-      },
-    ],
-    listMatchResults2: [
-      {
-        homeTeamShortName: 'Napoli',
-        awayTeamShortName: 'Barcelona',
-        homeTeamScore: 1,
-        awayTeamScore: 1,
-        homeTeamFlag: 'asd/asd',
-        awayTeamFlag: 'asd/asd',
-        matchTime: '6:00 am',
-        matchDate: '11/04',
-        matchStatus: 'Played',
-      },
-      {
-        homeTeamShortName: 'Napoli',
-        awayTeamShortName: 'Barcelona',
-        homeTeamScore: 1,
-        awayTeamScore: 1,
-        homeTeamFlag: 'asd/asd',
-        awayTeamFlag: 'asd/asd',
-        matchTime: '6:00 am',
-        matchDate: '11/04',
-        matchStatus: '',
-      },
-      {
-        homeTeamShortName: 'Napoli',
-        awayTeamShortName: 'Barcelona',
-        homeTeamScore: 1,
-        awayTeamScore: 1,
-        homeTeamFlag: 'asd/asd',
-        awayTeamFlag: 'asd/asd',
-        matchTime: '6:00 am',
-        matchDate: '11/04',
-        matchStatus: 'Playing',
-      },
-    ],
-  }
-  return dammyData
+const transform = data => {
+  const { items: result = {} } = data
+
+  return result
 }
 
 export default {
