f8ba3cafe779b7601c74e566e8533a2fd2754ef8
Alvaro <ahuamani@rmgperustaff.com>
2020-03-04 10:33:39 -0500

    Update: agregado resize al feature Imagen con enlace (#20317)

--- components/features/custom-image/default.jsx
+++ components/features/custom-image/default.jsx
@@ -1,60 +1,103 @@
 import React from 'react'
 import { useFusionContext } from 'fusion:context'
+import { useContent } from 'fusion:content'
 
 import customFields from './_dependencies/custom-fields'
 import { defaultImage } from '../../utilities/helpers'
+import { getResizedUrl } from '../../utilities/resizer'
 
 const classes = { lazy: 'lazy' }
 
-const CustomImage = props => {
+const CustomImage = ({
+  customFields: {
+    imgUrlDesktop = '',
+    imgTitle = '',
+    imgAlt,
+    imgLink = '',
+    imgHeight = '',
+    cols = '',
+    rows = '',
+  } = {},
+}) => {
   const { isAdmin, arcSite, contextPath, deployment } = useFusionContext()
 
   const lazyImg = defaultImage({ arcSite, contextPath, deployment, size: 'md' })
 
+  const containerClass = `block ${cols} md:${rows}`
+
+  let width = '307'
+  if (cols === 'col-2') width = '633'
+  if (cols === 'col-3') width = '960'
+
+  let height = '374'
+  if (rows === 'row-2') height = '768'
+  if (rows === 'w-full') height = imgHeight || '0'
+
   const {
-    customFields: {
-      imgUrlDesktop = '',
-      imgUrlMobile = '',
-      imgTitle = '',
-      imgAlt,
-      imgLink = '',
-      imgWidth = '',
+    resized_urls: {
+      desktop: adminResizeImg = '',
+      mobile: adminMobileImg = '',
     } = {},
-  } = props
+  } =
+    useContent(
+      isAdmin && imgUrlDesktop
+        ? {
+            source: 'photo-resizer',
+            query: {
+              url: imgUrlDesktop,
+              presets: `desktop:${width}x${height},mobile:450x0`,
+            },
+          }
+        : {}
+    ) || ''
+
+  const { desktop: publicResizeImg = '', mobile: publicMobileImg = '' } =
+    !isAdmin && imgUrlDesktop
+      ? getResizedUrl({
+          url: imgUrlDesktop,
+          presets: `desktop:${width}x${height},mobile:450x0`,
+          arcSite,
+        }) || {}
+      : {}
+
+  const resizedImg = isAdmin ? adminResizeImg : publicResizeImg
+  const mobileResizedImg = isAdmin ? adminMobileImg : publicMobileImg
 
   if (imgUrlDesktop === '')
-    return <div>Modulo imagen, clic en editar para configurar.</div>
+    return (
+      <div className={containerClass}>
+        Modulo imagen, clic en editar para configurar.
+      </div>
+    )
 
   const picture = (
     <picture>
-      {imgUrlMobile !== '' && (
-        <source
-          media="(max-width: 650px)"
-          srcSet={isAdmin ? imgUrlMobile : lazyImg}
-          data-srcset={imgUrlMobile}
-          className={isAdmin ? '' : classes.lazy}
-        />
-      )}
+      <source
+        media="(max-width: 650px)"
+        srcSet={isAdmin ? mobileResizedImg : lazyImg}
+        data-srcset={mobileResizedImg}
+        className={isAdmin ? '' : classes.lazy}
+      />
+
       <img
-        src={isAdmin ? imgUrlDesktop : lazyImg}
-        data-src={imgUrlDesktop}
+        src={isAdmin ? resizedImg : lazyImg}
+        data-src={resizedImg}
         alt={imgAlt}
         {...(imgTitle !== '' ? { title: imgTitle } : {})}
-        {...(imgWidth !== '' ? { width: imgWidth } : { width: '100%' })}
-        className={isAdmin ? '' : classes.lazy}
+        className={`${isAdmin ? '' : classes.lazy} w-full h-full object-cover`}
       />
     </picture>
   )
 
   if (imgLink !== '') {
     return (
-      <div>
-        <a href={imgLink}>{picture}</a>
-      </div>
+      <a className={containerClass} href={imgLink}>
+        {picture}
+      </a>
     )
   }
 
-  return <div>{picture}</div>
+  return <div className={containerClass}>{picture}</div>
 }
 
 CustomImage.propTypes = {
