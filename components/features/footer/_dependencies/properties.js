6e7f90b5fb3957553e7c5c2a6f4d09730fc42931
cajotafer <hello@cajotafer.com>
2020-03-05 09:10:11 +0700

    New: footer properties en archivo separado de siteProperties (#20077)

---
+++ components/features/footer/_dependencies/properties.js
@@ -0,0 +1,665 @@
+const depor = {
+  footer: {
+    siteLegal: [
+      'Empresa Editora Depor',
+      'Jr. Jorge Salazar Araoz. Nro. 171 Urb. Santa Catalina, La Victoria, Lima',
+      'Copyright © Depor.pe',
+      'Grupo Depor - Todos los derechos reservados',
+    ],
+
+    socialNetworks: [
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/tuitdepor',
+      },
+      {
+        name: 'Facebook',
+        url: 'https://www.facebook.com/depor.pe',
+      },
+
+      {
+        name: 'Google+',
+        url: 'https://plus.google.com/u/0/+deporpe',
+      },
+      {
+        name: 'Pinterest',
+        url: 'https://www.pinterest.com/deporpe',
+      },
+      {
+        name: 'RSS ',
+        url: 'https://depor.com/arcio/rss',
+      },
+    ],
+
+    contacts: [
+      {
+        position: 'Director Periodístico General de Deporte',
+        name: 'Guillermo Denegri',
+      },
+      {
+        position: 'Director Periodístico',
+        name: 'Miguel Morales.',
+      },
+    ],
+    corporateInfo: {
+      name: 'Grupo El Comercio',
+      direction:
+        'Jr. Jorge Salazar Araoz. Nro. 171 Urb. Santa Catalina, La Victoria, Lima',
+    },
+    draftingContact: [
+      {
+        name: 'Redacción',
+        value: '311-6500(2858) depor@depor.pe',
+      },
+      {
+        name: 'Publicidad Web',
+        value: 'Fonoavisos@comercio.com.pe',
+      },
+    ],
+    copyrightText:
+      'Copyright© Depor.pe - Grupo El Comercio - Todos los derechos reservados.',
+  },
+}
+
+const diariocorreo = {
+  footer: {
+    siteLegal: [
+      '© Empresa Editora El Comercio S.A.',
+      'Jirón Jorge Salazar Araoz 171',
+      'Lima 13 - Perú',
+      'Copyright© | diariocorreo.pe | Todos los derechos reservados',
+    ],
+    story: [
+      {
+        position: 'Director Periodístico',
+        name: 'DIRECTOR DE Correo',
+      },
+      {
+        position: 'Empresa Editora Correo',
+        name: 'Jr. Santa Rosa #300 Lima 1 Perú',
+      },
+      {
+        position: 'Copyright © diariocorreo.pe',
+        name: 'Grupo El Comercio - Todos los derechos reservados',
+      },
+    ],
+    socialNetworks: [
+      {
+        name: 'linkedin',
+        url: 'https://www.linkedin.com/',
+      },
+      {
+        name: 'facebook',
+        url: 'https://www.facebook.com/CorreoPeru/',
+      },
+      {
+        name: 'twitter',
+        url: 'https://twitter.com/diariocorreo',
+      },
+    ],
+
+    contacts: [
+      {
+        position: 'Contacto Administrativo',
+        name: 'contacto@prensmart.pe',
+      },
+      {
+        position: 'Publicidad Online',
+        name: 'fonoavisos@comercio.com.pe',
+      },
+      {
+        position: 'Call Center',
+        name: '+51 1708 9999',
+      },
+      {
+        position: 'Temas periodísticos',
+        name: '+51 1 631 1111',
+      },
+    ],
+  },
+}
+
+const elbocon = {
+  footer: {
+    siteLegal: [
+      '© Empresa Editora El Comercio S.A.',
+      'Jirón Jorge Salazar Araoz 171',
+      'Lima 13 - Perú',
+      'Copyright© | elbocon.pe | Todos los derechos reservados',
+    ],
+    story: [
+      {
+        position: 'Director Periodístico',
+        name: 'DIRECTOR DE EL BOCÓN',
+      },
+      {
+        position: 'Empresa Editora El Bocón',
+        name: 'Jr. Santa Rosa #300 Lima 1 Perú',
+      },
+      {
+        position: 'Copyright © elbocon.pe',
+        name: 'Grupo El Comercio - Todos los derechos reservados',
+      },
+    ],
+    socialNetworks: [
+      {
+        name: 'linkedin',
+        url: 'https://www.linkedin.com/company/diario-el-bocon/',
+      },
+      {
+        name: 'facebook',
+        url: 'https://www.facebook.com/DiarioElBocon',
+      },
+      {
+        name: 'twitter',
+        url: 'https://twitter.com/elbocononline',
+      },
+    ],
+
+    contacts: [
+      {
+        position: 'Contacto Administrativo',
+        name: 'contacto@prensmart.pe',
+      },
+      {
+        position: 'Publicidad Online',
+        name: 'fonoavisos@comercio.com.pe',
+      },
+      {
+        position: 'Call Center',
+        name: '+51 1708 9999',
+      },
+      {
+        position: 'Temas periodísticos',
+        name: '+51 1 631 1111',
+      },
+    ],
+  },
+}
+
+const elcomercio = {
+  footer: {
+    siteLegal: [
+      'Empresa Editora El Comercio',
+      'Jr. Santa Rosa #300 Lima 1 Perú',
+      'Copyright © Elcomercio.pe',
+      'Grupo El Comercio - Todos los derechos reservados',
+    ],
+    story: [
+      {
+        position: 'DIRECTOR PERIODÍSTICO:',
+        name: 'Juan José Garrido Koechlin',
+      },
+      {
+        position: 'Empresa Editora El Comercio',
+        name: 'Jr. Santa Rosa #300 Lima 1 Perú',
+      },
+      {
+        position: 'Copyright © elcomercio.pe',
+        name:
+          '© Empresa Editora El Comercio - Grupo El Comercio - Todos los derechos reservados.',
+      },
+    ],
+    socialNetworks: [
+      {
+        name: 'facebook',
+        url: 'https://www.facebook.com/elcomercio.pe',
+      },
+      {
+        name: 'twitter',
+        url: 'https://twitter.com/elcomercio_peru',
+      },
+      {
+        name: 'youtube',
+        url: 'https://plus.google.com/u/0/+elcomerciope',
+      },
+    ],
+
+    directors: [
+      {
+        position: 'Director Periodístico',
+        names: ['JUAN JOSÉ GARRIDO KOECHLIN'],
+      },
+      {
+        position: 'Directores Fundadores',
+        names: [
+          'Manuel Amunátegui [1839-1875] y',
+          'Alejandro Villota [1839-1861]',
+        ],
+      },
+      {
+        position: 'Directores',
+        names: [
+          'Francisco Miró Quesada Cantuarias',
+          'Luis Carranza [1875-1898]',
+          'José Antonio Miró Quesada [1875-1905]',
+          'Antonio Miró Quesada de la Guerra [1905-1935]',
+          'Aurelio Miró Quesada de la Guerra [1935-1950]',
+          'Luis Miró Quesada de la Guerra [1935-1974]',
+          'Óscar Miró Quesada de la Guerra [1980-1981]',
+          'Aurelio Miró Quesada Sosa [1980-1998]',
+          'Alejandro Miró Quesada Garland [1980-2011]',
+          'Alejandro Miró Quesada Cisneros [1999-2008]',
+          'Francisco Miró Quesada Rada [2008-2013]',
+          'Fritz Du Bois Freund [2013-2014]',
+          'Fernando Berckemeyer Olaechea [2014-2018]',
+        ],
+      },
+    ],
+
+    contacts: [
+      {
+        position: 'Suscripciones',
+        name: 'suscriptores@comercio.com.pe',
+      },
+      {
+        position: 'Publicidad',
+        name: 'fonoavisos@comercio.com.pe',
+      },
+      {
+        position: 'Club El Comercio',
+        name: 'clubelcomercio@comercio.com.pe',
+        link: {
+          name: 'Compromiso de Autorregulación Comercial',
+          url: 'https://elcomercio.pe/compromiso-autoregulacion-comercial.pdf',
+        },
+      },
+    ],
+  },
+}
+
+const elcomerciomag = {
+  footer: {
+    siteLegal: [
+      'Empresa Editora El Comercio',
+      'Jr. Santa Rosa #300 Lima 1 Perú',
+      'Copyright © Elcomercio.pe',
+      'Grupo El Comercio - Todos los derechos reservados',
+    ],
+    story: [
+      {
+        position: 'DIRECTOR PERIODÍSTICO:',
+        name: 'Juan José Garrido Koechlin',
+      },
+      {
+        position: 'Empresa Editora El Comercio',
+        name: 'Jr. Santa Rosa #300 Lima 1 Perú',
+      },
+      {
+        position: 'Copyright © mag.elcomercio.pe',
+        name:
+          '© Empresa Editora El Comercio - Copyright © Elcomercio.pe - Grupo El Comercio - Todos los derechos reservados.',
+      },
+    ],
+    socialNetworks: [
+      {
+        name: 'facebook',
+        url: 'https://www.facebook.com/elcomercio.pe',
+      },
+      {
+        name: 'twitter',
+        url: 'https://twitter.com/elcomercio_peru',
+      },
+      {
+        name: 'youtube',
+        url: 'https://plus.google.com/u/0/+elcomerciope',
+      },
+    ],
+
+    directors: [
+      {
+        position: 'Director General',
+        names: ['FRANCISCO MIRÓ QUESADA CANTUARIAS'],
+      },
+      {
+        position: 'Director Periodístico',
+        names: ['JUAN JOSÉ GARRIDO KOECHLIN'],
+      },
+      {
+        position: 'Directores Fundadores',
+        names: [
+          'Manuel Amunátegui [1839-1875] y',
+          'Alejandro Villota [1839-1861]',
+        ],
+      },
+      {
+        position: 'Directores',
+        names: [
+          'Francisco Miró Quesada Cantuarias',
+          'Luis Carranza [1875-1898]',
+          'José Antonio Miró Quesada [1875-1905]',
+          'Antonio Miró Quesada de la Guerra [1905-1935]',
+          'Aurelio Miró Quesada de la Guerra [1935-1950]',
+          'Luis Miró Quesada de la Guerra [1935-1974]',
+          'Óscar Miró Quesada de la Guerra [1980-1981]',
+          'Aurelio Miró Quesada Sosa [1980-1998]',
+          'Alejandro Miró Quesada Garland [1980-2011]',
+          'Alejandro Miró Quesada Cisneros [1999-2008]',
+          'Francisco Miró Quesada Rada [2008-2013]',
+          'Fritz Du Bois Freund [2013-2014]',
+          'Fernando Berckemeyer Olaechea [2014-2018]',
+        ],
+      },
+    ],
+
+    contacts: [
+      {
+        position: 'Suscripciones',
+        name: 'suscriptores@comercio.com.pe',
+      },
+      {
+        position: 'Publicidad',
+        name: 'fonoavisos@comercio.com.pe',
+      },
+      {
+        position: 'Club El Comercio',
+        name: 'clubelcomercio@comercio.com.pe',
+        link: {
+          name: 'Compromiso de Autorregulación Comercial',
+          url: 'https://elcomercio.pe/compromiso-autoregulacion-comercial.pdf',
+        },
+      },
+    ],
+  },
+}
+
+const gestion = {
+  footer: {
+    siteLegal: [
+      'Director periodístico',
+      'JULIO LIRA SEGURA',
+      '© Empresa Editora El Comercio S.A.',
+      'Jorge Salazar Araoz N° 171, La Victoria, Lima.',
+      'Copyright© | Gestion.pe | Grupo El Comercio | Todos los derechos reservados',
+    ],
+    story: [
+      {
+        position: 'Director Periodístico',
+        name: 'JULIO LIRA SEGURA',
+      },
+      {
+        position: 'Empresa Editora Gestión',
+        name: 'Jorge Salazar Araoz N° 171, La Victoria, Lima.',
+      },
+      {
+        position: 'Copyright © gestion.pe',
+        name: 'Grupo El Comercio - Todos los derechos reservados',
+      },
+    ],
+    socialNetworks: [
+      {
+        name: 'linkedin',
+        url: 'https://www.linkedin.com/company/diario-gestión/',
+      },
+      {
+        name: 'facebook',
+        url: 'https://www.facebook.com/Gestionpe',
+      },
+      {
+        name: 'twitter',
+        url: 'https://twitter.com/gestionpe',
+      },
+    ],
+
+    contacts: [
+      {
+        position: 'Director Periodístico',
+        name: 'JULIO LIRA SEGURA',
+      },
+      {
+        position: '',
+        name: '',
+      },
+    ],
+  },
+}
+
+// const laprensa = { aun sin definir }
+
+const ojo = {
+  footer: {
+    siteLegal: [
+      'Empresa Editora El Comercio',
+      'Prensmart Jirón Jorge Salazar Araoz 171  Lima 13 - Perú',
+      'Copyright © ojo.pe',
+      '2019 © PrenSmart S.A.C. Todos los derechos reservados',
+    ],
+    story: [
+      {
+        position: 'Publicidad Online: fonoavisos@comercio.com.pe ',
+        name: 'Contacto Administrativo: contacto@prensmart.pe',
+      },
+      {
+        position: 'Empresa Editora Ojo',
+        name: 'Prensmart Jirón Jorge Salazar Araoz 171  Lima 13 - Perú',
+      },
+      {
+        position: 'Copyright © ojo.pe',
+        name: '2019 © PrenSmart S.A.C. Todos los derechos reservados',
+      },
+    ],
+    socialNetworks: [
+      {
+        name: 'facebook',
+        url: 'https://www.facebook.com/DiarioOjo',
+      },
+      {
+        name: 'linkedin',
+        url: 'https://www.linkedin.com/company/ojo/',
+      },
+    ],
+
+    contacts: [
+      {
+        position: 'Director Periodístico',
+        name: 'Jaime Asián Domínguez   jasian@grupoepensa.pe',
+      },
+      {
+        position: 'Editor Web',
+        name: 'Chavely Chiroque Quiroz ichiroque@grupoepensa.pe',
+      },
+      {
+        position: '-',
+        name: 'Contacto comercial',
+      },
+      {
+        position: 'Publicidad Online',
+        name: 'fonoavisos@comercio.com.pe',
+      },
+
+      {
+        position: 'Call Center',
+        name: '+51 1708 9999',
+      },
+    ],
+  },
+}
+
+const peru21 = {
+  footer: {
+    siteLegal: [
+      'Empresa Editora El Comercio',
+      'Jr. Jorge Salazar Araoz. Nro. 171 Urb. Santa Catalina, La Victoria, Lima',
+      'Copyright © Peru21.pe',
+      'Grupo El Comercio - Todos los derechos reservados',
+    ],
+    story: [
+      {
+        position: 'Directora Periodística:',
+        name: 'Cecilia Valenzuela',
+      },
+      {
+        position: 'Diario Perú21: ',
+        name:
+          'Jr. Jorge Salazar Araoz. Nro. 171 Urb. Santa Catalina, La Victoria, Lima',
+      },
+      {
+        position: 'Copyright © Peru21.pe',
+        name: 'Grupo El Comercio - Todos los derechos reservados',
+      },
+    ],
+    socialNetworks: [
+      {
+        name: 'facebook',
+        url: 'https://www.facebook.com/peru21',
+      },
+      {
+        name: 'twitter',
+        url: 'https://twitter.com/peru21noticias',
+      },
+    ],
+
+    contacts: [
+      {
+        position: 'Directora Periodística',
+        name: 'Cecilia Valenzuela',
+      },
+      {
+        position: 'Editora Web',
+        name: 'Esther Vargas - esther.vargas@peru21.com',
+      },
+      {
+        position: 'Publicidad Web',
+        name: 'fonoavisos@comercio.pe',
+      },
+    ],
+  },
+}
+
+const peru21g21 = { ...peru21 }
+
+const publimetro = {
+  footer: {
+    siteLegal: [
+      'Empresa Editora Publimetro',
+      'Jr. Jorge Salazar Araoz. Nro. 171 Urb. Santa Catalina, La Victoria, Lima',
+      'Copyright © publimetro.pe',
+      'Grupo El Comercio - Todos los derechos reservados',
+    ],
+    story: [
+      {
+        position: 'Director Periodístico',
+        name: 'Luis Carlos Arias Schreiber (redaccion@publimetro.pe)',
+      },
+      {
+        position: 'Empresa Editora Publimetro',
+        name:
+          'Jr. Jorge Salazar Araoz. Nro. 171 Urb. Santa Catalina, La Victoria, Lima',
+      },
+      {
+        position: 'Copyright © publimetro.pe',
+        name: 'Grupo El Comercio - Todos los derechos reservados',
+      },
+    ],
+
+    socialNetworks: [
+      {
+        name: 'facebook',
+        url: 'https://www.facebook.com/publimetrope',
+      },
+      {
+        name: 'twitter',
+        url: 'https://twitter.com/publimetrope',
+      },
+    ],
+
+    contacts: [
+      {
+        position: 'Director Periodístico',
+        name: 'Luis Carlos Arias Schreiber (redaccion@publimetro.pe)',
+      },
+      {
+        position: 'Editor Web',
+        name: 'Christian Lengua Solís (christian.lengua@publimetro.pe)',
+      },
+      {
+        position: 'Contacto comercial',
+        name: 'ventaspublimetro@publimetro.pe',
+      },
+    ],
+  },
+}
+
+const trome = {
+  footer: {
+    siteLegal: [
+      'Empresa Editora Trome',
+      'Jr. Jorge Salazar Araoz. Nro. 171 Urb. Santa Catalina, La Victoria, Lima',
+      'Copyright © Trome.pe',
+      'Trome - Todos los derechos reservados',
+    ],
+
+    socialNetworks: [
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/tuittrome',
+      },
+      {
+        name: 'Facebook',
+        url: 'https://www.facebook.com/trome.pe',
+      },
+
+      {
+        name: 'Google+',
+        url: 'https://plus.google.com/u/0/+tromepe',
+      },
+      {
+        name: 'Pinterest',
+        url: 'https://www.pinterest.com/tromepe',
+      },
+      {
+        name: 'RSS ',
+        url: 'https://trome.com/arcio/rss',
+      },
+    ],
+
+    contacts: [
+      {
+        position: 'Editora web Marilyn Corrales',
+        name: 'mcorrales@trome.com',
+      },
+      {
+        position: 'Webmaster',
+        name: 'webmaster@trome.com',
+      },
+    ],
+    corporateInfo: {
+      name: 'Grupo El Comercio S.A.',
+      direction:
+        'Jr. Jorge Salazar Araoz. Nro. 171 Urb. Santa Catalina, La Victoria, Lima - Perú',
+    },
+    draftingContact: [
+      {
+        name: 'Redacción',
+        value: '311-6500(2858) trome@trome.pe',
+      },
+      {
+        name: 'Publicidad Web',
+        value: 'Fonoavisos@comercio.com.pe',
+      },
+    ],
+    copyrightText:
+      'Copyright© Trome.pe - Grupo El Comercio - Todos los derechos reservados.',
+  },
+}
+
+const sites = {
+  depor,
+  diariocorreo,
+  elbocon,
+  elcomercio,
+  elcomerciomag,
+  gestion,
+  // laprensa,
+  ojo,
+  peru21,
+  peru21g21,
+  publimetro,
+  trome,
+}
+
+const getFooterProperties = arcSite => {
+  return { ...sites[arcSite] }
+}
+
+export default getFooterProperties
