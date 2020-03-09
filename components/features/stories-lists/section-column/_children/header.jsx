e8801f60f0253a77277e3299f15b27a33d875cbb
Alvaro <ahuamani@rmgperustaff.com>
2020-03-09 10:43:35 -0500

    Update: optimizado feature Noticias por sección, nodos reducidos, estilos a BEM (#203090)

--- components/features/stories-lists/section-column/_children/header.jsx
+++ components/features/stories-lists/section-column/_children/header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const classes = {
-  header: `stories-l-header flex items-center w-auto pr-20 pl-20 stories-l-section__header-font-position`,
-  seeMore: `stories-l-header__see-more flex items-center bg-white pt-5 pb-5 pr-10 pl-10 text-gray-200 text-sm`,
-  urlTitle: 'stories-l-header__title flex items-center full-height',
-  title: 'uppercase font-bold stories-l-section__header-font-color',
+  header: `sec-col__header flex items-center w-auto pr-20 pl-20 mb-5`,
+  seeMore: `flex items-center bg-white pt-5 pb-5 pr-10 pl-10 text-gray-200 text-sm`,
+  urlTitle: 'flex items-center full-height',
+  title: 'sec-col__title uppercase font-bold ',
 }
 const StoriesListsCardChildHeader = ({
   titleList,
