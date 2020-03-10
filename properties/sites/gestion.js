b41df64ba6a166796795dae1b7fb61177f088e5c
Raul Contreras <rcontreras@rmgperustaff.com>
2020-03-10 15:38:37 -0500

    Update: Cambio pixel de gestion

--- properties/sites/gestion.js
+++ properties/sites/gestion.js
@@ -21,7 +21,7 @@ export default {
   ampGoogleTagManagerName: 'gestion',
   charbeatAccountNumber: 57773,
   idGoogleAnalitics: 'UA-3055636-3',
-  fbPixelId: '2760131437441699',
+  fbPixelId: '192820708803212',
   fbArticleStyle: 'LogoGestion',
   nameStoryRelated: 'VEA TAMBIÉN',
   siteDescription:
