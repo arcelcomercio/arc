a9510b0ecc1a8ddd4bf254173583f0880a5be805
cajotafer <hello@cajotafer.com>
2020-03-17 23:15:58 +0700

    Update: extraordinary-story/live-score optimizacion de renders (#20503)

--- components/features/extraordinary-story/live-score/default.jsx
+++ components/features/extraordinary-story/live-score/default.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { useContent, useEditableContent } from 'fusion:content'
+import { useContent } from 'fusion:content'
 import { useFusionContext } from 'fusion:context'
 import PropTypes from 'prop-types'
 
 import StoryData from '../../../utilities/story-data'
 import schemaFilter from './_dependencies/schema-filter'
 import { includePromoItems } from '../../../utilities/included-fields'
+import LiveScoreChild from './_children/live-score'
 
 const PHOTO_SOURCE = 'photo-resizer'
 
@@ -28,7 +29,6 @@ const ExtraordinaryStoryLifeScore = props => {
   } = customFields || {}
 
   const { arcSite, contextPath, deployment, isAdmin } = useFusionContext()
-  const { editableField } = useEditableContent()
 
   const [results, setResults] = useState({})
   const [count, setCount] = useState(0)
@@ -117,64 +117,29 @@ const ExtraordinaryStoryLifeScore = props => {
   const { resized_urls: { landscape_l: landscapeL } = {} } = customPhoto || {}
 
   const [firstTeam = {}, secondTeam = {}] = teams
+  const { nombre: firstName, score: firstScore } = firstTeam
+  const { nombre: secondName, score: secondScore } = secondTeam
 
   const imgUrl = landscapeL || imgField || multimediaLandscapeL
 
-  return (
-    <div className="extraordinary-l-score bg-gray-300 lg:flex flex-row-reverse">
-      <a
-        className="extraordinary-l-score__img-link block lg:p-20"
-        href={websiteLink}>
-        <picture className="extraordinary-l-score__picture">
-          <img
-            className={`${
-              isAdmin ? '' : 'lazy'
-            } extraordinary-l-score__img w-full object-cover`}
-            src={isAdmin ? imgUrl : multimediaLazyDefault}
-            data-src={imgUrl}
-            alt={title}
-          />
-        </picture>
-      </a>
-      <div className="extraordinary-l-score__content p-10 lg:p-20">
-        {codeField && (
-          <a
-            href={websiteLink}
-            className="extraordinary-l-score__score-content text-white flex mb-15">
-            <div className="extraordinary-l-score__team p-10 text-xl font-bold flex-1 flex justify-end items-center">
-              {firstTeam.nombre || ''}
-            </div>
-            <div className="extraordinary-l-score__score p-10 title-xs font-bold bg-black flex items-center">{`${firstTeam.score ||
-              ''} - ${secondTeam.score || ''}`}</div>
-            <div className="extraordinary-l-score__team p-10 text-xl font-bold flex-1 flex items-center">
-              {secondTeam.nombre || ''}
-            </div>
-          </a>
-        )}
-        <h1 className="extraordinary-l-score__title mb-15 overflow-hidden">
-          {isLive && (
-            <div className="extraordinary-l-score__live text-white inline-block mr-10">
-              <span className="extraordinary-l-score__live-icon inline-block rounded mr-5" />
-              EN VIVO
-            </div>
-          )}
-          <a
-            href={websiteLink}
-            className="extraordinary-l-score__title-link text-white title-md font-bold line-h-xs"
-            {...editableField('titleField')}
-            suppressContentEditableWarning>
-            {titleField || title}
-          </a>
-        </h1>
-        <p
-          className="extraordinary-l-score__subtitle text-white hidden md:block text-lg line-h-sm mb-20"
-          {...editableField('subTitleField')}
-          suppressContentEditableWarning>
-          {subTitleField || subTitle}
-        </p>
-      </div>
-    </div>
-  )
+  const params = {
+    isAdmin,
+    title,
+    subTitle,
+    websiteLink,
+    multimediaLazyDefault,
+    imgUrl,
+    codeField,
+    titleField,
+    subTitleField,
+    isLive,
+    firstName,
+    firstScore,
+    secondName,
+    secondScore,
+  }
+
+  return <LiveScoreChild {...params} />
 }
 
 ExtraordinaryStoryLifeScore.label = 'Apertura extraordinaria - En vivo'
