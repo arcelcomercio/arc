fc8719de3836b15cbc5177afe5712e81099cf3a7
cajotafer <hello@cajotafer.com>
2020-03-18 05:50:19 +0700

    Fix: grid-to-flex cross-browser (#21215)

--- components/utilities/grid-to-flex.js
+++ components/utilities/grid-to-flex.js
@@ -4,8 +4,9 @@
     const msie = ua.indexOf('MSIE ')
     const trident = ua.indexOf('Trident/')
     if (msie > 0 || trident > 0) {
-      document.querySelectorAll('.grid').forEach(grid => {
-        grid.classList.replace('grid', 'ie-flex')
+      ;[].slice.call(document.getElementsByClassName('grid')).forEach(grid => {
+        // eslint-disable-next-line no-param-reassign
+        grid.className = grid.className.replace('grid', 'ie-flex')
       })
     }
   }, 0)
