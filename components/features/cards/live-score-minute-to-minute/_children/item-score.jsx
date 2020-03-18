d5275627a6bcf7ea733a87e39cf0300203eb7863
Edgar Gamarra <edgar.gamarra21@gmail.com>
2020-03-18 16:50:15 -0500

    New: se agregaron los ids para el componente minuto a minuto

--- components/features/cards/live-score-minute-to-minute/_children/item-score.jsx
+++ components/features/cards/live-score-minute-to-minute/_children/item-score.jsx
@@ -4,14 +4,22 @@ import { FIXTURESTATE, POSTPONEDSTATE } from '../../../../utilities/constants'
 const classes = {
   scoreItem: 'score__team-number font-bold text-black primary-font',
 }
-const ItemScore = ({ scoreTeam = 0, matchstatus = '' }) => {
+const ItemScore = ({ scoreTeam = 0, matchstatus = '', homeTeam }) => {
   const textStatusValidation = [FIXTURESTATE, POSTPONEDSTATE].includes(
     matchstatus
   )
   return (
-    <div className={classes.scoreItem}>
-      {!textStatusValidation ? scoreTeam : ''}
-    </div>
+    <>
+      {homeTeam ? (
+        <div className={classes.scoreItem} id="hometeamScoreTextId">
+          {!textStatusValidation ? scoreTeam : ''}
+        </div>
+      ) : (
+        <div className={classes.scoreItem} id="awayteamScoreTextId">
+          {!textStatusValidation ? scoreTeam : ''}
+        </div>
+      )}
+    </>
   )
 }
 
--- components/features/cards/live-score-minute-to-minute/_children/item-team-name.jsx
+++ components/features/cards/live-score-minute-to-minute/_children/item-team-name.jsx
@@ -5,10 +5,18 @@ const classes = {
   scoreDetails: 'score__team-details flex items-center',
   scoreName: 'score__team-name font-bold text-black primary-font text-center',
 }
-const ItemTeamName = ({ name, flag }) => {
+const ItemTeamName = ({ name, flag, homeTeam }) => {
   return (
     <div className={classes.scoreDetails}>
-      <div className={classes.scoreName}>{name}</div>
+      {homeTeam ? (
+        <div className={classes.scoreName} id="homeTeamNameId">
+          {name}
+        </div>
+      ) : (
+        <div className={classes.scoreName} id="awayTeamNameId">
+          {name}
+        </div>
+      )}
       <ItemTeamFlag flag={flag} />
     </div>
   )
