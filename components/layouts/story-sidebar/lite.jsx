b465c64af0ffeb823bdb38ffc5bb5e1906e7f9fe
cajotafer <hello@cajotafer.com>
2020-03-24 07:25:46 +0700

    Update: estilos para layout/story-sidebar (#21542)

--- components/layouts/story-sidebar/lite.jsx
+++ components/layouts/story-sidebar/lite.jsx
@@ -2,23 +2,32 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const classes = {
-  layout: 'flex justify-center',
-  contentContainer:
-    'flex flex-col content-layout-container w-full position-relative bg-container',
+  layout: 'f just-center',
+  contentContainer: 'f f-col w-full pos-rel',
   separator: 'w-full ',
   heading: 'w-full',
-  content: 'content-sidebar flex mt-20 mb-20 ',
-  main: 'content-sidebar__left',
+  content: 'st-sidebar__content f',
+  main: 'st-sidebar__main',
+  sidebar: 'st-sidebar__side',
+  zocalo: 'ads__zocalo',
 }
 
 const StorySidebarLayout = ({ children = [] }) => {
   return (
     <>
+      {children[0] /* Barra de navegación Amp */}
       <div className={classes.layout}>
+        <div className={classes.zocalo}>
+          {children[1] /* Zocalo izquierda */}
+        </div>
         <div className={classes.contentContainer}>
           {children[2] /* Publicidad Top */}
           {children[3] /*  Barra de navegación */}
+
           {children[4] /* Cabecera de página */}
+          {children[5] && (
+            <div className={classes.separator}>{children[5]}</div>
+          ) /* Separador */}
           <section className={classes.content}>
             <div role="banner" className={classes.heading}>
               {children[6] /* Encabezado */}
@@ -26,8 +35,17 @@ const StorySidebarLayout = ({ children = [] }) => {
             <div role="main" className={classes.main}>
               {children[7] /* Contenido */}
             </div>
+            {children[8] && (
+              <aside className={classes.sidebar}>
+                {children[8] /* Barra lateral */}
+              </aside>
+            )}
+            {children[9] /* Contenido adicional */}
           </section>
           {children[10] /* Pie de página */}
+        </div>
+        <div className={classes.zocalo}>
+          {children[11] /* Zocalo derecha */}
         </div>
       </div>
     </>
