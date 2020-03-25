3bf61226949aecf743d5785c69ab665dbc48f465
Alvaro <ahuamani@rmgperustaff.com>
2020-03-25 16:07:28 -0500

    Update: agregado estilos y funcionalidad del sticky para el navbar básico (#21633)

--- components/features/header/basic/_dependencies/scripts.js
+++ components/features/header/basic/_dependencies/scripts.js
@@ -18,6 +18,41 @@
     }
   })
 }) */
-// eslint-disable-next-line import/prefer-default-export
 export const searchScript =
-  '"use strict";document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("h-basic_search-btn");e.addEventListener("click",function(){var t=document.getElementById("h-basic_search-input"),n=document.getElementById("h-basic_search-path");if(t.value){var d=encodeURIComponent(t.value).replace(/%20/g,"+");window.location.href="/buscar/".concat(d,"/todas/descendiente/?query=").concat(d)}else"150px"===t.style.width?(t.style="",e.style="",n.style=""):(t.style="width:150px;padding: 5px 8px;",e.style="background-color: white;border-top-right-radius: 4px;border-bottom-right-radius: 4px;",n.style="fill: #575757;")})});'
+  'document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("h-basic_search-btn");e.addEventListener("click",function(){var t=document.getElementById("h-basic_search-input"),n=document.getElementById("h-basic_search-path");if(t.value){var d=encodeURIComponent(t.value).replace(/%20/g,"+");window.location.href="/buscar/".concat(d,"/todas/descendiente/?query=").concat(d)}else"150px"===t.style.width?(t.style="",e.style="",n.style=""):(t.style="width:150px;padding: 5px 8px;",e.style="background-color: white;border-top-right-radius: 4px;border-bottom-right-radius: 4px;",n.style="fill: #575757;")})});'
+
+/* document.addEventListener('DOMContentLoaded', () => {
+  if ('IntersectionObserver' in window) {
+    const headerPointer = document.getElementById('h-basic-pointer')
+    const header = document.getElementById('h-basic')
+    const sectionOneObserver = new IntersectionObserver(function(entries) {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          header.classList.add('scrolled')
+          headerPointer.classList.add('scrolled')
+        } else {
+          header.classList.remove('scrolled')
+          headerPointer.classList.remove('scrolled')
+        }
+      })
+    })
+    sectionOneObserver.observe(headerPointer)
+  } else {
+    const header = document.getElementById('h-basic')
+    window.addEventListener('scroll', () => {
+      const { body = {}, documentElement = {} } = document
+      const { scrollTop: scrollBody = 0 } = body
+      const { scrollTop: scrollElement = 0 } = documentElement
+      const scroll = scrollBody || scrollElement
+  
+      const headerTop = 10
+      if (scroll > headerTop && !header.classList.contains('scrolled')) {
+        header.classList.add('scrolled')
+      } else if (scroll <= headerTop && header.classList.contains('scrolled')) {
+        header.classList.remove('scrolled')
+      }
+    })
+  }
+})   */
+export const stickyScript =
+  ';document.addEventListener("DOMContentLoaded",function(){if("IntersectionObserver"in window){var e=document.getElementById("h-basic-pointer"),s=document.getElementById("h-basic");new IntersectionObserver(function(o){o.forEach(function(o){o.isIntersecting?(s.classList.remove("scrolled"),e.classList.remove("scrolled")):(s.classList.add("scrolled"),e.classList.add("scrolled"))})}).observe(e)}else{var o=document.getElementById("h-basic");window.addEventListener("scroll",function(){var e=document,s=e.body,t=void 0===s?{}:s,c=e.documentElement,n=void 0===c?{}:c,d=t.scrollTop,l=void 0===d?0:d,i=n.scrollTop,r=l||(void 0===i?0:i);r>10&&!o.classList.contains("scrolled")?o.classList.add("scrolled"):r<=10&&o.classList.contains("scrolled")&&o.classList.remove("scrolled")})}});'
