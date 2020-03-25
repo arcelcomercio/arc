0f8cd837bcccdce82bf2f13ecf412c1131317dba
cajotafer <hello@cajotafer.com>
2020-03-25 13:26:24 +0700

    New: feature header/links-band para version Lite (#21631)

---
+++ components/features/header/links-band/_children/links-band.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const classes = {
+  band: 'links-band f w-full',
+}
+
+const HeaderLinksBandChild = ({ links = [], tags = 'Hoy interesa:' }) => {
+  return (
+    links &&
+    links.length > 0 && (
+      <>
+        <nav className={classes.band}>
+          {tags && <h4>{tags}</h4>}
+          {links.map(({ url, name }) => (
+            <a href={url} key={`band-${url}`}>
+              {name}
+            </a>
+          ))}
+        </nav>
+      </>
+    )
+  )
+}
+
+/* 
+  EN CASO DE QUE SE PUEDA USAR React.memo LUEGO
+
+  const compare = (prevProps, nextProps) => {
+  const { links = [], tags = '' } = prevProps
+  const { links: nextLinks = [], tags: nextTags = '' } = nextProps
+  let memo = true
+
+  if (tags !== nextTags) return false
+  links.forEach((link, i) => {
+    const { _id: id, url } = link
+    const { _id: nextId, url: nextUrl} = nextLinks[i]
+    if(id !== nextId || url !== nextUrl) memo = false
+  })
+  return memo
+}
+ */
+
+export default HeaderLinksBandChild
