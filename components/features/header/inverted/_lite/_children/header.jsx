56b99b221e084e48a1d26ce62fcc5ffc138c3ac6
cajotafer <hello@cajotafer.com>
2020-04-01 05:51:03 +0700

    Update: + a11y para header/inverted en version Lite (#22214)

--- components/features/header/inverted/_lite/_children/header.jsx
+++ components/features/header/inverted/_lite/_children/header.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /**
  * SVG optimizados con https://petercollingridge.appspot.com/svg-optimiser
  */
@@ -32,6 +34,9 @@ export default props => {
             placeholder="¿Qué Buscas?"
             id="h-basic_search-input"
           />
+          <label htmlFor="h-basic_search-input" className="hidden-label">
+            Cuadro de búsqueda
+          </label>
           <button
             id="h-basic_search-btn"
             type="button"
@@ -57,11 +62,14 @@ export default props => {
               className="h-basic__menu"
               height="23"
               viewBox="0 0 24 24">
+              <title>Menú</title>
               <path d="M4 6h16c0.6 0 1 0.5 1 1l0 0c0 0.6-0.4 1-1 1H4C3.5 8 3 7.6 3 7l0 0C3 6.5 3.5 6 4 6z" />
               <path d="M4 11h16c0.6 0 1 0.5 1 1l0 0c0 0.6-0.4 1-1 1H4c-0.5 0-1-0.4-1-1l0 0C3 11.5 3.5 11 4 11z" />
               <path d="M4 16h16c0.6 0 1 0.5 1 1l0 0c0 0.6-0.4 1-1 1H4c-0.5 0-1-0.4-1-1l0 0C3 16.5 3.5 16 4 16z" />
             </svg>
-            <span className="h-basic__menu-txt">Menú</span>
+            <span className="h-basic__menu-txt" aria-hidden="true">
+              Menú
+            </span>
           </button>
         </div>
 
@@ -82,11 +90,14 @@ export default props => {
           </button>
 
           <button type="button" className="h-basic__btn-user h-basic__btn">
-            <span className="h-basic__user-txt">INGRESA</span>
+            <span className="h-basic__user-txt" aria-hidden="true">
+              INGRESA
+            </span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-basic__user"
               viewBox="0 0 24 24">
+              <title>Iniciar sesión / Perfil</title>
               <path d="M12 2c-5.5 0-10 4.5-10 10 0 5.5 4.5 10 10 10 5.5 0 10-4.5 10-10C22 6.5 17.5 2 12 2zM12 20.2c-1.8 0-3.5-0.6-4.9-1.7 2.8-2.6 7.1-2.6 9.9 0C15.5 19.6 13.8 20.2 12 20.2zM9.3 12c0-1.5 1.2-2.7 2.7-2.7 1.5 0 2.7 1.2 2.7 2.7s-1.2 2.7-2.7 2.7C10.5 14.7 9.3 13.5 9.3 12zM18.3 17.3c-0.9-0.9-2-1.5-3.1-2 1.8-1.7 1.9-4.6 0.2-6.4 -1.7-1.8-4.6-1.9-6.4-0.2 -1.8 1.7-1.9 4.6-0.2 6.4 0.1 0.1 0.1 0.1 0.2 0.2 -1.2 0.4-2.2 1.1-3.1 2C2.8 13.8 3.3 8.6 6.7 5.7c3.5-2.9 8.6-2.4 11.5 1C20.8 9.8 20.8 14.2 18.3 17.3L18.3 17.3z" />
             </svg>
           </button>
