d98e89f22edcb0834fd29d5156f30b48aebbe48b
cajotafer <hello@cajotafer.com>
2020-04-02 08:17:19 +0700

    Fix: lite-ads script (#22069)

--- components/output-types/_children/lite-ads.jsx
+++ components/output-types/_children/lite-ads.jsx
@@ -8,10 +8,10 @@ const LiteAds = () => {
 
       adsSlots.forEach((ad = {}) => {
         const adName = ad.getAttribute('data-ads-name')
-        const adDimensions = ad.getAttribute('data-ads-dimensions')
+        const adDimensions = JSON.parse(ad.getAttribute('data-ads-dimensions'))
         const adId = ad.id
         googletag.cmd.push(function() {
-          googletag.defineSlot(adName, adDimensions, adId)
+          googletag.defineSlot(adName, adDimensions, adId).addService(googletag.pubads());
           googletag.pubads().enableSingleRequest()
           googletag.pubads().collapseEmptyDivs()
           googletag.enableServices()
@@ -36,17 +36,33 @@ const LiteAds = () => {
             const adsSlots = [...document.querySelectorAll('div[data-ads-name]')]
             if (adsSlots && adsSlots.length > 0) {
               window.googletag = window.googletag || { cmd: [] }
-        
+              
               adsSlots.forEach((ad = {}) => {
                 const adName = ad.getAttribute('data-ads-name')
-                const adDimensions = ad.getAttribute('data-ads-dimensions')
+                const adDimensions = JSON.parse(ad.getAttribute('data-ads-dimensions'))
                 const adId = ad.id
                 googletag.cmd.push(function() {
-                  googletag.defineSlot(adName, adDimensions, adId)
-                  googletag.pubads().enableSingleRequest()
-                  googletag.pubads().collapseEmptyDivs()
-                  googletag.enableServices()
+                  googletag.defineSlot(adName, adDimensions, adId).addService(googletag.pubads());
                 })
+              })
+          
+              googletag.cmd.push(function() {
+                googletag.pubads().enableSingleRequest()
+                /* googletag.pubads().enableLazyLoad({
+                  fetchMarginPercent: 400,  // Fetch slots within 4 viewports.
+                  renderMarginPercent: 200,  // Render slots within 2 viewports.
+                  mobileScaling: 2.0  // Double the above values on mobile.
+                }); 
+                
+                Si se activa Lazyload para probar, se debe deshabilitar 
+                .enableSingleRequest() y .collapseEmptyDivs()
+                */
+                googletag.pubads().collapseEmptyDivs()
+                googletag.enableServices()
+              })
+          
+              adsSlots.forEach((ad = {}) => {
+                const adId = ad.id
                 googletag.cmd.push(function() {
                   googletag.display(adId)
                 })
