bc4601c29e1095ba0396f12be98585bcd3af7623
Rolly <rolly.sanchez.26@gmail.com>
2020-04-21 17:16:28 -0500

    Update: agregando resize de imagenes para el listado links en el contenido de la nota (#23130)

--- components/features/story/contents/_children/link-list.jsx
+++ components/features/story/contents/_children/link-list.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { getResizedUrl } from '../../../../utilities/resizer'
 
 const classes = {
-  container: 'story-content__link-list position-relative p-20 mb-20 mt-20 mr-20',
+  container:
+    'story-content__link-list position-relative p-20 mb-20 mt-20 mr-20',
   title: 'story-content__link-list-title uppercase mb-20',
   multimedia: 'story-content__link-list-figure position-relative',
   image: 'w-full h-full lazy',
@@ -10,14 +12,28 @@ const classes = {
   titleLink: 'story-content__link-list-title-link underline font-bold',
 }
 
-const Item = ({ url, title, image }) => {
+const Item = ({ url, title, image, imageDefault, site }) => {
+  const presets = 'small:96x64'
+  const extractImage = urlImg => {
+    if (typeof window === 'undefined') {
+      return (
+        getResizedUrl({
+          url: urlImg,
+          presets,
+          arcSite: site,
+        }) || {}
+      )
+    }
+    return urlImg
+  }
+
   return (
     <div className={classes.item}>
       <figure className={classes.multimedia}>
         <a href={url}>
           <img
-            src={image}
-            data-src={image}
+            src={imageDefault}
+            data-src={extractImage(image).small || imageDefault}
             alt={title}
             className={classes.image}
           />
@@ -32,7 +48,7 @@ const Item = ({ url, title, image }) => {
   )
 }
 
-function LinkList({ items}) {
+function LinkList({ items, multimediaLazyDefault, arcSite }) {
   return (
     <div className={classes.container}>
       <div className={classes.title}>Mira también:</div>
@@ -40,7 +56,15 @@ function LinkList({ items}) {
         items.map(data => {
           const { url = '', content = '', image: { url: urlImg = '' } = {} } =
             data || {}
-          return <Item url={url} title={content} image={urlImg} />
+          return (
+            <Item
+              url={url}
+              title={content}
+              image={urlImg}
+              imageDefault={multimediaLazyDefault}
+              site={arcSite}
+            />
+          )
         })}
     </div>
   )
