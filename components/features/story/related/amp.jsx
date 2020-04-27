d4a94333476102efccdaf11cfa8ffaf17ec177cb
henrry11985 <henrry11985@gmail.com>
2020-04-27 14:46:01 -0500

    Update: retira la carga continua de AMP para peru21/ se cambia los iconos de redes sociales(#23526)

--- components/features/story/related/amp.jsx
+++ components/features/story/related/amp.jsx
@@ -8,7 +8,7 @@ import { getResizedUrl } from '../../../utilities/resizer'
 
 const classes = {
   storyInterest:
-    'amp-story-interest flex flex-col w-full h-auto pr-20 pl-20 mx-auto amp-story-header',
+    'amp-story-interest flex flex-col w-full h-auto pr-20 pl-20 mx-auto amp-sh',
   title:
     'amp-story-interest__titleList block w-full h-auto font-bold mb-10 uppercase p-15 text-center md:text-left',
 }
