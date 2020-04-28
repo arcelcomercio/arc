db8c2f7e13159b8529b8b69314318a6e46325822
henrry11985 <henrry11985@gmail.com>
2020-04-28 16:04:45 -0500

    Update: gregar carga diferida (async) al script de floor prices de depor.com / fix videos(#23618)

--- components/output-types/_children/ads-scripts/floor-prices.jsx
+++ components/output-types/_children/ads-scripts/floor-prices.jsx
@@ -2,19 +2,24 @@ import React from 'react'
 import { useFusionContext } from 'fusion:context'
 
 export default () => {
-  const { globalContent = {}, arcSite } = useFusionContext()
-  const {
+  const { arcSite } = useFusionContext()
+
+  /* const {
     section_id: sectionId,
     _id,
     taxonomy: { primary_section: { path: primarySection } = {} } = {},
   } = globalContent || {}
 
-  const section = (primarySection || sectionId || _id || '').split('/')[1]
+   const section = (primarySection || sectionId || _id || '').split('/')[1]
+*/
 
   return (
     <>
       {arcSite === 'depor' && (
-        <script src="https://storage.googleapis.com/acn-comercio-peru-floor-prices-dev/comercioperu/web-script/ayos-pro-comercio.js" />
+        <script
+          async
+          src="https://storage.googleapis.com/acn-comercio-peru-floor-prices-dev/comercioperu/web-script/ayos-pro-comercio.js"
+        />
       )}
       {arcSite === 'peru21' && (
         <script src="https://storage.googleapis.com/acn-comercio-peru-floor-prices-dev/comercioperu/web-script/ayos-opt.js" />
