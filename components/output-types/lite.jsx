be18f7f5d7ea082b5c05355331093a1812028f93
cajotafer <hello@cajotafer.com>
2020-05-26 00:46:30 +0700

    Fix: modificaciones en cabecera lite por subs y signwall (#24303)(CORETEAM)

--- components/output-types/lite.jsx
+++ components/output-types/lite.jsx
@@ -248,6 +248,26 @@ const LiteOutput = ({
   return (
     <html lang="es">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          if(typeof window !== "undefined"){
+            window.requestIdle = window.requestIdleCallback ||
+            function (cb) {
+              const start = Date.now();
+              return setTimeout(function () {
+                cb({
+                  didTimeout: false,
+                  timeRemaining: function () {
+                    return Math.max(0, 50 - (Date.now() - start));
+                  },
+                });
+              }, 1);
+            };
+          }
+          `,
+          }}
+        />
         <TagManager {...parameters} />
 
         <meta charSet="utf-8" />
