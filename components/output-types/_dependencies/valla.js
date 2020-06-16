76598a851c870313384d38ad3ccd883e5d2ac529
George <cocotopia@gmail.com>
2020-06-16 01:19:39 -0500

    Update: implementacion de extend sesion y refresh token popup version lite

--- components/output-types/_dependencies/valla.js
+++ components/output-types/_dependencies/valla.js
@@ -1,5 +1,4 @@
 // TODO: Agregar la lógica sin minificar de este script, no son iguales
-
 /*
 const arcEnv = '${arcEnv}'
 const arcSite = '${arcSite}'
@@ -22,6 +21,58 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
 
+    const postExtendSession = oldToken => {
+      const response = new Promise(resolve => {
+        fetch(
+          "https://api${arcEnv === 'sandbox' ? '-sandbox' : ''}.${arcSite}.pe/identity/public/v1/auth/token",
+          {
+            method: 'POST',
+            body: JSON.stringify({
+              grantType: 'refresh-token',
+              token: oldToken,
+            }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        ).then(res => resolve(res.json()))
+      })
+      return response
+    }
+
+    const getEntitlement = newToken => {
+      const response = new Promise(resolve => {
+        fetch(
+          "https://api${arcEnv === 'sandbox' ? '-sandbox' : ''}.${arcSite}.pe/sales/public/v1/entitlements",
+          {
+            method: 'GET',
+            headers: {
+              Authorization: newToken,
+            },
+          }
+        ).then(res => resolve(res.json()))
+      })
+      return response
+    }
+
+    const getListSubs = oldRefreshToken => {
+      return postExtendSession(oldRefreshToken).then(resExt => {
+        const checkEntitlement = getEntitlement(resExt.accessToken)
+          .then(res => {
+            if (res.skus) {
+              const result = Object.keys(res.skus).map(key => {
+                return res.skus[key].sku
+              })
+              return result
+            }
+            return []
+          })
+          .catch(err => window.console.error(err))
+
+        return checkEntitlement
+      })
+    }
+
     const checkSession = () => {
       if (typeof window !== 'undefined') {
         const profileStorage =
@@ -36,10 +87,6 @@ document.addEventListener('DOMContentLoaded', () => {
       return false
     }
 
-    const checkCookieMigration = () => {
-      document.cookie = `ArcId.USER_INFO=;path=/;domain=.${arcSite}.pe; expires=Thu, 01 Jan 1970 00:00:01 GMT`
-    }
-
     const getUrlSignwall = (typeDialog = 'signwallHard', hash = '1') => {
       return arcEnv === 'prod'
         ? `/signwall/?outputType=signwall&${typeDialog}=${hash}`
@@ -50,6 +97,18 @@ document.addEventListener('DOMContentLoaded', () => {
       return arcEnv === 'prod'
         ? `/suscripcionesdigitales/`
         : `/pf/suscripcionesdigitales/?_website=${arcSite}&outputType=paywall`
+    }
+
+    const getUrlLandingAuth = () => {
+      return arcEnv === 'prod'
+        ? '/auth-fia/?outputType=signwall'
+        : `/pf/auth-fia/?_website=${arcSite}&outputType=signwall`
+    }
+
+    const getUrlSignwallHash = () => {
+      return arcEnv === 'prod'
+        ? `/signwall/?outputType=signwall&reloginHash=1`
+        : `/signwall/?_website=${arcSite}&outputType=signwall&reloginHash=1`
     }
 
     const getBoottonclick = () => {
@@ -70,45 +129,122 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
 
-    const dataContentPremium = window.content_paywall
-    if (dataContentPremium !== 2 && dataContentPremium !== true) {
-      const dataContTyp = document.querySelector('meta[name="content-type"]')
-      const dataContSec = document.querySelector('meta[name="section-id"]')
-      const userInfo =
-        window.JSON.parse(window.localStorage.getItem('ArcId.USER_INFO')) || {}
-      window.ArcPOptions = {
-        paywallFunction: function(campaign) {
-          if (campaign.match(/signwallHard/) && !checkSession()) {
-            window.location.href = getUrlSignwall()
-          } else if (campaign.match(/signwallPaywall/) && checkSession()) {
-            const signwall = document.querySelector('#signwall-app')
-            signwall.className = 'active'
-            const elementosObtenidos = document.getElementsByTagName('body')
-            elementosObtenidos[0].style.overflow = 'hidden'
-            Taggeo('web_paywall_open')
-            getBoottonclick()
-          }
-        },
-        contentType: dataContTyp ? dataContTyp.getAttribute('content') : 'none',
-        section: dataContSec ? dataContSec.getAttribute('content') : 'none',
-        userName: userInfo.uuid || null,
-        jwt: userInfo.accessToken || null,
-        apiOrigin: "api${arcEnv === 'sandbox' ? '-sandbox' : ''}.${arcSite}.pe",
-      }
-      const script = document.createElement('script')
-      script.src = `https://elcomercio-${arcSite}-${arcEnv}.cdn.arcpublishing.com/arc/subs/p.js?v=${getdata}` // al pasar por babel debe quedar asi https://elcomercio-${arcSite}-${arcEnv}.cdn.arcpublishing.com/arc/subs/p.js?v=${getdata}
-      script.async = 'true'
-      document.head.appendChild(script)
+    const getCookie = name => {
+      const v = document.cookie.match(`(^|;) ?${name}=([^;]*)(;|$)`)
+      return v ? v[2] : null
     }
-    if (!checkSession()) {
-      checkCookieMigration()
+
+    const getQuery = name => {
+      const vars = {}
+      if (typeof window !== 'undefined') {
+        window.location.href.replace(
+          /[?&]+([^=&]+)=([^&]*)/gi,
+          (m, key, value) => {
+            vars[key] = value
+          }
+        )
+      }
+      return vars[name]
+    }
+
+    const checkCookieHash = () => {
+      if (typeof window !== 'undefined') {
+        window.document.cookie = `ArcId.USER_INFO=;path=/;domain=.${arcSite}.pe; expires=Thu, 01 Jan 1970 00:00:01 GMT`
+        const dataContType = window.document.querySelector(
+          'meta[name="content-type"]'
+        )
+        if (getCookie('arc_e_id') && dataContType) {
+          window.location.href = getUrlSignwallHash()
+        }
+      }
+      return null
+    }
+
+    const iOS =
+      /iPad|iPhone|iPod/.test(window.navigator.userAgent) && !window.MSStream
+    if (iOS && getQuery('surface') === 'meter_limit_reached') {
+      const artURL = decodeURIComponent(getQuery('article_url') || '')
+      window.sessionStorage.setItem('paywall_last_url', artURL)
+      window.location.href = getUrlLandingAuth()
+    } else {
+      const dataContentPremium = window.content_paywall
+      if (dataContentPremium !== 2 && dataContentPremium !== true) {
+        const dataContTyp = document.querySelector('meta[name="content-type"]')
+        const dataContSec = document.querySelector('meta[name="section-id"]')
+        const userInfo =
+          window.JSON.parse(window.localStorage.getItem('ArcId.USER_INFO')) ||
+          {}
+        window.ArcPOptions = {
+          paywallFunction: function(campaign) {
+            if (campaign.match(/signwallHard/) && !checkSession()) {
+              window.location.href = getUrlSignwall()
+            } else if (campaign.match(/signwallPaywall/) && checkSession()) {
+              const signwall = document.querySelector('#signwall-app')
+              signwall.className = 'active'
+              const elementosObtenidos = document.getElementsByTagName('body')
+              elementosObtenidos[0].style.overflow = 'hidden'
+              Taggeo('web_paywall_open')
+              getBoottonclick()
+            }
+          },
+          contentType: dataContTyp
+            ? dataContTyp.getAttribute('content')
+            : 'none',
+          section: dataContSec ? dataContSec.getAttribute('content') : 'none',
+          userName: userInfo.uuid || null,
+          jwt: userInfo.accessToken || null,
+          apiOrigin:
+            "https://api${arcEnv === 'sandbox' ? '-sandbox' : ''}.${arcSite}.pe",
+          customSubCheck: () => {
+            if (userInfo.accessToken) {
+              return getListSubs(userInfo.refreshToken).then(p => {
+                const isLoggedInSubs = checkSession()
+                return {
+                  s: isLoggedInSubs,
+                  p: p || null,
+                  timeTaken: 100,
+                  updated: Date.now(),
+                }
+              })
+            }
+            return {
+              s: false,
+              p: null,
+              timeTaken: 100,
+              updated: Date.now(),
+            }
+          },
+          customRegCheck: () => {
+            const start = Date.now()
+            const isLoggedIn = checkSession()
+            return Promise.resolve({
+              l: isLoggedIn,
+              timeTaken: Date.now() - start,
+              updated: Date.now(),
+            })
+          },
+        }
+
+        const script = document.createElement('script')
+        script.src = `https://elcomercio-${arcSite}-${arcEnv}.cdn.arcpublishing.com/arc/subs/p.js?v=${getdata}` // al pasar por babel debe quedar asi https://elcomercio-${arcSite}-${arcEnv}.cdn.arcpublishing.com/arc/subs/p.js?v=${getdata}
+        script.async = 'true'
+        document.head.appendChild(script)
+      }
+      if (!checkSession()) {
+        checkCookieHash()
+      }
     }
   })
-}) */
+})
+*/
 
 const vallaSignwall = ({ arcEnv, arcSite, getdata }) =>
-  `"use strict";var arcEnv="${arcEnv}",arcSite="${arcSite}",getdata="${getdata}";document.addEventListener("DOMContentLoaded",function(){window.requestIdle(function(){var e=function(e){if("undefined"!=typeof window){window.dataLayer=window.dataLayer||[];var n={event:"tag_signwall",eventCategory:"Web_Paywall_Hard",eventAction:e};window.dataLayer.push(n),"sandbox"===arcEnv&&window.console.log(n)}},n=function(){if("undefined"!=typeof window){var e=window.localStorage.getItem("ArcId.USER_PROFILE")||window.sessionStorage.getItem("ArcId.USER_PROFILE"),n=window.localStorage.getItem("ArcId.USER_INFO");if(e)return!("null"===e||"{}"===n)||!1}return!1},t=function(){var n=document.getElementById("btn-close-paywall"),t=document.getElementById("btn-ver-planes");n.onclick=function(){e("web_paywall_cerrar"),window.location.href="/?signwallPaywall=1&ref=".concat(window.location.pathname)},t.onclick=function(){e("web_paywall_boton_ver_planes"),window.sessionStorage.setItem("paywall_type_modal","paywall"),window.sessionStorage.setItem("paywall_last_url",window.location.pathname),window.location.href="prod"===arcEnv?"/suscripcionesdigitales/":"/pf/suscripcionesdigitales/?_website=".concat(arcSite,"&outputType=paywall")}},a=window.content_paywall;if(2!==a&&!0!==a){var o=document.querySelector('meta[name="content-type"]'),c=document.querySelector('meta[name="section-id"]'),i=window.JSON.parse(window.localStorage.getItem("ArcId.USER_INFO"))||{};window.ArcPOptions={paywallFunction:function(a){if(a.match(/signwallHard/)&&!n())window.location.href=function(){var e=arguments.length>0&&void 0!==arguments[0]?arguments[0]:"signwallHard",n=arguments.length>1&&void 0!==arguments[1]?arguments[1]:"1";return"prod"===arcEnv?"/signwall/?outputType=signwall&".concat(e,"=").concat(n):"/signwall/?_website=".concat(arcSite,"&outputType=signwall&").concat(e,"=").concat(n)}();else if(a.match(/signwallPaywall/)&&n()){document.querySelector("#signwall-app").className="active",document.getElementsByTagName("body")[0].style.overflow="hidden",e("web_paywall_open"),t()}},contentType:o?o.getAttribute("content"):"none",section:c?c.getAttribute("content"):"none",userName:i.uuid||null,jwt:i.accessToken||null,apiOrigin:"api${
+  `"use strict";var arcEnv="${arcEnv}",arcSite="${arcSite}",getdata="${getdata}";document.addEventListener("DOMContentLoaded",function(){window.requestIdle(function(){var e=function(e){if("undefined"!=typeof window){window.dataLayer=window.dataLayer||[];var n={event:"tag_signwall",eventCategory:"Web_Paywall_Hard",eventAction:e};window.dataLayer.push(n),"sandbox"===arcEnv&&window.console.log(n)}},n=function(e){return(n=e,new Promise(function(e){fetch("https://api${
     arcEnv === 'sandbox' ? '-sandbox' : ''
-  }.${arcSite}.pe"};var l=document.createElement("script");l.src="https://elcomercio-".concat(arcSite,"-").concat(arcEnv,".cdn.arcpublishing.com/arc/subs/p.js?v=").concat(getdata),l.async="true",document.head.appendChild(l)}n()||(document.cookie="ArcId.USER_INFO=;path=/;domain=.".concat(arcSite,".pe; expires=Thu, 01 Jan 1970 00:00:01 GMT"))})});`
+  }.${arcSite}.pe/identity/public/v1/auth/token",{method:"POST",body:JSON.stringify({grantType:"refresh-token",token:n}),headers:{"Content-Type":"application/json"}}).then(function(n){return e(n.json())})})).then(function(e){var n;return(n=e.accessToken,new Promise(function(e){fetch("https://api${
+    arcEnv === 'sandbox' ? '-sandbox' : ''
+  }.${arcSite}.pe/sales/public/v1/entitlements",{method:"GET",headers:{Authorization:n}}).then(function(n){return e(n.json())})})).then(function(e){return e.skus?Object.keys(e.skus).map(function(n){return e.skus[n].sku}):[]}).catch(function(e){return window.console.error(e)})});var n},t=function(){if("undefined"!=typeof window){var e=window.localStorage.getItem("ArcId.USER_PROFILE")||window.sessionStorage.getItem("ArcId.USER_PROFILE"),n=window.localStorage.getItem("ArcId.USER_INFO");if(e)return!("null"===e||"{}"===n)||!1}return!1},a=function(){var n=document.getElementById("btn-close-paywall"),t=document.getElementById("btn-ver-planes");n.onclick=function(){e("web_paywall_cerrar"),window.location.href="/?signwallPaywall=1&ref=".concat(window.location.pathname)},t.onclick=function(){e("web_paywall_boton_ver_planes"),window.sessionStorage.setItem("paywall_type_modal","paywall"),window.sessionStorage.setItem("paywall_last_url",window.location.pathname),window.location.href="prod"===arcEnv?"/suscripcionesdigitales/":"/pf/suscripcionesdigitales/?_website=".concat(arcSite,"&outputType=paywall")}},o=function(e){var n={};return"undefined"!=typeof window&&window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(e,t,a){n[t]=a}),n[e]};if(/iPad|iPhone|iPod/.test(window.navigator.userAgent)&&!window.MSStream&&"meter_limit_reached"===o("surface")){var i=decodeURIComponent(o("article_url")||"");window.sessionStorage.setItem("paywall_last_url",i),window.location.href="prod"===arcEnv?"/auth-fia/?outputType=signwall":"/pf/auth-fia/?_website=".concat(arcSite,"&outputType=signwall")}else{var c=window.content_paywall;if(2!==c&&!0!==c){var r=document.querySelector('meta[name="content-type"]'),l=document.querySelector('meta[name="section-id"]'),s=window.JSON.parse(window.localStorage.getItem("ArcId.USER_INFO"))||{};window.ArcPOptions={paywallFunction:function(n){if(n.match(/signwallHard/)&&!t())window.location.href=function(){var e=arguments.length>0&&void 0!==arguments[0]?arguments[0]:"signwallHard",n=arguments.length>1&&void 0!==arguments[1]?arguments[1]:"1";return"prod"===arcEnv?"/signwall/?outputType=signwall&".concat(e,"=").concat(n):"/signwall/?_website=".concat(arcSite,"&outputType=signwall&").concat(e,"=").concat(n)}();else if(n.match(/signwallPaywall/)&&t()){document.querySelector("#signwall-app").className="active",document.getElementsByTagName("body")[0].style.overflow="hidden",e("web_paywall_open"),a()}},contentType:r?r.getAttribute("content"):"none",section:l?l.getAttribute("content"):"none",userName:s.uuid||null,jwt:s.accessToken||null,apiOrigin:"https://api${
+    arcEnv === 'sandbox' ? '-sandbox' : ''
+  }.${arcSite}.pe",customSubCheck:function(){return s.accessToken?n(s.refreshToken).then(function(e){return{s:t(),p:e||null,timeTaken:100,updated:Date.now()}}):{s:!1,p:null,timeTaken:100,updated:Date.now()}},customRegCheck:function(){var e=Date.now(),n=t();return Promise.resolve({l:n,timeTaken:Date.now()-e,updated:Date.now()})}};var d=document.createElement("script");d.src="https://elcomercio-".concat(arcSite,"-").concat(arcEnv,".cdn.arcpublishing.com/arc/subs/p.js?v=").concat(getdata),d.async="true",document.head.appendChild(d)}t()||function(){if("undefined"!=typeof window){window.document.cookie="ArcId.USER_INFO=;path=/;domain=.".concat(arcSite,".pe; expires=Thu, 01 Jan 1970 00:00:01 GMT");var e=window.document.querySelector('meta[name="content-type"]');n="arc_e_id",(t=document.cookie.match("(^|;) ?".concat(n,"=([^;]*)(;|$)")))&&t[2]&&e&&(window.location.href="prod"===arcEnv?"/signwall/?outputType=signwall&reloginHash=1":"/signwall/?_website=".concat(arcSite,"&outputType=signwall&reloginHash=1"))}var n,t}()}})});`
 
 export default vallaSignwall
