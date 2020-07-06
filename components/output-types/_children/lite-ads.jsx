a028e2c4d333354b4500b2997bf889ff1c973772
cajotafer <hello@cajotafer.com>
2020-07-06 18:26:18 +0700

    Update: filtro de lazyAds en lite-ads ahora en segundo bloque de ejecucion (26191)(ARC)

--- components/output-types/_children/lite-ads.jsx
+++ components/output-types/_children/lite-ads.jsx
@@ -35,8 +35,7 @@ const LiteAds = ({ requestUri, tags, contentCode, siteProperties }) => {
           const userPaywallStat = userPaywall()
           const { section, arcSite, subsection } = window.section;
           const loadFirstAds = adsSlots.filter(slot => slot.getAttribute('data-ads-load-first')) || []
-          const loadLazilyAds = adsSlots.filter(slot => !slot.getAttribute('data-ads-load-first')) || []
-
+          
           const pushAds = (dimensions, name, id) => {
             if (dimensions.length > 0) {
               googletag.cmd.push(function() {
@@ -65,7 +64,7 @@ const LiteAds = ({ requestUri, tags, contentCode, siteProperties }) => {
             const adId = ad.id
             pushAds(isMobile ? adMobileDimensions : adDesktopDimensions, adName, adId)
           }
-
+          
           const displaySlot = (slot) => {
             const ad = slot || {}
             const adId = ad.id
@@ -78,10 +77,11 @@ const LiteAds = ({ requestUri, tags, contentCode, siteProperties }) => {
             loadFirstAds.forEach(pushSlot) 
             loadFirstAds.forEach(displaySlot)
           }
-
+          
           if(loadFirstAds.length > 0) firstRequest()
-
+          
           requestIdle(function() {
+            const loadLazilyAds = adsSlots.filter(slot => !slot.getAttribute('data-ads-load-first')) || []
             const adsObserver = (entries, observer) => {
               entries.forEach(entry => {
                 const { isIntersecting, target } = entry
@@ -109,7 +109,7 @@ const LiteAds = ({ requestUri, tags, contentCode, siteProperties }) => {
 
   const typeContent = contentCode === '' ? 'standar' : contentCode
   const targetingTags = tags.map(({ slug = '' }) => slug.split('-').join(''))
-  const scrp = `"use strict";window.addEventListener("load",function(){requestIdle(function(){var e=[].slice.call(document.querySelectorAll("div[data-ads-name]"));if(e&&e.length>0){window.googletag=window.googletag||{cmd:[]},googletag.cmd.push(function(){googletag.pubads().enableSingleRequest(),googletag.enableServices()});var t=/iPad|iPhone|iPod|android|webOS|Windows Phone/i.test(navigator.userAgent),a=function(){var e="no";if(window.localStorage&&window.localStorage.hasOwnProperty("ArcId.USER_INFO")&&"{}"!==window.localStorage.getItem("ArcId.USER_INFO")){var t=JSON.parse(window.localStorage.getItem("ArcId.USER_INFO")).uuid,a=JSON.parse(window.localStorage.getItem("ArcP")||"{}")[t];a&&a.sub.p.length&&(e="si")}else e="no";return e}(),n=window.section,o=n.section,i=n.arcSite,r=n.subsection,g=e.filter(function(e){return e.getAttribute("data-ads-load-first")})||[],s=e.filter(function(e){return!e.getAttribute("data-ads-load-first")})||[],d=function(e){var n,g,s,d=e||{},l=d.getAttribute("data-ads-name").replace("snota",o),c=JSON.parse(d.getAttribute("data-ads-dimensions")||"[]"),u=JSON.parse(d.getAttribute("data-ads-dimensions-m")||"[]"),f=d.id;g=l,s=f,(n=t?u:c).length>0&&googletag.cmd.push(function(){googletag.defineSlot(g,n,s).addService(googletag.pubads()).setTargeting("ab_test","").setTargeting("categoria",r).setTargeting("contenido","st_value3").setTargeting("fuente","WEB").setTargeting("paywall",a).setTargeting("phatname","st_value6").setTargeting("publisher",i).setTargeting("seccion",o).setTargeting("tags","st_value9").setTargeting("tipoplantilla","post").setTargeting("tmp_ad","")})},l=function(e){var t=(e||{}).id;googletag.cmd.push(function(){googletag.display(t)})};g.length>0&&(g.forEach(d),g.forEach(l)),requestIdle(function(){var e=function(e,t){e.forEach(function(e){var a=e.isIntersecting,n=e.target;a&&(d(n),l(n),t.unobserve(n))})};if("IntersectionObserver"in window){var t={rootMargin:"0px 0px 200px 0px"};s.forEach(function(a){new IntersectionObserver(e,t).observe(a)})}})}})});`
+  const scrp = `"use strict";window.addEventListener("load",function(){requestIdle(function(){var e=[].slice.call(document.querySelectorAll("div[data-ads-name]"));if(e&&e.length>0){window.googletag=window.googletag||{cmd:[]},googletag.cmd.push(function(){googletag.pubads().enableSingleRequest(),googletag.enableServices()});var t=/iPad|iPhone|iPod|android|webOS|Windows Phone/i.test(navigator.userAgent),a=function(){var e="no";if(window.localStorage&&window.localStorage.hasOwnProperty("ArcId.USER_INFO")&&"{}"!==window.localStorage.getItem("ArcId.USER_INFO")){var t=JSON.parse(window.localStorage.getItem("ArcId.USER_INFO")).uuid,a=JSON.parse(window.localStorage.getItem("ArcP")||"{}")[t];a&&a.sub.p.length&&(e="si")}else e="no";return e}(),n=window.section,o=n.section,i=n.arcSite,r=n.subsection,g=e.filter(function(e){return e.getAttribute("data-ads-load-first")})||[],s=function(e){var n,g,s,d=e||{},l=d.getAttribute("data-ads-name").replace("snota",o),c=JSON.parse(d.getAttribute("data-ads-dimensions")||"[]"),u=JSON.parse(d.getAttribute("data-ads-dimensions-m")||"[]"),f=d.id;g=l,s=f,(n=t?u:c).length>0&&googletag.cmd.push(function(){googletag.defineSlot(g,n,s).addService(googletag.pubads()).setTargeting("ab_test","").setTargeting("categoria",r).setTargeting("contenido","st_value3").setTargeting("fuente","WEB").setTargeting("paywall",a).setTargeting("phatname","st_value6").setTargeting("publisher",i).setTargeting("seccion",o).setTargeting("tags","st_value9").setTargeting("tipoplantilla","post").setTargeting("tmp_ad","")})},d=function(e){var t=(e||{}).id;googletag.cmd.push(function(){googletag.display(t)})};g.length>0&&(g.forEach(s),g.forEach(d)),requestIdle(function(){var t=e.filter(function(e){return!e.getAttribute("data-ads-load-first")})||[],a=function(e,t){e.forEach(function(e){var a=e.isIntersecting,n=e.target;a&&(s(n),d(n),t.unobserve(n))})};if("IntersectionObserver"in window){var n={rootMargin:"0px 0px 200px 0px"};t.forEach(function(e){new IntersectionObserver(a,n).observe(e)})}})}})});`
     .replace(/st_value3/g, typeContent)
     .replace(/st_value6/g, `${siteProperties.siteUrl}${requestUri}`)
     .replace(/st_value9/g, targetingTags)
