a3fb4ae80adcc98e2cb62d9c469f37ddfb345fe6
Rolly <rolly.sanchez.26@gmail.com>
2020-07-15 14:28:09 -0500

    Update: modificando el estilo del componente corrección de notas (#26828)(SITETEAM)

--- components/features/story/contents/_children/correction.jsx
+++ components/features/story/contents/_children/correction.jsx
@@ -12,21 +12,21 @@ export default ({
   const classes = {
     correction: `${
       isAmp ? 'amp-story-content__correction' : 'story-content__correction'
-    }  mb-10 mt-10 mr-20 pr-10 pb-10 pt-10 pl-10`,
+    }  mb-10 mt-10 pr-10 pb-10 pt-10 pl-10`,
     title: `${
       isAmp
         ? 'amp-story-content__correction__title'
         : 'story-content__correction__title'
-    } pb-10`,
+    }`,
   }
 
   return (
     <div className={classes.correction}>
-      <h5 className={classes.title}>
+      <span className={classes.title}>
         {correctionType === CORRECTION_TYPE_CORRECTION
-          ? 'Corrección'
-          : 'Aclaración'}
-      </h5>
+          ? 'Corrección: '
+          : 'Aclaración: '}
+      </span>
       {nlToBrTag(content)}
     </div>
   )
