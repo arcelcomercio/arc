112d7db14948881413fa39273bb16bbef6020f7c
Rolly <rolly.sanchez.26@gmail.com>
2020-08-13 00:53:22 -0500

    Update: borrando console logs de trustproject (#27919)(SITETEAM)

--- components/output-types/_children/meta-story.jsx
+++ components/output-types/_children/meta-story.jsx
@@ -292,8 +292,6 @@ export default ({
       : ''
 
   const { label: { trustproject = {} } = {} } = data || {}
-  console.log('====Data===', data)
-  console.log('====trustproject===', trustproject)
   const trustType = workType(trustproject) || '"NewsArticle"'
 
   const structuredData = `{  "@context":"http://schema.org", "@type":${trustType}, "datePublished":"${publishDateZone}",
