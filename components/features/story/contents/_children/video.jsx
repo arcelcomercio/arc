7ecdc494af0361e3454dcc5d1c6237ead56d869e
cajotafer <hello@cajotafer.com>
2020-08-18 14:48:30 +0700

    Update: los videos de noticias, seccion videos y FIA ahora vienen del CDN de ElComercio (27699)(ARC)

--- components/features/story/contents/_children/video.jsx
+++ components/features/story/contents/_children/video.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import ENV from 'fusion:environment'
 import { useFusionContext } from 'fusion:context'
 import { msToTime } from '../../../../utilities/date-time/time'
-// import { getResultVideo } from '../../../../utilities/story/helpers'
+import { getResultVideo } from '../../../../utilities/story/helpers'
 
 /**
  *
- * TODO:TEMP: Cambio temporal, cuando se indique, activar nuevamente el
- * getResultVideo que viene de story/helpers y borrar este.
+ * Si piden que los videos vengan del CDN de Arc en lugar del
+ * CDN de El Comercio, solo se debe comentar:
+ *
+ * import { getResultVideo } from '../../../../utilities/story/helpers'
+ *
+ * y descomentar la siguiente funcion
  */
-const getResultVideo = (streams, arcSite, type = 'ts') => {
+/* const getResultVideo = (streams, arcSite, type = 'ts') => {
   const resultVideo = streams
     .map(({ url = '', stream_type: streamType = '' }) => {
       return streamType === type ? url : []
@@ -18,7 +22,7 @@ const getResultVideo = (streams, arcSite, type = 'ts') => {
   const cantidadVideo = resultVideo.length
 
   return resultVideo[cantidadVideo - 1]
-}
+} */
 
 const classes = {
   caption: 'story-content__caption pt-10 secondary-font text-md',
