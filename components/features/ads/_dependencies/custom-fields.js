d98eeaab8144b57308897c6844b709166b7e7481
Jean Paul Gutierrez <jpgn.150@gmail.com>
2020-09-01 09:49:18 -0500

    Update: migracion de features en pagebuilder (28563)(SITETEAM)

---
+++ components/features/ads/_dependencies/custom-fields.js
@@ -0,0 +1,104 @@
+import PropTypes from 'prop-types'
+import { spacesAdsId, spacesAdsName } from '../../../utilities/config-params'
+
+const customFields = PropTypes.shape({
+  adElement: PropTypes.string.isRequired.tag({
+    name: 'Nombre',
+    description:
+      'El nombre definido en este campo formará parte del "id" (Ej. ads_d_[nombre]).',
+  }),
+  isDesktop: PropTypes.bool.tag({
+    name: 'Mostrar en "desktop" y "tablet"',
+  }),
+  isMobile: PropTypes.bool.tag({
+    name: 'Mostrar en "mobile"',
+  }),
+  isDfp: PropTypes.bool.tag({
+    name: 'Mostrar Publicidad DFP',
+    defaultValue: true,
+  }),
+  freeHtml: PropTypes.richtext.tag({
+    name: 'Código HTML Adicional',
+    group: 'Agregar bloque de HTML',
+    description:
+      'HTML a renderizar en el espacio disponible junto al módulo de publicidad. El HTML siempre se mostrará en "desktop", "tablet" y "mobile".',
+  }),
+  columns: PropTypes.oneOf(['w-full', 'col-1', 'col-2', 'col-3']).tag({
+    name: 'Ancho de la publicidad',
+    labels: {
+      'w-full': 'auto',
+      'col-1': '1 columna',
+      'col-2': '2 columnas',
+      'col-3': '3 columnas',
+    },
+    defaultValue: 'w-full',
+    group: 'Tamaño de la publicidad',
+  }),
+  rows: PropTypes.oneOf(['empty', 'row-1', 'row-2']).tag({
+    name: 'Alto de la publicidad',
+    labels: {
+      empty: 'auto',
+      'row-1': '1 fila',
+      'row-2': '2 filas',
+    },
+    defaultValue: 'empty',
+    group: 'Tamaño de la publicidad',
+  }),
+
+  adsSpace: PropTypes.oneOf(spacesAdsId()).tag({
+    name: 'Espacio',
+    group: 'Publicidad',
+    labels: spacesAdsName(),
+    defaultValue: 'none',
+  }),
+  adsBorder: PropTypes.oneOf(['border', 'containerp']).tag({
+    name: 'Borde',
+    group: 'Publicidad',
+    labels: {
+      border: 'ConBorde',
+      containerp: 'SinBorde',
+    },
+    defaultValue: false,
+  }),
+  liteAdId: PropTypes.string.tag({
+    name: 'ID',
+    group: 'Lite',
+  }),
+  liteAdName: PropTypes.string.tag({
+    name: 'Nombre',
+    group: 'Lite',
+  }),
+  liteAdDimensions: PropTypes.string.tag({
+    name: 'Dimensiones para Tablet y Desktop',
+    group: 'Lite',
+  }),
+  liteAdMobileDimensions: PropTypes.string.tag({
+    name: 'Dimensiones para Mobile',
+    group: 'Lite',
+  }),
+  liteAdInlineStyles: PropTypes.string.tag({
+    name: 'Estilos inline',
+    description:
+      'Incluye un JSON de estilos para el contenedor de publicidad. ej. {"position":"fixed","marginTop":0}',
+    group: 'Lite',
+  }),
+  liteAdLoadFirst: PropTypes.bool.tag({
+    name: 'Carga Inmediata',
+    description:
+      'Si se activa, este bloque carga tan pronto como sea posible. Si se desactiva, carga sólo cuando entra en la pantalla.',
+    defaultValue: false,
+    group: 'Lite',
+  }),
+  prebidAdEnabled: PropTypes.bool.tag({
+    name: 'Activar prebid para este elemento',
+    defaultValue: false,
+    description: `Para activar, debe existir el Campo Personalizado "Lite > Nombre"`,
+    group: 'Prebid lite',
+  }),
+  prebidAdDimensions: PropTypes.string.tag({
+    name: 'Dimensiones',
+    group: 'Prebid lite',
+  }),
+})
+
+export default customFields
--- components/features/custom-image/default.jsx
+++ components/features/custom-image/default.jsx
@@ -1,5 +1,124 @@
-import CustomImage from '../widgets/custom-image/default'
+import React from 'react'
+import { useFusionContext } from 'fusion:context'
+import { useContent } from 'fusion:content'
 
+import customFields from './_dependencies/custom-fields'
+import { defaultImage } from '../../utilities/assets'
+import { getResizedUrl } from '../../utilities/resizer'
+
+const classes = {
+  image: 'w-full h-full object-contain',
+}
+
+const CustomImage = ({
+  customFields: {
+    imgUrlDesktop,
+    imgTitle,
+    imgAlt,
+    imgLink,
+    imgHeight,
+    lazyload,
+    cols,
+    rows,
+  } = {},
+}) => {
+  const { isAdmin, arcSite, contextPath, deployment } = useFusionContext()
+  const lazyImg = defaultImage({ arcSite, contextPath, deployment, size: 'md' })
+  const containerClass = cols && rows ? `block ${cols} md:${rows}` : 'block'
+  const styleHeight = { height: imgHeight }
+
+  let width = '307'
+  if (cols === 'col-2') width = '633'
+  if (cols === 'col-3') width = '960'
+
+  let height = '0'
+  if (rows === 'row-2') height = '0'
+  if (rows === 'w-full') height = imgHeight || '0'
+
+  const {
+    resized_urls: {
+      desktop: adminResizeImg = '',
+      mobile: adminMobileImg = '',
+    } = {},
+  } =
+    useContent(
+      isAdmin && imgUrlDesktop
+        ? {
+            source: 'photo-resizer',
+            query: {
+              url: imgUrlDesktop,
+              presets: `desktop:${width}x${height},mobile:450x0`,
+            },
+          }
+        : {}
+    ) || ''
+
+  const { desktop: publicResizeImg = '', mobile: publicMobileImg = '' } =
+    !isAdmin && imgUrlDesktop
+      ? getResizedUrl({
+          url: imgUrlDesktop,
+          presets: `desktop:${width}x${height},mobile:450x0`,
+          arcSite,
+        }) || {}
+      : {}
+
+  const resizedImg = isAdmin ? adminResizeImg : publicResizeImg
+  const mobileResizedImg = isAdmin ? adminMobileImg : publicMobileImg
+
+  if (!imgUrlDesktop)
+    return (
+      <div className={containerClass}>
+        Modulo imagen, clic en editar para configurar.
+      </div>
+    )
+
+  const picture = lazyload ? (
+    <picture>
+      <source
+        media="(max-width: 639px)"
+        srcSet={isAdmin ? mobileResizedImg : lazyImg}
+        data-srcset={mobileResizedImg}
+        className={isAdmin ? '' : 'lazy'}
+      />
+      <img
+        src={isAdmin ? resizedImg : lazyImg}
+        data-src={resizedImg}
+        alt={imgAlt || ''}
+        style={styleHeight}
+        {...(imgTitle ? { title: imgTitle } : {})}
+        className={`${isAdmin ? '' : 'lazy'} ${classes.image}`}
+      />
+    </picture>
+  ) : (
+    <picture>
+      <source media="(max-width: 639px)" srcSet={mobileResizedImg} />
+      <img
+        src={resizedImg}
+        alt={imgAlt || ''}
+        style={styleHeight}
+        {...(imgTitle ? { title: imgTitle } : {})}
+        className={classes.image}
+      />
+    </picture>
+  )
+
+  if (imgLink) {
+    return (
+      <div>
+        <a itemProp="url" className={containerClass} href={imgLink}>
+          {picture}
+        </a>
+      </div>
+    )
+  }
+
+  return <div className={containerClass}>{picture}</div>
+}
+
+CustomImage.propTypes = {
+  customFields,
+}
 CustomImage.label = 'Imagen con enlace'
+CustomImage.static = true
 
 export default CustomImage
