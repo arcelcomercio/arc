960a9b79a92fdc7585925892fe7c71c36b61abcc
cajotafer <hello@cajotafer.com>
2020-09-22 20:11:39 +0700

    Fix: regex para idrams en promoItems (ARC)

--- components/output-types/amp.jsx
+++ components/output-types/amp.jsx
@@ -130,13 +130,14 @@ const AmpOutputType = ({
   })
 
   let rawHtmlContent = contentElementsHtml
+  const regexYoutube = /<iframe.+youtu\.be|youtube\.com/
 
   const hasGallery = quantityGalleryItem > 0
   const hasYoutube =
-    idYoutube ||
-    /<iframe.+(facebook.com\/plugins|youtu\.be|youtube\.com)/.test(content) ||
-    /youtu\.be|youtube\.com/.test(rawHtmlContent)
-  const hasFacebook = rawHtmlContent.includes('facebook.com/plugins/')
+    idYoutube || regexYoutube.test(content) || regexYoutube.test(rawHtmlContent)
+  const hasFacebook =
+    /<iframe.+facebook.com\/plugins\//.test(content) ||
+    rawHtmlContent.includes('facebook.com/plugins/')
   const hasInstagram = rawHtmlContent.includes('instagram-media')
   const hasTwitter = rawHtmlContent.includes('twitter.com')
   const hasSoundcloud = rawHtmlContent.includes('soundcloud.com/playlists/')
