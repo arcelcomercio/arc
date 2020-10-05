655ebde306b7cfc9749d93b63ed311309642dfd8
cajotafer <hello@cajotafer.com>
2020-10-05 17:10:46 +0700

    Fix: alto para video-list en section-video (#30434)

--- components/features/video/section/_children/section-video.jsx
+++ components/features/video/section/_children/section-video.jsx
@@ -76,7 +76,7 @@ export default ({
         )
         const playList = sectionVideo.querySelector('.play-list')
         const videoNavBar = sectionVideo.querySelector('.video-navbar')
-        const videoList = sectionVideo.querySelector('.video-list')
+        const videoList = document.body.querySelector('.video-list')
         const adsMiddle = document.getElementById('ads_d_middle1')
         const mTop = 450
 
