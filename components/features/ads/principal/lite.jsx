17bd847e83ef465ba8dd34fc58dd3869d6392ec8
cajotafer <hello@cajotafer.com>
2020-11-09 22:42:40 +0700

    New: ads/principal para lite (#32016)

---
+++ components/features/ads/principal/lite.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import customFields from '../_dependencies/custom-fields'
+
+const AdsFeatLite = props => {
+  const {
+    customFields: {
+      liteAdId,
+      liteAdName,
+      liteAdDimensions,
+      liteAdMobileDimensions,
+      liteAdLoadFirst,
+      liteAdLoadBlock,
+      liteAdInlineStyles,
+      prebidAdEnabled,
+      prebidAdDimensions,
+    } = {},
+  } = props
+
+  return (
+    <>
+      {(liteAdId || liteAdName || liteAdDimensions) && (
+        <div
+          id={liteAdId}
+          data-ads-name={liteAdName}
+          data-ads-dimensions={liteAdDimensions}
+          data-ads-dimensions-m={liteAdMobileDimensions}
+          data-ads-load-first={liteAdLoadFirst}
+          data-bloque={liteAdLoadBlock}
+          data-prebid-enabled={prebidAdEnabled}
+          data-prebid-dimensions={prebidAdDimensions}
+          style={liteAdInlineStyles && (JSON.parse(liteAdInlineStyles) || {})}
+        />
+      )}
+    </>
+  )
+}
+
+AdsFeatLite.propTypes = {
+  customFields,
+}
+
+AdsFeatLite.label = 'Publicidad - Beta'
+AdsFeatLite.static = true
+
+export default AdsFeatLite
