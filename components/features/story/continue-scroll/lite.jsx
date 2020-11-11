ed820139ff7273c0e67078f812fdaeee673fffd4
cajotafer <hello@cajotafer.com>
2020-11-11 16:07:14 +0700

    Update: ahora notas continuas filtra notas de /somos y /archivo-elcomercio (#32016)

--- components/features/story/continue-scroll/lite.jsx
+++ components/features/story/continue-scroll/lite.jsx
@@ -283,6 +283,7 @@ const StoryContinueLite = props => {
     } = story
     // Filtra las historias que no son Galeria horizontal
     return (
+      !/^\/(somos|archivo-elcomercio)\//.test(requestUri) &&
       requestUri !== websiteUrl &&
       ((type === ELEMENT_GALLERY && story.subtype !== GALLERY_SLIDER) ||
         (type !== ELEMENT_GALLERY && story.subtype === GALLERY_SLIDER))
