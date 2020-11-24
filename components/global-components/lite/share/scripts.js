6eb788d1cf2c9b5a023bfcd67fa3970aecce98e6
cajotafer <hello@cajotafer.com>
2020-11-24 17:53:30 +0700

    Update: estilos y logica para mostrar copiar enlace al final de notas (#33618-33624)

---
+++ components/global-components/lite/share/scripts.js
@@ -0,0 +1,40 @@
+/* window.addEventListener('load', () => {requestIdle(() => {
+  if(!window.shareButtons){
+    const windowW = 600
+    const windowH = 400
+    const $shareButtons = document.body.querySelectorAll('a[data-share]')
+    if ($shareButtons && $shareButtons.length > 0) {
+      const wLeft = window.screen.width / 2 - windowW / 2
+      const wTop = window.screen.height / 2 - windowH / 2
+      $shareButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+          e.preventDefault()
+          window.open(
+            button.getAttribute('href'),
+            '',
+            `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${windowW}, height=${windowH}, top=${wTop}, left=${wLeft}`
+          )
+        })
+      })
+    }
+  }
+})}) */
+export const popup =
+  '"use strict";window.addEventListener("load",function(){requestIdle(function(){var t=document.body.querySelectorAll("a[data-share]");if(t&&t.length>0){var n=window.screen.width/2-300,o=window.screen.height/2-200;t.forEach(function(t){t.addEventListener("click",function(e){e.preventDefault(),window.open(t.getAttribute("href"),"","toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=".concat(600,", height=").concat(400,", top=").concat(o,", left=").concat(n))})})}})});'
+
+/* window.addEventListener('load', () => {requestIdle(() => {
+  function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+      // fallbackCopyTextToClipboard(text);
+      return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+      console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+  }
+})})
+*/
+
+export const copyLink = ''
