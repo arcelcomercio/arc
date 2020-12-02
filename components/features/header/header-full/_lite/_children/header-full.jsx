49730b4e1c8ab531ff114f40b1d0b18084d48f80
Alvaro <alvaroenriquehl@gmail.com>
2020-12-02 17:40:02 -0500

    Update: agregado boton de google news en el header de Depor lite (#33619)

--- components/features/header/header-full/_lite/_children/header-full.jsx
+++ components/features/header/header-full/_lite/_children/header-full.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-nested-ternary */
 import React from 'react'
+import getProperties from 'fusion:properties'
 
 import {
   menuScript,
@@ -124,6 +125,8 @@ export default ({
     )
   }
 
+  const { googleNewsUrl } = getProperties(arcSite)
+
   return (
     <>
       <div data-story-header={`${isStory}`} className={classes.headerFull}>
@@ -229,9 +232,26 @@ export default ({
             {isStory && (
               <div className={classes.navStorySocialNetwork}>
                 <ul className={classes.listIcon}>
-                  <li className={classes.shareItem}>
-                    <span>Comparte</span>
-                  </li>
+                  {arcSite === 'depor' ? (
+                    <a
+                      itemProp="url"
+                      href={googleNewsUrl}
+                      className="share-btn f f-center share-btn--gnews">
+                      <span className="share-btn--gnews-txt">
+                        Síguenos en Google News
+                      </span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="25"
+                        viewBox="0 100 512.3 512.3">
+                        <path d="m444.6 231.6h-377v265h377zm-127 117.5h82v30h-82zm82-60v30h-82v-30zm-199.5 162.5c-48.2 0-87.5-39.3-87.5-87.5s39.3-87.5 87.5-87.5c22.6 0 44 8.6 60.3 24.1l-20.7 21.7c-10.7-10.2-24.8-15.9-39.7-15.9-31.7 0-57.5 25.8-57.5 57.5s25.8 57.5 57.5 57.5c26.5 0 48.9-18 55.5-42.5h-55.5v-30h87.5v15c0 48.2-39.3 87.5-87.5 87.5zm117.5-42.5h82v30h-82z" />
+                      </svg>
+                    </a>
+                  ) : (
+                    <li className={classes.shareItem}>
+                      <span>Comparte</span>
+                    </li>
+                  )}
                   {shareButtons.map(item => (
                     <li key={item.icon} className={classes.shareItem}>
                       <a
