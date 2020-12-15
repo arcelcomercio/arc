d90ddc39d096eb28924268613d5296b070f476e3
Rolly <rolly.sanchez@fractalservicios.pe>
2020-12-14 20:31:47 -0500

    New: agregando content source para redirección de tag comercio (#34511)(SITETEAM)

--- content/sources/redirect-by-url.js
+++ content/sources/redirect-by-url.js
@@ -4,17 +4,13 @@ import request from 'request-promise-native'
 import { CONTENT_BASE, ARC_ACCESS_TOKEN } from 'fusion:environment'
 import RedirectError from '../../components/utilities/redirect-error'
 import { storyContent } from '../filters/story-content'
+import { SITE_DEPOR, SITE_ELCOMERCIO } from '../../components/utilities/constants/sitenames'
 
 const schemaName = 'story-dev'
 const params = [
   {
     name: 'website_url',
-    displayName: 'URL de la nota',
-    type: 'text',
-  },
-  {
-    name: 'section',
-    displayName: 'Sección / Categoría (sin slash)',
+    displayName: 'URL',
     type: 'text',
   },
 ]
@@ -28,14 +24,19 @@ const options = {
 }
 
 const excludedFieldsStory = '&_sourceExclude=owner,websites,language'
-const redirectUrls = (websiteUrl) => {
-  return `${websiteUrl.replace('/amp/depor/', '/amp/')}`
+const redirectUrls = (websiteUrl, arcSite) => {
+  if(arcSite === SITE_DEPOR) {
+    return `${websiteUrl.replace('/amp/depor/', '/amp/')}`
+  } 
+  if(arcSite === SITE_ELCOMERCIO) {
+    return `${websiteUrl.replace('%E2%80%8E', '')}`
+  }
+  return websiteUrl
 }
 
-const fetch = ({
+const fetchData = ({
   website_url: websiteUrl,
   'arc-site': website,
-  section = '',
 } = {}) => {
   if (!websiteUrl) {
     throw new Error('Esta fuente de contenido requiere una URI y un sitio web')
@@ -44,20 +45,35 @@ const fetch = ({
     throw new Error('Arc Site no está definido')
   }
 
-  return request({
-    uri: `${CONTENT_BASE}/content/v4/stories/?website_url=${section}${redirectUrls(websiteUrl)}&website=${website}${excludedFieldsStory}`,
+  console.log('===========================================')
+  console.log(websiteUrl)
+  console.log('===========================================')
+
+  const urlRedirect = redirectUrls(websiteUrl, website)
+
+  console.log('==================urlRedirect 3=========================')
+  console.log(website, urlRedirect)
+  console.log('===========================================')
+
+  /* return request({
+    uri: `${CONTENT_BASE}/content/v4/stories/?website_url=${section}${urlRedirect}&website=${website}${excludedFieldsStory}`,
     ...options,
   }).then(storyResp => {
     if (storyResp.type === 'redirect' && storyResp.redirect_url)
       throw new RedirectError(storyResp.redirect_url, 301)
     return storyResp
-  })
+  }) */
+
+  throw new RedirectError(urlRedirect, 301)
 }
 
+const resolve = key => fetchData(key)
+
 export default {
-  fetch,
-  schemaName,
+  // fetch,
+  // schemaName,
+  resolve,
   params,
   ttl: 300,
-  filter: storyContent,
+  // filter: storyContent,
 }
