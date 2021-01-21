1cb21c2aadf1f3db042254a54b3fa8bc5419b33b
Rolly <rolly.sanchez@fractalservicios.pe>
2021-01-20 22:50:37 -0500

    New: agregando html para el componente cintillo (#36261)(SITETEAM)

--- components/features/stories-lists/headband-videos/_children/process-item.jsx
+++ components/features/stories-lists/headband-videos/_children/process-item.jsx
@@ -5,12 +5,23 @@ import { useFusionContext } from 'fusion:context'
 import { LANDSCAPE_XXS } from '../../../../utilities/constants/image-sizes'
 import schemaFilter from '../_dependencies/schema-filters'
 
+const classes = {
+  containerItem: 'headband__container-item',
+  boxVideo: 'headband__box-video',
+  titleStory: 'headband__title-story',
+  image: 'headband__image',
+  boxTimerLive: 'headband__box-timer-live',
+  timer: 'headband__timer',
+  live: 'headband__live',
+
+}
+
 function HeadBandProcessItem({ storyUrl = '', storyLive = false }) {
   const CONTENT_SOURCE = 'story-by-url'
   const { arcSite, deployment, contextPath, isAdmin } = useFusionContext()
 
   const processData = data => {
-      return data
+    return data
   }
 
   const story =
@@ -29,7 +40,21 @@ function HeadBandProcessItem({ storyUrl = '', storyLive = false }) {
         : {}
     ) || {}
 
-  return <div>aquí va el video</div>
+  return (
+    <div className={classes.containerItem}>
+      <div className={classes.boxVideo}>
+        <img className={classes.image} src="" alt="" />
+        <div className={classes.boxTimerLive}>
+          <span className={classes.timer}>12:00</span>
+          <div className={classes.live}>EN VIVO</div>
+        </div>
+        <i className="icon-play"></i>
+      </div>
+      <div className={classes.titleStory}>
+        Ministro Incháustegui: Esta semana se aprobaría cambio regulatorio
+      </div>
+    </div>
+  )
 }
 
 HeadBandProcessItem.propTypes = {
--- components/features/stories-lists/headband-videos/default.jsx
+++ components/features/stories-lists/headband-videos/default.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-// import { useFusionContext } from 'fusion:context'
+import { useFusionContext } from 'fusion:context'
+import { getAssetsPath } from '../../../utilities/assets'
 import customFieldsInput from './_dependencies/custom-fields'
 import ProcessItem from './_children/process-item'
 
 const classes = {
   container: 'headband__container',
+  boxLogo: 'headband__box-logo',
+  logo: 'headband__logo',
+  title: 'headband__title',
+  boxItems: 'headband__box-items',
+  swipe: 'headband__swipe',
+  next: 'headband__next',
 }
 
 const HeadbandVideo = props => {
-  // const { contextPath, arcSite } = useFusionContext()
+  const { contextPath, arcSite } = useFusionContext()
 
   const {
     customFields: {
@@ -28,19 +35,35 @@ const HeadbandVideo = props => {
   const stories = [story1, story2, story3, story4, story5]
   const storiesLive = [live1, live2, live3, live4, live5]
 
+  const logoImg = `${getAssetsPath(
+    arcSite,
+    contextPath
+  )}/resources/dist/${arcSite}/images/Logo_P21TV.png?d=1`
+
   return (
     <div className={classes.container}>
-      cintillo video
-      {stories.map((url, index) => {
-        const uniqueKey = Math.floor(Math.random() * index)
-        return (
-          <ProcessItem
-            storyUrl={url}
-            storyLive={storiesLive[0]}
-            key={`cintillo-p21tv-${uniqueKey}`}
-          />
-        )
-      })}
+      <div className={classes.boxLogo}>
+        <div className={classes.title} >VIDEOS</div>
+        <img className={classes.logo} src={logoImg}  alt="Logo de Perú21TV"/>
+        <div className={classes.swipe}>
+          Desliza
+        </div>
+      </div>
+      <div className={classes.boxItems}>
+        {stories.map((url, index) => {
+          const randomKey = Math.floor(Math.random() * index)
+          return (
+            <ProcessItem
+              storyUrl={url}
+              storyLive={storiesLive[0]}
+              key={randomKey}
+            />
+          )
+        })}
+      </div>
+      <div className={classes.next}>
+        &gt;
+      </div>
     </div>
   )
 }
