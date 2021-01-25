5b4872fb8c5e88ab3c32c74a20d91b7b04c5fe15
cajotafer <hello@cajotafer.com>
2021-01-25 19:17:55 +0700

    Update: estilos para inicio de trivia (#36264)

--- components/features/trivias/main/_children/start.jsx
+++ components/features/trivias/main/_children/start.jsx
@@ -4,58 +4,62 @@ import Image from '../../../../global-components/image'
 
 const classes = {
   container: 'trivias-start',
+  image: 'trivias-start__image',
   title: 'trivias-start__title',
-  button: 'trivias-start__button'
+  button: 'trivias-start__button',
 }
 
-const TriviasMainStart = ({
-  title, 
-  image,
-  alt,
-  start, 
-}) => {
+const TriviasMainStart = ({ title, image, alt, start }) => {
   return (
-    <div style={{
-      margin: '0 auto',
-      width: '100%',
-      maxWidth: '1170px',
-    }}>
-      <div style={{
+    <div
+      className={classes.container}
+      style={{
+        margin: '0 auto',
         width: '100%',
+        maxWidth: '1170px',
+        height: '100%',
+        minHeight: '660px',
+        // minHeight: '555px',
+        // maxHeight: '660px',
         position: 'relative',
-        paddingTop: '56.25%',
-        overflow: 'hidden'
       }}>
-        <Image 
-          src={image}
-          width={1170}
-          height={660}
-          sizes='(max-width: 360px) 360px, 1170px'
-          alt={alt}
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right:0,
-            bottom:0
-          }}
-          loading="eager"
-        />
-        <h1 className={classes.title} style={{
+      <Image
+        src={image}
+        width={1170}
+        height={660}
+        sizes="(max-width: 360px) 360px, (max-width: 480px) 480px, (max-width: 768px) 768px, 1170px"
+        sizesHeight={[555, 555]}
+        alt={alt}
+        className={classes.image}
+        style={{
           position: 'absolute',
-          top: '8%',
-          left: '50%',
-          transform: 'translateX(-50%)',
-        }}>{title}</h1>
-        <button type="button" onClick={start} className={classes.button} style={{
+          objectFit: 'cover',
+          objectPosition: 'center',
+          height: '100%',
+          width: '100%',
+        }}
+        loading="eager"
+      />
+      <h1
+        className={classes.title}
+        style={{
           position: 'absolute',
-          top: '92%',
           left: '50%',
           transform: 'translateX(-50%)',
         }}>
-          Empezar
-        </button>
-      </div>
+        {title}
+      </h1>
+      <button
+        type="button"
+        onClick={start}
+        className={classes.button}
+        style={{
+          position: 'absolute',
+          left: '50%',
+          transform: 'translateX(-50%)',
+        }}>
+        Empezar
+      </button>
     </div>
   )
 }
