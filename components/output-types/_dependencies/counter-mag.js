c8a349609663cbcbc3e9fb32bbd64f682185388f
George <cocotopia@gmail.com>
2021-02-04 19:03:51 -0500

    Update: agregar url canonical contador mag

--- components/output-types/_dependencies/counter-mag.js
+++ components/output-types/_dependencies/counter-mag.js
@@ -13,8 +13,7 @@
 //     arcEnv === 'prod'
 //       ? 'https://mag.elcomercio.pe'
 //       : 'https://elcomercio-elcomerciomag-sandbox.cdn.arcpublishing.com'
-//   const pathCompareMag =
-//     arcEnv === 'prod' ? /mag.elcomercio.pe/ : /elcomercio-elcomerciomag-sandbox/
+//   const pathCompareMag = /mag.elcomercio.pe/
 
 //   const originDomainSpecial = 'https://especiales.elcomercio.pe'
 //   const pathCompareSpecial = /especiales.elcomercio.pe/
@@ -27,7 +26,7 @@
 //     ArcP.run({
 //       paywallFunction: function(e) {},
 //       customPageData: function() {
-//         return { c: 'story', s: 'mag', ci: e.data }
+//         return { c: 'story', s: '/counter/external', ci: e.data }
 //       },
 //       customSubCheck: function() {
 //         return Promise.resolve({
@@ -48,6 +47,6 @@
 // window.addEventListener('message', handleMessage, !1)
 
 const listenCounterMag = arcEnv =>
-  `"use strict";var arcEnv="${arcEnv}";function checkSession(){if("undefined"!=typeof window){var e=window.localStorage.getItem("ArcId.USER_PROFILE"),o=window.localStorage.getItem("ArcId.USER_INFO");if(e)return!("null"===e||"{}"===o)||!1}return!1}function handleMessage(e){var o="prod"===arcEnv?"https://mag.elcomercio.pe":"https://elcomercio-elcomerciomag-sandbox.cdn.arcpublishing.com",c="prod"===arcEnv?/mag.elcomercio.pe/:/elcomercio-elcomerciomag-sandbox/;(e.origin===o&&e.data.match(c)||"https://especiales.elcomercio.pe"===e.origin&&e.data.match(/especiales.elcomercio.pe/))&&(console.log("entro URL => ",e.data),ArcP.run({paywallFunction:function(e){},customPageData:function(){return{c:"story",s:"mag",ci:e.data}},customSubCheck:function(){return Promise.resolve({s:!1,p:[],timeTaken:100,updated:Date.now()})},customRegCheck:function(){var e=Date.now(),o=checkSession();return Promise.resolve({l:o,timeTaken:Date.now()-e})}}))}window.addEventListener("message",handleMessage,!1);`
+  `"use strict";var arcEnv="${arcEnv}";function checkSession(){if("undefined"!=typeof window){var e=window.localStorage.getItem("ArcId.USER_PROFILE"),o=window.localStorage.getItem("ArcId.USER_INFO");if(e)return!("null"===e||"{}"===o)||!1}return!1}function handleMessage(e){var o="prod"===arcEnv?"https://mag.elcomercio.pe":"https://elcomercio-elcomerciomag-sandbox.cdn.arcpublishing.com";(e.origin===o&&e.data.match(/mag.elcomercio.pe/)||"https://especiales.elcomercio.pe"===e.origin&&e.data.match(/especiales.elcomercio.pe/))&&(console.log("entro URL => ",e.data),ArcP.run({paywallFunction:function(e){},customPageData:function(){return{c:"story",s:"/counter/external",ci:e.data}},customSubCheck:function(){return Promise.resolve({s:!1,p:[],timeTaken:100,updated:Date.now()})},customRegCheck:function(){var e=Date.now(),o=checkSession();return Promise.resolve({l:o,timeTaken:Date.now()-e})}}))}window.addEventListener("message",handleMessage,!1);`
 
 export default listenCounterMag
