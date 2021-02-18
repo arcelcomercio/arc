593d84a203f72045ad2027677dded371dbc9a7ad
cajotafer <hello@cajotafer.com>
2021-02-19 00:20:49 +0700

    New: fusion:static type file

---
+++ types/fusion:static/index.d.ts
@@ -0,0 +1,14 @@
+// import { AnyObject } from '../utils'
+
+// declare module 'fusion:static' {
+//   export interface UseContentConfig<Query = AnyObject> {
+//     source: string
+//     query: Query
+//     filter?: string
+//     transform?: (data: AnyObject) => any
+//   }
+
+//   export function useContent<Query = AnyObject>(
+//     config: UseContentConfig<Query>
+//   ): any
+// }
