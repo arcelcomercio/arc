6deebc834c53e5fe7925016d7923bdae5225d151
Jean Paul Gutierrez <jpgn.150@gmail.com>
2021-02-18 18:28:24 -0500

    New: cabecera y compartir de redes sociales (#37912-38052)(SITETEAM)

--- components/features/statics/covid/default.jsx
+++ components/features/statics/covid/default.jsx
@@ -1,11 +1,22 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
+import Home from './_children/home'
 
 /**
  * @see estilos `src/websites/elcomercio/covid.scss`
  */
 const StaticsCovid = () => {
-  return <></>
+  return (
+    <>
+      <Home
+        contagiados="2214785"
+        recuperados="1991382"
+        fallecidos="56093"
+        uci="13"
+        vacunados="43927"
+      />
+    </>
+  )
 }
 
 StaticsCovid.static = true
