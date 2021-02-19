ed6b455f9189b259aed76dc138c6f9195c69ff13
Edgar <henrry11985@gmail.com>
2021-02-19 08:55:40 -0500

    New: platilla de sub seccion de contagiados(#37912)

--- components/features/statics/covid/default.jsx
+++ components/features/statics/covid/default.jsx
@@ -1,23 +1,21 @@
 import * as React from 'react'
-import { useAppContext } from 'fusion:context'
+// import { useAppContext } from 'fusion:context'
 import PropTypes from 'prop-types'
 
-import InfectedAverage from './_children/infected-average'
+// import InfectedAverage from './_children/infected-average'
 
 /**
  * @see estilos `src/websites/elcomercio/covid.scss`
  */
 
 const StaticsCovid = () => {
+  /*
   const { globalContent: data, requestUri } = useAppContext()
   const uri = requestUri.split('/')[2]
   const distrito = data.filter(el => el.dist_prov_slug === uri)[0]
   console.log(distrito)
-  return (
-    <>
-      <InfectedAverage {...distrito}></InfectedAverage>
-    </>
-  )
+  */
+  return <></>
 }
 
 StaticsCovid.static = true
