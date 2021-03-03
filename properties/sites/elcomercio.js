915e6987d34d27d581c743a80baa1d5a2e730d26
George Dueñas <cocotopia@gmail.com>
2021-03-03 12:31:03 -0500

    Update: actualizar id nota tratamiento de datos PROD

--- properties/sites/elcomercio.js
+++ properties/sites/elcomercio.js
@@ -50,7 +50,7 @@ export default {
     cookiesPolicy: 'IBXQQAQRGVAURMCAIB7MRN34WE',
     aboutUs: '',
     frequentQuestions: '',
-    dataTreatment: '2PQFBPZDNBAUDBEFXC6UAJM55A',
+    dataTreatment: 'AL4JFHDL4BGIRCF7A3FH7AR6HI',
   },
 
   assets: {
