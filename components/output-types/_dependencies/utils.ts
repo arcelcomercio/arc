1adcea5eab03b5602c3298fd998299b9c46fae32
cajotafer <hello@cajotafer.com>
2021-03-09 21:22:27 +0700

    Update: getIsStory en utils.ts

--- components/output-types/_dependencies/utils.ts
+++ components/output-types/_dependencies/utils.ts
@@ -21,3 +21,11 @@ export const getIframeStory = (): boolean => {
   const { requestUri } = useAppContext()
   return requestUri.includes('/carga-continua')
 }
+
+export const getIsStory = (): boolean => {
+  const { requestUri, metaValue } = useAppContext()
+  return (
+    metaValue('id') === 'meta_story' ||
+    /^\/preview\/([A-Z0-9]{26})\/?/.test(requestUri)
+  )
+}
