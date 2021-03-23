da1e6bc64b5b75da1ce9ca7cbd90bbe31abaae03
Jean Paul Gutierrez <jpgn.150@gmail.com>
2021-03-23 17:16:38 -0500

    New: destaque componente - estilos y configuracion (#39748)(SITETEAM)

---
+++ components/features/saltar-intro/destaque/_dependencies/custom-fields.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types'
+
+const customFields = PropTypes.shape({
+  storyConfig: PropTypes.contentConfig('story').isRequired.tag({
+    name: 'Configuración del contenido',
+  }),
+})
+
+export default customFields
---
+++ components/features/saltar-intro/destaque/default.jsx
@@ -0,0 +1,84 @@
+import * as React from 'react'
+import { useContent } from 'fusion:content'
+import { useAppContext } from 'fusion:context'
+import getProperties from 'fusion:properties'
+
+// import FeaturedStory from '../../../global-components/featured-story'
+import FeaturedStory from './_children/destaque'
+import schemaFilter from '../../../global-components/featured-story/schema-filter'
+
+import StoryData from '../../../utilities/story-data'
+import { featuredStoryFields } from '../../../utilities/included-fields'
+
+import customFields from './_dependencies/custom-fields'
+
+const CardFeaturedStoryAdvanced = props => {
+  const {
+    customFields: {
+      storyConfig: { contentService = '', contentConfigValues = {} } = {},
+    } = {},
+  } = props
+
+  const { arcSite, contextPath, deployment } = useAppContext()
+
+  const { siteName } = getProperties(arcSite)
+  const includedFields = featuredStoryFields
+
+  const data =
+    useContent({
+      source: contentService,
+      query: Object.assign(contentConfigValues, {
+        presets: 'no-presets',
+        includedFields,
+      }),
+      filter: schemaFilter(arcSite),
+    }) || {}
+
+  const {
+    primarySection,
+    primarySectionLink,
+    title,
+    websiteLink,
+    author,
+    authorLink,
+    multimediaType,
+    multimediaCaption,
+    multimedia,
+  } = new StoryData({
+    data,
+    deployment,
+    contextPath,
+    arcSite,
+  })
+
+  return (
+    <>
+      {(() => {
+        return (
+          <FeaturedStory
+            primarySection={primarySection}
+            primarySectionLink={primarySectionLink}
+            title={title}
+            websiteLink={websiteLink}
+            author={author}
+            authorLink={authorLink}
+            multimediaType={multimediaType}
+            multimediaCaption={multimediaCaption}
+            multimedia={multimedia}
+            arcSite={arcSite}
+            siteName={siteName}
+          />
+        )
+      })()}
+    </>
+  )
+}
+
+CardFeaturedStoryAdvanced.propTypes = {
+  customFields,
+}
+
+CardFeaturedStoryAdvanced.label = 'Destaque Template - SI'
+CardFeaturedStoryAdvanced.static = true
+
+export default CardFeaturedStoryAdvanced
