1e188933ba4dfef82080a60a65ec534fd08375fe
cajotafer <hello@cajotafer.com>
2021-03-23 19:18:12 +0700

    Update: conflicto en import de /utils en outputTypes

--- components/output-types/_dependencies/utils.ts
+++ components/output-types/_dependencies/utils.ts
@@ -29,3 +29,94 @@ export const getIsStory = (): boolean => {
     /^\/preview\/([A-Z0-9]{26})\/?/.test(requestUri)
   )
 }
+
+export const skipAdvertising = (data: { slug: string }[] = []): boolean =>
+  data.some(({ slug }) => slug === 'noads')
+
+type GetKeywordsProps = { siteName: string }
+
+export const getKeywords = ({ siteName }: GetKeywordsProps): string => {
+  const { metaValue } = useAppContext()
+  return metaValue('keywords') && !metaValue('keywords').match(/content/)
+    ? metaValue('keywords')
+    : `Noticias, ${siteName}, Peru, Mundo, Deportes, Internacional, Tecnologia, Diario, Cultura, Ciencias, Economía, Opinión`
+}
+
+type GetTitleProps = {
+  siteTitle: string
+  storyTitleRe: string
+  pageNumber: number
+}
+
+export const getTitle = ({
+  siteTitle,
+  storyTitleRe,
+  pageNumber,
+}: GetTitleProps): string => {
+  const { requestUri, metaValue } = useAppContext()
+  const isStory = getIsStory()
+
+  const seoTitle =
+    metaValue('title') &&
+    !metaValue('title').match(/content/) &&
+    metaValue('title')
+  const siteTitleSuffix = siteTitle.toUpperCase()
+  let title = `${seoTitle} | ${siteTitleSuffix}`
+
+  if (isStory) {
+    // title = `${seoTitle}: ${
+    //   storyTitleRe ? storyTitleRe.substring(0, 70) : ''
+    // } | ${siteTitleSuffix}`
+    const urlTitle = requestUri.split('/')
+    const sectionName = urlTitle[1] && urlTitle[1].toUpperCase()
+    const siteTitleSuffixR = siteTitleSuffix.replace('NOTICIAS ', '')
+    title = `${storyTitleRe} | ${sectionName} | ${siteTitleSuffixR}`
+  } else if (
+    pageNumber > 1 &&
+    (metaValue('id') === 'meta_tag' || metaValue('id') === 'meta_author')
+    /*  || metaValue('id') === "meta_search" */
+  ) {
+    title = `${seoTitle} | Página ${pageNumber} | ${siteTitleSuffix}`
+  } else if (metaValue('id') === 'meta_archive') {
+    const hasDate = /\d{4}-\d{2}-\d{2}/.test(requestUri)
+    const hasSection =
+      /\/archivo\/([\w\d-]+)/.test(requestUri) &&
+      !/\/archivo\/todas/.test(requestUri)
+    if (!hasDate && !hasSection) {
+      title = `Archivo de Noticias | ${siteTitleSuffix}`
+    }
+  }
+  return title
+}
+
+type GetDescriptionProps = {
+  siteName: string
+  pageNumber: number
+}
+
+export const getDescription = ({
+  siteName,
+  pageNumber,
+}: GetDescriptionProps): string => {
+  const { requestUri, metaValue } = useAppContext()
+  let description = `Últimas noticias, fotos, y videos de Perú y el mundo en ${siteName}.`
+  if (metaValue('description') && !metaValue('description').match(/content/)) {
+    description = `${metaValue('description')}`
+    if (
+      (pageNumber > 1 && metaValue('id') === 'meta_tag') ||
+      metaValue('id') === 'meta_author'
+      /*  || metaValue('id') === "meta_search" */
+    ) {
+      description = `${metaValue('description')} Página ${pageNumber}.`
+    } else if (metaValue('id') === 'meta_archive') {
+      const hasDate = /\d{4}-\d{2}-\d{2}/.test(requestUri)
+      const hasSection =
+        /\/archivo\/([\w\d-]+)/.test(requestUri) &&
+        !/\/archivo\/todas/.test(requestUri)
+      if (!hasDate && !hasSection) {
+        description = `Archivo de noticias de ${siteName}. Noticias actualizadas del Perú y el Mundo con fotos, videos y galerías sobre actualidad, deportes, economía y otros.`
+      }
+    }
+  }
+  return description
+}
