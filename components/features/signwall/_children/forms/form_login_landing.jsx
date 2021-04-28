bb8e81b22c4174ef6b4de27c48652fb02b5dc43d
George Dueñas <cocotopia@gmail.com>
2021-04-29 00:17:53 +0200

    Update: resolviendo conflictos

--- components/features/signwall/_children/forms/form_login_landing.jsx
+++ components/features/signwall/_children/forms/form_login_landing.jsx
@@ -211,6 +211,7 @@ export const FormLoginPaywall = ({ valTemplate, attributes }) => {
                         typeForm="login"
                         activeNewsletter={activeNewsletter}
                         showMsgVerify={() => triggerShowVerify()}
+                        dataTreatment={checkedPolits ? '1' : '0'}
                       />
                     ))}
                   </>
