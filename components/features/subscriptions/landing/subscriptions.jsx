cc4c4a4d339bece2f2bac9176200fc81895ce997
cajotafer <hello@cajotafer.com>
2021-05-03 22:21:21 +0700

    Update: eliminado polyfill para IntersectionObserver en landing

--- components/features/subscriptions/landing/subscriptions.jsx
+++ components/features/subscriptions/landing/subscriptions.jsx
@@ -452,12 +452,6 @@ const LandingSubscriptions = (props) => {
           />
         ) : null}
       </>
-
-      <script
-        type="text/javascript"
-        src="https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver"
-      />
-
       <script
         type="text/javascript"
         dangerouslySetInnerHTML={{
