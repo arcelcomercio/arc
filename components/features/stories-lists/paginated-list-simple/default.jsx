c666b23295319a46ca9b9aa5e7720cef93a27225
victor.castillo <victor.castillo@comercio.com.pe>
2021-05-07 15:24:38 -0500

    split paginated list components

--- components/features/stories-lists/paginated-list-simple/default.jsx
+++ components/features/stories-lists/paginated-list-simple/default.jsx
@@ -4,7 +4,7 @@ import getProperties from 'fusion:properties'
 import { useContent } from 'fusion:content'
 
 import { customFields } from '../_dependencies/custom-fields'
-import StoryItem from '../../../global-components/story-item'
+import StorySimple from '../../../global-components/story-simple'
 import Pagination from '../../../global-components/pagination'
 import Ads from '../../../global-components/ads'
 import StructuredData from './_children/structured-data'
@@ -12,6 +12,7 @@ import { SITE_DEPOR } from '../../../utilities/constants/sitenames'
 
 const classes = {
   adsBox: 'flex items-center flex-col no-desktop pb-20',
+  storySimpleHeader: 'story-simple__header',
 }
 
 const StoriesListPaginatedList = props => {
@@ -24,7 +25,6 @@ const StoriesListPaginatedList = props => {
     contextPath,
     arcSite,
     requestUri,
-    isAdmin,
   } = useFusionContext()
   const { customFields: customFieldsProps = {} } = props
   const { isDfp = false } = getProperties(arcSite)
@@ -83,14 +83,44 @@ const StoriesListPaginatedList = props => {
 
   return (
     <>
-      <div>
+      <div className="pl-20 pr-20">
+        {author && (
+          <div className={classes.storySimpleHeader}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 53 54">
+              <defs />
+              <g clip-path="url(#clip0)">
+                <path
+                  fill="#000"
+                  d="M26.5 54c14.498 0 26.25-12.088 26.25-27S40.998 0 26.5 0C12.003 0 .25 12.088.25 27S12.003 54 26.5 54z"
+                />
+                <path
+                  fill="#fff"
+                  d="M38.28 22.06H26.9v13.82c.06 1.823.401 2.588 2.23 2.588 2.574 0 2.46-1.884 2.574-4l.056-1.645h7.835v2.765c0 8.41-3.031 10.294-11.036 10.294-6.29 0-11.494-.825-11.494-8.588V22.059h-3.66v-7.411h3.66V8.12h9.836v6.53h11.38v7.408z"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0">
+                  <path
+                    fill="#fff"
+                    d="M0 0h52.5v54H0z"
+                    transform="translate(.25)"
+                  />
+                </clipPath>
+              </defs>
+            </svg>
+            <h2>Las noticias de {author.firstName}</h2>
+          </div>
+        )}
         {stories.map((story, index) => {
           const ads = hasAds(index + 1, activeAdsArray)
           return (
             <Fragment key={`Paginated-list-${story._id}`}>
-              <StoryItem
+              <StorySimple
                 data={story}
-                {...{ deployment, contextPath, arcSite, isAdmin }}
+                {...{ deployment, contextPath, arcSite }}
               />
               {ads.length > 0 && (
                 <div className={classes.adsBox}>
@@ -106,6 +136,7 @@ const StoriesListPaginatedList = props => {
           )
         })}
       </div>
+
       {arcSite == SITE_DEPOR && isSearchSection && (
         <div id="afscontainer1"></div>
       )}
@@ -132,7 +163,7 @@ StoriesListPaginatedList.propTypes = {
   customFields,
 }
 
-StoriesListPaginatedList.label = 'Listado con paginación'
+StoriesListPaginatedList.label = 'Listado con paginación simple'
 StoriesListPaginatedList.static = true
 
 export default StoriesListPaginatedList
