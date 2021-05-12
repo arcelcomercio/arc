b6b2994cf4ca53bd9ed57439e456c6e384dfba7c
cajotafer <hello@cajotafer.com>
2021-05-12 17:04:15 +0700

    Update: isProd y isSandbox booleans en /arc/env + eliminados de properties

--- components/features/subscriptions/_children/fb-account-linking.jsx
+++ components/features/subscriptions/_children/fb-account-linking.jsx
@@ -17,7 +17,6 @@ const FbEventTag = React.memo(
         ...props,
       },
     })
-    console.log({ content })
     if (content && props.debug) {
       if (typeof window !== 'undefined') {
         window.console.log(`SignedUri: ${content?.uri}`)
@@ -92,14 +91,3 @@ LogIntoAccountEventTag.propTypes = {
   debug: PropTypes.bool,
   subscriptionId: PropTypes.string.isRequired,
 }
-
-// https://www.facebook.com/tr
-// ?id=1252229265121278
-// &ev=LogIntoAccount
-// &cd%5Bsubscription_id%5D=676dd1b1-2afd-40d7-a61d-f393102a550e
-// &cd%5Barc-site%5D=elcomercio
-// &cd%5Bis_subscriber%5D=true
-// &noscript=1
-// &eid=e30df99f-4080-4321-b7e8-26e9873104c5
-// &ts=1620660496624
-// &sig=WGrTsbgSk0D0mD8NheX6x5anHB795hr8mTZKOWEJS2s%3D
