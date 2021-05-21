f6d353bd8164f54d0cf42e65c97efd97f1911dfe
Alvaro Enrique <alvaroenriquehl@gmail.com>
2021-05-21 08:59:20 -0500

    Update: wip features para la homepage de Depor La Polla (#43125)

--- components/features/polla/home-landing/default.tsx
+++ components/features/polla/home-landing/default.tsx
@@ -44,35 +44,6 @@ const PollaHomepage: FC<Props> = () => {
           alt="Imagen de interacción"
         />
       </div>
-      <div className="polla-home__rank-container">
-        <img
-          className="polla-home__rank-img"
-          src="https://tl.vhv.rs/dpng/s/407-4077902_no-1-logo-png-h-with-laurel-wreath.png"
-          alt="Trofeo"
-        />
-        <div className="polla-home__rank-table">
-          <h3 className="polla-home__rank-title">RANKING</h3>
-          {Array.from(Array(10).keys()).map((_, i) => (
-            <div className="polla-home__rank-item">
-              <div className="polla-home__rank-first">{i + 1}.</div>
-              <div className="polla-home__rank-second">Alessandra Quispe</div>
-              <div className="polla-home__rank-third">30</div>
-            </div>
-          ))}
-          <a className="polla-home__rank-link" href="/">
-            VER TABLA COMPLETA
-          </a>
-        </div>
-      </div>
-      <div className="polla-home__aw">
-        <h2 className="polla-home__aw-title">
-          ¡Estos son algunos de los premios!
-        </h2>
-        <div>Aqui va el HTML</div>
-        <a href="/" className="polla-home__aw-link">
-          VER MÁS
-        </a>
-      </div>
     </>
   )
 }
