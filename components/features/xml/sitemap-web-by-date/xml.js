7a01a3b775a0bfa2909ce85ae0769b2797f49e38
victor.castillo <victor.castillo@comercio.com.pe>
2021-05-31 17:41:19 -0500

    Update: sitemap by date completado

---
+++ components/features/xml/sitemap-web-by-date/xml.js
@@ -0,0 +1,87 @@
+import Consumer from 'fusion:consumer'
+import { localISODate } from '../../../utilities/helpers'
+
+/**
+ * @description Muestra listado de historias para fecha especifica.
+ *
+ * @returns {Object} Objeto con estructura manipulable por
+ * xmlBuilder, para construir listado de historias para fecha especifica.
+ */
+
+@Consumer
+class XmlSitemapWebByDate {
+  constructor(props) {
+    this.props = props
+    const { globalContent } = props
+    const { year, month, section } = globalContent || {}
+
+    this.fetchContent(this.getStates(section, year, month))
+  }
+
+  getStates = (section, year, month) => {
+    const interval = 100
+    const states = {}
+
+    let count = 0
+    // MAX 1000 historias
+    while (count <= 9) {
+      states[`data${count}`] = {
+        source: 'sitemap-feed-by-section-date',
+        query: {
+          section,
+          year,
+          month,
+          from: count * interval,
+          size: interval,
+        },
+      }
+      count += 1
+    }
+    return { ...states }
+  }
+
+  render() {
+    const { arcSite, siteProperties: { siteUrl = '' } = {} } = this.props
+
+    const stories = []
+    if (this.state)
+      Object.keys(this.state).forEach((key) => {
+        const { content_elements: contentElements = [] } =
+          (key && this.state[key]) || {}
+        stories.push(...contentElements)
+      })
+
+    if (!stories) {
+      return null
+    }
+
+    const sitemap = {
+      urlset: stories.map((story) => {
+        const {
+          websites: { [arcSite]: { website_url: websiteLink } = {} } = {},
+        } = story
+
+        return {
+          url: {
+            loc: `${siteUrl}${websiteLink || ''}`,
+            lastmod: localISODate(
+              arcSite === 'elcomercio'
+                ? story.publish_date
+                : story.display_date || ''
+            ),
+            changefreq: arcSite === 'elcomercio' ? 'always' : 'hourly',
+            priority: arcSite === 'elcomercio' ? '0.5' : '1.0',
+          },
+        }
+      }),
+    }
+
+    sitemap.urlset.push({
+      '@xmlns': 'http://www.sitemaps.org/schemas/sitemap/0.9',
+    })
+
+    return sitemap
+  }
+}
+
+export default XmlSitemapWebByDate
---
+++ content/sources/sitemap-feed-by-section-date.js
@@ -0,0 +1,130 @@
+import RedirectError from '../../components/utilities/redirect-error'
+import { getYYYYMMDDfromISO } from '../../components/utilities/date-time/dates'
+
+const schemaName = 'stories-dev'
+
+const params = [
+  {
+    name: 'section',
+    displayName: 'Sección',
+    type: 'text',
+  },
+  {
+    name: 'year',
+    displayName: 'Año',
+    type: 'number',
+  },
+  {
+    name: 'month',
+    displayName: 'Mes',
+    type: 'number',
+  },
+  {
+    name: 'from',
+    displayName: 'Noticia inicial',
+    type: 'number',
+  },
+]
+
+const getLastDate = (date) => {
+  const requestedDate = new Date(date)
+  const lastDate = new Date(
+    requestedDate.getFullYear(),
+    requestedDate.getMonth() + 1,
+    0
+  )
+  return getYYYYMMDDfromISO(lastDate)
+}
+
+const getQueryFilter = (section, website, date) => {
+  const body = {
+    query: {
+      bool: {
+        must: [
+          {
+            term: {
+              type: 'story',
+            },
+          },
+        ],
+      },
+    },
+  }
+
+  if (section !== '/') {
+    //  Solo si hay una seccion definida
+    body.query.bool.must.push({
+      nested: {
+        path: 'taxonomy.sections',
+        query: {
+          bool: {
+            must: [
+              {
+                terms: {
+                  'taxonomy.sections._id': [section],
+                },
+              },
+              {
+                term: {
+                  'taxonomy.sections._website': website,
+                },
+              },
+            ],
+          },
+        },
+      },
+    })
+  }
+
+  if (date) {
+    body.query.bool.must.push({
+      range: {
+        display_date: {
+          gte: `${date}T05:00:00`,
+          lte: `${getLastDate(date)}T23:59:59`,
+        },
+      },
+    })
+  }
+
+  const queryFilter = `body=${encodeURI(JSON.stringify(body))}`
+
+  return queryFilter
+}
+
+const resolve = (key = {}) => {
+  const website = key['arc-site'] || 'Arc Site no está definido'
+  const {
+    section: auxSection = '/',
+    year: auxYear,
+    month: auxMonth,
+    from,
+    size,
+    includedFields,
+  } = key
+
+  if (auxYear < 2009) throw new RedirectError(`/410`, 410)
+
+  const section = auxSection === null || !auxSection ? '/' : auxSection
+  const date = auxYear && auxMonth ? `${auxYear}-${auxMonth}-01` : null
+
+  const queryFilter = getQueryFilter(`/${section}`, website, date)
+
+  const sourceInclude = includedFields
+    ? `&_sourceInclude=${includedFields}`
+    : `&_sourceInclude=websites.${website}.website_url,_id,display_date,publish_date`
+
+  const requestUri = `/content/v4/search/published?${queryFilter}&sort=display_date:desc&website=${website}&from=${
+    from || 0
+  }&size=${size || 100}${sourceInclude}`
+  return requestUri
+}
+
+const source = {
+  resolve,
+  schemaName,
+  params,
+  ttl: 120,
+}
+
+export default source
