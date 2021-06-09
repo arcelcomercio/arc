11531cdcea2c0c68b0cd8f9af674b494bd10a8ec
Luis Bustamante <ljbustamante@gmail.com>
2021-06-09 08:46:10 -0500

    Update: positions table service polla

--- components/features/polla/positions-table/default.tsx
+++ components/features/polla/positions-table/default.tsx
@@ -1,226 +1,101 @@
 /* eslint-disable react/button-has-type */
+import PropTypes from 'prop-types'
 import * as React from 'react'
 import { FC } from 'types/features'
 
-const PollaPositionsTable: FC = () => {
+const DEFAULT_ENDPOINT =
+  'https://cdna-resultadosopta.minoticia.pe/api-soccer/statistics/leagues/'
+
+const UUID_COMPETITION = '45db8orh1qttbsqq9hqapmbit'
+
+interface Props {
+  customFields?: {
+    serviceEndPoint?: string
+  }
+}
+
+const PollaPositionsTable: FC<Props> = (props) => {
   // eslint-disable-next-line no-console
-  console.log('TODO')
+  const [list, setList] = React.useState<any[]>([])
+
+  const { customFields: { serviceEndPoint } = {} } = props
+
+  React.useEffect(() => {
+    fetch(
+      `${serviceEndPoint || DEFAULT_ENDPOINT}/${UUID_COMPETITION}/positions`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setList(res)
+      })
+  }, [])
 
   return (
     <>
-    <div className="polla-positions">
-      <div className="polla-positions__title">
-        <div className="polla-positions__title-left">COPA AMÉRICA 2021</div>
-        <div className="polla-positions__title-right">POSICIONES</div>
-      </div>
-      <div className="polla-positions__group">
-        <div className="polla-positions__group-left">
-          <div className="polla-positions__group-left__title">
-            Grupo A
+      <div className="polla-positions">
+        <div className="polla-positions__title">
+          <div className="polla-positions__title-left">COPA AMÉRICA 2021</div>
+          <div className="polla-positions__title-right">POSICIONES</div>
+        </div>
+        {list.map((group) => (
+          <div className="polla-positions__group">
+            <div className="polla-positions__group-left">
+              <div className="polla-positions__group-left__title">
+                Grupo {group.name}
+              </div>
+              {group.map((team, i) => (
+                <ul className="polla-positions__group-left__teams">
+                  <li>
+                    <span className="polla-positions__group-left__teams-number">
+                      {i + 1}
+                    </span>
+                    <span className="polla-positions__group-left__teams-flag">
+                      <img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag" />
+                    </span>
+                    <span className="polla-positions__group-left__teams-name">
+                      {team?.name}
+                    </span>
+                  </li>
+                </ul>
+              ))}
+            </div>
+            <div className="polla-positions__group-right">
+              <ul className="polla-positions__group-right__title">
+                <li>Pts.</li>
+                <li>PJ</li>
+                <li>PG</li>
+                <li>PE</li>
+                <li>PP</li>
+              </ul>
+              {group.map((team) => (
+                <ul className="polla-positions__group-right__points">
+                  <li>{team?.points}</li>
+                  <li>{team?.played}</li>
+                  <li>{team?.won}</li>
+                  <li>{team?.drawn}</li>
+                  <li>{team?.lost}</li>
+                </ul>
+              ))}
+            </div>
           </div>
-          <ul className="polla-positions__group-left__teams">
-            <li>
-              <span className="polla-positions__group-left__teams-number">1</span>
-              <span className="polla-positions__group-left__teams-flag"><img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag"/></span>
-              <span className="polla-positions__group-left__teams-name">Argentina</span>
-            </li>
-          </ul>
-          <ul className="polla-positions__group-left__teams">
-            <li>
-              <span className="polla-positions__group-left__teams-number">2</span>
-              <span className="polla-positions__group-left__teams-flag"><img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag"/></span>
-              <span className="polla-positions__group-left__teams-name">Argentina</span>
-            </li>
-          </ul>
-          <ul className="polla-positions__group-left__teams">
-            <li>
-              <span className="polla-positions__group-left__teams-number">3</span>
-              <span className="polla-positions__group-left__teams-flag"><img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag"/></span>
-              <span className="polla-positions__group-left__teams-name">Argentina</span>
-            </li>
-          </ul>
-          <ul className="polla-positions__group-left__teams">
-            <li>
-              <span className="polla-positions__group-left__teams-number">4</span>
-              <span className="polla-positions__group-left__teams-flag"><img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag"/></span>
-              <span className="polla-positions__group-left__teams-name">Argentina</span>
-            </li>
-          </ul>
-          <ul className="polla-positions__group-left__teams">
-            <li>
-              <span className="polla-positions__group-left__teams-number">5</span>
-              <span className="polla-positions__group-left__teams-flag"><img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag"/></span>
-              <span className="polla-positions__group-left__teams-name">Argentina</span>
-            </li>
-          </ul>
-          <ul className="polla-positions__group-left__teams">
-            <li>
-              <span className="polla-positions__group-left__teams-number">6</span>
-              <span className="polla-positions__group-left__teams-flag"><img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag"/></span>
-              <span className="polla-positions__group-left__teams-name">Argentina</span>
-            </li>
-          </ul>
-        </div>
-        <div className="polla-positions__group-right">
-          <ul className="polla-positions__group-right__title">
-            <li>Pts.</li>
-            <li>PJ</li>
-            <li>PG</li>
-            <li>PE</li>
-            <li>PP</li>
-          </ul>
-          <ul className="polla-positions__group-right__points">
-            <li>0</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-          </ul>
-          <ul className="polla-positions__group-right__points">
-            <li>0</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-          </ul>
-          <ul className="polla-positions__group-right__points">
-            <li>0</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-          </ul>
-          <ul className="polla-positions__group-right__points">
-            <li>0</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-          </ul>
-          <ul className="polla-positions__group-right__points">
-            <li>0</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-          </ul>
-          <ul className="polla-positions__group-right__points">
-            <li>0</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-          </ul>
+        ))}
+        <div className="polla-positions__button">
+          <button>VER TABLA COMPLETA</button>
         </div>
       </div>
-      <div className="polla-positions__group">
-        <div className="polla-positions__group-left">
-          <div className="polla-positions__group-left__title">
-            Grupo B
-          </div>
-          <ul className="polla-positions__group-left__teams">
-            <li>
-              <span className="polla-positions__group-left__teams-number">1</span>
-              <span className="polla-positions__group-left__teams-flag"><img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag"/></span>
-              <span className="polla-positions__group-left__teams-name">Argentina</span>
-            </li>
-          </ul>
-          <ul className="polla-positions__group-left__teams">
-            <li>
-              <span className="polla-positions__group-left__teams-number">2</span>
-              <span className="polla-positions__group-left__teams-flag"><img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag"/></span>
-              <span className="polla-positions__group-left__teams-name">Argentina</span>
-            </li>
-          </ul>
-          <ul className="polla-positions__group-left__teams">
-            <li>
-              <span className="polla-positions__group-left__teams-number">3</span>
-              <span className="polla-positions__group-left__teams-flag"><img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag"/></span>
-              <span className="polla-positions__group-left__teams-name">Argentina</span>
-            </li>
-          </ul>
-          <ul className="polla-positions__group-left__teams">
-            <li>
-              <span className="polla-positions__group-left__teams-number">4</span>
-              <span className="polla-positions__group-left__teams-flag"><img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag"/></span>
-              <span className="polla-positions__group-left__teams-name">Argentina</span>
-            </li>
-          </ul>
-          <ul className="polla-positions__group-left__teams">
-            <li>
-              <span className="polla-positions__group-left__teams-number">5</span>
-              <span className="polla-positions__group-left__teams-flag"><img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag"/></span>
-              <span className="polla-positions__group-left__teams-name">Argentina</span>
-            </li>
-          </ul>
-          <ul className="polla-positions__group-left__teams">
-            <li>
-              <span className="polla-positions__group-left__teams-number">6</span>
-              <span className="polla-positions__group-left__teams-flag"><img src="http://i.imgur.com/oSlQ5BZ.png" alt="flag"/></span>
-              <span className="polla-positions__group-left__teams-name">Argentina</span>
-            </li>
-          </ul>
-        </div>
-        <div className="polla-positions__group-right">
-          <ul className="polla-positions__group-right__title">
-            <li>Pts.</li>
-            <li>PJ</li>
-            <li>PG</li>
-            <li>PE</li>
-            <li>PP</li>
-          </ul>
-          <ul className="polla-positions__group-right__points">
-            <li>0</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-          </ul>
-          <ul className="polla-positions__group-right__points">
-            <li>0</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-          </ul>
-          <ul className="polla-positions__group-right__points">
-            <li>0</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-          </ul>
-          <ul className="polla-positions__group-right__points">
-            <li>0</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-          </ul>
-          <ul className="polla-positions__group-right__points">
-            <li>0</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-          </ul>
-          <ul className="polla-positions__group-right__points">
-            <li>0</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-            <li>-</li>
-          </ul>
-        </div>
-      </div>
-      <div className="polla-positions__button">
-        <button>VER TABLA COMPLETA</button>
-      </div>
-    </div>
     </>
   )
 }
 
 PollaPositionsTable.label = 'La Polla - Tabla de Posiciones'
+PollaPositionsTable.propTypes = {
+  customFields: PropTypes.shape({
+    serviceEndPoint: PropTypes.string.tag({
+      name: 'URL del servicio',
+      description:
+        'Por defecto la URL es https://cdna-resultadosopta.minoticia.pe/api-soccer/statistics/leagues/',
+    }),
+  }),
+}
 
 export default PollaPositionsTable
