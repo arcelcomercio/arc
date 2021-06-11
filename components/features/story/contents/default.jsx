ac41941a58c5fa11ae322a54b5df55df2cf8b8c5
victor.castillo <victor.castillo@comercio.com.pe>
2021-06-11 09:28:57 -0500

    agregado script jwplayer story/contents/default & _dependencies/jwplayer-script

--- components/features/story/contents/default.jsx
+++ components/features/story/contents/default.jsx
@@ -85,6 +85,7 @@ const classes = {
   premiumText:
     'premium__text flex justify-center items-center text-black font-bold icon-padlock',
 }
+
 @Consumer
 class StoryContents extends React.PureComponent {
   render() {
@@ -193,6 +194,7 @@ class StoryContents extends React.PureComponent {
     const isJwVideo = rawHtmlContent.includes('cdn.jwplayer.com')
     const isPreview = /^\/preview\//.test(requestUri)
 
+    const comscoreScript = `jwplayer().on('ready', function () { ns_.StreamingAnalytics.JWPlayer(jwplayer(), { publisherId: "8429002"  }); });`
     return (
       <>
         <div className={classes.news}>
@@ -332,8 +334,11 @@ class StoryContents extends React.PureComponent {
                         ? playerId.playerAds
                         : playerId.player
                       return (
-                        <>
+                        <div className="jwp-id">
+                          <script src="https://sb.scorecardresearch.com/c2/plugins/streamingtag_plugin_jwplayer.js" />
+
                           <div
+                            aria-label="jwp-video"
                             className="jwplayer-lazy"
                             id={`botr_${mediaId}_${jwplayerId}_div`}>
                             <div className="jwplayer-lazy-icon-play" />
@@ -348,13 +353,20 @@ class StoryContents extends React.PureComponent {
                               loading="lazy"
                             />
                           </div>
+                          <span className="jwp-aaa" />
                           <figcaption
                             className="story-content__caption"
                             dangerouslySetInnerHTML={{
                               __html: descriptionTxt,
                             }}
                           />
-                        </>
+
+                          <script
+                            dangerouslySetInnerHTML={{
+                              __html: comscoreScript,
+                            }}
+                          />
+                        </div>
                       )
                     }
                     if (sub === VIDEO_JWPLAYER_MATCHING) {
@@ -396,7 +408,8 @@ class StoryContents extends React.PureComponent {
                           className={alignmentClass}
                           dangerouslySetInnerHTML={{
                             __html: replaceTags(content),
-                          }} />
+                          }}
+                        />
                       </>
                     )
                   }
@@ -468,7 +481,8 @@ class StoryContents extends React.PureComponent {
                                       '<a itemprop="url"'
                                     )
                                   : '',
-                              }} />
+                              }}
+                            />
                           ))}
                         </ListType>
                       )
--- components/output-types/_dependencies/jwplayer-script.js
+++ components/output-types/_dependencies/jwplayer-script.js
@@ -13,6 +13,7 @@ const jwplayerObserver = (entries, observer) => {
         node.type = 'text/javascript'
         node.src = linkElem
         document.head.append(node)
+        jwplayer().on('ready', function () { ns_.StreamingAnalytics.JWPlayer(jwplayer(), { publisherId: "8429002"  }); });
       }
       observer.unobserve(target)
     }
@@ -34,7 +35,7 @@ window.addEventListener('load', function() {
 })
 */
 
-const videoScript = `"use strict";var jwplayerObserver=function(e,r){e.forEach(function(e){var t=e.isIntersecting,n=e.target;if(t){console.log("target",n);var o=n.getAttribute("id");if((o=o.split("_"))[1]){var a="https://cdn.jwplayer.com/players/"+o[1]+"-"+o[2]+".js",i=document.createElement("script");i.type="text/javascript",i.src=a,document.head.append(i)}r.unobserve(n)}})};window.addEventListener("load",function(){requestIdle(function(){if("IntersectionObserver"in window){var e=Array.from(document.body.querySelectorAll(".jwplayer-lazy")),r=new IntersectionObserver(jwplayerObserver,{rootMargin:"0px"});e.forEach(function(e){r.observe(e)})}})});
-`
+// const videoScript = `"use strict";var jwplayerObserver=function(e,r){e.forEach(function(e){var t=e.isIntersecting,n=e.target;if(t){console.log("target",n);var o=n.getAttribute("id");if((o=o.split("_"))[1]){var a="https://cdn.jwplayer.com/players/"+o[1]+"-"+o[2]+".js",i=document.createElement("script");i.type="text/javascript",i.src=a,document.head.append(i)}r.unobserve(n)}})};window.addEventListener("load",function(){requestIdle(function(){if("IntersectionObserver"in window){var e=Array.from(document.body.querySelectorAll(".jwplayer-lazy")),r=new IntersectionObserver(jwplayerObserver,{rootMargin:"0px"});e.forEach(function(e){r.observe(e)})}})});`
+const videoScript = `const jwplayerObserver=(e,r)=>{e.forEach(e=>{const{isIntersecting:t,target:n}=e;if(t){console.log("target",n);let e=n.getAttribute("id");if((e=e.split("_"))[1]){const r="https://cdn.jwplayer.com/players/"+e[1]+"-"+e[2]+".js",t=document.createElement("script");t.type="text/javascript",t.src=r,document.head.append(t),jwplayer().on("ready",function(){ns_.StreamingAnalytics.JWPlayer(jwplayer(),{publisherId:"8429002"})})}r.unobserve(n)}})};window.addEventListener("load",function(){requestIdle(function(){if("IntersectionObserver"in window){var e=Array.from(document.body.querySelectorAll(".jwplayer-lazy")),r=new IntersectionObserver(jwplayerObserver,{rootMargin:"0px"});e.forEach(function(e){r.observe(e)})}})});`
 
 export default videoScript
