603cea03bf274897c00b946e5baaf655c006bac8
danielpolo <danny00788@gmail.com>
2021-06-23 18:29:25 -0500

    Update: amp fluid caja2 - correo

--- components/utilities/story/helpers-amp.js
+++ components/utilities/story/helpers-amp.js
@@ -24,6 +24,7 @@ export const publicidadAmp = ({
   arcSite = '',
   prebidSlot = '',
   size = '320x100',
+  fluid = false
 }) => {
   const secctionPrimary = primarySectionLink.split('/')
   let resultData = ''
@@ -36,10 +37,15 @@ export const publicidadAmp = ({
       prebidSlot != '' &&
       `rtc-config='{"vendors": {"prebidrubicon": {"REQUEST_ID": "${prebidSlot}", "ACCOUNT_ID": "19186"}}}'`) ||
     ''
-  const adsLoadAmp = `data-loading-strategy="prefer-viewability-over-views"`
+
+  const heightAmp = (fluid && SITE_DIARIOCORREO === arcSite && `height="fluid"`) || `height="${height}"`
+  const widthtAmp = (fluid && SITE_DIARIOCORREO === arcSite && ' ') || ` width="${width}"`
+  const fluidAmp = (fluid && SITE_DIARIOCORREO === arcSite && `layout="fluid"`) || ``
+
+  const adsLoadAmp = `data-loading-strategy="prefer-viewability-over-views ${fluid}"`
   if (secctionPrimary[1] !== 'respuestas') {
     resultData = `
-  <amp-ad width="${width}" height="${height}" ${adsLoadAmp} type="doubleclick"
+  <amp-ad${widthtAmp} ${fluidAmp} ${heightAmp} ${adsLoadAmp} type="doubleclick"
   data-slot="${dataSlot}" ${json} ${prebidAmp}></amp-ad>`
   }
   return createMarkup(resultData)
@@ -53,6 +59,7 @@ export const publicidadAmpAd = ({
   arcSite = '',
   prebidSlot = '',
   size = '320x100,320x50,300x1',
+  fluid = false
 }) => {
   const secctionPrimary = primarySectionLink.split('/')
   let resultData = ''
@@ -66,9 +73,12 @@ export const publicidadAmpAd = ({
     ''
 
   const adsLoadAmp = `data-loading-strategy="prefer-viewability-over-views"`
+  const heightAmp = (fluid && SITE_DIARIOCORREO === arcSite && `height="fluid"`) || `height="${height}"`
+  const widthtAmp = (fluid && SITE_DIARIOCORREO === arcSite && ' ') || ` width="${width}"`
+  const fluidAmp = (fluid && SITE_DIARIOCORREO === arcSite && `layout="fluid"`) || ``
 
   if (secctionPrimary[1] !== 'respuestas') {
-    resultData = `<amp-ad width="${width}" height="${height}" ${adsLoadAmp} type="doubleclick"
+    resultData = `<amp-ad${widthtAmp} ${fluidAmp} ${heightAmp} ${adsLoadAmp} type="doubleclick"
   data-slot="${dataSlot}" ${json} ${prebidAmp}></amp-ad>`
   }
   return createMarkup(resultData)
