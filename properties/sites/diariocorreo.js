4aa80779cf3ac28f2977182b43a996f6cf4dcfea
Jorge Dueñas <cocotopia@gmail.com>
2021-07-09 16:45:10 -0500

    Update: activación verificación de correo ojo y diario correo

--- properties/sites/diariocorreo.js
+++ properties/sites/diariocorreo.js
@@ -121,6 +121,7 @@ export default {
     'https://d1r08wok4169a5.cloudfront.net/ads-diariocorreo/ads-fia-28253241-cor_ia_interna5-300x250-div-gpt-ad-8599377-10.html',
   ],
   activeSignwall: true,
+  activeVerifyEmail: true,
   activeDataTreatment: true,
   signwall: {
     mainColorBg: '#000000',
--- properties/sites/ojo.js
+++ properties/sites/ojo.js
@@ -103,6 +103,7 @@ export default {
     'https://d1r08wok4169a5.cloudfront.net/ads-ojo/ads-fia-28253241-ojo_ia_interna5-300x250-div-gpt-ad-8599377-30.html',
   ],
   activeSignwall: true,
+  activeVerifyEmail: true,
   activeDataTreatment: true,
   signwall: {
     mainColorBg: '#333333',
