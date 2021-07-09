440ecbb2cdf1a2cd52f8670bf896d900feacf30e
Luis Bustamante <ljbustamante@gmail.com>
2021-07-08 23:00:55 -0500

    New: search form video section and update search util query section param add

---
+++ components/features/video/search/default.tsx
@@ -0,0 +1,81 @@
+import { useFusionContext } from 'fusion:context'
+import React from 'react'
+import { FC } from 'types/features'
+
+import Button from '../../../global-components/button'
+import searchQuery from '../../../utilities/client/search'
+import customFields from './_dependencies/custom-fields'
+
+const classes = {
+  btnSearch: `header-inverted-featured__btn-search active`,
+  iconSearch: 'header-inverted-featured__icon-search icon-search active',
+  search: `header-inverted-featured__search active`,
+}
+
+interface Props {
+  customFields?: {
+    label?: string
+    inputLabel?: string
+  }
+}
+
+const SearchForm: FC<Props> = (props) => {
+  const {
+    customFields: { label, inputLabel },
+  } = props
+
+  const { globalContentConfig: { query = {} } = {} } = useFusionContext()
+
+  const search = decodeURIComponent(query.query || '').replace(/\+/g, ' ')
+
+  const inputSearch = React.useRef()
+
+  const handleSearch = () => {
+    const { value } = inputSearch.current
+    searchQuery(value, 'descendiente', 'videos')
+  }
+
+  const handleKeyDown = (e) => {
+    e.preventDefault()
+    const { value } = e.target
+    if (value !== '' && e.which === 13) {
+      handleSearch()
+    }
+  }
+
+  // const optionButtonClick = () => {
+  //   if (statusSearch) _handleSearch()
+  //   else focusInputSearch()
+  //   setStatusSearch(!statusSearch)
+  // }
+
+  return (
+    <div>
+      <div>{label}</div>
+      <form className={classes.form} onSubmit={(e) => e.preventDefault()}>
+        <input
+          id="header-search-input"
+          ref={inputSearch}
+          type="search"
+          defaultValue={search}
+          onKeyUp={handleKeyDown}
+          placeholder={inputLabel}
+          className={classes.search}
+        />
+        <Button
+          iconClass={classes.iconSearch}
+          btnClass={`${classes.btnSearch}`}
+          onClick={handleSearch}
+        />
+      </form>
+    </div>
+  )
+}
+
+SearchForm.propTypes = {
+  customFields,
+}
+
+SearchForm.label = 'Búsqueda desde formulario'
+
+export default SearchForm
