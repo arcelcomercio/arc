8366cf94026c6100298ee36c560fcea32d3400a3
cajotafer <hello@cajotafer.com>
2021-07-14 23:15:20 +0700

    Update: eliminados tokens de propiedades subscriptions

--- components/features/subscriptions/_dependencies/Properties.ts
+++ components/features/subscriptions/_dependencies/Properties.ts
@@ -640,19 +640,6 @@ const PropertiesCommon = {
     howItWork:    'https://cip.pagoefectivo.pe/CNT/QueEsPagoEfectivo.aspx'
   },
 
-  tokens: {
-    paymentTracker: isProd
-      ? '5088cbc5ceb807c702b4e3487173ef792eb50be4'
-      : 'deb904a03a4e31d420a014534514b8cc8ca4d111',
-    payEfectivoService: isProd ? '1290' : '1290',
-    payEfectivoAccessKey: isProd
-      ? 'ZDVkMmMxZmI1MGExMTE2'
-      : 'ZDVkMmMxZmI1MGExMTE2',
-    payEfectivoSecretKey: isProd
-      ? 'uVlNKocbAxDNxcvYPAnrkNtrMcaB5jRMS/kxnipx'
-      : 'uVlNKocbAxDNxcvYPAnrkNtrMcaB5jRMS/kxnipx',
-  },
-
   // prettier-ignore
   texts: {
     login:           'Bienvenido. Inicia sesión',
