e6ec13d1787167166a61ed55b903d461cc84c771
Alvaro Enrique <alvaroenriquehl@gmail.com>
2021-08-03 16:44:36 -0500

    Update: fix para mostrar los custom fields del footer (#47860)

--- components/features/layout/footer/lite.jsx
+++ components/features/layout/footer/lite.jsx
@@ -1,9 +1,10 @@
-import * as React from 'react'
 import { useAppContext } from 'fusion:context'
 import getProperties from 'fusion:properties'
+import PropTypes from 'prop-types'
+import * as React from 'react'
+
 import { getAssetsPath } from '../../../utilities/assets'
 import getFooterProperties from '../../footer/_dependencies/properties'
-
 import StoryFooter from './_lite/_children/story'
 
 const LayoutFooter = () => {
@@ -24,5 +25,23 @@ const LayoutFooter = () => {
 
 LayoutFooter.label = 'Pie de Página'
 LayoutFooter.static = true
+
+LayoutFooter.propTypes = {
+  customFields: PropTypes.shape({
+    footerType: PropTypes.oneOf(['standard', 'secondary', 'story']).tag({
+      name: 'Diseño del Pie de página',
+      labels: {
+        standard: 'Footer estándar',
+        secondary: 'Footer 2',
+        story: 'Footer - Notas',
+      },
+      defaultValue: 'standard',
+    }),
+    sectionsHierarchyConfig: PropTypes.contentConfig('navigation').tag({
+      name: 'Editar navegación de "secciones"',
+      group: 'Configuración del contenido',
+    }),
+  }),
+}
 
 export default LayoutFooter
