6ff2c2616648b6ea29612542c5031a62d5a290fc
cajotafer <hello@cajotafer.com>
2021-08-05 22:18:39 +0700

    Update: reestablecimiento de paywall-tracking

--- content/sources/paywall-tracking.js
+++ content/sources/paywall-tracking.js
@@ -1,23 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { PAYMENT_TRACKER_TOKEN } from 'fusion:environment'
 import request from 'request-promise-native'
-import { ConentSourceBase } from 'types/content-source'
 
 import { PropertiesCommon } from '../../components/features/subscriptions/_dependencies/Properties'
 
-export type PaywallTrackingQuery = {
-  url_referer: 'text'
-  confirm_subscription: 'text'
-  medium: 'text'
-  is_pwa: 'text'
-  user_agent: 'text'
-  uuid: 'text'
-  arc_order: 'text'
-}
-
-type PaywallTrackingParams = PaywallTrackingQuery & ConentSourceBase
-
-const fetch = (key: PaywallTrackingParams): request.RequestPromise<any> => {
+const fetch = (key) => {
   const website = key?.['arc-site']
   const data = key || {}
   const { urls: urlCommon } = PropertiesCommon
@@ -37,13 +24,4 @@ const fetch = (key: PaywallTrackingParams): request.RequestPromise<any> => {
 
 export default {
   fetch,
-  params: {
-    url_referer: 'text',
-    confirm_subscription: 'text',
-    medium: 'text',
-    is_pwa: 'text',
-    user_agent: 'text',
-    uuid: 'text',
-    arc_order: 'text',
-  },
 }
