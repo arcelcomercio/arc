3608dd5752a4c9f46bf08f51af48ae831e15cf46
cajotafer <hello@cajotafer.com>
2021-08-17 00:22:40 +0700

    Fix: validacion taboola premium

--- components/output-types/_children/meta-story.jsx
+++ components/output-types/_children/meta-story.jsx
@@ -709,7 +709,7 @@ export default ({
         </>
       )}
       {isAmp !== true &&
-        (!isPremium && primarySectionLink !== '/archivo-elcomercio')(
+        !(isPremium && primarySectionLink === '/archivo-elcomercio')(
           <script dangerouslySetInnerHTML={{ __html: scriptTaboola }} />
         )}
       {isAmp === true &&
