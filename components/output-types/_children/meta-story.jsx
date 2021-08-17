e3579a2a73a2879e3a0081108db50c3cd9a49f4e
danielpolo <danny00788@gmail.com>
2021-08-17 15:51:33 -0500

    Update: archivoelcomercio premium sin taboola

--- components/output-types/_children/meta-story.jsx
+++ components/output-types/_children/meta-story.jsx
@@ -711,7 +711,7 @@ export default ({
         </>
       )}
       {isAmp !== true &&
-        !(isPremium && primarySection === '/archivo-elcomercio/') && (
+        !(isPremium && primarySectionLink === '/archivo-elcomercio/') && (
           <script dangerouslySetInnerHTML={{ __html: scriptTaboola }} />
         )}
       {isAmp === true &&
