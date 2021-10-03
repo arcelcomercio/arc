faaf053daf9ac3372c3d4ecb247c1633dbf7fb6a
Mr PaPer <p.perez@rpalatam.com.pe>
2021-10-03 16:40:43 -0500

    Update: estilos cajas de suscripcion

--- components/features/cards/subscribe/default.tsx
+++ components/features/cards/subscribe/default.tsx
@@ -38,14 +38,11 @@ const CardSubscribe: FC = () => {
 
   return (
     <div
-      className={classes.container}
+      className="items-center tabloid row-1 flex flex-col icono"
       style={{
         border: `3.5px solid ${mainColorLink}`,
-        height: '374px',
-        minWidth: '313px',
         backgroundImage:
           'url("https://cdn.shopify.com/s/files/1/0449/4229/5199/files/diario-correo-background.png")',
-        backgroundSize: '100% 100%',
       }}>
       <img
         src={`${getAssetsPath(
@@ -53,15 +50,8 @@ const CardSubscribe: FC = () => {
           contextPath
         )}/resources/dist/${arcSite}/images/${mainLogo}?d=1`}
         alt={`Logo ${siteName}`}
-        style={{
-          width: '90px',
-          height: '110px',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          marginTop: '25px',
-          // boxShadow:'0 15px 10px -10px rgb(0 0 0 / 50%), 0 1px 4px rgb(0 0 0 / 30%), 0 0 40px rgb(0 0 0 / 10%) inset',
-        }}
-        className={classes.logo}
+       
+        className="logo"
       />
       <div
         className="mt-25 ml-20 mr-20"
@@ -69,27 +59,14 @@ const CardSubscribe: FC = () => {
           textAlign: 'center',
         }}>
         <p
-          className={classes.text}
-          style={{
-            fontSize: '25px',
-            fontWeight: 501,
-            color: 'black',
-            lineHeight: '35px',
-          }}>
+          className={classes.text}>
           ¿Aún no tienes una cuenta?
         </p>
         <a
           href="/signwall/?outputType=subscriptions&signwallOrganic=1"
           className={classes.button}
           style={{
-            color: 'white',
             background: mainColorLink,
-            textDecoration: 'none',
-            width: '100%',
-            display: 'block',
-            marginTop: '35px',
-            borderRadius: '8px',
-            fontSize: '19px',
           }}>
           Regístrate ¡Gratis!
         </a>
