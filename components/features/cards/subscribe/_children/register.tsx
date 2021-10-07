f4e55be5e5505ce5a0782397cbae08c999fb7999
Mr PaPer <p.perez@rpalatam.com.pe>
2021-10-07 11:39:25 -0500

    Update: actualizacion caja registro v3.3

--- components/features/cards/subscribe/_children/register.tsx
+++ components/features/cards/subscribe/_children/register.tsx
@@ -8,7 +8,7 @@ const classes = {
   welcome: 'subscribe__welcome',
   info: 'subscribe__info',
   image: 'subscribe__image',
-  cajainfo: 'subscribe__cajainfo ml-20'
+  cajainfo: 'subscribe__cajainfo ml-30',
 }
 const CardSubscribeResgister: React.FC = () => {
   const {
@@ -19,32 +19,36 @@ const CardSubscribeResgister: React.FC = () => {
 
   return (
     <>
-    <div
-      id="register-suscribe"
-      className={classes.container}
-      style={{
-        display: 'none',
-        minWidth: '200px',
-        maxWidth: '270px',
-      }}>
-      <p id="suscriber-user" className={classes.hello}>
-        ¡Hola!
-      </p>
-      <p
+      <div
+        id="register-suscribe"
+        className={classes.container}
         style={{
-          color: mainColorLink,
-        }}
-        className={classes.welcome}>
-        Bienvenido a nuestra comunidad digital
-      </p>
-    </div>
-     <div className={classes.cajainfo} > 
-     <p className={classes.info}>
-       Ahora podrás seguir artículos y noticias de interés
-     </p>
-     <img className={classes.image}src="https://cdn.shopify.com/s/files/1/0449/4229/5199/files/imagrn_admin_correo.png" alt="carta correo" />
-     </div>
-     </>
+          display: 'none',
+          minWidth: '200px',
+          maxWidth: '270px',
+        }}>
+        <p id="suscriber-user" className={classes.hello}>
+          ¡Hola!
+        </p>
+        <p
+          style={{
+            color: mainColorLink,
+          }}
+          className={classes.welcome}>
+          Bienvenido a nuestra comunidad digital
+        </p>
+      </div>
+      <div className={classes.cajainfo}>
+        <p className={classes.info}>
+          Ahora podrás seguir artículos y noticias de interés
+        </p>
+        <img
+          className={classes.image}
+          src="https://cdn.shopify.com/s/files/1/0449/4229/5199/files/imagrn_admin_correo.png"
+          alt="carta correo"
+        />
+      </div>
+    </>
   )
 }
 
