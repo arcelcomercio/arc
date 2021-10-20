53ff0c137a7acaa653abac895cefff5814e30f62
Rolly <rolly.sanchez@fractalservicios.pe>
2021-10-20 17:53:30 -0500

    New: componente meses - saltar intro

--- components/features/saltar-intro/search-calendario/default.tsx
+++ components/features/saltar-intro/search-calendario/default.tsx
@@ -9,17 +9,109 @@ const classes = {
   inputSearch: 'saltar-intro-search-calendario__input-search',
   btnSearch: 'saltar-intro-search-calendario__btn-search',
   iconSearch: 'saltar-intro-search-calendario__icon-search',
+  months: 'saltar-intro-search-calendario__months',
+  boxItem: 'saltar-intro-search-calendario__box-item',
+  item: 'saltar-intro-search-calendario__item',
+  month: 'saltar-intro-search-calendario__month',
 }
 
 const SaltarIntroSearchCalendario = (props: any) => {
   const {
     customFields: { block = false, inlineBlock = false },
   } = props
+  const now = new Date()
+  const year = now.getFullYear()
   return (
     <div
       className={`flex ${classes.container} ${block ? 'block' : ''} ${
         inlineBlock ? 'inline-block' : ''
       }`}>
+      <ul className={classes.boxItem}>
+        <li className={classes.item}>
+          <a
+            className={classes.month}
+            href={`/saltar-intro/calendario/${year}-01/`}>
+            Enero
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a
+            className={classes.month}
+            href={`/saltar-intro/calendario/${year}-02/`}>
+            Febrero
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a
+            className={classes.month}
+            href={`/saltar-intro/calendario/${year}-03/`}>
+            Marzo
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a
+            className={classes.month}
+            href={`/saltar-intro/calendario/${year}-04/`}>
+            Abril
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a
+            className={classes.month}
+            href={`/saltar-intro/calendario/${year}-05/`}>
+            Mayo
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a
+            className={classes.month}
+            href={`/saltar-intro/calendario/${year}-06/`}>
+            Junio
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a
+            className={classes.month}
+            href={`/saltar-intro/calendario/${year}-07/`}>
+            Julio
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a
+            className={classes.month}
+            href={`/saltar-intro/calendario/${year}-08/`}>
+            Agosto
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a
+            className={classes.month}
+            href={`/saltar-intro/calendario/${year}-09/`}>
+            Septiembre
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a
+            className={classes.month}
+            href={`/saltar-intro/calendario/${year}-10/`}>
+            Octubre
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a
+            className={classes.month}
+            href={`/saltar-intro/calendario/${year}-11/`}>
+            Noviembre
+          </a>
+        </li>
+        <li className={classes.item}>
+          <a
+            className={classes.month}
+            href={`/saltar-intro/calendario/${year}-12/`}>
+            Diciembre
+          </a>
+        </li>
+      </ul>
       <form
         className={`${classes.formSearch} position-relative`}
         onSubmit={(e) => e.preventDefault()}>
