1ff3ac992fa556a86481d72dba3fd1c1ee5b3747
Leonel Navarro Auccasi <l.navarro@rpalatam.com.pe>
2021-10-25 22:14:32 -0500

    New: avance newsletter - landing (#48297)(Site&Core)

--- components/features/statics/newsletter-landing/default.tsx
+++ components/features/statics/newsletter-landing/default.tsx
@@ -18,6 +18,40 @@ interface FeatureProps {
 
 const classes = {
   nnContainer: 'newsletter--landing',
+  nnCont1: 'newsletter--landing__container1',
+  nnTitle: 'newsletter--landing__container1__title',
+  nnLogo: 'newsletter--landing__container1__logo',
+  nnText: 'newsletter--landing__container1__Text',
+
+  nnCont2: 'newsletter--landing__container2',
+
+  nnLeftCont: 'newsletter--landing__container2__left-container',
+  nnBox: 'newsletter--landing__container2__left-container__box',
+  nnLeftSvg: 'newsletter--landing__container2__left-container__left-svg',
+  nnInputTerm: 'newsletter--landing__container2__left-container__input-term',
+  nnLeftText: 'newsletter--landing__container2__left-container__left-text',
+
+  nnRightCont: 'newsletter--landing__container2__right-container',
+  // nnBox: 'newsletter--landing__container2__left-container__box',
+  nnRightSvg: 'newsletter--landing__container2__right-container__right-svg',
+  // nnBoxCheckbox2: 'newsletter--landing__container2__right-container__box-checkbox2',
+  // nnRightText: 'newsletter--landing__container2__right-container__right-text',
+
+  nnCont3: 'newsletter--landing__container3',
+
+  nnForm6: 'newsletter--landing__container3__form6',
+  nnTextBox: 'newsletter--landing__container3__form6__text-box',
+  nnButtonReceive: 'newsletter--landing__container3__form6__button-receive',
+  // nnInputTerm: 'newsletter--landing__container2__left-container__input-term',
+  nnTextTerm: 'newsletter--landing__container3__form6__text-term',
+
+  nnForm7: 'newsletter--landing__container3__form7',
+  nnMessageSubscribed:
+    'newsletter--landing__container3__form7__message-subscribed',
+  nnButtonHome: 'newsletter--landing__container3__form7__button-home',
+
+  // nnForm1: 'newsletter--landing__container3__form1',
+  // nnButtonReceive2: 'newsletter--landing__container3__form1__button-receive2',
 }
 
 // URL DE ESTILOS EN DEPOR
@@ -29,7 +63,11 @@ const NewsletterLanding: FC<FeatureProps> = () => (
   // const { newsletterBrand } = getProperties(arcSite)
 
   <>
-    <div className={classes.nnContainer}>HOLAAAA</div>
+    <div className={classes.nnContainer}>
+      <div />
+      <div />
+      <div />
+    </div>
   </>
 )
 
