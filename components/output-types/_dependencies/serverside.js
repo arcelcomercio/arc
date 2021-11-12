28107bf86a620b6ac25a5bf26a4b21dda9e2b424
danielpolo <danny00788@gmail.com>
2021-11-12 15:12:50 -0500

    Update: prebid serverside gestion

--- components/output-types/_dependencies/serverside.js
+++ components/output-types/_dependencies/serverside.js
@@ -11,6 +11,8 @@ export const getScriptAdPushup = arcSite => {
     idPushup = '43080'
   } else if (arcSite === 'peru21') {
     idPushup = '42612'
+  } else if (arcSite === 'gestion') {
+    idPushup = '43082'
   }
   pushup = `setTimeout(function(){var e,t;window,e=document,(t=e.createElement("script")).src="//cdn.adpushup.com/${idPushup}/adpushup.js",t.crossOrigin="anonymous",t.type="text/javascript",t.async=!0,(e.getElementsByTagName("head")[0]||e.getElementsByTagName("body")[0]).appendChild(t)},5e3);`
   return pushup
@@ -23,7 +25,8 @@ export const getEnabledServerside = arcSite => {
     arcSite === 'elbocon' ||
     arcSite === 'ojo' || 
     arcSite === 'elcomercio' || 
-    arcSite === 'peru21'
+    arcSite === 'peru21' || 
+    arcSite === 'gestion'
   ) {
     epushud = true
   }
