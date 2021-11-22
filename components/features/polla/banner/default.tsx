6fea2e9f06e238deb00d14fe355e9c15817726f0
Leonel Navarro Auccasi <l.navarro@rpalatam.com.pe>
2021-11-22 14:44:26 -0500

    New: avance2  borrador reglas (polla depor) (#54348)(Site&Core)

--- components/features/polla/banner/default.tsx
+++ components/features/polla/banner/default.tsx
@@ -5,7 +5,7 @@ import { FC } from 'types/features'
 interface Props {
   customFields?: {
     bottomText?: string
-    // showTitle?: bool
+    showTitle?: boolean
   }
 }
 
@@ -20,7 +20,9 @@ const PollaBanner: FC<Props> = (props) => {
           src="https://d1ts5g4ys243sh.cloudfront.net/proyectos_especiales_general/depor/prod/polla-peru-vs-argentina-nndd-xvisual/img/polla-depor.png"
           alt="Logo La Polla"
         />
-        {/* {showTitle && <h1 className="polla-banner__logo-title">LIGA 1</h1>} */}
+        {customFields?.showTitle && (
+          <h1 className="polla-banner__logo-title">LIGA 1</h1>
+        )}
       </div>
       {customFields?.bottomText && (
         <h3 className="polla-banner__text">{customFields?.bottomText}</h3>
@@ -36,9 +38,9 @@ PollaBanner.propTypes = {
     bottomText: PropTypes.string.tag({
       name: 'Texto inferior',
     }),
-    // showTitle: PropTypes.bool.tag({
-    //   name: '¿Mostrar titulo?',
-    // }),
+    showTitle: PropTypes.bool.tag({
+      name: '¿Mostrar titulo de banner?',
+    }),
   }),
 }
 
