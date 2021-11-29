b9c8846f01b30ec5c6196717401ab8cecaae8ce5
Carlos Cespedes <j.cespedes@rpalatam.com.pe>
2021-11-29 10:49:24 -0500

    Update: implementacion prebid analytics trome

--- components/output-types/default.jsx
+++ components/output-types/default.jsx
@@ -7,8 +7,8 @@ import { PREMIUM } from '../utilities/constants/content-tiers'
 import { META_HOME } from '../utilities/constants/meta'
 import {
   SITE_DEPOR,
-  SITE_ELBOCON,
   SITE_DIARIOCORREO,
+  SITE_ELBOCON,
   SITE_ELCOMERCIO,
   SITE_ELCOMERCIOMAG,
   SITE_GESTION,
@@ -630,6 +630,12 @@ export default ({
               />
             )}
             <Dfp />
+            {indPrebid && arcSite === SITE_TROME && (
+              <script
+                defer
+                src="https://boot.pbstck.com/v1/tag/6e13d7a6-e4f7-4063-8d09-248ed9b1f70b"
+              />
+            )}
           </>
         )}
         {/* Scripts de AdManager - Fin */}
