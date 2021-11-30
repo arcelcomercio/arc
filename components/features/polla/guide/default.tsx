074efcc355d3fe8ec125fb8dd7ace2fa2b2ac700
Leonel Navarro Auccasi <l.navarro@rpalatam.com.pe>
2021-11-30 15:26:04 -0500

    New: sandbox entregable pagina posiciones (polla depor)(55016)(Site&Core)

--- components/features/polla/guide/default.tsx
+++ components/features/polla/guide/default.tsx
@@ -10,9 +10,16 @@ interface Props {
   customFields?: {
     type?: 'with_score' | 'with_social'
     title?: string
-    subtitle?: string
+    // subtitle?: string
   }
 }
+
+let isMobile: boolean
+
+if (typeof window !== 'undefined')
+  isMobile = /iPad|iPhone|iPod|android|webOS|Windows Phone/i.test(
+    window.navigator.userAgent
+  )
 
 const PollaGuide: FC<Props> = (props) => {
   const { customFields } = props
@@ -37,39 +44,42 @@ const PollaGuide: FC<Props> = (props) => {
     <div className={`polla-guide ${customFields?.type || ''}`}>
       <div className="polla-guide__title-cont">
         <h2>{customFields?.title || 'Copa América'}</h2>
-        <span>{customFields?.subtitle || ' - Fase de grupos'}</span>
+        {/* <span>{customFields?.subtitle}</span> */}
+        {customFields?.type === 'with_social' ? (
+          <div className="polla-guide__results with_social">
+            <button
+              type="button"
+              className="polla-guide__share-btn"
+              onClick={() => {
+                popUpWindow(urlsShareList.facebook, '', 600, 400)
+              }}>
+              <svg
+                width="8"
+                height="15"
+                viewBox="0 0 8 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M2.04221 15V7.96156H0V5.42739H2.04221V3.26287C2.04221 1.56198 3.24856 0 6.02823 0C7.15368 0 7.98589 0.098325 7.98589 0.098325L7.92032 2.46481C7.92032 2.46481 7.07159 2.45728 6.14543 2.45728C5.14303 2.45728 4.98244 2.87826 4.98244 3.57697V5.42739H8L7.8687 7.96156H4.98244V15H2.04221Z"
+                  fill="white"
+                />
+              </svg>
+              {isMobile ? '' : <span>Compartir</span>}
+            </button>
+          </div>
+        ) : (
+          <div className="polla-guide__results">
+            <span>Marcador:</span>
+            <span className="polla-guide__results-b">5pts</span>
+            <span className="polla-guide__results-s">|</span>
+            <span>Partido (gana - pierde) :</span>
+            <span className="polla-guide__results-b">3pts</span>
+            <span className="polla-guide__results-s">|</span>
+            <span>Ninguno :</span>
+            <span className="polla-guide__results-b">0pts</span>
+          </div>
+        )}
       </div>
-      {customFields?.type === 'with_social' ? (
-        <div className="polla-guide__results with_social">
-          <button
-            type="button"
-            className="polla-guide__share-btn"
-            onClick={() => {
-              console.log('asdasdasd')
-              popUpWindow(urlsShareList.facebook, '', 600, 400)
-            }}>
-            <svg width="8" height="15" viewBox="0 0 8 15" fill="none">
-              <path
-                d="M2.04221 15V7.96156H0V5.42739H2.04221V3.26287C2.04221 1.56198 3.24856 0 6.02823 0C7.15368 0 7.98589 0.098325 7.98589 0.098325L7.92032 2.46481C7.92032 2.46481 7.07159 2.45728 6.14543 2.45728C5.14303 2.45728 4.98244 2.87826 4.98244 3.57697V5.42739H8L7.8687 7.96156H4.98244V15H2.04221Z"
-                fill="white"
-              />
-            </svg>
-
-            <span>Compartir</span>
-          </button>
-        </div>
-      ) : (
-        <div className="polla-guide__results">
-          <span>Marcador:</span>
-          <span className="polla-guide__results-b">5pts</span>
-          <span className="polla-guide__results-s">|</span>
-          <span>Partido (gana - pierde) :</span>
-          <span className="polla-guide__results-b">3pts</span>
-          <span className="polla-guide__results-s">|</span>
-          <span>Ninguno :</span>
-          <span className="polla-guide__results-b">0pts</span>
-        </div>
-      )}
     </div>
   )
 }
@@ -89,9 +99,9 @@ PollaGuide.propTypes = {
     title: PropTypes.string.tag({
       name: 'Título',
     }),
-    subtitle: PropTypes.string.tag({
-      name: 'Subtítulo',
-    }),
+    // subtitle: PropTypes.string.tag({
+    //   name: 'Subtítulo',
+    // }),
   }),
 }
 
