ef7dcbd0ecd31a232001af75e2c24d3c61e89f19
danielpolo <danny00788@gmail.com>
2021-12-01 16:32:54 -0500

    Update: perucom sin prebid

--- components/output-types/default.jsx
+++ components/output-types/default.jsx
@@ -190,6 +190,7 @@ export default ({
       arcSite === SITE_ELCOMERCIOMAG ||
       arcSite === SITE_ELBOCON ||
       arcSite === SITE_DIARIOCORREO ||
+      arcSite === SITE_PERUCOM ||
       (arcSite === 'peru21' && requestUri.match(`^/cheka`))
     ) {
       prebid = false
