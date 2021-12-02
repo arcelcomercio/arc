7f22530a98ae8f4ad16b25035bf44d8964fc2ecd
cajotafer <hello@cajotafer.com>
2021-12-03 00:37:02 +0700

    Update: pruebas arreglando sandbox ci

--- config/deploy/deploy.js
+++ config/deploy/deploy.js
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
-const FormData = require("form-data");
-const fs = require("fs");
+const fetch = require('node-fetch')
+const FormData = require('form-data')
+const fs = require('fs')
 
 const {
   PB_VERSION: pbVersion,
@@ -12,76 +12,84 @@ const {
   TOKEN_ARC: tokenSandbox,
 } = process.env
 
-const baseURL = `https://${username}:${password}@${endpoint}/deployments/fusion/`;
+const baseURL = `https://${username}:${password}@${endpoint}/deployments/fusion/`
 const newBaseURL = `https://${endpoint}/deployments/fusion/`
 
-console.log("Environment:", environment);
-console.log("PageBuilder Version:", pbVersion);
+console.log('Environment:', environment)
+console.log('PageBuilder Version:', pbVersion)
 console.log('bundleName', bundleName)
 
-const form = new FormData();
-form.append("name", bundleName);
-form.append("bundle", fs.createReadStream("bundle.zip"));
+const form = new FormData()
+form.append('name', bundleName)
+form.append('bundle', fs.createReadStream('bundle.zip'))
 
 const notifySideEffect = (logMessage, slackMessage) => () => {
-  logMessage && console.log(logMessage);
+  logMessage && console.log(logMessage)
   //slackMessage && webhook.send(slackMessage);
-};
+}
 
 const deploy = () =>
   fetch(`${newBaseURL}services?bundle=${bundleName}&version=${pbVersion}`, {
     method: 'post',
-    headers: { 'Content-Type': 'text/plain', 'Authorization': `Bearer ${tokenSandbox}` },
-
+    headers: {
+      'Content-Type': 'text/plain',
+      Authorization: `Bearer ${tokenSandbox}`,
+    },
   })
-    .catch(err => {
-      console.log("error-->", err.response);
+    .then((response) => response.json())
+    .then(({ event: { id } = {} }) => id)
+    // .then(id => {
+    //   fetch(`${newBaseURL}deployments/${id}`, {})
+    // })
+    .catch((err) => {
+      console.log('error-->', err.response)
       throw { err, message: 'deploy bundle' }
-    });
-
+    })
 
 const handleError = (error, stepName) => {
-  console.log("Error encountered during deployment");
+  console.log('Error encountered during deployment')
   if (error.response) {
-    console.log("An Error in the response.");
-    console.log(error.response.data);
-    console.log(error.response.status);
-    console.log(error.response.headers);
+    console.log('An Error in the response.')
+    console.log(error.response.data)
+    console.log(error.response.status)
+    console.log(error.response.headers)
   } else if (error.request) {
-    console.log("An error in the request occurred");
-    console.log(error.request);
+    console.log('An error in the request occurred')
+    console.log(error.request)
   } else {
-    console.log("An unknown error occured");
-    error && console.log(error);
+    console.log('An unknown error occured')
+    error && console.log(error)
   }
 
   throw new Error('Errors during deployment', stepName)
-};
+}
 
-const headers = form.getHeaders();
-headers["Authorization"] = `Bearer ${tokenSandbox}`;
-console.log(headers);
+const headers = form.getHeaders()
+headers['Authorization'] = `Bearer ${tokenSandbox}`
+console.log(headers)
 fetch(`${newBaseURL}bundles`, {
   method: 'post',
   body: form,
   headers,
-  maxContentLength: Infinity
+  maxContentLength: Infinity,
 })
-  .catch(err => {
+  .catch((err) => {
     throw { err, message: 'upload bundle' }
   })
-  .then(res => res.json())
-  .then(response => {
-    console.log(response);
+  .then((res) => res.json())
+  .then((response) => {
+    console.log(response)
   })
   .then(deploy)
-  .then((response) => { console.log("response deploy-->", response) })
+  .then((response) => {
+    console.log('response deploy-->', response)
+  })
   .then(
     notifySideEffect(
-      "Notifying Slack channel of promotion.",
+      'Notifying Slack channel of promotion.',
       `PageBuilder bundle \`${bundleName}\` has been promoted in the \`${environment}\` environment! :partyparrot: :philosoraptor:`
     )
   )
   .catch((err, message = 'end to process') => {
     handleError(err, message)
-  });
+  })
