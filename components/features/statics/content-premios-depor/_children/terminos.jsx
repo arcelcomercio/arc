2f82f22c368a3a660b813cc5a4595d31762f0c6f
Leonel Navarro Auccasi <l.navarro@rpalatam.com.pe>
2021-12-10 12:47:15 -0500

    New: avance viernes 12pm terminos (premios depor)(55413)(Site&Core)

--- components/features/statics/content-premios-depor/_children/terminos.jsx
+++ components/features/statics/content-premios-depor/_children/terminos.jsx
@@ -24,28 +24,38 @@ const Terms = () => (
       <div className={classes.text}>
         <div className={classes.title}>Términos y Condiciones</div>
         <div className={classes.content}>
-          Para participar de la campaña “<b>PREMIOS DEPOR 2021</b>” a nivel
-          nacional, el lector deberá ingresar a la página web https://depor.com/
-          o escanear el código QR que se encuentra en las piezas publicitarias
-          de la campaña. Luego de ello, el lector será redirigido a la web site
-          de la campaña Premios Depor en la cual podrá votar por los/las
-          deportistas más destacados en las siguientes categorías: futbolista
-          del año, jugador revelación, mejor técnico, mejor peruanos en el
-          extranjero, futbolista femenina, voley, lucha, freestyle, surf, equipo
-          de e-sports y deportista del año. Cada categoría tendrá 5 nominados y
-          el lector deberá votar por aquel deportista que, a su criterio, mejor
-          represente cada una de estas. Solo se permite un voto por cada
-          categoría. Al deportista ganador de cada categoría se le hará entrega
-          de un trofeo “Premios Depor 2021” correspondiente al rubro en el que
-          haya destacado. Entre los lectores votantes de la campaña se sorteará
-          una Playstation 5. Fecha del sorteo: miércoles 29 de diciembre del
-          2021. El ganador será anunciado en un plazo máximo de siete días
-          calendarios posteriores al sorteo, a través de una publicación en el
-          Diario Depor versión Print. Así mismo, tendrá cinco días calendarios
-          para reclamar el premio, contados desde la publicación. Posterior a
-          ese plazo, el ganador perderá el derecho de reclamarlo. El lector
-          podrá votar hasta el 28 de diciembre del 2021 y los resultados finales
-          serán publicados en el Print del diario Depor el 01 de enero de 2022.
+          <p>
+            Para participar de la campaña “<b>PREMIOS DEPOR 2021</b>” a nivel
+            nacional, el lector deberá ingresar a la página web
+            https://depor.com/ o escanear el código QR que se encuentra en las
+            piezas publicitarias de la campaña.
+            <br /> Luego de ello, el lector será redirigido a la web site de la
+            campaña Premios Depor en la cual podrá votar por los/las deportistas
+            más destacados en las siguientes categorías: futbolista del año,
+            jugador revelación, mejor técnico, mejor peruanos en el extranjero,
+            futbolista femenina, voley, lucha, freestyle, surf, equipo de
+            e-sports y deportista del año.
+            <br /> Cada categoría tendrá 5 nominados y el lector deberá votar
+            por aquel deportista que, a su criterio, mejor represente cada una
+            de estas.
+            <br /> Solo se permite un voto por cada categoría. Al deportista
+            ganador de cada categoría se le hará entrega de un trofeo “Premios
+            Depor 2021” correspondiente al rubro en el que haya destacado.
+          </p>
+          <p>
+            Entre los lectores votantes de la campaña se sorteará una
+            Playstation 5. Fecha del sorteo: miércoles 29 de diciembre del 2021.
+            El ganador será anunciado en un plazo máximo de siete días
+            calendarios posteriores al sorteo, a través de una publicación en el
+            Diario Depor versión Print. Así mismo, tendrá cinco días calendarios
+            para reclamar el premio, contados desde la publicación. Posterior a
+            ese plazo, el ganador perderá el derecho de reclamarlo.
+          </p>
+          <p>
+            El lector podrá votar hasta el 28 de diciembre del 2021 y los
+            resultados finales serán publicados en el Print del diario Depor el
+            01 de enero de 2022.
+          </p>
         </div>
       </div>
       {isMobile ? (
