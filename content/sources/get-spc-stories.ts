4978c69f1d4c399e74de4385b28e817ebec98981
Alexis Martinez <alexis.martinez@comercio.com.pe>
2021-12-16 17:59:10 -0500

    Update: add content source for spc stories

---
+++ content/sources/get-spc-stories.ts
@@ -0,0 +1,7 @@
+const resolve = (): string =>
+  'https://cdn-sfo.socy.cloud/ZECO/amix/ZECO_453_877_194/70/7cbbc409ec990f19c78c75bd1e06f215/80/1/ZZ/json/v2.json'
+
+export default {
+  resolve,
+  ttl: 300,
+}
